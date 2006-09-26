nwjs/blink

commit 420c5aa0f2b7d7d17d2bd49a2a44b2657ff6a5fa

Reviewed by eseidel.
        Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=6001
        WebKit does not handle fallback custom cursors
        Fix for http://bugzilla.opendarwin.org/show_bug.cgi?id=6002
        WebKit does not properly handle SVG <cursor> element

        Add support for svg cursor images. Also make sure hotspot
        settings are handled correctly. Add tests for handling of css3
        cursor syntax with hotspots in strict and quirks mode. Finally
        implement fallback.



git-svn-id: svn://svn.chromium.org/blink/trunk@16549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/css/cssparser.cpp
@@ -25,6 +25,7 @@
 #include "cssparser.h"
 
 #include "CSSBorderImageValue.h"
+#include "CSSCursorImageValue.h"
 #include "CSSImageValue.h"
 #include "CSSCharsetRule.h"
 #include "CSSImportRule.h"
@@ -694,26 +695,68 @@ bool CSSParser::parseValue(int propId, bool important)
         }
         break;
 
-    case CSS_PROP_CURSOR:
+    case CSS_PROP_CURSOR: {
         // [<uri>,]*  [ auto | crosshair | default | pointer | progress | move | e-resize | ne-resize |
         // nw-resize | n-resize | se-resize | sw-resize | s-resize | w-resize | ew-resize | 
         // ns-resize | nesw-resize | nwse-resize | col-resize | row-resize | text | wait | help ] ] | inherit
-        if (!strict && id == CSS_VAL_HAND) { // MSIE 5 compatibility :/
+        CSSValueList* list = 0;
+        while (value && value->unit == CSSPrimitiveValue::CSS_URI) {
+            String uri = parseURL(domString(value->string));
+            Vector<int> coords;
+            value = valueList->next();
+            while (value && value->unit == CSSPrimitiveValue::CSS_NUMBER) {
+                coords.append(value->fValue);
+                value = valueList->next();
+            }
+            IntPoint hotspot;
+            int nrcoords = coords.size();
+            if (nrcoords > 0 && nrcoords != 2) {
+                if (strict) { // only support hotspot pairs in strict mode
+                    delete list;
+                    return false;
+                }
+            } else if(strict && nrcoords == 2)
+                hotspot = IntPoint(coords[0], coords[1]);
+            if (strict || coords.size() == 0) {
+#if SVG_SUPPORT
+            if (uri.startsWith("#")) {
+                if (!list)
+                    list = new CSSValueList; 
+                list->append(new CSSPrimitiveValue(uri, CSSPrimitiveValue::CSS_URI));
+            } else
+#endif
+            if (!uri.isEmpty()) {
+                if (!list)
+                    list = new CSSValueList; 
+                list->append(new CSSCursorImageValue(
+                             String(KURL(styleElement->baseURL().deprecatedString(), uri.deprecatedString()).url()),
+                             hotspot, styleElement));
+            }
+            }
+            if ((strict && !value) || (value && !(value->unit == Value::Operator && value->iValue == ',')))
+                return false;
+            value = valueList->next(); // comma
+        }
+        if (list) {
+            if (!value) { // no value after url list (MSIE 5 compatibility)
+                if (list->length() > 1)
+                    return false;
+            } else if (!strict && value->id == CSS_VAL_HAND) // MSIE 5 compatibility :/
+                list->append(new CSSPrimitiveValue(CSS_VAL_POINTER));
+            else if (value && value->id >= CSS_VAL_AUTO && value->id <= CSS_VAL_HELP)
+                list->append(new CSSPrimitiveValue(value->id));
+            valueList->next();
+            parsedValue = list;
+            break;
+        }
+        id = value->id;
+        if (!strict && value->id == CSS_VAL_HAND) { // MSIE 5 compatibility :/
             id = CSS_VAL_POINTER;
             valid_primitive = true;
-        } else if (id >= CSS_VAL_AUTO && id <= CSS_VAL_HELP)
+        } else if (value->id >= CSS_VAL_AUTO && value->id <= CSS_VAL_HELP)
             valid_primitive = true;
-        else if (value->unit == CSSPrimitiveValue::CSS_URI) {
-            String uri = parseURL(domString(value->string));
-            if (!uri.isEmpty()) {
-                parsedValue = new CSSImageValue(String(KURL(styleElement->baseURL().deprecatedString(), uri.deprecatedString()).url()), styleElement);
-                // FIXME: we don't support fallback cursors yet, but ignoring the remaining values
-                // will at least let compliant declarations parse.
-                addProperty(propId, parsedValue, important);
-                return true;
-            }
-        }
         break;
+    }
 
     case CSS_PROP_BACKGROUND_ATTACHMENT:
     case CSS_PROP__WEBKIT_BACKGROUND_CLIP:
@@ -1295,8 +1338,8 @@ bool CSSParser::parseValue(int propId, bool important)
     }
 
     if (valid_primitive) {
-        if (id != 0)
-            parsedValue = new CSSPrimitiveValue(id);
+        if (id != 0) {
+            parsedValue = new CSSPrimitiveValue(id); }
         else if (value->unit == CSSPrimitiveValue::CSS_STRING)
             parsedValue = new CSSPrimitiveValue(domString(value->string), (CSSPrimitiveValue::UnitTypes) value->unit);
         else if (value->unit >= CSSPrimitiveValue::CSS_NUMBER && value->unit <= CSSPrimitiveValue::CSS_KHZ)

WebCore/css/cssstyleselector.cpp
@@ -26,6 +26,7 @@
 #include "cssstyleselector.h"
 
 #include "CSSBorderImageValue.h"
+#include "CSSCursorImageValue.h"
 #include "CSSImageValue.h"
 #include "CSSImportRule.h"
 #include "CSSMediaRule.h"
@@ -2410,24 +2411,41 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
     case CSS_PROP_CURSOR:
         if (isInherit) {
             style->setCursor(parentStyle->cursor());
-            style->setCursorImage(parentStyle->cursorImage());
+            style->setCursorList(parentStyle->cursors());
             return;
         }
+        style->clearCursorList();
         if (isInitial) {
             style->setCursor(RenderStyle::initialCursor());
-            style->setCursorImage(0);
             return;
         }
-        if (primitiveValue) {
-            int type = primitiveValue->primitiveType();
-            if (type == CSSPrimitiveValue::CSS_IDENT) {
-                style->setCursor((ECursor)(primitiveValue->getIdent() - CSS_VAL_AUTO));
-                style->setCursorImage(0);
-            } else if (type == CSSPrimitiveValue::CSS_URI) {
-                CSSImageValue* image = static_cast<CSSImageValue*>(primitiveValue);
-                style->setCursor(CURSOR_AUTO);
-                style->setCursorImage(image->image(element->document()->docLoader()));
+        if (value->isValueList()) {
+            CSSValueList* list = static_cast<CSSValueList*>(value);
+            int len = list->length();
+            style->setCursor(CURSOR_AUTO);
+            for (int i = 0; i < len; i++) {
+                CSSValue* item = list->item(i);
+                if (!item->isPrimitiveValue())
+                    continue;
+                primitiveValue = static_cast<CSSPrimitiveValue*>(item);
+                int type = primitiveValue->primitiveType();
+                if (type == CSSPrimitiveValue::CSS_URI) {
+#if SVG_SUPPORT
+                    if (primitiveValue->getStringValue().find("#") == 0)
+                        style->addSVGCursor(primitiveValue->getStringValue().substring(1));
+                    else
+#endif
+                    {
+                        CSSCursorImageValue* image = static_cast<CSSCursorImageValue*>(primitiveValue);
+                        style->addCursor(image->image(element->document()->docLoader()), image->hotspot());
+                    }
+                } else if (type == CSSPrimitiveValue::CSS_IDENT)
+                    style->setCursor((ECursor)(primitiveValue->getIdent() - CSS_VAL_AUTO));
             }
+        } else if (primitiveValue) {
+            int type = primitiveValue->primitiveType();
+            if (type == CSSPrimitiveValue::CSS_IDENT)
+                style->setCursor((ECursor)(primitiveValue->getIdent() - CSS_VAL_AUTO));
         }
         break;
 // colors || inherit

WebCore/page/FrameView.cpp
@@ -48,10 +48,20 @@
 #include "SelectionController.h"
 #include "cssstyleselector.h"
 
+#if SVG_SUPPORT
+#include "XLinkNames.h"
+#include "SVGNames.h"
+#include "SVGCursorElement.h"
+#include "SVGLength.h"
+#endif
+
 namespace WebCore {
 
 using namespace EventNames;
 using namespace HTMLNames;
+#if SVG_SUPPORT
+using namespace SVGNames;
+#endif
 
 struct ScheduledEvent {
     RefPtr<Event> m_event;
@@ -646,11 +656,30 @@ static Cursor selectCursor(const MouseEventWithHitTestResults& event, Frame* fra
     RenderObject* renderer = node ? node->renderer() : 0;
     RenderStyle* style = renderer ? renderer->style() : 0;
 
-    if (style && style->cursorImage() && !style->cursorImage()->image()->isNull())
-        if (!style->cursorImage()->isErrorImage())
-            return style->cursorImage()->image();
-        else 
-            style = 0; // Fallback to CURSOR_AUTO
+    if (style && style->cursors()) {
+        const CursorList* cursors = style->cursors();
+        for (unsigned i = 0; i < cursors->size(); ++i) {
+            CachedImage* cimage = (*cursors)[i].cursorImage;
+            IntPoint hotSpot = (*cursors)[i].hotSpot;
+#if SVG_SUPPORT
+            if (!cimage) {
+                Element* e = node->document()->getElementById((*cursors)[i].cursorFragmentId);
+                if (e && e->hasTagName(cursorTag)) {
+                    hotSpot.setX(int(static_cast<SVGCursorElement*>(e)->x()->value()));
+                    hotSpot.setY(int(static_cast<SVGCursorElement*>(e)->y()->value()));
+                    cimage = static_cast<SVGCursorElement*>(e)->cachedImage();
+                }
+            }
+#endif
+            if (!cimage)
+                continue;
+            if (cimage->image()->isNull())
+                break;
+            if (!cimage->isErrorImage()) {
+                return Cursor(cimage->image(), hotSpot);
+}
+        }
+    }
 
     switch (style ? style->cursor() : CURSOR_AUTO) {
         case CURSOR_AUTO: {

WebCore/WebCore.xcodeproj/project.pbxproj
@@ -1788,6 +1788,8 @@
 		A8FEFB0B0979F472005839FD /* RenderForeignObject.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A8FEFB090979F472005839FD /* RenderForeignObject.cpp */; };
 		A8FEFB310979F4F6005839FD /* SVGForeignObjectElement.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A8FEFB2F0979F4F6005839FD /* SVGForeignObjectElement.cpp */; };
 		A8FEFB320979F4F6005839FD /* SVGForeignObjectElement.h in Headers */ = {isa = PBXBuildFile; fileRef = A8FEFB300979F4F6005839FD /* SVGForeignObjectElement.h */; };
+		AA21ECCA0ABF0FBA002B834C /* CSSCursorImageValue.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AA0978ED0ABAA6E100874480 /* CSSCursorImageValue.cpp */; };
+		AA21ECCD0ABF0FC6002B834C /* CSSCursorImageValue.h in Headers */ = {isa = PBXBuildFile; fileRef = AA0978EE0ABAA6E100874480 /* CSSCursorImageValue.h */; };
 		AA98B87D0AAA02F8001A44C2 /* Path.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AA98B87C0AAA02F8001A44C2 /* Path.cpp */; };
 		AAC8DAB10AA1002000DC0907 /* SVGMetadataElement.cpp in Sources */ = {isa = PBXBuildFile; fileRef = AAC8DAAF0AA1002000DC0907 /* SVGMetadataElement.cpp */; };
 		AAC8DAB20AA1002000DC0907 /* SVGMetadataElement.h in Headers */ = {isa = PBXBuildFile; fileRef = AAC8DAB00AA1002000DC0907 /* SVGMetadataElement.h */; };
@@ -4145,6 +4147,8 @@
 		A8FEFB090979F472005839FD /* RenderForeignObject.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = RenderForeignObject.cpp; sourceTree = "<group>"; };
 		A8FEFB2F0979F4F6005839FD /* SVGForeignObjectElement.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = SVGForeignObjectElement.cpp; sourceTree = "<group>"; };
 		A8FEFB300979F4F6005839FD /* SVGForeignObjectElement.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; path = SVGForeignObjectElement.h; sourceTree = "<group>"; };
+		AA0978ED0ABAA6E100874480 /* CSSCursorImageValue.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = CSSCursorImageValue.cpp; sourceTree = "<group>"; };
+		AA0978EE0ABAA6E100874480 /* CSSCursorImageValue.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; path = CSSCursorImageValue.h; sourceTree = "<group>"; };
 		AA98B87C0AAA02F8001A44C2 /* Path.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = Path.cpp; sourceTree = "<group>"; };
 		AAC8DAAF0AA1002000DC0907 /* SVGMetadataElement.cpp */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.cpp.cpp; path = SVGMetadataElement.cpp; sourceTree = "<group>"; };
 		AAC8DAB00AA1002000DC0907 /* SVGMetadataElement.h */ = {isa = PBXFileReference; fileEncoding = 30; lastKnownFileType = sourcecode.c.h; path = SVGMetadataElement.h; sourceTree = "<group>"; };
@@ -7285,6 +7289,8 @@
 		F523D18402DE42E8018635CA /* css */ = {
 			isa = PBXGroup;
 			children = (
+				AA0978ED0ABAA6E100874480 /* CSSCursorImageValue.cpp */,
+				AA0978EE0ABAA6E100874480 /* CSSCursorImageValue.h */,
 				A80E6CDA0A1989CA007FB8C5 /* Counter.h */,
 				930705C709E0C95F00B17FE4 /* Counter.idl */,
 				A80E6CBB0A1989CA007FB8C5 /* CSSBorderImageValue.cpp */,
@@ -8682,6 +8688,7 @@
 				853BF4DB0ABB6B55008647BB /* DOMNode.h in Headers */,
 				853BF4EB0ABB6E97008647BB /* DOMNodePrivate.h in Headers */,
 				85BCBC130ABBA87D00381160 /* DOMHTMLDocument.h in Headers */,
+				AA21ECCD0ABF0FC6002B834C /* CSSCursorImageValue.h in Headers */,
 				851EE80C0ABCA28F00A6AA33 /* DOMAbstractView.h in Headers */,
 				851EE8110ABCA39600A6AA33 /* DOMEventException.h in Headers */,
 				851EE8210ABCA58100A6AA33 /* DOMRangeException.h in Headers */,
@@ -9852,6 +9859,7 @@
 				B20111070AB7740500DB0E68 /* JSSVGAElement.cpp in Sources */,
 				853BF4DC0ABB6B55008647BB /* DOMNode.mm in Sources */,
 				85BCBC140ABBA87D00381160 /* DOMHTMLDocument.mm in Sources */,
+				AA21ECCA0ABF0FBA002B834C /* CSSCursorImageValue.cpp in Sources */,
 				851EE80D0ABCA28F00A6AA33 /* DOMAbstractView.mm in Sources */,
 				A8415F9B0AC10C5D00F7D0DF /* KCanvasClipper.cpp in Sources */,
 				A8415F9D0AC10C5D00F7D0DF /* KCanvasResource.cpp in Sources */,

WebCore/css/CSSComputedStyleDeclaration.cpp
@@ -607,57 +607,89 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(int proper
     case CSS_PROP_COUNTER_RESET:
         // FIXME: unimplemented
         break;
-    case CSS_PROP_CURSOR:
-        if (style->cursorImage())
-            return new CSSPrimitiveValue(style->cursorImage()->url(), CSSPrimitiveValue::CSS_URI);
+    case CSS_PROP_CURSOR: {
+        CSSValueList* list = 0;
+        CSSValue* value = 0;
+        CursorList* cursors = style->cursors();
+        if (cursors && cursors->size() > 0) {
+            list = new CSSValueList;
+            for (unsigned i = 0; i < cursors->size(); ++i)
+                list->append(new CSSPrimitiveValue((*cursors)[i].cursorImage->url(), CSSPrimitiveValue::CSS_URI));
+        }
         switch (style->cursor()) {
             case CURSOR_AUTO:
-                return new CSSPrimitiveValue(CSS_VAL_AUTO);
+                value = new CSSPrimitiveValue(CSS_VAL_AUTO);
+                break;
             case CURSOR_CROSS:
-                return new CSSPrimitiveValue(CSS_VAL_CROSSHAIR);
+                value = new CSSPrimitiveValue(CSS_VAL_CROSSHAIR);
+                break;
             case CURSOR_DEFAULT:
-                return new CSSPrimitiveValue(CSS_VAL_DEFAULT);
+                value = new CSSPrimitiveValue(CSS_VAL_DEFAULT);
+                break;
             case CURSOR_POINTER:
-                return new CSSPrimitiveValue(CSS_VAL_POINTER);
+                value = new CSSPrimitiveValue(CSS_VAL_POINTER);
+                break;
             case CURSOR_MOVE:
-                return new CSSPrimitiveValue(CSS_VAL_MOVE);
+                value = new CSSPrimitiveValue(CSS_VAL_MOVE);
+                break;
             case CURSOR_E_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_E_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_E_RESIZE);
+                break;
             case CURSOR_NE_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_NE_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_NE_RESIZE);
+                break;
             case CURSOR_NW_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_NW_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_NW_RESIZE);
+                break;
             case CURSOR_N_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_N_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_N_RESIZE);
+                break;
             case CURSOR_SE_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_SE_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_SE_RESIZE);
+                break;
             case CURSOR_SW_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_SW_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_SW_RESIZE);
+                break;
             case CURSOR_S_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_S_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_S_RESIZE);
+                break;
             case CURSOR_W_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_W_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_W_RESIZE);
+                break;
             case CURSOR_EW_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_EW_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_EW_RESIZE);
+                break;
             case CURSOR_NS_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_NS_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_NS_RESIZE);
+                break;
             case CURSOR_NESW_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_NESW_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_NESW_RESIZE);
+                break;
             case CURSOR_NWSE_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_NWSE_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_NWSE_RESIZE);
+                break;
             case CURSOR_COL_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_COL_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_COL_RESIZE);
+                break;
             case CURSOR_ROW_RESIZE:
-                return new CSSPrimitiveValue(CSS_VAL_ROW_RESIZE);
+                value = new CSSPrimitiveValue(CSS_VAL_ROW_RESIZE);
+                break;
             case CURSOR_TEXT:
-                return new CSSPrimitiveValue(CSS_VAL_TEXT);
+                value = new CSSPrimitiveValue(CSS_VAL_TEXT);
+                break;
             case CURSOR_WAIT:
-                return new CSSPrimitiveValue(CSS_VAL_WAIT);
+                value = new CSSPrimitiveValue(CSS_VAL_WAIT);
+                break;
             case CURSOR_HELP:
-                return new CSSPrimitiveValue(CSS_VAL_HELP);
+                value = new CSSPrimitiveValue(CSS_VAL_HELP);
         }
-        ASSERT_NOT_REACHED();
-        return 0;
+        ASSERT(value);
+        if (list) {
+            list->append(value);
+            return list;
+        }
+        return value;
+    }
     case CSS_PROP_DIRECTION:
         switch (style->direction()) {
             case LTR:

WebCore/css/CSSCursorImageValue.cpp
@@ -1,0 +1,35 @@
+/**
+ * This file is part of the DOM implementation for KDE.
+ *
+ * Copyright (C) 2006, Rob Buis <buis@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+#include "config.h"
+#include "CSSCursorImageValue.h"
+
+namespace WebCore {
+
+CSSCursorImageValue::CSSCursorImageValue(const String& url, const IntPoint& p, StyleBase* style)
+    : CSSImageValue(url, style)
+    , m_hotspot(p)
+{
+}
+
+CSSCursorImageValue::~CSSCursorImageValue()
+{
+}
+}

WebCore/css/CSSCursorImageValue.h
@@ -1,0 +1,43 @@
+/*
+ * This file is part of the DOM implementation for KDE.
+ *
+ * Copyright (C) 2006, Rob Buis <buis@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef CSSCursorImageValue_H
+#define CSSCursorImageValue_H
+
+#include "IntPoint.h"
+#include "CSSImageValue.h"
+
+namespace WebCore {
+
+class CSSCursorImageValue : public CSSImageValue
+{
+public:
+    CSSCursorImageValue(const String& url, const IntPoint&, StyleBase*);
+    virtual ~CSSCursorImageValue();
+
+    IntPoint hotspot() const { return m_hotspot; }
+
+protected:
+    IntPoint m_hotspot;
+};
+} // namespace
+
+#endif

WebCore/ksvg2/svg/SVGCursorElement.cpp
@@ -32,7 +32,7 @@
 #include "SVGHelper.h"
 #include "SVGLength.h"
 
-using namespace WebCore;
+namespace WebCore {
 
 SVGCursorElement::SVGCursorElement(const QualifiedName& tagName, Document *doc)
     : SVGElement(tagName, doc)
@@ -78,6 +78,8 @@ void SVGCursorElement::parseMappedAttribute(MappedAttribute *attr)
 
         SVGElement::parseMappedAttribute(attr);
     }
+}
+
 }
 
 // vim:ts=4:noet

commit 4763268d0b4cc79f5c8d4385705d0da5fd67d629

2006-09-26  Graham Dennis  <graham.dennis@gmail.com>
        Reviewed by mitzpettel.

        - fix http://bugzilla.opendarwin.org/show_bug.cgi?id=11020
        No-SVG build broken since r16549
        
        Change #if SVG_SUPPORT to #ifdef SVG_SUPPORT

        * css/cssparser.cpp:
        (WebCore::CSSParser::parseValue):
        * css/cssstyleselector.cpp:
        (WebCore::CSSStyleSelector::applyProperty):
        * page/FrameView.cpp:
        (WebCore::selectCursor):
        * platform/qt/GraphicsContextQt.cpp:



git-svn-id: svn://svn.chromium.org/blink/trunk@16571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/css/cssparser.cpp
@@ -718,7 +718,7 @@ bool CSSParser::parseValue(int propId, bool important)
             } else if(strict && nrcoords == 2)
                 hotspot = IntPoint(coords[0], coords[1]);
             if (strict || coords.size() == 0) {
-#if SVG_SUPPORT
+#ifdef SVG_SUPPORT
             if (uri.startsWith("#")) {
                 if (!list)
                     list = new CSSValueList; 

WebCore/css/cssstyleselector.cpp
@@ -2440,7 +2440,7 @@ void CSSStyleSelector::applyProperty(int id, CSSValue *value)
                 primitiveValue = static_cast<CSSPrimitiveValue*>(item);
                 int type = primitiveValue->primitiveType();
                 if (type == CSSPrimitiveValue::CSS_URI) {
-#if SVG_SUPPORT
+#ifdef SVG_SUPPORT
                     if (primitiveValue->getStringValue().find("#") == 0)
                         style->addSVGCursor(primitiveValue->getStringValue().substring(1));
                     else

WebCore/page/FrameView.cpp
@@ -48,7 +48,7 @@
 #include "SelectionController.h"
 #include "cssstyleselector.h"
 
-#if SVG_SUPPORT
+#ifdef SVG_SUPPORT
 #include "XLinkNames.h"
 #include "SVGNames.h"
 #include "SVGCursorElement.h"
@@ -59,7 +59,7 @@ namespace WebCore {
 
 using namespace EventNames;
 using namespace HTMLNames;
-#if SVG_SUPPORT
+#ifdef SVG_SUPPORT
 using namespace SVGNames;
 #endif
 
@@ -661,7 +661,7 @@ static Cursor selectCursor(const MouseEventWithHitTestResults& event, Frame* fra
         for (unsigned i = 0; i < cursors->size(); ++i) {
             CachedImage* cimage = (*cursors)[i].cursorImage;
             IntPoint hotSpot = (*cursors)[i].hotSpot;
-#if SVG_SUPPORT
+#ifdef SVG_SUPPORT
             if (!cimage) {
                 Element* e = node->document()->getElementById((*cursors)[i].cursorFragmentId);
                 if (e && e->hasTagName(cursorTag)) {

WebCore/platform/qt/GraphicsContextQt.cpp
@@ -46,7 +46,7 @@
 #define M_PI 3.14159265358979323846
 #endif
 
-#if SVG_SUPPORT
+#ifdef SVG_SUPPORT
 #include "KRenderingDeviceQt.h"
 #endif
 
@@ -777,7 +777,7 @@ void GraphicsContext::concatCTM(const AffineTransform& transform)
     m_data->p().setMatrix(transform, true);
 }
 
-#if SVG_SUPPORT
+#ifdef SVG_SUPPORT
 KRenderingDeviceContext* GraphicsContext::createRenderingDeviceContext()
 {
     return new KRenderingDeviceContextQt(platformContext());
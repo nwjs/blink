nwjs/blink

commit 82ee16e54d8a1358d6af97946feb76dc87c3f335

LayoutTests:
        Reviewed by Maciej, Darin and Mark.

        rdar://problem/4922454
        - No longer allow remote sites to access local resources.

        * fast/loader/local-JavaScript-from-local-expected.txt: Added.
        * fast/loader/local-JavaScript-from-local.html: Added.
        * fast/loader/local-iFrame-source-from-local-expected.txt: Added.
        * fast/loader/local-iFrame-source-from-local.html: Added.
        * fast/loader/local-image-from-local-expected.txt: Added.
        * fast/loader/local-image-from-local.html: Added.
        * http/tests/security/local-JavaScript-from-remote-expected.txt: Added.
        * http/tests/security/local-JavaScript-from-remote.html: Added.
        * http/tests/security/local-iFrame-from-remote-expected.txt: Added.
        * http/tests/security/local-iFrame-from-remote.html: Added.
        * http/tests/security/local-image-from-remote-expected.txt: Added.
        * http/tests/security/local-image-from-remote.html: Added.
        * http/tests/security/resources/compass.jpg: Added.
        * http/tests/security/resources/localPage.html: Added.
        * http/tests/security/resources/localPage.html.orig: Added.
        * http/tests/security/resources/localScript.js: Added.

WebCore:

        Reviewed by Maciej, Darin, and Mark.

        rdar://problem/4922454
        - Prevents remote sites from executing local scripts.

        * bindings/objc/DOM.mm: - renamed a function that is now in the base class
        (-[DOMElement image]):
        (-[DOMElement _imageTIFFRepresentation]):
        * dom/XMLTokenizer.cpp: - removed needless asserts
        (WebCore::XMLTokenizer::notifyFinished):
        * html/HTMLImageLoader.cpp: - renamed a function that is now in the base class
        (WebCore::HTMLImageLoader::dispatchLoadEvent):
        * html/HTMLTokenizer.cpp: - removed needless asserts
        (WebCore::HTMLTokenizer::reset):
        (WebCore::HTMLTokenizer::notifyFinished):

WebCore/loader/DocLoader.cpp
@@ -121,16 +121,25 @@ CachedResource* DocLoader::requestResource(CachedResource::Type type, const Stri
 {
     KURL fullURL = m_doc->completeURL(url.deprecatedString());
 
-    if (m_frame && m_frame->loader()->isReloading())
+    FrameLoader* fl = 0;
+    if (m_frame)
+        fl = m_frame->loader();
+    if (fl && fl->isReloading())
         setCachePolicy(CachePolicyReload);
 
     checkForReload(fullURL);
 
-    CachedResource* resource = cache()->requestResource(this, type, fullURL, m_expireDate, charset);
-    if (resource) {
-        m_docResources.set(resource->url(), resource);
-        checkCacheObjectStatus(resource);
+    bool hideReferrer;
+    CachedResource* resource = 0;
+    //If you are not allowed to load you may not get from the cache either.
+    if (fl && fl->canLoad(fullURL, fl->outgoingReferrer(), hideReferrer)) {
+        resource = cache()->requestResource(this, type, fullURL, m_expireDate, charset);
+        if (resource) {
+            m_docResources.set(resource->url(), resource);
+            checkCacheObjectStatus(resource);
+        }
     }
+
     return resource;
 }
 
@@ -172,7 +181,7 @@ void DocLoader::removeCachedResource(CachedResource* resource) const
 void DocLoader::setLoadInProgress(bool load)
 {
     m_loadInProgress = load;
-    if (!load)
+    if (!load && m_frame)
         m_frame->loader()->loadDone();
 }
 

LayoutTests/fast/loader/local-JavaScript-from-local-expected.txt
@@ -1,0 +1,4 @@
+This test is to see if a local file can run a local script.
+
+Test Passed
+

LayoutTests/fast/loader/local-JavaScript-from-local.html
@@ -1,0 +1,29 @@
+<html>
+<head>
+        <script>
+            var secretness = 0;
+        </script>
+        <script src="file:///tmp/LayoutTests/http/tests/security/resources/localScript.js"/>
+        <script>
+            function test() {
+                if (window.layoutTestController)
+                    layoutTestController.dumpAsText();
+
+                    var tag = document.getElementById("result");
+                    if (secretness == 13)
+                        tag.innerHTML = "Test Passed";
+                    else
+                        tag.innerHTML = "Test Failed: Local script not run by local file.";
+            }
+        </script>
+</head>
+<body onload="test()">
+    <div id="div0">
+        This test is to see if a local file can run a local script.
+    </div>
+    </br>
+    <div id="result">
+        Test not run correctly.
+    </div>
+</body>
+</html>

LayoutTests/fast/loader/local-iFrame-source-from-local-expected.txt
@@ -1,0 +1,4 @@
+This test is to see if a local file can include a local page in an iFrame.
+
+Test Passed.
+

LayoutTests/fast/loader/local-iFrame-source-from-local.html
@@ -1,0 +1,33 @@
+<html>
+<head>
+    <script>
+        if (window.layoutTestController) {
+            layoutTestController.dumpAsText();
+        }
+    
+        function iFrameTest() {
+            if (window.layoutTestController)
+                layoutTestController.dumpAsText();
+
+            var result = document.getElementById("result");
+
+            var myFrameDocument = document.getElementById("myFrame").contentDocument;
+            if (myFrameDocument) {
+                result.innerHTML = "Test Passed.";
+            } else {
+                result.innerHTML = "Test Failed: Local page not locally loaded into iFrame.";
+            }
+        }
+    </script>
+</head>
+<body>
+    <div id="div0">
+        This test is to see if a local file can include a local page in an iFrame.
+    </div>
+    </br>
+    <div id="result">
+        Test not run correctly.
+    </div>
+    <iFrame id="myFrame" onload="iFrameTest()" src="file:///tmp/LayoutTests/http/tests/security/resources/localPage.html" />
+</body>
+</html>

LayoutTests/fast/loader/local-image-from-local-expected.txt
@@ -1,0 +1,5 @@
+This test is to see if a local file can include a local image.
+
+Test Passed
+
+

LayoutTests/fast/loader/local-image-from-local.html
@@ -1,0 +1,30 @@
+<html>
+<head>
+    <script>        
+        function imgTest() {
+            if (window.layoutTestController)
+                layoutTestController.dumpAsText();
+            
+            var result = document.getElementById("result");
+            
+            var myImg = document.getElementById("myImg");
+            if (myImg.width > 0 && myImg.height > 0) {
+                result.innerHTML = "Test Passed";
+            } else {
+                result.innerHTML = "Test Failed: Image width or height <= 0";
+            }
+        }
+    </script>
+</head>
+<body onload="imgTest()">
+    <div id="div0">
+        This test is to see if a local file can include a local image.
+    </div>
+    </br>
+    <div id="result">
+        Test has not run.
+    </div>
+    </br>
+    <img id="myImg" src="file:///tmp/LayoutTests/http/tests/security/resources/compass.jpg" />
+</body>
+</html>

LayoutTests/http/tests/security/local-JavaScript-from-remote-expected.txt
@@ -1,0 +1,4 @@
+This test is to see if a remote file can run a local script.
+
+Test Passed
+

commit 025c5b74510c78798136824cef0212230dfe7763

Reviewed by Brady.
        - Fixing a performance regression caused by r19500.  The heart of the matter
        is that we now call canLoad on every resource request even ones in the cache,
        to avoid a security issue. So I could not take out the canLoad call, but I
        but I could try to make it more efficient.

        * loader/DocLoader.cpp:
        (WebCore::DocLoader::requestResource):
        (WebCore::DocLoader::setLoadInProgress):
        * loader/FrameLoader.cpp:
        (WebCore::FrameLoader::canLoad):



git-svn-id: svn://svn.chromium.org/blink/trunk@19580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/loader/DocLoader.cpp
@@ -127,17 +127,20 @@ CachedResource* DocLoader::requestResource(CachedResource::Type type, const Stri
     KURL fullURL = m_doc->completeURL(url.deprecatedString());
 
     FrameLoader* fl = 0;
+    CachedResource* resource = 0;
     if (m_frame)
         fl = m_frame->loader();
-    if (fl && fl->isReloading())
+    if (!fl)
+        return resource;
+
+    if (fl->isReloading())
         setCachePolicy(CachePolicyReload);
 
     checkForReload(fullURL);
 
-    bool hideReferrer;
-    CachedResource* resource = 0;
+    static bool hideReferrer;
     //If you are not allowed to load you may not get from the cache either.
-    if (skipCanLoadCheck || (fl && fl->canLoad(fullURL, fl->outgoingReferrer(), hideReferrer))) {
+    if (fl->canLoad(fullURL, fl->outgoingReferrer(), hideReferrer)) {
         resource = cache()->requestResource(this, type, fullURL, m_expireDate, charset);
         if (resource) {
             m_docResources.set(resource->url(), resource);
@@ -185,8 +188,10 @@ void DocLoader::removeCachedResource(CachedResource* resource) const
 
 void DocLoader::setLoadInProgress(bool load)
 {
+    ASSERT(m_frame);
+
     m_loadInProgress = load;
-    if (!load && m_frame)
+    if (!load)
         m_frame->loader()->loadDone();
 }
 

WebCore/loader/FrameLoader.cpp
@@ -1894,11 +1894,11 @@ void FrameLoader::load(DocumentLoader* loader, FrameLoadType type, PassRefPtr<Fo
 
 bool FrameLoader::canLoad(const KURL& url, const String& referrer, bool& hideReferrer)
 {
-    bool referrerIsWebURL = referrer.startsWith("http:", false) || referrer.startsWith("https:", false);
+    bool referrerIsSecureURL = referrer.startsWith("https:", false);
+    bool referrerIsWebURL = referrerIsSecureURL || referrer.startsWith("http:", false);
     bool referrerIsLocalURL = referrer.startsWith("file:", false) || referrer.startsWith("applewebdata:");
     bool URLIsFileURL = url.protocol().startsWith("file", false);
-    bool referrerIsSecureURL = referrer.startsWith("https:", false);
-    bool URLIsSecureURL = url.protocol().startsWith("https", false);
+    bool URLIsSecureURL = url.protocol().startsWith("https", false);    
     
     hideReferrer = !referrerIsWebURL || (referrerIsSecureURL && !URLIsSecureURL);
     return !URLIsFileURL || referrerIsLocalURL;
nwjs/blink

commit 867be3d53560f662a5a5586cc2d1123066214c08

LayoutTests:
        Reviewed by David Kilzer.

        <rdar://problem/5313502>
        Many webarchive tests fail due to different NSURLResponse serialization on Leopard
        
        * mac/leopard/Skipped:
        * webarchive/archive-with-unencoded-url-expected.txt:
        * webarchive/test-body-background-expected.txt:
        * webarchive/test-duplicate-resources-expected.txt:
        * webarchive/test-frameset-expected.txt:
        * webarchive/test-img-src-expected.txt:
        * webarchive/test-input-src-expected.txt:
        * webarchive/test-link-href-expected.txt:
        * webarchive/test-object-data-expected.txt:
        * webarchive/test-script-src-expected.txt:
        * webarchive/test-table-background-expected.txt:
        * webarchive/test-td-background-expected.txt:

WebKitTools:

        Reviewed by David Kilzer.

        <rdar://problem/5313502>
        Many webarchive tests fail due to different NSURLResponse serialization on Leopard

        Don't dump the serialized form of NSURLResponse. Instead, create a dictionary with the
        response's attributes. Also change the JavaScript MIME type to be "text/javascript".
        
        * DumpRenderTree/DumpRenderTree.m:
        (convertMIMEType):
        (convertWebResourceDataToString):
        (convertWebResourceResponseToDictionary):
        (serializeWebArchiveToXML):



git-svn-id: svn://svn.chromium.org/blink/trunk@24076 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -653,10 +653,18 @@ static void dumpFrameScrollPosition(WebFrame *f)
     }
 }
 
+static void convertMIMEType(NSMutableString *mimeType)
+{
+    if ([mimeType isEqualToString:@"application/x-javascript"])
+        [mimeType setString:@"text/javascript"];
+}
+
 static void convertWebResourceDataToString(NSMutableDictionary *resource)
 {
-    NSString *mimeType = [resource objectForKey:@"WebResourceMIMEType"];
-    if ([mimeType hasPrefix:@"text/"] || [mimeType isEqualToString:@"application/x-javascript"]) {
+    NSMutableString *mimeType = [resource objectForKey:@"WebResourceMIMEType"];
+    convertMIMEType(mimeType);
+    
+    if ([mimeType hasPrefix:@"text/"]) {
         NSData *data = [resource objectForKey:@"WebResourceData"];
         NSString *dataAsString = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
         [resource setObject:dataAsString forKey:@"WebResourceData"];
@@ -671,7 +679,7 @@ static void normalizeWebResourceURL(NSMutableString *webResourceURL, NSString *o
                                          range:NSMakeRange(0, [webResourceURL length])];
 }
 
-static void normalizeWebResourceResponse(NSMutableDictionary *propertyList, NSString *oldURLBase)
+static void convertWebResourceResponseToDictionary(NSMutableDictionary *propertyList, NSString *oldURLBase)
 {
     NSURLResponse *response = nil;
     NSData *responseData = [propertyList objectForKey:@"WebResourceResponse"]; // WebResourceResponseKey in WebResource.m
@@ -681,25 +689,31 @@ static void normalizeWebResourceResponse(NSMutableDictionary *propertyList, NSSt
         response = [unarchiver decodeObjectForKey:@"WebResourceResponse"]; // WebResourceResponseKey in WebResource.m
         [unarchiver finishDecoding];
         [unarchiver release];
-
-        // Create replacement NSURLReponse
-        NSMutableString *URL = [[[NSMutableString alloc] initWithContentsOfURL:[response URL]] autorelease];
-        normalizeWebResourceURL(URL, oldURLBase);
-        NSURLResponse *newResponse = [[NSURLResponse alloc] initWithURL:[[[NSURL alloc] initWithString:URL] autorelease]
-                                                               MIMEType:[response MIMEType]
-                                                  expectedContentLength:[response expectedContentLength]
-                                                       textEncodingName:[response textEncodingName]];
-        [newResponse autorelease];
-
-        // Encode replacement NSURLResponse
-        NSMutableData *newResponseData = [[NSMutableData alloc] init];
-        NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc] initForWritingWithMutableData:newResponseData];
-        [archiver encodeObject:newResponse forKey:@"WebResourceResponse"];
-        [archiver finishEncoding];
-        [archiver release];
-        [propertyList setObject:newResponseData forKey:@"WebResourceResponse"]; // WebResourceResponseKey in WebResource.m
-        [newResponseData release];
+    }        
+    
+    NSMutableDictionary *responseDictionary = [[NSMutableDictionary alloc] init];
+    
+    NSMutableString *urlString = [[[response URL] description] mutableCopy];
+    normalizeWebResourceURL(urlString, oldURLBase);
+    [responseDictionary setObject:urlString forKey:@"URL"];
+    
+    NSMutableString *mimeTypeString = [[response MIMEType] mutableCopy];
+    convertMIMEType(mimeTypeString);
+    [responseDictionary setObject:mimeTypeString forKey:@"MIMEType"];
+    NSString *textEncodingName = [response textEncodingName];
+    if (textEncodingName)
+        [responseDictionary setObject:textEncodingName forKey:@"textEncodingName"];
+    [responseDictionary setObject:[NSNumber numberWithLongLong:[response expectedContentLength]] forKey:@"expectedContentLength"];
+    
+    if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+        
+        [responseDictionary setObject:[httpResponse allHeaderFields] forKey:@"allHeaderFields"];
+        [responseDictionary setObject:[NSNumber numberWithInt:[httpResponse statusCode]] forKey:@"statusCode"];
     }
+    
+    [propertyList setObject:responseDictionary forKey:@"WebResourceResponse"];
+    [responseDictionary release];
 }
 
 static NSString *serializeWebArchiveToXML(WebArchive *webArchive)
@@ -714,7 +728,7 @@ static NSString *serializeWebArchiveToXML(WebArchive *webArchive)
 
     // Normalize WebResourceResponse and WebResourceURL values in plist for testing
     NSString *cwdURL = [@"file://" stringByAppendingString:[[[NSFileManager defaultManager] currentDirectoryPath] stringByExpandingTildeInPath]];
-
+    
     NSMutableArray *resources = [NSMutableArray arrayWithCapacity:1];
     [resources addObject:propertyList];
 
@@ -736,7 +750,7 @@ static NSString *serializeWebArchiveToXML(WebArchive *webArchive)
         NSMutableDictionary *subresourcePropertyList;
         while ((subresourcePropertyList = [enumerator nextObject])) {
             normalizeWebResourceURL([subresourcePropertyList objectForKey:@"WebResourceURL"], cwdURL);
-            normalizeWebResourceResponse(subresourcePropertyList, cwdURL);
+            convertWebResourceResponseToDictionary(subresourcePropertyList, cwdURL);
             convertWebResourceDataToString(subresourcePropertyList);
         }
     }

LayoutTests/mac/leopard/Skipped
@@ -30,21 +30,6 @@ http/tests/navigation/document-location-onload.html
 fast/forms/attributed-strings.html
 fast/text/attributed-substring-from-range-001.html
 
-# <rdar://problem/5313502> Many webarchive tests fail due to different plist DOCTYPE on Leopard
-webarchive/archive-empty-frame-dom.html
-webarchive/archive-empty-frame-source.html
-webarchive/archive-with-unencoded-url.html
-webarchive/test-body-background.html
-webarchive/test-duplicate-resources.html
-webarchive/test-frameset.html
-webarchive/test-img-src.html
-webarchive/test-input-src.html
-webarchive/test-link-href.html
-webarchive/test-object-data.html
-webarchive/test-script-src.html
-webarchive/test-table-background.html
-webarchive/test-td-background.html
-
 # <rdar://problem/5313497> Some international characters in layout tests have different metrics on Leopard vs. Tiger
 css2.1/t1202-counter-09-b.html
 css2.1/t1202-counters-09-b.html

LayoutTests/webarchive/archive-with-unencoded-url-expected.txt
@@ -72,16 +72,14 @@
 			<key>WebResourceMIMEType</key>
 			<string>image/gif</string>
 			<key>WebResourceResponse</key>
-			<data>
-			YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVyWCR2ZXJzaW9uVCR0
-			b3BYJG9iamVjdHNfEA9OU0tleWVkQXJjaGl2ZXISAAGGoNEICV8Q
-			E1dlYlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRudWxs1w0ODxAR
-			EhMUFRYXGBkVUiQyUiQzUiQ0UiQwViRjbGFzc1IkNVIkMYACgAAR
-			BcQQB4ADI3/v////////gABZaW1hZ2UvZ2lm0h0eHyBYJGNsYXNz
-			ZXNaJGNsYXNzbmFtZaIgIV1OU1VSTFJlc3BvbnNlWE5TT2JqZWN0
-			CBEbJCkyRElMYmRpb36BhIeKkZSXmZueoKKrrbe8xdDT4QAAAAAA
-			AAEBAAAAAAAAACIAAAAAAAAAAAAAAAAAAADq
-			</data>
+			<dict>
+				<key>MIMEType</key>
+				<string>image/gif</string>
+				<key>URL</key>
+				<string>file:///LayoutTests/webarchive/resources/apple.gif?foo%7Cbar</string>
+				<key>expectedContentLength</key>
+				<integer>1476</integer>
+			</dict>
 			<key>WebResourceURL</key>
 			<string>file:///LayoutTests/webarchive/resources/apple.gif?foo|bar</string>
 		</dict>

LayoutTests/webarchive/test-body-background-expected.txt
@@ -70,16 +70,14 @@
 			<key>WebResourceMIMEType</key>
 			<string>image/gif</string>
 			<key>WebResourceResponse</key>
-			<data>
-			YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVyWCR2ZXJzaW9uVCR0
-			b3BYJG9iamVjdHNfEA9OU0tleWVkQXJjaGl2ZXISAAGGoNEICV8Q
-			E1dlYlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRudWxs1w0ODxAR
-			EhMUFRYXGBkVUiQyUiQzUiQ0UiQwViRjbGFzc1IkNVIkMYACgAAR
-			BcQQB4ADI3/v////////gABZaW1hZ2UvZ2lm0h0eHyBYJGNsYXNz
-			ZXNaJGNsYXNzbmFtZaIgIV1OU1VSTFJlc3BvbnNlWE5TT2JqZWN0
-			CBEbJCkyRElMYmRpb36BhIeKkZSXmZueoKKrrbe8xdDT4QAAAAAA
-			AAEBAAAAAAAAACIAAAAAAAAAAAAAAAAAAADq
-			</data>
+			<dict>
+				<key>MIMEType</key>
+				<string>image/gif</string>
+				<key>URL</key>
+				<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
+				<key>expectedContentLength</key>
+				<integer>1476</integer>
+			</dict>
 			<key>WebResourceURL</key>
 			<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
 		</dict>

LayoutTests/webarchive/test-duplicate-resources-expected.txt
@@ -72,20 +72,14 @@
 					<key>WebResourceMIMEType</key>
 					<string>text/css</string>
 					<key>WebResourceResponse</key>
-					<data>
-					YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVy
-					WCR2ZXJzaW9uVCR0b3BYJG9iamVjdHNfEA9O
-					U0tleWVkQXJjaGl2ZXISAAGGoNEICV8QE1dl
-					YlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRu
-					dWxs1w0ODxAREhMUFRYXGBkVUiQyUiQzUiQ0
-					UiQwViRjbGFzc1IkNVIkMYACgAAQPxAHgAMj
-					f+////////+AAFh0ZXh0L2Nzc9IdHh8gWCRj
-					bGFzc2VzWiRjbGFzc25hbWWiICFdTlNVUkxS
-					ZXNwb25zZVhOU09iamVjdAgRGyQpMkRJTGJk
-					aW9+gYSHipGUl5mbnZ+hqqy1usPO0d8AAAAA
-					AAABAQAAAAAAAAAiAAAAAAAAAAAAAAAAAAAA
-					6A==
-					</data>
+					<dict>
+						<key>MIMEType</key>
+						<string>text/css</string>
+						<key>URL</key>
+						<string>file:///LayoutTests/webarchive/resources/test-style.css</string>
+						<key>expectedContentLength</key>
+						<integer>63</integer>
+					</dict>
 					<key>WebResourceURL</key>
 					<string>file:///LayoutTests/webarchive/resources/test-style.css</string>
 				</dict>
@@ -151,20 +145,14 @@
 					<key>WebResourceMIMEType</key>
 					<string>image/gif</string>
 					<key>WebResourceResponse</key>
-					<data>
-					YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVy
-					WCR2ZXJzaW9uVCR0b3BYJG9iamVjdHNfEA9O
-					U0tleWVkQXJjaGl2ZXISAAGGoNEICV8QE1dl
-					YlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRu
-					dWxs1w0ODxAREhMUFRYXGBkVUiQyUiQzUiQ0
-					UiQwViRjbGFzc1IkNVIkMYACgAARBcQQB4AD
-					I3/v////////gABZaW1hZ2UvZ2lm0h0eHyBY
-					JGNsYXNzZXNaJGNsYXNzbmFtZaIgIV1OU1VS
-					TFJlc3BvbnNlWE5TT2JqZWN0CBEbJCkyRElM
-					YmRpb36BhIeKkZSXmZueoKKrrbe8xdDT4QAA
-					AAAAAAEBAAAAAAAAACIAAAAAAAAAAAAAAAAA
-					AADq
-					</data>
+					<dict>
+						<key>MIMEType</key>
+						<string>image/gif</string>
+						<key>URL</key>
+						<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
+						<key>expectedContentLength</key>
+						<integer>1476</integer>
+					</dict>
 					<key>WebResourceURL</key>
 					<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
 				</dict>
@@ -179,19 +167,16 @@
     layoutTestController.dumpDOMAsWebArchive();
 </string>
 			<key>WebResourceMIMEType</key>
-			<string>application/x-javascript</string>
+			<string>text/javascript</string>
 			<key>WebResourceResponse</key>
-			<data>
-			YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVyWCR2ZXJzaW9uVCR0
-			b3BYJG9iamVjdHNfEA9OU0tleWVkQXJjaGl2ZXISAAGGoNEICV8Q
-			E1dlYlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRudWxs1w0ODxAR
-			EhMUFRYXGBkVUiQyUiQzUiQ0UiQwViRjbGFzc1IkNVIkMYACgAAQ
-			URAHgAMjf+////////+AAF8QGGFwcGxpY2F0aW9uL3gtamF2YXNj
-			cmlwdNIdHh8gWCRjbGFzc2VzWiRjbGFzc25hbWWiICFdTlNVUkxS
-			ZXNwb25zZVhOU09iamVjdAgRGyQpMkRJTGJkaW9+gYSHipGUl5mb
-			nZ+hqqzHzNXg4/EAAAAAAAABAQAAAAAAAAAiAAAAAAAAAAAAAAAA
-			AAAA+g==
-			</data>
+			<dict>
+				<key>MIMEType</key>
+				<string>text/javascript</string>
+				<key>URL</key>
+				<string>file:///LayoutTests/webarchive/resources/dump-webarchive.js</string>
+				<key>expectedContentLength</key>
+				<integer>81</integer>
+			</dict>
 			<key>WebResourceURL</key>
 			<string>file:///LayoutTests/webarchive/resources/dump-webarchive.js</string>
 		</dict>
@@ -202,16 +187,14 @@
 			<key>WebResourceMIMEType</key>
 			<string>text/css</string>
 			<key>WebResourceResponse</key>
-			<data>
-			YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVyWCR2ZXJzaW9uVCR0
-			b3BYJG9iamVjdHNfEA9OU0tleWVkQXJjaGl2ZXISAAGGoNEICV8Q
-			E1dlYlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRudWxs1w0ODxAR
-			EhMUFRYXGBkVUiQyUiQzUiQ0UiQwViRjbGFzc1IkNVIkMYACgAAQ
-			PxAHgAMjf+////////+AAFh0ZXh0L2Nzc9IdHh8gWCRjbGFzc2Vz
-			WiRjbGFzc25hbWWiICFdTlNVUkxSZXNwb25zZVhOU09iamVjdAgR
-			GyQpMkRJTGJkaW9+gYSHipGUl5mbnZ+hqqy1usPO0d8AAAAAAAAB
-			AQAAAAAAAAAiAAAAAAAAAAAAAAAAAAAA6A==
-			</data>
+			<dict>
+				<key>MIMEType</key>
+				<string>text/css</string>
+				<key>URL</key>
+				<string>file:///LayoutTests/webarchive/resources/test-style.css</string>
+				<key>expectedContentLength</key>
+				<integer>63</integer>
+			</dict>
 			<key>WebResourceURL</key>
 			<string>file:///LayoutTests/webarchive/resources/test-style.css</string>
 		</dict>
@@ -260,16 +243,14 @@
 			<key>WebResourceMIMEType</key>
 			<string>image/gif</string>
 			<key>WebResourceResponse</key>
-			<data>
-			YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVyWCR2ZXJzaW9uVCR0
-			b3BYJG9iamVjdHNfEA9OU0tleWVkQXJjaGl2ZXISAAGGoNEICV8Q
-			E1dlYlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRudWxs1w0ODxAR
-			EhMUFRYXGBkVUiQyUiQzUiQ0UiQwViRjbGFzc1IkNVIkMYACgAAR
-			BcQQB4ADI3/v////////gABZaW1hZ2UvZ2lm0h0eHyBYJGNsYXNz
-			ZXNaJGNsYXNzbmFtZaIgIV1OU1VSTFJlc3BvbnNlWE5TT2JqZWN0
-			CBEbJCkyRElMYmRpb36BhIeKkZSXmZueoKKrrbe8xdDT4QAAAAAA
-			AAEBAAAAAAAAACIAAAAAAAAAAAAAAAAAAADq
-			</data>
+			<dict>
+				<key>MIMEType</key>
+				<string>image/gif</string>
+				<key>URL</key>
+				<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
+				<key>expectedContentLength</key>
+				<integer>1476</integer>
+			</dict>
 			<key>WebResourceURL</key>
 			<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
 		</dict>

LayoutTests/webarchive/test-frameset-expected.txt
@@ -61,20 +61,14 @@
 					<key>WebResourceMIMEType</key>
 					<string>text/css</string>
 					<key>WebResourceResponse</key>
-					<data>
-					YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVy
-					WCR2ZXJzaW9uVCR0b3BYJG9iamVjdHNfEA9O
-					U0tleWVkQXJjaGl2ZXISAAGGoNEICV8QE1dl
-					YlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRu
-					dWxs1w0ODxAREhMUFRYXGBkVUiQyUiQzUiQ0
-					UiQwViRjbGFzc1IkNVIkMYACgAAQPxAHgAMj
-					f+////////+AAFh0ZXh0L2Nzc9IdHh8gWCRj
-					bGFzc2VzWiRjbGFzc25hbWWiICFdTlNVUkxS
-					ZXNwb25zZVhOU09iamVjdAgRGyQpMkRJTGJk
-					aW9+gYSHipGUl5mbnZ+hqqy1usPO0d8AAAAA
-					AAABAQAAAAAAAAAiAAAAAAAAAAAAAAAAAAAA
-					6A==
-					</data>
+					<dict>
+						<key>MIMEType</key>
+						<string>text/css</string>
+						<key>URL</key>
+						<string>file:///LayoutTests/webarchive/resources/test-style.css</string>
+						<key>expectedContentLength</key>
+						<integer>63</integer>
+					</dict>
 					<key>WebResourceURL</key>
 					<string>file:///LayoutTests/webarchive/resources/test-style.css</string>
 				</dict>
@@ -140,20 +134,14 @@
 					<key>WebResourceMIMEType</key>
 					<string>image/gif</string>
 					<key>WebResourceResponse</key>
-					<data>
-					YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVy
-					WCR2ZXJzaW9uVCR0b3BYJG9iamVjdHNfEA9O
-					U0tleWVkQXJjaGl2ZXISAAGGoNEICV8QE1dl
-					YlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRu
-					dWxs1w0ODxAREhMUFRYXGBkVUiQyUiQzUiQ0
-					UiQwViRjbGFzc1IkNVIkMYACgAARBcQQB4AD
-					I3/v////////gABZaW1hZ2UvZ2lm0h0eHyBY
-					JGNsYXNzZXNaJGNsYXNzbmFtZaIgIV1OU1VS
-					TFJlc3BvbnNlWE5TT2JqZWN0CBEbJCkyRElM
-					YmRpb36BhIeKkZSXmZueoKKrrbe8xdDT4QAA
-					AAAAAAEBAAAAAAAAACIAAAAAAAAAAAAAAAAA
-					AADq
-					</data>
+					<dict>
+						<key>MIMEType</key>
+						<string>image/gif</string>
+						<key>URL</key>
+						<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
+						<key>expectedContentLength</key>
+						<integer>1476</integer>
+					</dict>
 					<key>WebResourceURL</key>
 					<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
 				</dict>

LayoutTests/webarchive/test-img-src-expected.txt
@@ -71,16 +71,14 @@
 			<key>WebResourceMIMEType</key>
 			<string>image/gif</string>
 			<key>WebResourceResponse</key>
-			<data>
-			YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVyWCR2ZXJzaW9uVCR0
-			b3BYJG9iamVjdHNfEA9OU0tleWVkQXJjaGl2ZXISAAGGoNEICV8Q
-			E1dlYlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRudWxs1w0ODxAR
-			EhMUFRYXGBkVUiQyUiQzUiQ0UiQwViRjbGFzc1IkNVIkMYACgAAR
-			BcQQB4ADI3/v////////gABZaW1hZ2UvZ2lm0h0eHyBYJGNsYXNz
-			ZXNaJGNsYXNzbmFtZaIgIV1OU1VSTFJlc3BvbnNlWE5TT2JqZWN0
-			CBEbJCkyRElMYmRpb36BhIeKkZSXmZueoKKrrbe8xdDT4QAAAAAA
-			AAEBAAAAAAAAACIAAAAAAAAAAAAAAAAAAADq
-			</data>
+			<dict>
+				<key>MIMEType</key>
+				<string>image/gif</string>
+				<key>URL</key>
+				<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
+				<key>expectedContentLength</key>
+				<integer>1476</integer>
+			</dict>
 			<key>WebResourceURL</key>
 			<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
 		</dict>

LayoutTests/webarchive/test-input-src-expected.txt
@@ -73,16 +73,14 @@
 			<key>WebResourceMIMEType</key>
 			<string>image/gif</string>
 			<key>WebResourceResponse</key>
-			<data>
-			YnBsaXN0MDDUAQIDBAUGBwpZJGFyY2hpdmVyWCR2ZXJzaW9uVCR0
-			b3BYJG9iamVjdHNfEA9OU0tleWVkQXJjaGl2ZXISAAGGoNEICV8Q
-			E1dlYlJlc291cmNlUmVzcG9uc2WAAaQLDBscVSRudWxs1w0ODxAR
-			EhMUFRYXGBkVUiQyUiQzUiQ0UiQwViRjbGFzc1IkNVIkMYACgAAR
-			BcQQB4ADI3/v////////gABZaW1hZ2UvZ2lm0h0eHyBYJGNsYXNz
-			ZXNaJGNsYXNzbmFtZaIgIV1OU1VSTFJlc3BvbnNlWE5TT2JqZWN0
-			CBEbJCkyRElMYmRpb36BhIeKkZSXmZueoKKrrbe8xdDT4QAAAAAA
-			AAEBAAAAAAAAACIAAAAAAAAAAAAAAAAAAADq
-			</data>
+			<dict>
+				<key>MIMEType</key>
+				<string>image/gif</string>
+				<key>URL</key>
+				<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
+				<key>expectedContentLength</key>
+				<integer>1476</integer>
+			</dict>
 			<key>WebResourceURL</key>
 			<string>file:///LayoutTests/webarchive/resources/apple.gif</string>
 		</dict>

commit 2ae181b9f6f28fe7ebc9013ce457f4fc432bf587

2007-07-07  Mark Rowe  <mrowe@apple.com>
        Reviewed by Oliver.

        * DumpRenderTree/DumpRenderTree.m:
        (convertWebResourceResponseToDictionary): Fix leak of two NSMutableString's introduced in r24076.


git-svn-id: svn://svn.chromium.org/blink/trunk@24086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebKitTools/DumpRenderTree/DumpRenderTree.m
@@ -696,10 +696,13 @@ static void convertWebResourceResponseToDictionary(NSMutableDictionary *property
     NSMutableString *urlString = [[[response URL] description] mutableCopy];
     normalizeWebResourceURL(urlString, oldURLBase);
     [responseDictionary setObject:urlString forKey:@"URL"];
+    [urlString release];
     
     NSMutableString *mimeTypeString = [[response MIMEType] mutableCopy];
     convertMIMEType(mimeTypeString);
     [responseDictionary setObject:mimeTypeString forKey:@"MIMEType"];
+    [mimeTypeString release];
+
     NSString *textEncodingName = [response textEncodingName];
     if (textEncodingName)
         [responseDictionary setObject:textEncodingName forKey:@"textEncodingName"];
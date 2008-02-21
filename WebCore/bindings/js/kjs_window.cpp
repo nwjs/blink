nwjs/blink

commit 55d7fb2dc37b29dc40ea41c9672197655d4bffa3

WebCore:
        Reviewed by Geoff.
        
        Fix <rdar://problem/5698200>
        eBay photo uploading hangs and causes slow script warning to pop up
        
        In a case like this
        
            var f = window.parent.parentFunction;
            document.domain = document.domain; // this makes window.parent inaccessible
            f();
        
        Firefox allows parentFunction to access parents properties. Match this behavior.
        
        In a domain security check against the dynamic global object fails for the specific reason that one of 
        the frames has written to the document.domain property and another has not (but they match otherwise), 
        then recheck against the lexical global object.

        Test: http/tests/security/cross-frame-access-callback-explicit-domain-ALLOW.html
              http/tests/security/cross-frame-access-callback-explicit-domain-DENY.html

        * bindings/js/kjs_window.cpp:
        (KJS::Window::allowsAccessFrom):
        (KJS::Window::printErrorMessage):
        * bindings/js/kjs_window.h:
        * loader/FrameLoader.cpp:
        (WebCore::FrameLoader::shouldAllowNavigation):
        * platform/SecurityOrigin.cpp:
        (WebCore::SecurityOrigin::canAccess):
        * platform/SecurityOrigin.h:
        (WebCore::SecurityOrigin::):

LayoutTests:

        Reviewed by Geoff.
        
        Test for <rdar://problem/5698200>
        eBay photo uploading hangs and causes slow script warning to pop up

        * http/tests/security/cross-frame-access-callback-explicit-domain-ALLOW-expected.txt: Added.
        * http/tests/security/cross-frame-access-callback-explicit-domain-ALLOW.html: Added.
        * http/tests/security/cross-frame-access-callback-explicit-domain-DENY-expected.txt: Added.
        * http/tests/security/cross-frame-access-callback-explicit-domain-DENY.html: Added.
        * http/tests/security/resources/cross-frame-iframe-callback-explicit-domain-ALLOW.html: Added.

WebCore/bindings/js/kjs_window.cpp
@@ -815,13 +815,43 @@ void Window::put(ExecState* exec, const Identifier& propertyName, JSValue* value
 
 bool Window::allowsAccessFrom(const JSGlobalObject* other) const
 {
+    SecurityOrigin::Reason reason;
+    String message;
+    if (allowsAccessFrom(other, reason, message))
+        return true;
+    printErrorMessage(message);
+    return false;
+}
+    
+bool Window::allowsAccessFrom(ExecState* exec) const
+{ 
+    SecurityOrigin::Reason reason;
+    String message;
+    if (allowsAccessFrom(exec->dynamicGlobalObject(), reason, message))
+        return true;
+    if (reason == SecurityOrigin::DomainSetInDOMMismatch) {
+        // If the only reason the access failed was a domainSetInDOM bit mismatch, try again against 
+        // lexical global object <rdar://problem/5698200>
+        if (allowsAccessFrom(exec->lexicalGlobalObject(), reason, message))
+            return true;
+    }
+    printErrorMessage(message);
+    return false;
+}
+    
+bool Window::allowsAccessFrom(const JSGlobalObject* other, SecurityOrigin::Reason& reason, String& message) const
+{
     const Frame* originFrame = static_cast<const Window*>(other)->impl()->frame();
-    if (!originFrame)
+    if (!originFrame) {
+        reason = SecurityOrigin::GenericMismatch;
         return false;
+    }
 
     const Frame* targetFrame = impl()->frame();
-    if (!targetFrame)
+    if (!targetFrame) {
+        reason = SecurityOrigin::GenericMismatch;
         return false;
+    }
 
     if (originFrame == targetFrame)
         return true;
@@ -839,22 +869,32 @@ bool Window::allowsAccessFrom(const JSGlobalObject* other) const
     const SecurityOrigin* originSecurityOrigin = originDocument->securityOrigin();
     const SecurityOrigin* targetSecurityOrigin = targetDocument->securityOrigin();
 
-    if (originSecurityOrigin->canAccess(targetSecurityOrigin))
+    if (originSecurityOrigin->canAccess(targetSecurityOrigin, reason))
         return true;
 
-    if (!targetFrame->settings()->privateBrowsingEnabled()) {
-        // FIXME: this error message should contain more specifics of why the same origin check has failed.
-        String message = String::format("Unsafe JavaScript attempt to access frame with URL %s from frame with URL %s. Domains, protocols and ports must match.\n",
-                                        targetDocument->url().utf8().data(), originDocument->url().utf8().data());
-
-        if (Interpreter::shouldPrintExceptions())
-            printf("%s", message.utf8().data());
-
-        if (Page* page = targetFrame->page())
-            page->chrome()->addMessageToConsole(JSMessageSource, ErrorMessageLevel, message, 1, String()); // FIXME: provide a real line number and source URL.
-    }
-
+    // FIXME: this error message should contain more specifics of why the same origin check has failed.
+    message = String::format("Unsafe JavaScript attempt to access frame with URL %s from frame with URL %s. Domains, protocols and ports must match.\n",
+                                    targetDocument->url().utf8().data(), originDocument->url().utf8().data());
     return false;
+}
+ 
+void Window::printErrorMessage(const String& message) const
+{
+    if (!message.length())
+        return;
+
+    Frame* frame = impl()->frame();
+    if (!frame)
+        return;
+
+    if (frame->settings()->privateBrowsingEnabled())
+        return;
+
+    if (Interpreter::shouldPrintExceptions())
+        printf("%s", message.utf8().data());
+
+    if (Page* page = frame->page())
+        page->chrome()->addMessageToConsole(JSMessageSource, ErrorMessageLevel, message, 1, String()); // FIXME: provide a real line number and source URL.
 }
 
 ExecState* Window::globalExec()

WebCore/bindings/js/kjs_window.h
@@ -23,6 +23,7 @@
 #include "PlatformString.h"
 #include "kjs_binding.h"
 #include <kjs/protect.h>
+#include "SecurityOrigin.h"
 #include <wtf/HashMap.h>
 #include <wtf/Noncopyable.h>
 #include <wtf/OwnPtr.h>
@@ -121,8 +122,10 @@ namespace KJS {
 
     virtual bool shouldInterruptScript() const;
 
+    bool allowsAccessFrom(ExecState*) const;
+
+    // Don't call this version of allowsAccessFrom -- it's a slightly incorrect implementation used only by WebScriptObject
     virtual bool allowsAccessFrom(const JSGlobalObject*) const;
-    bool allowsAccessFrom(ExecState* exec) const { return allowsAccessFrom(exec->dynamicGlobalObject()); }
 
     enum {
         // Attributes
@@ -155,6 +158,9 @@ namespace KJS {
     void clearHelperObjectProperties();
     void clearAllTimeouts();
     int installTimeout(WebCore::ScheduledAction*, int interval, bool singleShot);
+      
+    bool allowsAccessFrom(const JSGlobalObject*, WebCore::SecurityOrigin::Reason&, WebCore::String& message) const;
+    void printErrorMessage(const WebCore::String&) const;
 
     RefPtr<WebCore::DOMWindow> m_impl;
     OwnPtr<WindowPrivate> d;

LayoutTests/http/tests/security/cross-frame-access-callback-explicit-domain-ALLOW-expected.txt
@@ -1,0 +1,5 @@
+The child frame will get a function from the parent frame, set the document.domain property and then try to use the function. The function should still be able to access parent frame properties.  
+
+PASS: canGet('window.location.href') should be 'true' and is.
+PASS: window.thisIsTopFrame should be 'true' and is.
+

LayoutTests/http/tests/security/cross-frame-access-callback-explicit-domain-ALLOW.html
@@ -1,0 +1,25 @@
+<html>
+<head>
+    <script src="resources/cross-frame-access.js"></script>
+</head>
+<body>
+<p>The child frame will get a function from the parent frame, set the document.domain property 
+and then try to use the function. The function should still be able to access parent frame properties.
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+window.thisIsTopFrame = true;
+
+window.testFunction = function()
+{
+    shouldBeTrue("canGet('window.location.href')");
+    shouldBeTrue("window.thisIsTopFrame");
+    if (window.layoutTestController)
+        layoutTestController.notifyDone();
+}
+</script>
+<iframe src="resources/cross-frame-iframe-callback-explicit-domain-ALLOW.html" style=""></iframe>
+<pre id="console"></pre>
+</body>
+</html>

LayoutTests/http/tests/security/cross-frame-access-callback-explicit-domain-DENY-expected.txt
@@ -1,0 +1,10 @@
+CONSOLE MESSAGE: line 1: Unsafe JavaScript attempt to access frame with URL http://127.0.0.1:8000/security/cross-frame-access-callback-explicit-domain-DENY.html from frame with URL http://127.0.0.1:8000/security/resources/cross-frame-iframe-callback-explicit-domain-DENY.html. Domains, protocols and ports must match.
+
+Test that a child frame can't define a function and the use it to access parent properties after document.domain write blocks the access.  
+
+
+
+--------
+Frame: '<!--framePath //<!--frame0-->-->'
+--------
+PASS: canGet('parentWindow.location.href') should be 'false' and is.

LayoutTests/http/tests/security/cross-frame-access-callback-explicit-domain-DENY.html
@@ -1,0 +1,13 @@
+<html>
+<body>
+<p>Test that a child frame can't define a function and the use it to access parent properties after document.domain write blocks the access.
+<script>
+if (window.layoutTestController) {
+    layoutTestController.dumpAsText();
+    layoutTestController.dumpChildFramesAsText();
+}
+</script>
+<iframe src="resources/cross-frame-iframe-callback-explicit-domain-DENY.html" style=""></iframe>
+<pre id="console"></pre>
+</body>
+</html>

LayoutTests/http/tests/security/resources/cross-frame-iframe-callback-explicit-domain-ALLOW.html
@@ -1,0 +1,6 @@
+<script>
+var func = window.parent.testFunction;
+document.domain = document.domain; // after this window.parent properties are inaccessible
+func(); // yet this should still work
+</script>
+

LayoutTests/http/tests/security/resources/cross-frame-iframe-callback-explicit-domain-DENY.html
@@ -1,0 +1,17 @@
+<script src="cross-frame-access.js"></script>
+<body>
+<div id=console></div>
+<script>
+var parentWindow = window.parent;
+parentWindow.testFunction = function()
+{
+    shouldBeFalse("canGet('parentWindow.location.href')");
+    if (window.layoutTestController)
+        layoutTestController.notifyDone();
+}
+var func = parentWindow.testFunction;
+document.domain = document.domain; // after this window.parent properties are inaccessible
+func();
+</script>
+
+

commit 02e96c753cf65971e8bec1808f4b3072624f17c6

Reviewed by Sam Weinig.
        <rdar://problem/5753789>
        REGRESSION: 1.5% -2% Sunspider regression from r30009 (ebay photo upload hang)
        
        Ensure all versions of allowsAccessFrom are inlined to single functions. 
        This is a 2% win in browser hosted Sunspider. 

        * bindings/js/kjs_window.cpp:
        (KJS::Window::allowsAccessFrom):
        (KJS::Window::allowsAccessFromPrivate):
        * bindings/js/kjs_window.h:



git-svn-id: svn://svn.chromium.org/blink/trunk@30461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/bindings/js/kjs_window.cpp
@@ -825,7 +825,7 @@ bool Window::allowsAccessFrom(const JSGlobalObject* other) const
 {
     SecurityOrigin::Reason reason;
     String message;
-    if (allowsAccessFrom(other, reason, message))
+    if (allowsAccessFromPrivate(other, reason, message))
         return true;
     printErrorMessage(message);
     return false;
@@ -834,7 +834,7 @@ bool Window::allowsAccessFrom(const JSGlobalObject* other) const
 bool Window::allowsAccessFrom(ExecState* exec) const
 {
     String message;
-    if (allowsAccessFrom(exec, message))
+    if (allowsAccessFromPrivate(exec, message))
         return true;
     printErrorMessage(message);
     return false;
@@ -842,19 +842,24 @@ bool Window::allowsAccessFrom(ExecState* exec) const
 
 bool Window::allowsAccessFrom(ExecState* exec, String& message) const
 {
+    return allowsAccessFromPrivate(exec, message);
+}
+    
+inline bool Window::allowsAccessFromPrivate(ExecState* exec, String& message) const
+{
     SecurityOrigin::Reason reason;
-    if (allowsAccessFrom(exec->dynamicGlobalObject(), reason, message))
+    if (allowsAccessFromPrivate(exec->dynamicGlobalObject(), reason, message))
         return true;
     if (reason == SecurityOrigin::DomainSetInDOMMismatch) {
         // If the only reason the access failed was a domainSetInDOM bit mismatch, try again against 
         // lexical global object <rdar://problem/5698200>
-        if (allowsAccessFrom(exec->lexicalGlobalObject(), reason, message))
+        if (allowsAccessFromPrivate(exec->lexicalGlobalObject(), reason, message))
             return true;
     }
     return false;
 }
 
-bool Window::allowsAccessFrom(const JSGlobalObject* other, SecurityOrigin::Reason& reason, String& message) const
+inline bool Window::allowsAccessFromPrivate(const JSGlobalObject* other, SecurityOrigin::Reason& reason, String& message) const
 {
     const Frame* originFrame = static_cast<const Window*>(other)->impl()->frame();
     if (!originFrame) {
@@ -863,13 +868,14 @@ bool Window::allowsAccessFrom(const JSGlobalObject* other, SecurityOrigin::Reaso
     }
 
     const Frame* targetFrame = impl()->frame();
+
+    if (originFrame == targetFrame)
+        return true;
+    
     if (!targetFrame) {
         reason = SecurityOrigin::GenericMismatch;
         return false;
     }
-
-    if (originFrame == targetFrame)
-        return true;
 
     WebCore::Document* targetDocument = targetFrame->document();
 

WebCore/bindings/js/kjs_window.h
@@ -161,7 +161,8 @@ namespace KJS {
     void clearAllTimeouts();
     int installTimeout(WebCore::ScheduledAction*, int interval, bool singleShot);
 
-    bool allowsAccessFrom(const JSGlobalObject*, WebCore::SecurityOrigin::Reason&, WebCore::String& message) const;
+    bool allowsAccessFromPrivate(const JSGlobalObject*, WebCore::SecurityOrigin::Reason&, WebCore::String& message) const;
+    bool allowsAccessFromPrivate(ExecState*, WebCore::String& message) const;
 
     RefPtr<WebCore::DOMWindow> m_impl;
     OwnPtr<WindowPrivate> d;
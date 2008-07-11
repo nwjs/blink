nwjs/blink

commit 9a7d1bf5d8a79bc0c02a3b12554b1b54e7d5bbff

Reviewed by John Sullivan.
        <rdar://problem/5297146>
        Crash in Mail at WebCore::Frame::setUserStyleSheetLocation (reproducible w/ particular message)
        
        * loader/Cache.cpp:
        (WebCore::Cache::requestResource):
        Only insert the resource in the LRU list if the cache is enabled.
        
        (WebCore::Cache::insertInLRUList):
        (WebCore::Cache::resourceAccessed):
        Assert that the resource is in the cache.



git-svn-id: svn://svn.chromium.org/blink/trunk@23923 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/loader/Cache.cpp
@@ -107,9 +107,12 @@ CachedResource* Cache::requestResource(DocLoader* docLoader, CachedResource::Typ
         resource = createResource(type, docLoader, url, charset, skipCanLoadCheck, sendResourceLoadCallbacks);
         ASSERT(resource);
         ASSERT(resource->inCache());
-        if (!disabled())
+        if (!disabled()) {
             m_resources.set(url.url(), resource);  // The size will be added in later once the resource is loaded and calls back to us with the new size.
-        else {
+            
+            // This will move the resource to the front of its LRU list and increase its access count.
+            resourceAccessed(resource);
+        } else {
             // Kick the resource out of the cache, because the cache is disabled.
             resource->setInCache(false);
             if (resource->errorOccurred()) {
@@ -121,9 +124,6 @@ CachedResource* Cache::requestResource(DocLoader* docLoader, CachedResource::Typ
             }
         }
     }
-
-    // This will move the resource to the front of its LRU list and increase its access count.
-    resourceAccessed(resource);
 
     if (resource->type() != type)
         return 0;
@@ -364,7 +364,8 @@ void Cache::insertInLRUList(CachedResource* resource)
 {
     // Make sure we aren't in some list already.
     ASSERT(!resource->m_nextInAllResourcesList && !resource->m_prevInAllResourcesList);
-
+    ASSERT(resource->inCache());
+    
     LRUList* list = lruListFor(resource);
 
     resource->m_nextInAllResourcesList = list->m_head;
@@ -392,6 +393,8 @@ void Cache::insertInLRUList(CachedResource* resource)
 
 void Cache::resourceAccessed(CachedResource* resource)
 {
+    ASSERT(resource->inCache());
+    
     // Need to make sure to remove before we increase the access count, since
     // the queue will possibly change.
     removeFromLRUList(resource);

commit 08cfed8615ca704b78d5607e88f6d4f97155d9e6

Reviewed by Darin Adler.
        - fix popularity tracking for cached resources, which regressed in r23923

        * loader/Cache.cpp:
        (WebCore::Cache::requestResource): Changed to increase the access count
        whenever this function returns a CachedResource and the cache is
        enabled, instead of only when creating a new CachedResources. The
        incorrect behavior resulted in all resources in the cache having an
        access count of exactly 1 at all times.
        (WebCore::Cache::requestUserCSSStyleSheet): Ditto.



git-svn-id: svn://svn.chromium.org/blink/trunk@35128 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/loader/Cache.cpp
@@ -125,12 +125,9 @@ CachedResource* Cache::requestResource(DocLoader* docLoader, CachedResource::Typ
         
         resource->load(docLoader);
         
-        if (!disabled()) {
+        if (!disabled())
             m_resources.set(url.string(), resource);  // The size will be added in later once the resource is loaded and calls back to us with the new size.
-            
-            // This will move the resource to the front of its LRU list and increase its access count.
-            resourceAccessed(resource);
-        } else {
+        else {
             // Kick the resource out of the cache, because the cache is disabled.
             resource->setInCache(false);
             resource->setDocLoader(docLoader);
@@ -154,28 +151,39 @@ CachedResource* Cache::requestResource(DocLoader* docLoader, CachedResource::Typ
         return 0;
 #endif
 
+    if (!disabled()) {
+        // This will move the resource to the front of its LRU list and increase its access count.
+        resourceAccessed(resource);
+    }
+
     return resource;
 }
     
 CachedCSSStyleSheet* Cache::requestUserCSSStyleSheet(DocLoader* docLoader, const String& url, const String& charset)
 {
-    if (CachedResource* existing = m_resources.get(url))
-        return existing->type() == CachedResource::CSSStyleSheet ? static_cast<CachedCSSStyleSheet*>(existing) : 0;
+    CachedCSSStyleSheet* userSheet;
+    if (CachedResource* existing = m_resources.get(url)) {
+        if (existing->type() != CachedResource::CSSStyleSheet)
+            return 0;
+        userSheet = static_cast<CachedCSSStyleSheet*>(existing);
+    } else {
+        userSheet = new CachedCSSStyleSheet(url, charset);
 
-    CachedCSSStyleSheet* userSheet = new CachedCSSStyleSheet(url, charset);
+        // Pretend the resource is in the cache, to prevent it from being deleted during the load() call.
+        // FIXME: CachedResource should just use normal refcounting instead.
+        userSheet->setInCache(true);
+        // Don't load incrementally, skip load checks, don't send resource load callbacks.
+        userSheet->load(docLoader, false, true, false);
+        if (!disabled())
+            m_resources.set(url, userSheet);
+        else
+            userSheet->setInCache(false);
+    }
 
-    // Pretend the resource is in the cache, to prevent it from being deleted during the load() call.
-    // FIXME: CachedResource should just use normal refcounting instead.
-    userSheet->setInCache(true);
-    // Don't load incrementally, skip load checks, don't send resource load callbacks.
-    userSheet->load(docLoader, false, true, false);
     if (!disabled()) {
-        m_resources.set(url, userSheet);
         // This will move the resource to the front of its LRU list and increase its access count.
         resourceAccessed(userSheet);
     }
-    else
-        userSheet->setInCache(false);
 
     return userSheet;
 }
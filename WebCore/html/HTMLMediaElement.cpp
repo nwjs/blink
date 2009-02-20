nwjs/blink

commit 28bfcfce23f49f991c81a5b955edb2951769f572

2009-02-12  Eric Carlson  <eric.carlson@apple.com>
        Reviewed by Simon Fraser.

        https://bugs.webkit.org/show_bug.cgi?id=23923
        Implement mediaPlayerVolumeChanged so a media engine can report autonomous volume changes.
        Supporting this requires that we know when we are processing a media engine callback so
        we don't turn around and tell the media player to change the volume when it is reporting
        a volume change.
        
        * html/HTMLMediaElement.cpp:
        (WebCore::HTMLMediaElement::HTMLMediaElement): initialize m_processingMediaPlayerCallback
        (WebCore::HTMLMediaElement::mediaPlayerNetworkStateChanged): call begin/endProcessingMediaPlayerCallback
        (WebCore::HTMLMediaElement::setNetworkState): logic split out of mediaPlayerNetworkStateChanged 
        to make it easier to maintain m_processingMediaPlayerCallback.
        (WebCore::HTMLMediaElement::mediaPlayerReadyStateChanged): call begin/endProcessingMediaPlayerCallback
        (WebCore::HTMLMediaElement::mediaPlayerTimeChanged): Ditto.
        (WebCore::HTMLMediaElement::mediaPlayerRepaint): Ditto.
        (WebCore::HTMLMediaElement::mediaPlayerVolumeChanged): New
        (WebCore::HTMLMediaElement::updateVolume): Don't change media engine volume if we are processing
        a callback from the engine.
        * html/HTMLMediaElement.h:
        (WebCore::HTMLMediaElement::processingMediaPlayerCallback): New, return true if (m_processingMediaPlayerCallback>0)
        (WebCore::HTMLMediaElement::beginProcessingMediaPlayerCallback): New, increment m_processingMediaPlayerCallback
        (WebCore::HTMLMediaElement::endProcessingMediaPlayerCallback): New, decrement m_processingMediaPlayerCallback



git-svn-id: svn://svn.chromium.org/blink/trunk@40943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/html/HTMLMediaElement.cpp
@@ -85,6 +85,7 @@ HTMLMediaElement::HTMLMediaElement(const QualifiedName& tagName, Document* doc)
     , m_inActiveDocument(true)
     , m_player(0)
     , m_loadRestrictions(NoLoadRestriction)
+    , m_processingMediaPlayerCallback(0)
     , m_sendProgressEvents(true)
 {
     document()->registerForDocumentActivationCallbacks(this);
@@ -376,14 +377,23 @@ end:
     m_loadNestingLevel--;
 }
 
+
 void HTMLMediaElement::mediaPlayerNetworkStateChanged(MediaPlayer*)
 {
-    if (!m_begun || m_networkState == EMPTY)
+    if (!m_begun)
+        return;
+    
+    beginProcessingMediaPlayerCallback();
+    setNetworkState(m_player->networkState());
+    endProcessingMediaPlayerCallback();
+}
+
+void HTMLMediaElement::setNetworkState(MediaPlayer::NetworkState state)
+{
+    if (m_networkState == EMPTY)
         return;
     
     m_terminateLoadBelowNestingLevel = m_loadNestingLevel;
-
-    MediaPlayer::NetworkState state = m_player->networkState();
     
     // 3.14.9.4. Loading the media resource
     // 14
@@ -463,8 +473,12 @@ void HTMLMediaElement::mediaPlayerNetworkStateChanged(MediaPlayer*)
 
 void HTMLMediaElement::mediaPlayerReadyStateChanged(MediaPlayer*)
 {
+    beginProcessingMediaPlayerCallback();
+
     MediaPlayer::ReadyState state = m_player->readyState();
     setReadyState((ReadyState)state);
+
+    endProcessingMediaPlayerCallback();
 }
 
 void HTMLMediaElement::setReadyState(ReadyState state)
@@ -941,6 +955,8 @@ void HTMLMediaElement::checkIfSeekNeeded()
 
 void HTMLMediaElement::mediaPlayerTimeChanged(MediaPlayer*)
 {
+    beginProcessingMediaPlayerCallback();
+
     if (readyState() >= CAN_PLAY)
         m_seeking = false;
     
@@ -958,12 +974,23 @@ void HTMLMediaElement::mediaPlayerTimeChanged(MediaPlayer*)
     }
 
     updatePlayState();
+
+    endProcessingMediaPlayerCallback();
 }
 
 void HTMLMediaElement::mediaPlayerRepaint(MediaPlayer*)
 {
+    beginProcessingMediaPlayerCallback();
     if (renderer())
         renderer()->repaint();
+    endProcessingMediaPlayerCallback();
+}
+
+void HTMLMediaElement::mediaPlayerVolumeChanged(MediaPlayer*)
+{
+    beginProcessingMediaPlayerCallback();
+    updateVolume();
+    endProcessingMediaPlayerCallback();
 }
 
 PassRefPtr<TimeRanges> HTMLMediaElement::buffered() const
@@ -1031,10 +1058,13 @@ void HTMLMediaElement::updateVolume()
     if (!m_player)
         return;
 
-    Page* page = document()->page();
-    float volumeMultiplier = page ? page->mediaVolume() : 1;
-
-    m_player->setVolume(m_muted ? 0 : m_volume * volumeMultiplier);
+    // Avoid recursion when the player reports volume changes.
+    if (!processingMediaPlayerCallback()) {
+        Page* page = document()->page();
+        float volumeMultiplier = page ? page->mediaVolume() : 1;
+    
+        m_player->setVolume(m_muted ? 0 : m_volume * volumeMultiplier);
+    }
     
     if (renderer())
         renderer()->updateFromElement();

WebCore/html/HTMLMediaElement.h
@@ -145,12 +145,15 @@ protected:
     void dispatchEventAsync(const AtomicString& eventName);
     
     void setReadyState(ReadyState);
+    void setNetworkState(MediaPlayer::NetworkState state);
+
     
 private: // MediaPlayerObserver
     virtual void mediaPlayerNetworkStateChanged(MediaPlayer*);
     virtual void mediaPlayerReadyStateChanged(MediaPlayer*);
     virtual void mediaPlayerTimeChanged(MediaPlayer*);
     virtual void mediaPlayerRepaint(MediaPlayer*);
+    virtual void mediaPlayerVolumeChanged(MediaPlayer*);
 
 private:
     void loadTimerFired(Timer<HTMLMediaElement>*);
@@ -160,6 +163,9 @@ private:
     void checkIfSeekNeeded();
     
     bool processingUserGesture() const;
+    bool processingMediaPlayerCallback() const { return m_processingMediaPlayerCallback > 0; }
+    void beginProcessingMediaPlayerCallback() { ++m_processingMediaPlayerCallback; }
+    void endProcessingMediaPlayerCallback() { ASSERT(m_processingMediaPlayerCallback); --m_processingMediaPlayerCallback; }
 
     String selectMediaURL(String& mediaMIMEType);
     void updateVolume();
@@ -221,7 +227,11 @@ protected:
     OwnPtr<MediaPlayer> m_player;
 
     LoadRestrictions m_loadRestrictions;
-    
+
+    // counter incremented while processing a callback from the media player, so we can avoid
+    //  calling the media engine recursively
+    int m_processingMediaPlayerCallback;
+
     // Not all media engines provide enough information about a file to be able to
     // support progress events so setting m_sendProgressEvents disables them 
     bool m_sendProgressEvents;

commit 183218d680475d074073f65967688250c31560f4

2009-02-20  Eric Carlson  <eric.carlson@apple.com>
        Simon Fraser.

        https://bugs.webkit.org/show_bug.cgi?id=24063
        Make it possible for a port to require a user gesture to play/pause an <audio> or <video> element

        * html/HTMLMediaElement.cpp:
        (WebCore::HTMLMediaElement::HTMLMediaElement):  Rename m_loadRestrictions to m_Restrictions. 
        Initialize m_internalCall.
        (WebCore::HTMLMediaElement::loadTimerFired): Increment m_internalCall around call to load().
        (WebCore::HTMLMediaElement::load): Call loadInternal if restrictions check out.
        (WebCore::HTMLMediaElement::loadInternal): New, guts of old load()
        (WebCore::HTMLMediaElement::setNetworkState): Fix bug introduced in r40943
        (WebCore::HTMLMediaElement::play): Call playInternal if restrictions check out.
        (WebCore::HTMLMediaElement::playInternal): New, guts of old play()
        (WebCore::HTMLMediaElement::pause): Call pauseInternal if restrictions check out.
        (WebCore::HTMLMediaElement::pauseInternal): New, guts of old pause()
        (WebCore::HTMLMediaElement::togglePlayState): Call playInternal/pauseInternal
        (WebCore::HTMLMediaElement::deliverNotification): Remove unnecessary white space.
        * html/HTMLMediaElement.h: Rename m_loadRestrictions to m_Restrictions, add m_internalCall,
        add RequireUserGestureRateChangeRestriction.


git-svn-id: svn://svn.chromium.org/blink/trunk@41117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/html/HTMLMediaElement.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008, 2009 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -91,7 +91,7 @@ HTMLMediaElement::HTMLMediaElement(const QualifiedName& tagName, Document* doc)
     , m_pausedInternal(false)
     , m_inActiveDocument(true)
     , m_player(0)
-    , m_loadRestrictions(NoLoadRestriction)
+    , m_restrictions(NoRestrictions)
     , m_processingMediaPlayerCallback(0)
     , m_sendProgressEvents(true)
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
@@ -225,7 +225,8 @@ void HTMLMediaElement::dispatchEventAsync(const AtomicString& eventName)
 void HTMLMediaElement::loadTimerFired(Timer<HTMLMediaElement>*)
 {
     ExceptionCode ec;
-    load(ec);
+
+    loadInternal(ec);
 }
 
 void HTMLMediaElement::asyncEventTimerFired(Timer<HTMLMediaElement>*)
@@ -308,11 +309,16 @@ float HTMLMediaElement::bufferingRate()
 
 void HTMLMediaElement::load(ExceptionCode& ec)
 {
-    if ((m_loadRestrictions & RequireUserGestureLoadRestriction) && !processingUserGesture()) {
+    if (m_restrictions & RequireUserGestureForLoadRestriction && !processingUserGesture()) {
         ec = INVALID_STATE_ERR;
         return;
     }
 
+    loadInternal(ec);
+}
+
+void HTMLMediaElement::loadInternal(ExceptionCode& ec)
+{
     String mediaSrc;
     String mediaMIMEType;
 
@@ -386,6 +392,9 @@ void HTMLMediaElement::load(ExceptionCode& ec)
 #if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
     m_player.clear();
     m_player.set(new MediaPlayer(this));
+#else
+    if (!m_player)
+        m_player.set(new MediaPlayer(this));
 #endif
 
     updateVolume();
@@ -423,7 +432,7 @@ void HTMLMediaElement::mediaPlayerNetworkStateChanged(MediaPlayer*)
 
 void HTMLMediaElement::setNetworkState(MediaPlayer::NetworkState state)
 {
-    if (m_networkState == EMPTY) {
+    if (state == MediaPlayer::Empty) {
         // just update the cached state and leave, we can't do anything 
         m_networkState = EMPTY;
         return;
@@ -483,11 +492,13 @@ void HTMLMediaElement::setNetworkState(MediaPlayer::NetworkState state)
             return;
         
         m_loadedFirstFrame = true;
+#if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
         if (renderer()) {
             ASSERT(!renderer()->isImage());
             static_cast<RenderVideo*>(renderer())->videoSizeChanged();
         }
-        
+#endif        
+
         dispatchEventForType(eventNames().loadedfirstframeEvent, false, true);
         if (m_loadNestingLevel < m_terminateLoadBelowNestingLevel)
             return;
@@ -716,6 +727,16 @@ void HTMLMediaElement::setAutoplay(bool b)
 
 void HTMLMediaElement::play(ExceptionCode& ec)
 {
+    if (m_restrictions & RequireUserGestureForRateChangeRestriction && !processingUserGesture()) {
+        ec = INVALID_STATE_ERR;
+        return;
+    }
+
+    playInternal(ec);
+}
+
+void HTMLMediaElement::playInternal(ExceptionCode& ec)
+{
     // 3.14.9.7. Playing the media resource
     if (!m_player || networkState() == EMPTY) {
         ec = 0;
@@ -741,6 +762,17 @@ void HTMLMediaElement::play(ExceptionCode& ec)
 }
 
 void HTMLMediaElement::pause(ExceptionCode& ec)
+{
+    if (m_restrictions & RequireUserGestureForRateChangeRestriction && !processingUserGesture()) {
+        ec = INVALID_STATE_ERR;
+        return;
+    }
+
+    pauseInternal(ec);
+}
+
+
+void HTMLMediaElement::pauseInternal(ExceptionCode& ec)
 {
     // 3.14.9.7. Playing the media resource
     if (!m_player || networkState() == EMPTY) {
@@ -883,25 +915,27 @@ void HTMLMediaElement::setMuted(bool muted)
 
 void HTMLMediaElement::togglePlayState(ExceptionCode& ec)
 {
+    // We can safely call the internal play/pause methods, which don't check restrictions, because
+    // this method is only called from the built-in media controller
     if (canPlay())
-        play(ec);
+        playInternal(ec);
     else 
-        pause(ec);
+        pauseInternal(ec);
 }
 
 void HTMLMediaElement::beginScrubbing()
 {
     if (!paused()) {
         if (ended()) {
-            // because a media element stays in non-paused state when it reaches end, playback resumes 
-            //  when the slider is dragged from the end to another position unless we pause first. do 
-            //  a "hard pause" so an event is generated, since we want to stay paused after scrubbing finishes
+            // Because a media element stays in non-paused state when it reaches end, playback resumes 
+            // when the slider is dragged from the end to another position unless we pause first. Do 
+            // a "hard pause" so an event is generated, since we want to stay paused after scrubbing finishes.
             ExceptionCode ec;
             pause(ec);
         } else {
-            // not at the end but we still want to pause playback so the media engine doesn't try to
-            //  continue playing during scrubbing. pause without generating an event as we will 
-            //  unpause after scrubbing finishes
+            // Not at the end but we still want to pause playback so the media engine doesn't try to
+            // continue playing during scrubbing. Pause without generating an event as we will 
+            // unpause after scrubbing finishes.
             setPausedInternal(true);
         }
     }
@@ -1213,7 +1247,7 @@ void HTMLMediaElement::deliverNotification(MediaPlayerProxyNotificationType noti
     if (notification == MediaPlayerNotificationPlayPauseButtonPressed) {
         ExceptionCode ec;
         togglePlayState(ec);
-         return;
+        return;
     }
 
     if (m_player)

WebCore/html/HTMLMediaElement.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008, 2009 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -172,6 +172,11 @@ private:
     void progressEventTimerFired(Timer<HTMLMediaElement>*);
     void seek(float time, ExceptionCode& ec);
     void checkIfSeekNeeded();
+
+    // These "internal" functions do not check user gesture restrictions.
+    void loadInternal(ExceptionCode& ec);
+    void playInternal(ExceptionCode& ec);
+    void pauseInternal(ExceptionCode& ec);
     
     bool processingUserGesture() const;
     bool processingMediaPlayerCallback() const { return m_processingMediaPlayerCallback > 0; }
@@ -188,12 +193,13 @@ private:
     bool activelyPlaying() const;
     bool endedPlayback() const;
 
-    // Control media load restrictions. This is a effectively a compile time choice at the moment
+    // Restrictions to change default behaviors. This is a effectively a compile time choice at the moment
     //  because there are no accessor methods.
-    enum LoadRestrictions 
+    enum BehaviorRestrictions 
     { 
-        NoLoadRestriction = 0,
-        RequireUserGestureLoadRestriction = 1 << 0, 
+        NoRestrictions = 0,
+        RequireUserGestureForLoadRestriction = 1 << 0,
+        RequireUserGestureForRateChangeRestriction = 1 << 1,
     };
 
 
@@ -237,7 +243,7 @@ protected:
 
     OwnPtr<MediaPlayer> m_player;
 
-    LoadRestrictions m_loadRestrictions;
+    BehaviorRestrictions m_restrictions;
 
     // counter incremented while processing a callback from the media player, so we can avoid
     //  calling the media engine recursively
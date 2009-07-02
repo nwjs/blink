nwjs/blink

commit acebcb1bb8be6602bba583c98ed5170cb2c10fb8

2009-06-22  Simon Fraser  <simon.fraser@apple.com>
        Reviewed by Dave Hyatt.

        https://bugs.webkit.org/show_bug.cgi?id=26430
        <rdar://problem/6944442> Elements jump around when they become composited (WWDC checkers demo)
        <rdar://problem/6989006> Lots of time spent in RenderLayerCompositor::calculateCompositedBounds()
            during window resize when using hardware layers

        Fix an ordering dependency which caused the compositing position of a layer
        to be affected by some later sibling, which affected the compositing ancestor's
        composited bounds.

        The fix is to only update the compositing positions of layers in some subtree
        when updateAfterLayout() has been called on all the layers in that subtree,
        from the ancestor down.

        Also cache the composited bounds of a RenderLayerBacking, now that we can
        guarantee that those bounds are accurate.

        Test: compositing/sibling-positioning.html

        * rendering/RenderLayer.cpp:
        (WebCore::RenderLayer::updateLayerPositions):
        (WebCore::RenderLayer::hitTestLayer):
        * rendering/RenderLayerBacking.cpp:
        (WebCore::RenderLayerBacking::RenderLayerBacking):
        (WebCore::RenderLayerBacking::updateAfterLayout):
        (WebCore::RenderLayerBacking::updateGraphicsLayerGeometry):
        (WebCore::RenderLayerBacking::contentOffsetInCompostingLayer):
        (WebCore::RenderLayerBacking::paintContents):
        (WebCore::RenderLayerBacking::compositedBounds):
        (WebCore::RenderLayerBacking::setCompositedBounds):
        * rendering/RenderLayerBacking.h:
        * rendering/RenderLayerCompositor.cpp:
        (WebCore::RenderLayerCompositor::updateBacking):
        (WebCore::RenderLayerCompositor::updateLayerCompositingState):
        (WebCore::RenderLayerCompositor::calculateCompositedBounds):
        (WebCore::RenderLayerCompositor::layerWillBeRemoved):

WebCore/rendering/RenderLayerBacking.cpp
@@ -58,7 +58,6 @@ RenderLayerBacking::RenderLayerBacking(RenderLayer* layer)
     , m_contentsLayer(0)
     , m_clippingLayer(0)
     , m_hasDirectlyCompositedContent(false)
-    , m_compositingContentOffsetDirty(true)
 {
     createGraphicsLayer();
 }
@@ -129,9 +128,21 @@ void RenderLayerBacking::updateLayerTransform()
 
 void RenderLayerBacking::updateAfterLayout()
 {
-    // Only need to update geometry if there isn't a layer update pending.
-    if (!compositor()->compositingLayersNeedUpdate())
-        updateGraphicsLayerGeometry();
+    RenderLayerCompositor* layerCompositor = compositor();
+    if (!layerCompositor->compositingLayersNeedUpdate()) {
+        // Calling updateGraphicsLayerGeometry() here gives incorrect results, because the
+        // position of this layer's GraphicsLayer depends on the position of our compositing
+        // ancestor's GraphicsLayer. That cannot be determined until all the descendant 
+        // RenderLayers of that ancestor have been processed via updateLayerPositions().
+        //
+        // The solution is to update compositing children of this layer here,
+        // via updateCompositingChildrenGeometry().
+        setCompositedBounds(layerCompositor->calculateCompositedBounds(m_owningLayer, m_owningLayer));
+        layerCompositor->updateCompositingChildrenGeometry(m_owningLayer, m_owningLayer);
+        
+        if (!m_owningLayer->parent())
+            layerCompositor->updateRootLayerPosition();
+    }
 }
 
 bool RenderLayerBacking::updateGraphicsLayerConfiguration()
@@ -181,16 +192,16 @@ void RenderLayerBacking::updateGraphicsLayerGeometry()
     m_graphicsLayer->setPreserves3D(style->transformStyle3D() == TransformStyle3DPreserve3D);
     m_graphicsLayer->setBackfaceVisibility(style->backfaceVisibility() == BackfaceVisibilityVisible);
 
-    m_compositingContentOffsetDirty = true;
-    
     RenderLayer* compAncestor = m_owningLayer->ancestorCompositingLayer();
     
     // We compute everything relative to the enclosing compositing layer.
     IntRect ancestorCompositingBounds;
-    if (compAncestor)
-        ancestorCompositingBounds = compositor()->calculateCompositedBounds(compAncestor, compAncestor);
-    
-    IntRect localCompositingBounds = compositor()->calculateCompositedBounds(m_owningLayer, m_owningLayer);
+    if (compAncestor) {
+        ASSERT(compAncestor->backing());
+        ancestorCompositingBounds = compAncestor->backing()->compositedBounds();
+    }
+
+    IntRect localCompositingBounds = compositedBounds();
 
     IntRect relativeCompositingBounds(localCompositingBounds);
     int deltaX = 0, deltaY = 0;
@@ -646,14 +657,10 @@ FloatPoint RenderLayerBacking::computePerspectiveOrigin(const IntRect& borderBox
 // Return the offset from the top-left of this compositing layer at which the renderer's contents are painted.
 IntSize RenderLayerBacking::contentOffsetInCompostingLayer()
 {
-    if (!m_compositingContentOffsetDirty)
-        return m_compositingContentOffset;
+    if (m_compositedBounds != compositor()->calculateCompositedBounds(m_owningLayer, m_owningLayer) && !compositor()->compositingLayersNeedUpdate())
+        fprintf(stderr, "Stale compositing offset\n");
 
-    IntRect relativeCompositingBounds = compositor()->calculateCompositedBounds(m_owningLayer, m_owningLayer);
-    m_compositingContentOffset = IntSize(-relativeCompositingBounds.x(), -relativeCompositingBounds.y());
-    m_compositingContentOffsetDirty = false;
-
-    return m_compositingContentOffset;
+    return IntSize(-m_compositedBounds.x(), -m_compositedBounds.y());
 }
 
 IntRect RenderLayerBacking::contentsBox(const GraphicsLayer*)
@@ -881,7 +888,7 @@ void RenderLayerBacking::paintContents(const GraphicsLayer*, GraphicsContext& co
 {
     // We have to use the same root as for hit testing, because both methods
     // can compute and cache clipRects.
-    IntRect enclosingBBox = compositor()->calculateCompositedBounds(m_owningLayer, m_owningLayer);
+    IntRect enclosingBBox = compositedBounds();
 
     IntRect clipRect(clip);
     
@@ -1000,6 +1007,16 @@ void RenderLayerBacking::resumeAnimations()
     m_graphicsLayer->resumeAnimations();
 }
 
+IntRect RenderLayerBacking::compositedBounds() const
+{
+    return m_compositedBounds;
+}
+
+void RenderLayerBacking::setCompositedBounds(const IntRect& bounds)
+{
+    m_compositedBounds = bounds;
+
+}
 int RenderLayerBacking::graphicsLayerToCSSProperty(AnimatedPropertyID property)
 {
     int cssProperty = CSSPropertyInvalid;

LayoutTests/compositing/sibling-positioning.html
@@ -1,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Initial backing position</title>
+  <style type="text/css" media="screen">
+    #parent {
+      position: relative;
+      height: 200px;
+      width: 200px;
+      border: 1px solid black;
+    }
+    
+    #panel {
+      position: absolute;
+      height: 100px;
+      width: 100px;
+      left: -50px;
+      top: -50px;
+      opacity: 0.2;
+      background-color: gray;
+    }
+    
+    #piece {
+      position: absolute;
+      left: 50px;
+      top: 50px;
+      height: 60px;
+      width: 60px;
+      background-color: green;
+    }
+    
+    #piece:hover {
+      -webkit-transform: translate3d(0, 0, 0);
+    }
+
+    #piece.moved {
+      -webkit-transform: translate3d(0, 0, 0);
+    }
+
+  </style>
+  <script type="text/javascript" charset="utf-8">
+    if (window.layoutTestController)
+      layoutTestController.waitUntilDone();
+
+    function startTest()
+    {
+      window.setTimeout(function() {
+        var mover = document.getElementById('piece');
+        mover.className = 'moved';
+        
+        window.setTimeout(function() {
+          if (window.layoutTestController)
+            layoutTestController.notifyDone();
+        }, 0);
+        
+      }, 0);
+    }
+
+    window.addEventListener('load', startTest, false);
+  </script>
+</head>
+<body>
+
+  <div id="parent">
+      <div id="piece"></div>
+      <div id="panel"></div>
+  </div>
+  <p><a href="https://bugs.webkit.org/show_bug.cgi?id=26430">https://bugs.webkit.org/show_bug.cgi?id=26430</a></p>
+  <p>Green box should not move when hovered</p>
+</body>
+</html>

LayoutTests/platform/mac/compositing/sibling-positioning-expected.txt
@@ -1,0 +1,18 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x294
+  RenderBlock {HTML} at (0,0) size 800x294
+    RenderBody {BODY} at (8,8) size 784x270
+      RenderBlock {P} at (0,218) size 784x18
+        RenderInline {A} at (0,0) size 305x18 [color=#0000EE]
+          RenderText {#text} at (0,0) size 305x18
+            text run at (0,0) width 305: "https://bugs.webkit.org/show_bug.cgi?id=26430"
+      RenderBlock {P} at (0,252) size 784x18
+        RenderText {#text} at (0,0) size 270x18
+          text run at (0,0) width 270: "Green box should not move when hovered"
+layer at (8,8) size 202x202
+  RenderBlock (relative positioned) {DIV} at (0,0) size 202x202 [border: (1px solid #000000)]
+layer at (59,59) size 60x60
+  RenderBlock (positioned) {DIV} at (51,51) size 60x60 [bgcolor=#008000]
+layer at (-41,-41) size 100x100 backgroundClip at (0,0) size 800x600 clip at (0,0) size 800x600 outlineClip at (0,0) size 800x600
+  RenderBlock (positioned) {DIV} at (-49,-49) size 100x100 [bgcolor=#808080]

WebCore/rendering/RenderLayer.cpp
@@ -310,9 +310,6 @@ void RenderLayer::updateLayerPositions(bool doFullRepaint, bool checkForRepaint)
         child->updateLayerPositions(doFullRepaint, checkForRepaint);
 
 #if USE(ACCELERATED_COMPOSITING)
-    if (!parent())
-        compositor()->updateRootLayerPosition();
-
     if (isComposited())
         backing()->updateAfterLayout();
 #endif
@@ -2301,7 +2298,7 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
 #if USE(ACCELERATED_COMPOSITING)
         if (isComposited()) {
             // It doesn't make sense to project hitTestRect into the plane of this layer, so use the same bounds we use for painting.
-            localHitTestRect = compositor()->calculateCompositedBounds(this, this);
+            localHitTestRect = backing()->compositedBounds();
         } else
 #endif
             localHitTestRect = newTransformState->mappedQuad().enclosingBoundingBox();

WebCore/rendering/RenderLayerBacking.h
@@ -57,8 +57,10 @@ public:
     
     // Returns true if layer configuration changed.
     bool updateGraphicsLayerConfiguration();
-    void updateGraphicsLayerGeometry();
-    void updateInternalHierarchy();
+    // Update graphics layer position and bounds.
+    void updateGraphicsLayerGeometry(); // make private
+    // Update contents and clipping structure.
+    void updateInternalHierarchy(); // make private
     
     GraphicsLayer* graphicsLayer() const { return m_graphicsLayer; }
 
@@ -99,6 +101,9 @@ public:
 
     void suspendAnimations();
     void resumeAnimations();
+
+    IntRect compositedBounds() const;
+    void setCompositedBounds(const IntRect&);
 
     FloatPoint graphicsLayerToContentsCoordinates(const GraphicsLayer*, const FloatPoint&);
     FloatPoint contentsToGraphicsLayerCoordinates(const GraphicsLayer*, const FloatPoint&);
@@ -159,10 +164,9 @@ private:
     GraphicsLayer* m_contentsLayer;         // only used in cases where we need to draw the foreground separately
     GraphicsLayer* m_clippingLayer;         // only used if we have clipping on a stacking context, with compositing children
 
-    IntSize m_compositingContentOffset;
+    IntRect m_compositedBounds;
 
-    bool m_hasDirectlyCompositedContent: 1;
-    bool m_compositingContentOffsetDirty: 1;
+    bool m_hasDirectlyCompositedContent;
 };
 
 } // namespace WebCore

WebCore/rendering/RenderLayerCompositor.cpp
@@ -175,12 +175,11 @@ void RenderLayerCompositor::updateCompositingLayers(RenderLayer* updateRoot)
     ASSERT(updateRoot || !m_compositingLayersNeedUpdate);
 }
 
-bool RenderLayerCompositor::updateLayerCompositingState(RenderLayer* layer, CompositingChangeRepaint shouldRepaint)
+bool RenderLayerCompositor::updateBacking(RenderLayer* layer, CompositingChangeRepaint shouldRepaint)
 {
-    bool needsLayer = needsToBeComposited(layer);
     bool layerChanged = false;
 
-    if (needsLayer) {
+    if (needsToBeComposited(layer)) {
         enableCompositingMode();
         if (!layer->backing()) {
 
@@ -209,7 +208,13 @@ bool RenderLayerCompositor::updateLayerCompositingState(RenderLayer* layer, Comp
         video->acceleratedRenderingStateChanged();
     }
 #endif
-    
+    return layerChanged;
+}
+
+bool RenderLayerCompositor::updateLayerCompositingState(RenderLayer* layer, CompositingChangeRepaint shouldRepaint)
+{
+    bool layerChanged = updateBacking(layer, shouldRepaint);
+
     // See if we need content or clipping layers. Methods called here should assume
     // that the compositing state of descendant layers has not been updated yet.
     if (layer->backing() && layer->backing()->updateGraphicsLayerConfiguration())
@@ -235,7 +240,7 @@ void RenderLayerCompositor::repaintOnCompositingChange(RenderLayer* layer)
 
 // The bounds of the GraphicsLayer created for a compositing layer is the union of the bounds of all the descendant
 // RenderLayers that are rendered by the composited RenderLayer.
-IntRect RenderLayerCompositor::calculateCompositedBounds(const RenderLayer* layer, const RenderLayer* ancestorLayer, IntRect* layerBoundingBox)
+IntRect RenderLayerCompositor::calculateCompositedBounds(const RenderLayer* layer, const RenderLayer* ancestorLayer)
 {
     IntRect boundingBoxRect, unionBounds;
     boundingBoxRect = unionBounds = layer->localBoundingBox();
@@ -285,11 +290,6 @@ IntRect RenderLayerCompositor::calculateCompositedBounds(const RenderLayer* laye
     layer->convertToLayerCoords(ancestorLayer, ancestorRelX, ancestorRelY);
     unionBounds.move(ancestorRelX, ancestorRelY);
 
-    if (layerBoundingBox) {
-        boundingBoxRect.move(ancestorRelX, ancestorRelY);
-        *layerBoundingBox = boundingBoxRect;
-    }
-    
     return unionBounds;
 }
 
@@ -300,18 +300,22 @@ void RenderLayerCompositor::layerWasAdded(RenderLayer* /*parent*/, RenderLayer* 
 
 void RenderLayerCompositor::layerWillBeRemoved(RenderLayer* parent, RenderLayer* child)
 {
-    if (child->isComposited())
-        setCompositingParent(child, 0);
-    
-    // If the document is being torn down (document's renderer() is null), then there's
-    // no need to do any layer updating.
-    if (parent->renderer()->documentBeingDestroyed())
+    if (!child->isComposited() || parent->renderer()->documentBeingDestroyed())
         return;
 
+    setCompositingParent(child, 0);
+    
     RenderLayer* compLayer = parent->enclosingCompositingLayer();
     if (compLayer) {
-        IntRect ancestorRect = calculateCompositedBounds(child, compLayer);
-        compLayer->setBackingNeedsRepaintInRect(ancestorRect);
+        ASSERT(compLayer->backing());
+        IntRect compBounds = child->backing()->compositedBounds();
+
+        int offsetX = 0, offsetY = 0;
+        child->convertToLayerCoords(compLayer, offsetX, offsetY);
+        compBounds.move(offsetX, offsetY);
+
+        compLayer->setBackingNeedsRepaintInRect(compBounds);
+
         // The contents of this layer may be moving from a GraphicsLayer to the window,
         // so we need to make sure the window system synchronizes those changes on the screen.
         m_renderView->frameView()->setNeedsOneShotDrawingSynchronization();
@@ -431,6 +435,9 @@ void RenderLayerCompositor::computeCompositingRequirements(RenderLayer* layer, s
     // this can affect the answer to needsToBeComposited() when clipping,
     // but that's ok here.
     layer->setHasCompositingDescendant(childState.m_subtreeIsCompositing);
+
+    // Update backing now, so that we can use isComposited() reliably during tree traversal in rebuildCompositingLayerTree().
+    updateBacking(layer, CompositingChangeRepaintNow);
 }
 
 void RenderLayerCompositor::setCompositingParent(RenderLayer* childLayer, RenderLayer* parentLayer)
@@ -487,12 +494,24 @@ bool RenderLayerCompositor::canAccelerateVideoRendering(RenderVideo* o) const
 
 void RenderLayerCompositor::rebuildCompositingLayerTree(RenderLayer* layer, struct CompositingState& ioCompState)
 {
-    bool wasComposited = layer->isComposited();
-
     // Make the layer compositing if necessary, and set up clipping and content layers.
     // Note that we can only do work here that is independent of whether the descendant layers
     // have been processed. computeCompositingRequirements() will already have done the repaint if necessary.
-    updateLayerCompositingState(layer, CompositingChangeWillRepaintLater);
+    RenderLayerBacking* layerBacking = layer->backing();
+    if (layerBacking) {
+        // The compositing state of all our children has been updated already, so now
+        // we can compute and cache the composited bounds for this layer.
+        layerBacking->setCompositedBounds(calculateCompositedBounds(layer, layer));
+
+        layerBacking->updateGraphicsLayerConfiguration();
+        layerBacking->updateGraphicsLayerGeometry();
+
+        if (!layer->parent())
+            updateRootLayerPosition();
+
+        // FIXME: make this more incremental
+        layerBacking->parentForSublayers()->removeAllChildren();
+    }
 
     // host the document layer in the RenderView's root layer
     if (layer->isRootLayer())
@@ -506,14 +525,6 @@ void RenderLayerCompositor::rebuildCompositingLayerTree(RenderLayer* layer, stru
     ++childState.m_depth;
 #endif
 
-    RenderLayerBacking* layerBacking = layer->backing();
-    
-    // FIXME: make this more incremental
-    if (layerBacking) {
-        layerBacking->parentForSublayers()->removeAllChildren();
-        layerBacking->updateInternalHierarchy();
-    }
-    
     // The children of this stacking context don't need to composite, unless there is
     // a compositing layer among them, so start by assuming false.
     childState.m_subtreeIsCompositing = false;
@@ -562,16 +573,43 @@ void RenderLayerCompositor::rebuildCompositingLayerTree(RenderLayer* layer, stru
             }
         }
     }
+}
+
+
+// Recurs down the RenderLayer tree until its finds the compositing descendants of compositingAncestor and updates their geometry.
+void RenderLayerCompositor::updateCompositingChildrenGeometry(RenderLayer* compositingAncestor, RenderLayer* layer)
+{
+    if (layer != compositingAncestor) {
+        if (RenderLayerBacking* layerBacking = layer->backing()) {
+            layerBacking->setCompositedBounds(calculateCompositedBounds(layer, layer));
+            layerBacking->updateGraphicsLayerGeometry();
+            return;
+        }
+    }
+
+    if (!layer->hasCompositingDescendant())
+        return;
     
-    if (layerBacking) {
-        // Do work here that requires that we've processed all of the descendant layers
-        layerBacking->updateGraphicsLayerGeometry();
-    } else if (wasComposited) {
-        // We stopped being a compositing layer. Now that our descendants have been udated, we can
-        // repaint our new rendering destination.
-        repaintOnCompositingChange(layer);
+    if (layer->isStackingContext()) {
+        if (Vector<RenderLayer*>* negZOrderList = layer->negZOrderList()) {
+            for (size_t i = 0; i < negZOrderList->size(); ++i)
+                updateCompositingChildrenGeometry(compositingAncestor, negZOrderList->at(i));
+        }
+    }
+
+    if (Vector<RenderLayer*>* normalFlowList = layer->normalFlowList()) {
+        for (size_t i = 0; i < normalFlowList->size(); ++i)
+            updateCompositingChildrenGeometry(compositingAncestor, normalFlowList->at(i));
+    }
+    
+    if (layer->isStackingContext()) {
+        if (Vector<RenderLayer*>* posZOrderList = layer->posZOrderList()) {
+            for (size_t i = 0; i < posZOrderList->size(); ++i)
+                updateCompositingChildrenGeometry(compositingAncestor, posZOrderList->at(i));
+        }
     }
 }
+
 
 void RenderLayerCompositor::repaintCompositedLayersAbsoluteRect(const IntRect& absRect)
 {

WebCore/rendering/RenderLayerCompositor.h
@@ -69,6 +69,9 @@ public:
     enum CompositingChangeRepaint { CompositingChangeRepaintNow, CompositingChangeWillRepaintLater };
     bool updateLayerCompositingState(RenderLayer*, CompositingChangeRepaint = CompositingChangeRepaintNow);
 
+    // Update the geometry for compositing children of compositingAncestor.
+    void updateCompositingChildrenGeometry(RenderLayer* compositingAncestor, RenderLayer* layer);
+    
     // Whether layer's backing needs a graphics layer to do clipping by an ancestor (non-stacking-context parent with overflow).
     bool clippedByAncestor(RenderLayer*) const;
     // Whether layer's backing needs a graphics layer to clip z-order children of the given layer.
@@ -78,7 +81,7 @@ public:
     bool needsContentsCompositingLayer(const RenderLayer*) const;
     // Return the bounding box required for compositing layer and its childern, relative to ancestorLayer.
     // If layerBoundingBox is not 0, on return it contains the bounding box of this layer only.
-    IntRect calculateCompositedBounds(const RenderLayer* layer, const RenderLayer* ancestorLayer, IntRect* layerBoundingBox = 0);
+    IntRect calculateCompositedBounds(const RenderLayer* layer, const RenderLayer* ancestorLayer);
 
     // Repaint the appropriate layers when the given RenderLayer starts or stops being composited.
     void repaintOnCompositingChange(RenderLayer*);
@@ -115,6 +118,9 @@ private:
     bool needsToBeComposited(const RenderLayer*) const;
     // Whether the layer has an intrinsic need for compositing layer.
     bool requiresCompositingLayer(const RenderLayer*) const;
+
+    // Make or destroy the backing for this layer; returns true if backing changed.
+    bool updateBacking(RenderLayer*, CompositingChangeRepaint shouldRepaint);
 
     // Repaint the given rect (which is layer's coords), and regions of child layers that intersect that rect.
     void recursiveRepaintLayerRect(RenderLayer* layer, const IntRect& rect);

commit 11ba1e2e6c4a970c9285c1d428f3467667958c52

2009-07-01  Simon Fraser  <simon.fraser@apple.com>
        Reviewed by Dan Bernstein.

        <video> fails to show on http://camendesign.com/code/video_for_everybody
        <rdar://problem/7026010>
        https://bugs.webkit.org/show_bug.cgi?id=26919

        Fix an issue introduced in r44961. In that revision we changed to only update
        compositing layer geometry when all siblings had been laid out (i.e. we pushed
        the updates one level down). However, that left out the root layer, so this
        fix ensures that the root layer geometry gets updated at the end.

        Test: compositing/geometry/root-layer-update.html

        * rendering/RenderLayerBacking.cpp:
        (WebCore::RenderLayerBacking::updateAfterLayout):

git-svn-id: svn://svn.chromium.org/blink/trunk@45468 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/rendering/RenderLayerBacking.cpp
@@ -140,8 +140,10 @@ void RenderLayerBacking::updateAfterLayout(UpdateDepth updateDepth)
         setCompositedBounds(layerCompositor->calculateCompositedBounds(m_owningLayer, m_owningLayer));
         layerCompositor->updateCompositingDescendantGeometry(m_owningLayer, m_owningLayer, updateDepth);
         
-        if (!m_owningLayer->parent())
+        if (!m_owningLayer->parent()) {
+            updateGraphicsLayerGeometry();
             layerCompositor->updateRootLayerPosition();
+        }
     }
 }
 

LayoutTests/compositing/geometry/root-layer-update.html
@@ -1,0 +1,71 @@
+<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-GB">
+<!-- =========================================== kroc camen of camen design ============================================= -->
+<head>
+  <title>camen design - Video for Everybody!</title>
+  <style type="text/css" media="screen">
+    body {
+      width: 600px;
+      margin: 0 auto;
+    }
+
+    #sidebar {
+      position: absolute;
+      height: 100px;
+      width: 100px; 
+      margin-left: -200px;
+      padding-left: 200px;
+      background: gray;
+    }
+
+    #article {
+      position: relative;
+      border: 1px solid black;
+    }
+
+    #compositing {
+      height: 100px;
+      width: 100px;
+      background-color: green;
+      margin: 0 auto;
+      -webkit-transform: translateZ(0);
+    }
+    
+    #tester {
+      position: absolute;
+      top: 1px;
+      left: 350px;
+      height: 100px;
+      width: 100px;
+      background-color: red;
+    }
+  </style>
+  <script type="text/javascript" charset="utf-8">
+  if (window.layoutTestController)
+    layoutTestController.waitUntilDone();
+
+  function startTest()
+  {
+    window.setTimeout(function() {
+      document.documentElement.style.width = '800px'
+      window.setTimeout(function() {
+        if (window.layoutTestController)
+          layoutTestController.notifyDone();
+      }, 0);
+    }, 0);
+  }
+
+  window.addEventListener('load', startTest, false);
+  </script>
+</head>
+<body>
+
+  <div id="tester"></div>
+  <div id="sidebar"></div>
+
+  <div id="article">
+    <div id="compositing"></div>
+  </div>
+  <p><a href="rdar://problem/7026010">rdar://problem/7026010</a><br>
+    Test for the root layer getting correctly positioned. You should see no red.</p>
+</body>
+</html>

LayoutTests/platform/mac/compositing/geometry/root-layer-update-expected.checksum
@@ -1,0 +1,1 @@
+55f112a681ed84f66d9a3d51c68744df

LayoutTests/platform/mac/compositing/geometry/root-layer-update-expected.txt
@@ -1,0 +1,20 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x170
+  RenderBlock {HTML} at (0,0) size 800x170
+    RenderBody {BODY} at (100,0) size 600x154
+      RenderBlock {P} at (0,118) size 600x36
+        RenderInline {A} at (0,0) size 149x18 [color=#0000EE]
+          RenderText {#text} at (0,0) size 149x18
+            text run at (0,0) width 149: "rdar://problem/7026010"
+        RenderBR {BR} at (149,0) size 0x18
+        RenderText {#text} at (0,18) size 465x18
+          text run at (0,18) width 465: "Test for the root layer getting correctly positioned. You should see no red."
+layer at (350,1) size 100x100
+  RenderBlock (positioned) {DIV} at (350,1) size 100x100 [bgcolor=#FF0000]
+layer at (-100,0) size 300x100 backgroundClip at (0,0) size 800x600 clip at (0,0) size 800x600 outlineClip at (0,0) size 800x600
+  RenderBlock (positioned) {DIV} at (-100,0) size 300x100 [bgcolor=#808080]
+layer at (100,0) size 600x102
+  RenderBlock (relative positioned) {DIV} at (0,0) size 600x102 [border: (1px solid #000000)]
+layer at (350,1) size 100x100
+  RenderBlock {DIV} at (250,1) size 100x100 [bgcolor=#008000]
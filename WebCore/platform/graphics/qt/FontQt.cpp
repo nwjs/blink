nwjs/blink

commit 819dc8fc4a561f7d19d64593bb63511da5f4b916

[Qt] Make text filling work together with text stroke.
Patch by Kenneth Rohde Christiansen <kenneth@webkit.org> on 2010-05-24
Reviewed by Simon Hausmann.

When the text has stroke a new QPen was set, overriding the pen
set for text filling. This patch fixes that by storing the two
pens and using where appropriate.

* platform/graphics/qt/FontQt.cpp:
(WebCore::Font::drawComplexText):

git-svn-id: svn://svn.chromium.org/blink/trunk@60169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/platform/graphics/qt/FontQt.cpp
@@ -73,28 +73,31 @@ void Font::drawComplexText(GraphicsContext* ctx, const TextRun& run, const Float
 
     QPainter *p = ctx->platformContext();
 
+    QPen textFillPen;
     if (ctx->textDrawingMode() & cTextFill) {
         if (ctx->fillGradient()) {
             QBrush brush(*ctx->fillGradient()->platformGradient());
             brush.setTransform(ctx->fillGradient()->gradientSpaceTransform());
-            p->setPen(QPen(brush, 0));
+            textFillPen = QPen(brush, 0);
         } else if (ctx->fillPattern()) {
             AffineTransform affine;
-            p->setPen(QPen(QBrush(ctx->fillPattern()->createPlatformPattern(affine)), 0));
+            textFillPen = QPen(QBrush(ctx->fillPattern()->createPlatformPattern(affine)), 0);
         } else
-            p->setPen(QColor(ctx->fillColor()));
+            textFillPen = QPen(QColor(ctx->fillColor()));
     }
 
+    QPen textStrokePen;
     if (ctx->textDrawingMode() & cTextStroke) {
         if (ctx->strokeGradient()) {
             QBrush brush(*ctx->strokeGradient()->platformGradient());
             brush.setTransform(ctx->strokeGradient()->gradientSpaceTransform());
-            p->setPen(QPen(brush, ctx->strokeThickness()));
+            textStrokePen = QPen(brush, ctx->strokeThickness());
         } else if (ctx->strokePattern()) {
             AffineTransform affine;
-            p->setPen(QPen(QBrush(ctx->strokePattern()->createPlatformPattern(affine)), ctx->strokeThickness()));
+            QBrush brush(ctx->strokePattern()->createPlatformPattern(affine));
+            textStrokePen = QPen(brush, ctx->strokeThickness());
         } else
-            p->setPen(QPen(QColor(ctx->strokeColor()), ctx->strokeThickness()));
+            textStrokePen = QPen(QColor(ctx->strokeColor()), ctx->strokeThickness());
     }
 
     String sanitized = Font::normalizeSpaces(String(run.characters(), run.length()));
@@ -163,10 +166,13 @@ void Font::drawComplexText(GraphicsContext* ctx, const TextRun& run, const Float
     if (ctx->textDrawingMode() & cTextStroke) {
         QPainterPath path;
         path.addText(pt, font(), string);
+        p->setPen(textStrokePen);
         p->strokePath(path, p->pen());
     }
-    if (ctx->textDrawingMode() & cTextFill)
+    if (ctx->textDrawingMode() & cTextFill) {
+        p->setPen(textFillPen);
         p->drawText(pt, string, flags, run.padding());
+    }
 }
 
 float Font::floatWidthForComplexText(const TextRun& run, HashSet<const SimpleFontData*>*, GlyphOverflow*) const

commit 1409f78d833a8d7067df4dee7925c721bf2da683

[Qt] Fix text selection drawing. https://bugs.webkit.org/show_bug.cgi?id=40221
Reviewed by Simon Hausmann.

The regression was introduced in r60169.

* platform/graphics/qt/FontQt.cpp:
(WebCore::drawTextCommon):

git-svn-id: svn://svn.chromium.org/blink/trunk@60772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/platform/graphics/qt/FontQt.cpp
@@ -146,6 +146,7 @@ void Font::drawComplexText(GraphicsContext* ctx, const TextRun& run, const Float
             line.draw(p, pt);
             p->restore();
         }
+        p->setPen(textFillPen);
         line.draw(p, pt);
         p->restore();
         return;
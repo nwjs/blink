nwjs/blink

commit b6ddcca352dbaa4b1d3dd51c596b277596b3c5d7

Implement proper handling of events with a related target in regard to shadow DOM boundaries. https://bugs.webkit.org/show_bug.cgi?id=65899
Reviewed by Dimitri Glazkov.

Fixes issues in the following corner cases:
1. When both a target node and a relatedTarget node are immediate children of
the same shadow root, an event is not dispatched.
2. If a target node is an ancestor of a relatedTarget node, crossing
shadow boundaries, or vice verse, an event is not dispatched or wrongly
dispatched.

Source/WebCore:

Test: fast/dom/shadow/shadow-boundary-events.html

* dom/EventDispatcher.cpp:
(WebCore::EventDispatcher::adjustToShadowBoundaries):

LayoutTests:

* fast/dom/shadow/shadow-boundary-crossing-expected.txt: Renamed from LayoutTests/fast/events/shadow-boundary-crossing-expected.txt.
* fast/dom/shadow/shadow-boundary-crossing.html: Renamed from LayoutTests/fast/events/shadow-boundary-crossing.html.
* fast/dom/shadow/shadow-boundary-events-expected.txt: Added.
* fast/dom/shadow/shadow-boundary-events.html: Added.

git-svn-id: svn://svn.chromium.org/blink/trunk@92922 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/dom/EventDispatcher.cpp
@@ -164,20 +164,22 @@ PassRefPtr<EventTarget> EventDispatcher::adjustToShadowBoundaries(PassRefPtr<Nod
     if (!diverged) {
         // The relatedTarget is an ancestor or shadowHost of the target.
         // FIXME: Remove the first check once conversion to new shadow DOM is complete <http://webkit.org/b/48698>
-        if (m_node->shadowHost() == relatedTarget.get() || isShadowHost(relatedTarget.get()))
-            lowestCommonBoundary = m_ancestors.begin();
+        if (m_node->shadowHost() == relatedTarget.get() || isShadowHost(relatedTarget.get())) {
+            ASSERT(targetAncestor - 1 >= m_ancestors.begin());
+            lowestCommonBoundary = targetAncestor - 1;
+        }
     } else if ((*firstDivergentBoundary) == m_node.get()) {
         // Since ancestors does not contain target itself, we must account
         // for the possibility that target is a shadowHost of relatedTarget
         // and thus serves as the lowestCommonBoundary.
         // Luckily, in this case the firstDivergentBoundary is target.
         lowestCommonBoundary = m_ancestors.begin();
+        m_shouldPreventDispatch = true;
     }
 
     if (lowestCommonBoundary != m_ancestors.end()) {
         // Trim ancestors to lowestCommonBoundary to keep events inside of the common shadow DOM subtree.
         m_ancestors.shrink(lowestCommonBoundary - m_ancestors.begin());
-        m_shouldPreventDispatch = !m_ancestors.size();
     }
     // Set event's related target to the first encountered shadow DOM boundary in the divergent subtree.
     return firstDivergentBoundary != relatedTargetAncestors.begin() ? *firstDivergentBoundary : relatedTarget;

LayoutTests/fast/dom/shadow/shadow-boundary-events-expected.txt
@@ -1,0 +1,64 @@
+Tests to ensure that shadow DOM boundary is not crossed during event propagation. Can only run within DRT.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+
+Move mouse from a node to its sibling node. All nodes are outside of shadow boundary.
+Moving mouse from divB to divC
+PASS dispatchedEvent("mouseover") is ["divC(<-divB)(@divC)", "divC(<-divB)(@divA)"]
+PASS dispatchedEvent("mouseout") is ["divB(<-divC)(@divB)", "divB(<-divC)(@divA)"]
+
+Target is an ancestor of relatedTarget. All nodes are outside of shadow boundary.
+Moving mouse from divB to divA
+PASS dispatchedEvent("mouseover") is ["divA(<-divB)(@divA)"]
+PASS dispatchedEvent("mouseout") is ["divB(<-divA)(@divB)", "divB(<-divA)(@divA)"]
+
+RelatedTarget is an ancestor of target. All nodes are outside of shadow boundary.
+Moving mouse from divA to divB
+PASS dispatchedEvent("mouseover") is ["divB(<-divA)(@divB)", "divB(<-divA)(@divA)"]
+PASS dispatchedEvent("mouseout") is ["divA(<-divB)(@divA)"]
+
+Both target and relatedTarget are immediate children of the same shadow root.
+Moving mouse from shadowD/shadowF/shadowG/divH to shadowD/shadowF/shadowG/divI
+PASS dispatchedEvent("mouseover") is ["divI(<-divH)(@divI)"]
+PASS dispatchedEvent("mouseout") is ["divH(<-divI)(@divH)"]
+
+Target is an ancestor of relatedTarget.
+Moving mouse from shadowD/shadowF/shadowG/divI to shadowD/divE
+PASS dispatchedEvent("mouseover") is ["divE(<-shadowF)(@divE)"]
+PASS dispatchedEvent("mouseout") is ["divI(<-divE)(@divI)", "shadowG(<-divE)(@shadowG)", "shadowF(<-divE)(@shadowF)", "shadowF(<-divE)(@divE)"]
+
+Target (shadow host) is an ancestor of relatedTarget.
+Moving mouse from shadowD/shadowF/shadowG/divI to shadowD/shadowF
+PASS dispatchedEvent("mouseover") is []
+PASS dispatchedEvent("mouseout") is ["divI(<-shadowF)(@divI)", "shadowG(<-shadowF)(@shadowG)"]
+
+Target (shadow host) is an ancestor of relatedTarget (shadow host).
+Moving mouse from shadowD/shadowF/shadowG to shadowD
+PASS dispatchedEvent("mouseover") is []
+PASS dispatchedEvent("mouseout") is ["shadowG(<-shadowD)(@shadowG)", "shadowF(<-shadowD)(@shadowF)", "shadowF(<-shadowD)(@divE)"]
+
+RelatedTarget is ancestor of target.
+Moving mouse from shadowD/divE to shadowD/shadowF/shadowG/divI
+PASS dispatchedEvent("mouseover") is ["divI(<-divE)(@divI)", "shadowG(<-divE)(@shadowG)", "shadowF(<-divE)(@shadowF)", "shadowF(<-divE)(@divE)"]
+PASS dispatchedEvent("mouseout") is ["divE(<-shadowF)(@divE)"]
+
+RelatedTarget (shadow host) is ancestor of target.
+Moving mouse from shadowD/shadowF to shadowD/shadowF/shadowG/divI
+PASS dispatchedEvent("mouseover") is ["divI(<-shadowF)(@divI)", "shadowG(<-shadowF)(@shadowG)"]
+PASS dispatchedEvent("mouseout") is []
+
+RelatedTarget (shadow host) is an ancestor of target (shadow host).
+Moving mouse from shadowD to shadowD/shadowF/shadowG
+PASS dispatchedEvent("mouseover") is ["shadowG(<-shadowD)(@shadowG)", "shadowF(<-shadowD)(@shadowF)", "shadowF(<-shadowD)(@divE)"]
+PASS dispatchedEvent("mouseout") is []
+
+Target and relatedTarget exist in separated subtree, crossing shadow boundaries. Making sure that event is not dispatched beyond the lowest common boundary.
+Moving mouse from shadowD/shadowF/shadowG/divH to shadowD/shadowK/divL
+PASS dispatchedEvent("mouseover") is ["divL(<-shadowF)(@divL)", "shadowK(<-shadowF)(@shadowK)", "shadowK(<-shadowF)(@divJ)"]
+PASS dispatchedEvent("mouseout") is ["divH(<-shadowK)(@divH)", "shadowG(<-shadowK)(@shadowG)", "shadowF(<-shadowK)(@shadowF)", "shadowF(<-shadowK)(@divE)"]
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/dom/shadow/shadow-boundary-events.html
@@ -1,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="../../js/resources/js-test-pre.js"></script>
+<script src="resources/create-dom.js"></script>
+</head>
+<body>
+<p id="description"></p>
+<div id="sandbox"></div>
+<pre id="console"></pre>
+<script>
+description("Tests to ensure that shadow DOM boundary is not crossed during event propagation. Can only run within DRT.");
+
+function moveMouseOver(element)
+{
+    if (!window.eventSender || !window.internals)
+        return;
+
+    var defaultPaddingSize = 20;
+    var x = element.offsetLeft + element.offsetWidth / 2;
+    var y;
+    if (element.hasChildNodes() || window.internals.shadowRoot(element))
+        y = element.offsetTop + defaultPaddingSize;
+    else
+        y = element.offsetTop + element.offsetHeight / 2;
+    eventSender.mouseMoveTo(x, y);
+}
+
+var eventRecords = {};
+
+function clearEventRecords()
+{
+    eventRecords = {};
+}
+
+function dispatchedEvent(eventType)
+{
+    var events = eventRecords[eventType];
+    if (!events)
+        return [];
+    return events;
+}
+
+function recordEvent(event)
+{
+    var eventType = event.type
+    if (!eventRecords[eventType]) {
+        eventRecords[eventType] = []
+    }
+    // Records each event in the following format per event type:
+    //   eventRecords[eventType] = ['target.id(<-relatedTarget.id)(@currentTarget.id)',,,]
+    //   * RelatedTarget and currentTarget may be omitted if they are not defined.
+    // A new event is pushed back to the array of its event type.
+    var eventString = '';
+    eventString += event.target.id;
+    if (event.relatedTarget)
+        eventString += '(<-' + event.relatedTarget.id + ')';
+    if (event.currentTarget)
+        eventString += '(@' + event.currentTarget.id + ')';
+    eventRecords[eventType].push(eventString);
+}
+
+function getElementInShadow(path)
+{
+    var ids = path.split('/');
+    var element = document.getElementById(ids[0]);
+    for (var i = 1; element != null && i < ids.length; ++i) {
+        var shadowRoot = internals.shadowRoot(element);
+        element = internals.getElementByIdInShadowRoot(shadowRoot, ids[i]);
+    }
+    return element;
+}
+
+function prepareDomTree(parent)
+{
+    parent.appendChild(
+        createDom('div', {'id': 'divA', 'style': 'padding-top: 40px'},
+                  createDom('div', {'id': 'divB', 'style': 'width: 40px; height: 40px'}),
+                  createDom('div', {'id': 'divC', 'style': 'width: 40px; height: 40px'}),
+                  createShadow('div', {'id': 'shadowD', 'style': 'padding-top: 40px'},
+                               createDom('div', {'id': 'divE', 'style': 'padding-top: 40px'},
+                                         createShadow('div', {'id': 'shadowF', 'style': 'padding-top: 40px'},
+                                                      createShadow('div', {'id': 'shadowG', 'style': 'padding-top: 40px'},
+                                                                   createDom('div', {'id': 'divH', 'style': 'width: 40px; height: 40px'}),
+                                                                   createDom('div', {'id': 'divI', 'style': 'width: 40px; height: 40px'})))),
+                               createDom('div', {'id': 'divJ', 'style': 'padding-top: 40px'},
+                                         createShadow('div', {'id': 'shadowK', 'style': 'padding-top: 40px'},
+                                                      createDom('div', {'id': 'divL', 'style': 'width: 40px; height: 40px'}))))));
+
+    var ids = ['divA', 'divB', 'divC',
+               'shadowD', 'shadowD/divE', 'shadowD/shadowF', 'shadowD/shadowF/shadowG',
+               'shadowD/shadowF/shadowG/divH', 'shadowD/shadowF/shadowG/divI',
+               'shadowD/divJ', 'shadowD/shadowK', 'shadowD/shadowK/divL'];
+    for (var i = 0; i < ids.length; ++i) {
+        var element = getElementInShadow(ids[i]);
+        element.addEventListener('mouseover', recordEvent, false);
+        element.addEventListener('mouseout', recordEvent, false);
+    }
+}
+
+function moveMouse(oldElementId, newElementId, message)
+{
+    debug('\n' + message + '\n' + 'Moving mouse from ' + oldElementId + ' to ' + newElementId);
+    moveMouseOver(getElementInShadow(oldElementId));
+    clearEventRecords();
+    moveMouseOver(getElementInShadow(newElementId));
+}
+
+function test()
+{
+    if (window.layoutTestController)
+        layoutTestController.dumpAsText();
+    prepareDomTree(document.getElementById('sandbox'));
+
+    moveMouse('divB', 'divC',
+              'Move mouse from a node to its sibling node. All nodes are outside of shadow boundary.');
+    shouldBe('dispatchedEvent("mouseover")', '["divC(<-divB)(@divC)", "divC(<-divB)(@divA)"]');
+    shouldBe('dispatchedEvent("mouseout")', '["divB(<-divC)(@divB)", "divB(<-divC)(@divA)"]');
+
+    moveMouse('divB', 'divA',
+              'Target is an ancestor of relatedTarget. All nodes are outside of shadow boundary.');
+    shouldBe('dispatchedEvent("mouseover")', '["divA(<-divB)(@divA)"]');
+    shouldBe('dispatchedEvent("mouseout")', '["divB(<-divA)(@divB)", "divB(<-divA)(@divA)"]');
+
+    moveMouse('divA', 'divB',
+              'RelatedTarget is an ancestor of target. All nodes are outside of shadow boundary.');
+    shouldBe('dispatchedEvent("mouseover")', '["divB(<-divA)(@divB)", "divB(<-divA)(@divA)"]');
+    shouldBe('dispatchedEvent("mouseout")', '["divA(<-divB)(@divA)"]');
+
+    moveMouse('shadowD/shadowF/shadowG/divH', 'shadowD/shadowF/shadowG/divI',
+              'Both target and relatedTarget are immediate children of the same shadow root.');
+    shouldBe('dispatchedEvent("mouseover")', '["divI(<-divH)(@divI)"]');
+    shouldBe('dispatchedEvent("mouseout")', '["divH(<-divI)(@divH)"]');
+
+    moveMouse('shadowD/shadowF/shadowG/divI', 'shadowD/divE',
+              'Target is an ancestor of relatedTarget.');
+    shouldBe('dispatchedEvent("mouseover")', '["divE(<-shadowF)(@divE)"]');
+    shouldBe('dispatchedEvent("mouseout")', '["divI(<-divE)(@divI)", "shadowG(<-divE)(@shadowG)", "shadowF(<-divE)(@shadowF)", "shadowF(<-divE)(@divE)"]');
+
+    moveMouse('shadowD/shadowF/shadowG/divI', 'shadowD/shadowF',
+              'Target (shadow host) is an ancestor of relatedTarget.');
+    shouldBe('dispatchedEvent("mouseover")', '[]');
+    shouldBe('dispatchedEvent("mouseout")', '["divI(<-shadowF)(@divI)", "shadowG(<-shadowF)(@shadowG)"]');
+
+    moveMouse('shadowD/shadowF/shadowG', 'shadowD',
+              'Target (shadow host) is an ancestor of relatedTarget (shadow host).');
+    shouldBe('dispatchedEvent("mouseover")', '[]');
+    shouldBe('dispatchedEvent("mouseout")', '["shadowG(<-shadowD)(@shadowG)", "shadowF(<-shadowD)(@shadowF)", "shadowF(<-shadowD)(@divE)"]');
+
+    moveMouse('shadowD/divE', 'shadowD/shadowF/shadowG/divI',
+              'RelatedTarget is ancestor of target.');
+    shouldBe('dispatchedEvent("mouseover")', '["divI(<-divE)(@divI)", "shadowG(<-divE)(@shadowG)", "shadowF(<-divE)(@shadowF)", "shadowF(<-divE)(@divE)"]');
+    shouldBe('dispatchedEvent("mouseout")', '["divE(<-shadowF)(@divE)"]');
+
+    moveMouse('shadowD/shadowF', 'shadowD/shadowF/shadowG/divI',
+              'RelatedTarget (shadow host) is ancestor of target.');
+    shouldBe('dispatchedEvent("mouseover")', '["divI(<-shadowF)(@divI)", "shadowG(<-shadowF)(@shadowG)"]');
+    shouldBe('dispatchedEvent("mouseout")', '[]');
+
+    moveMouse('shadowD', 'shadowD/shadowF/shadowG',
+              'RelatedTarget (shadow host) is an ancestor of target (shadow host).');
+    shouldBe('dispatchedEvent("mouseover")', '["shadowG(<-shadowD)(@shadowG)", "shadowF(<-shadowD)(@shadowF)", "shadowF(<-shadowD)(@divE)"]');
+    shouldBe('dispatchedEvent("mouseout")', '[]');
+
+    moveMouse('shadowD/shadowF/shadowG/divH', 'shadowD/shadowK/divL',
+              'Target and relatedTarget exist in separated subtree, crossing shadow boundaries. Making sure that event is not dispatched beyond the lowest common boundary.');
+    shouldBe('dispatchedEvent("mouseover")', '["divL(<-shadowF)(@divL)", "shadowK(<-shadowF)(@shadowK)", "shadowK(<-shadowF)(@divJ)"]');
+    shouldBe('dispatchedEvent("mouseout")', '["divH(<-shadowK)(@divH)", "shadowG(<-shadowK)(@shadowG)", "shadowF(<-shadowK)(@shadowF)", "shadowF(<-shadowK)(@divE)"]');
+}
+
+test();
+
+var successfullyParsed = true;
+</script>
+<script src="../../js/resources/js-test-post.js"></script>
+</body>
+</html>

commit 09dc6f85c269a1faf778556d3afbecdaa00540d5

Fix crash when mouse moves from <summary> element to parent <details> element. https://bugs.webkit.org/show_bug.cgi?id=66210
Reviewed by Dimitri Glazkov.

This is a regression caused by r92922, which wrongly assumes that
a shadow host always has a shadow root as an immediate child in
ancestors chain. This assumption does not apply to <details>
element. <details> element is implemented as a shadow host, but
may have a <summary> element as an immediate child element in
ancestors chain.

Source/WebCore:

Test: fast/dom/shadow/details-summary-mouseover.html

* dom/EventDispatcher.cpp:
(WebCore::EventDispatcher::adjustToShadowBoundaries):

LayoutTests:

* fast/dom/shadow/details-summary-mouseover-expected.txt: Added.
* fast/dom/shadow/details-summary-mouseover.html: Added.

git-svn-id: svn://svn.chromium.org/blink/trunk@93079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/dom/EventDispatcher.cpp
@@ -165,8 +165,9 @@ PassRefPtr<EventTarget> EventDispatcher::adjustToShadowBoundaries(PassRefPtr<Nod
         // The relatedTarget is an ancestor or shadowHost of the target.
         // FIXME: Remove the first check once conversion to new shadow DOM is complete <http://webkit.org/b/48698>
         if (m_node->shadowHost() == relatedTarget.get() || isShadowHost(relatedTarget.get())) {
-            ASSERT(targetAncestor - 1 >= m_ancestors.begin());
-            lowestCommonBoundary = targetAncestor - 1;
+            lowestCommonBoundary = targetAncestor;
+            if (targetAncestor - 1 >= m_ancestors.begin() && isShadowRootOrSVGShadowRoot((targetAncestor - 1)->node()))
+                lowestCommonBoundary = targetAncestor - 1;
         }
     } else if ((*firstDivergentBoundary) == m_node.get()) {
         // Since ancestors does not contain target itself, we must account

LayoutTests/fast/dom/shadow/details-summary-mouseover-expected.txt
@@ -1,0 +1,11 @@
+Tests to ensure that moving mouse from 'summary' to 'details' should not crash. Note that 'details' is a shadow host and has a 'summary' element as a immediate child elment. There is no shadow root between them.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+summary
+details
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/dom/shadow/details-summary-mouseover.html
@@ -1,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="../../js/resources/js-test-pre.js"></script>
+</head>
+<body>
+<p id="description"></p>
+<div id="sandbox">
+    <details open id="details" style="height: 200px">
+      <summary id="summary">summary</summary>
+      details
+    </details>
+</div>
+<pre id="console"></pre>
+<script>
+description("Tests to ensure that moving mouse from 'summary' to 'details' should not crash. "
+    + "Note that 'details' is a shadow host and has a 'summary' element as a immediate child elment. "
+    + "There is no shadow root between them.");
+
+function moveMouseOver(element)
+{
+    if (!window.eventSender)
+        return;
+    var x = element.offsetLeft + element.offsetWidth / 2;
+    var y = element.offsetTop + element.offsetHeight / 2;
+    eventSender.mouseMoveTo(x, y);
+}
+
+function test()
+{
+    if (window.layoutTestController)
+        layoutTestController.dumpAsText();
+    moveMouseOver(document.getElementById('summary'));
+    moveMouseOver(document.getElementById('details'));
+}
+
+test();
+
+var successfullyParsed = true;
+</script>
+<script src="../../js/resources/js-test-post.js"></script>
+</body>
+</html>
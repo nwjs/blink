nwjs/blink

commit 664a4497cb5da60dd23f44465b5e0e18a8df456a

canvas arc() missing line to start of arc if swing is zero https://bugs.webkit.org/show_bug.cgi?id=55696
Source/WebCore:

Patch by Tom Zakrajsek <tomz@codeaurora.org> on 2011-08-29
Reviewed by Andreas Kling.

Test: fast/canvas/canvas-arc-zero-lineto.html

* html/canvas/CanvasRenderingContext2D.cpp:
(WebCore::CanvasRenderingContext2D::arc):

LayoutTests:

LayoutTests/canvas/philip/tests/2d.path.stroke.prune.arc.html misinterprets pruning requirement
https://bugs.webkit.org/show_bug.cgi?id=66832

Patch by Tom Zakrajsek <tomz@codeaurora.org> on 2011-08-29
Reviewed by Andreas Kling.

Update to 2d.path.stroke.prune.arc.html was reviewed by Philip Taylor and will be part of the
next update to http://philip.html5.org/tests/canvas/suite/tests/2d.path.stroke.prune.arc.html

* canvas/philip/tests/2d.path.stroke.prune.arc.html:
* fast/canvas/canvas-arc-zero-lineto-expected.txt: Added.
* fast/canvas/canvas-arc-zero-lineto.html: Added.

git-svn-id: svn://svn.chromium.org/blink/trunk@93982 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -833,8 +833,17 @@ void CanvasRenderingContext2D::arc(float x, float y, float r, float sa, float ea
         return;
     }
 
-    if (sa == ea)
+    if (!r || sa == ea) {
+        // A subpath consisting of a single empty arc should draw nothing.
+        // However, an empty arc may comprise one point in a non-empty
+        // subpath, so do not ignore empty arcs entirely.
+        FloatPoint pt(x + r * cosf(sa), y + r * sinf(sa));
+        if (!m_path.hasCurrentPoint())
+            m_path.moveTo(pt);
+        else
+            m_path.addLineTo(pt);
         return;
+    }
 
     if (!state().m_invertibleCTM)
         return;

LayoutTests/canvas/philip/tests/2d.path.stroke.prune.arc.html
@@ -26,7 +26,7 @@ ctx.arcTo(50, 25, 150, 25, 10);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.moveTo(50, 25);
+ctx.moveTo(60, 25);
 ctx.arc(50, 25, 10, 0, 0, false);
 ctx.stroke();
 

LayoutTests/fast/canvas/canvas-arc-zero-lineto-expected.txt
@@ -1,0 +1,13 @@
+Bug 55696: Series of tests to ensure zero-length arc extends current subpath
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS Pixel (50,20) is black.
+PASS Pixel (50,40) is black.
+PASS Pixel (50,60) is black.
+PASS Pixel (50,80) is black.
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/canvas/canvas-arc-zero-lineto.html
@@ -1,0 +1,58 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<script src="../js/resources/js-test-pre.js"></script>
+</head>
+<body>
+<p id="description"></p>
+<div id="console"></div>
+<canvas id="canvas" width="100" height="100"></canvas>
+<script>
+description("Bug 55696: Series of tests to ensure zero-length arc extends current subpath");
+var ctx = document.getElementById('canvas').getContext('2d');
+ctx.lineWidth = 4;
+
+function shouldBeBlackPixel(x,y)
+{
+    var data = ctx.getImageData(x, y, 1, 1).data;
+    if (data[0] != 0 || data[1] != 0 || data[2] != 0 || data[3] != 255) {
+        testFailed("Pixel (" + x + "," + y + ") should be black; " +
+                   "was [" + data[0] + "," + data[1] + "," + data[2] + "," + data[3] + "]");
+    } else {
+        testPassed("Pixel (" + x + "," + y + ") is black.");
+    }
+}
+
+// moveTo + empty arc (swing == 0)
+ctx.beginPath();
+ctx.moveTo(20, 20);
+ctx.arc(80, 30, 10, -Math.PI/2, -Math.PI/2, true);
+ctx.stroke();
+shouldBeBlackPixel(50, 20);
+
+// moveTo + empty arc (radius == 0)
+ctx.beginPath();
+ctx.moveTo(20, 40);
+ctx.arc(80, 40, 0, 0, 6, false);
+ctx.stroke();
+shouldBeBlackPixel(50, 40)
+
+// empty arc (swing == 0) + lineTo
+ctx.beginPath();
+ctx.arc(20, 50, 10, Math.PI/2, Math.PI/2, false);
+ctx.lineTo(80, 60);
+ctx.stroke();
+shouldBeBlackPixel(50, 60);
+
+// empty arc (radius == 0) + lineTo
+ctx.beginPath();
+ctx.arc(20, 80, 0, 0, 6, false);
+ctx.lineTo(80, 80);
+ctx.stroke();
+shouldBeBlackPixel(50, 80);
+
+var successfullyParsed = true;
+</script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>

commit 05f5917b22b00bf72a9f165ef47f2a8e85661b93

Fix snow-leopard regression caused by r93982 https://bugs.webkit.org/show_bug.cgi?id=67301
Patch by Tom Zakrajsek <tomz@codeaurora.org> on 2011-08-31
Reviewed by Kenneth Russell.

Test: canvas/philip/tests/2d.path.stroke.prune.arc.html

* html/canvas/CanvasRenderingContext2D.cpp:
(WebCore::CanvasRenderingContext2D::arc):

git-svn-id: svn://svn.chromium.org/blink/trunk@94273 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -834,14 +834,8 @@ void CanvasRenderingContext2D::arc(float x, float y, float r, float sa, float ea
     }
 
     if (!r || sa == ea) {
-        // A subpath consisting of a single empty arc should draw nothing.
-        // However, an empty arc may comprise one point in a non-empty
-        // subpath, so do not ignore empty arcs entirely.
-        FloatPoint pt(x + r * cosf(sa), y + r * sinf(sa));
-        if (!m_path.hasCurrentPoint())
-            m_path.moveTo(pt);
-        else
-            m_path.addLineTo(pt);
+        // The arc is empty but we still need to draw the connecting line
+        lineTo(x + r * cosf(sa), y + r * sinf(sa));
         return;
     }
 
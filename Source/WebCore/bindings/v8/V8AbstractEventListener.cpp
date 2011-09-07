nwjs/blink

commit 5f203e1c55c099253371de9fdf59bbeb9e386b18

Return value of window.onerror has inverted semantics https://bugs.webkit.org/show_bug.cgi?id=67119
Source/WebCore:

Error is now treated as handled only when window.onerror function returns true.

Reviewed by Darin Adler.

Test: fast/events/window-onerror13.html

* bindings/js/JSErrorHandler.cpp:
(WebCore::JSErrorHandler::handleEvent):
* bindings/v8/V8WindowErrorHandler.cpp:
(WebCore::V8WindowErrorHandler::callListenerFunction):
* bindings/v8/V8WorkerContextErrorHandler.cpp:
(WebCore::V8WorkerContextErrorHandler::callListenerFunction):

LayoutTests:

Reviewed by Darin Adler.

* fast/events/window-onerror1.html:
* fast/events/window-onerror11.html:
* fast/events/window-onerror12.html:
* fast/events/window-onerror13-expected.txt: Added.
* fast/events/window-onerror13.html: Copied from LayoutTests/fast/events/window-onerror11.html.
* fast/events/window-onerror2.html:
* fast/events/window-onerror3.html:
* fast/events/window-onerror4.html:
* fast/events/window-onerror5.html:
* fast/events/window-onerror6.html:
* fast/events/window-onerror8.html:
* fast/events/window-onerror9.html:
* fast/workers/resources/worker-script-error-bubbled.js:
(onerror):
* fast/workers/resources/worker-script-error-handled.js:
(onerror):
* http/tests/security/window-onerror-exception-in-iframe.html:

git-svn-id: svn://svn.chromium.org/blink/trunk@94061 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/bindings/v8/V8WindowErrorHandler.cpp
@@ -57,7 +57,7 @@ v8::Local<v8::Value> V8WindowErrorHandler::callListenerFunction(ScriptExecutionC
         v8::TryCatch tryCatch;
         tryCatch.SetVerbose(true);
         returnValue = callFunction->Call(thisValue, 3, parameters);
-        if (!tryCatch.HasCaught() && !returnValue.IsEmpty() && returnValue->IsBoolean() && !returnValue->BooleanValue())
+        if (!tryCatch.HasCaught() && !returnValue.IsEmpty() && returnValue->IsBoolean() && returnValue->BooleanValue())
             event->preventDefault();
     }
     return returnValue;

Source/WebCore/bindings/v8/V8WorkerContextErrorHandler.cpp
@@ -55,7 +55,7 @@ v8::Local<v8::Value> V8WorkerContextErrorHandler::callListenerFunction(ScriptExe
         v8::Local<v8::Object> thisValue = v8::Context::GetCurrent()->Global();
         v8::Handle<v8::Value> parameters[3] = { v8String(errorEvent->message()), v8String(errorEvent->filename()), v8::Integer::New(errorEvent->lineno()) };
         returnValue = callFunction->Call(thisValue, 3, parameters);
-        if (!returnValue.IsEmpty() && returnValue->IsBoolean() && !returnValue->BooleanValue())
+        if (!returnValue.IsEmpty() && returnValue->IsBoolean() && returnValue->BooleanValue())
             event->preventDefault();
     }
     return returnValue;

LayoutTests/fast/events/window-onerror1.html
@@ -20,7 +20,7 @@ function test()
     window.onerror = function (error, url, line) {
         url = url ? url.match( /[^\/]+\/?$/ )[0] : url;
         print("PASS: caught global error: " + error + " at " + url + ":" + line, "green");
-        return false;
+        return true;
     };
     hahaha_good_luck_finding_me(); // caught by window.onerror
 }

LayoutTests/fast/events/window-onerror12.html
@@ -20,7 +20,7 @@ window.onerror = function(msg, url, line)
     log("msg: " + msg);
     log("url: " + url);
     log("line: " + line);
-    return false;
+    return true;
 }
 
 var evt = document.createEvent("Event");

LayoutTests/fast/events/window-onerror13-expected.txt
@@ -1,0 +1,6 @@
+CONSOLE MESSAGE: line 0: An Error
+Test that error is treated as not handled when window.onerror return value is not 'true'. Bug 67119.
+
+Uncaught error should be printed in console when window.onerror returns not true.
+DONE.
+

LayoutTests/fast/events/window-onerror13.html
@@ -4,7 +4,8 @@
 </script>
 </head>
 <body>
-<p>Test that window.onerror is called on window object. <a href="https://bugs.webkit.org/show_bug.cgi?id=8519">Bug 8519</a>.</p>
+<p>Test that error is treated as not handled when window.onerror return
+value is not 'true'. <a href="https://bugs.webkit.org/show_bug.cgi?id=67119">Bug 67119</a>.</p>
 <div id="console"></div>
 <script>
 if (window.layoutTestController)
@@ -16,13 +17,11 @@ function log(msg) {
 
 window.onerror = function(msg, url, line)
 {
-    url = url ? url.match( /[^\/]+\/?$/ )[0] : url;
-    log("Main frame window.onerror: " + msg + " at " + url + ":" + line);
-    return true;
+  log("Uncaught error should be printed in console when window.onerror returns not true.");
+  log("DONE.");
 }
 
-throwException();
-
+throw "An Error";
 </script>
 </body>
 </html>

LayoutTests/fast/events/window-onerror2.html
@@ -21,7 +21,7 @@ window.onerror = function(msg, url, line)
     if (++unhandledErrorCount === 3 && window.layoutTestController)
         layoutTestController.notifyDone();
 
-    return false;
+    return true;
 }
 
 function delayedThrowException()

LayoutTests/fast/events/window-onerror3.html
@@ -11,7 +11,7 @@ function test1()
     window.onerror = function (error, url, line) {
         url = url ? url.match( /[^\/]+\/?$/ )[0] : url;
         log("Error caught successfully: " + error + "\nFile: " + url + "\nLine: " + line)
-        return false;
+        return true;
     };
     unknownObject.unknownProperty++;
 }

commit 01fa769ed925e4c2814e6220358b7a7ba1a4c403

fast/workers/worker-script-error.html fails on Chromium after r94061 https://bugs.webkit.org/show_bug.cgi?id=67206
Source/WebCore:

Default action should be prevented if window.onerror returned true and stay
not prevented otherwise.

Reviewed by Dmitry Titov.

Tests: fast/events/window-onerror14.html
       fast/events/window-onerror15.html
       fast/events/window-onerror16.html

* bindings/v8/V8AbstractEventListener.cpp:
(WebCore::V8AbstractEventListener::invokeEventHandler):
(WebCore::V8AbstractEventListener::shouldPreventDefault): allow specific
event listeners to decide when to prevent default action based on the handler
return value.
* bindings/v8/V8AbstractEventListener.h:
* bindings/v8/V8WindowErrorHandler.cpp:
(WebCore::V8WindowErrorHandler::callListenerFunction):
(WebCore::V8WindowErrorHandler::shouldPreventDefault):
* bindings/v8/V8WindowErrorHandler.h:
* bindings/v8/V8WorkerContextErrorHandler.cpp:
(WebCore::V8WorkerContextErrorHandler::callListenerFunction):
(WebCore::V8WorkerContextErrorHandler::shouldPreventDefault):
* bindings/v8/V8WorkerContextErrorHandler.h:

LayoutTests:

Add a couple more tests for window.onerror handler that check whether
error message is printed to the console pending on the handler return
value.

Reviewed by Dmitry Titov.

* fast/events/window-onerror14-expected.txt: Added.
* fast/events/window-onerror14.html: Added.
* fast/events/window-onerror15-expected.txt: Added.
* fast/events/window-onerror15.html: Added.
* fast/events/window-onerror16-expected.txt: Added.
* fast/events/window-onerror16.html: Added.
* platform/chromium/fast/events/window-onerror16-expected.txt: Added.

git-svn-id: svn://svn.chromium.org/blink/trunk@94643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/bindings/v8/V8AbstractEventListener.cpp
@@ -181,10 +181,15 @@ void V8AbstractEventListener::invokeEventHandler(ScriptExecutionContext* context
     if (!returnValue->IsNull() && !returnValue->IsUndefined() && event->storesResultAsString())
         event->storeResult(toWebCoreString(returnValue));
 
-    // Prevent default action if the return value is false;
-    // FIXME: Add example, and reference to bug entry.
-    if (m_isAttribute && returnValue->IsBoolean() && !returnValue->BooleanValue())
+    if (m_isAttribute && shouldPreventDefault(returnValue))
         event->preventDefault();
+}
+
+bool V8AbstractEventListener::shouldPreventDefault(v8::Local<v8::Value> returnValue)
+{
+    // Prevent default action if the return value is false in accord with the spec
+    // http://www.w3.org/TR/html5/webappapis.html#event-handler-attributes
+    return returnValue->IsBoolean() && !returnValue->BooleanValue();
 }
 
 v8::Local<v8::Object> V8AbstractEventListener::getReceiverObject(Event* event)

Source/WebCore/bindings/v8/V8AbstractEventListener.h
@@ -123,6 +123,8 @@ namespace WebCore {
 
         virtual v8::Local<v8::Value> callListenerFunction(ScriptExecutionContext*, v8::Handle<v8::Value> jsevent, Event*) = 0;
 
+        virtual bool shouldPreventDefault(v8::Local<v8::Value> returnValue);
+
         v8::Persistent<v8::Object> m_listener;
 
         // Indicates if this is an HTML type listener.

Source/WebCore/bindings/v8/V8WindowErrorHandler.cpp
@@ -57,10 +57,13 @@ v8::Local<v8::Value> V8WindowErrorHandler::callListenerFunction(ScriptExecutionC
         v8::TryCatch tryCatch;
         tryCatch.SetVerbose(true);
         returnValue = callFunction->Call(thisValue, 3, parameters);
-        if (!tryCatch.HasCaught() && !returnValue.IsEmpty() && returnValue->IsBoolean() && returnValue->BooleanValue())
-            event->preventDefault();
     }
     return returnValue;
+}
+
+bool V8WindowErrorHandler::shouldPreventDefault(v8::Local<v8::Value> returnValue)
+{
+    return returnValue->IsBoolean() && returnValue->BooleanValue();
 }
 
 } // namespace WebCore

Source/WebCore/bindings/v8/V8WindowErrorHandler.h
@@ -48,6 +48,7 @@ private:
     V8WindowErrorHandler(v8::Local<v8::Object> listener, bool isInline, const WorldContextHandle& worldContext);
 
     virtual v8::Local<v8::Value> callListenerFunction(ScriptExecutionContext*, v8::Handle<v8::Value> jsEvent, Event*);
+    virtual bool shouldPreventDefault(v8::Local<v8::Value> returnValue);
 };
 
 } // namespace WebCore

Source/WebCore/bindings/v8/V8WorkerContextErrorHandler.cpp
@@ -55,10 +55,13 @@ v8::Local<v8::Value> V8WorkerContextErrorHandler::callListenerFunction(ScriptExe
         v8::Local<v8::Object> thisValue = v8::Context::GetCurrent()->Global();
         v8::Handle<v8::Value> parameters[3] = { v8String(errorEvent->message()), v8String(errorEvent->filename()), v8::Integer::New(errorEvent->lineno()) };
         returnValue = callFunction->Call(thisValue, 3, parameters);
-        if (!returnValue.IsEmpty() && returnValue->IsBoolean() && returnValue->BooleanValue())
-            event->preventDefault();
     }
     return returnValue;
+}
+
+bool V8WorkerContextErrorHandler::shouldPreventDefault(v8::Local<v8::Value> returnValue)
+{
+    return returnValue->IsBoolean() && returnValue->BooleanValue();
 }
 
 } // namespace WebCore

Source/WebCore/bindings/v8/V8WorkerContextErrorHandler.h
@@ -50,6 +50,7 @@ private:
     V8WorkerContextErrorHandler(v8::Local<v8::Object> listener, bool isInline, const WorldContextHandle& worldContext);
 
     virtual v8::Local<v8::Value> callListenerFunction(ScriptExecutionContext*, v8::Handle<v8::Value> jsEvent, Event*);
+    virtual bool shouldPreventDefault(v8::Local<v8::Value> returnValue);
 };
 
 } // namespace WebCore

LayoutTests/fast/events/window-onerror14-expected.txt
@@ -1,0 +1,5 @@
+Test that error is treated as handled when window.onerror return value is 'true'. Bug 67119.
+
+Uncaught error should *not* be printed in console when window.onerror returns true.
+window.onerror invoked.
+

LayoutTests/fast/events/window-onerror14.html
@@ -1,0 +1,28 @@
+<html>
+<head>
+<script src="resources/window-onerror.js">
+</script>
+</head>
+<body>
+<p>Test that error is treated as handled when window.onerror return
+value is 'true'. <a href="https://bugs.webkit.org/show_bug.cgi?id=67119">Bug 67119</a>.</p>
+<div id="console"></div>
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+function log(msg) {
+    document.getElementById("console").innerHTML += msg + "<br>";
+}
+
+window.onerror = function(msg, url, line)
+{
+  log("Uncaught error should *not* be printed in console when window.onerror returns true.");
+  log("window.onerror invoked.");
+  return true;
+}
+
+throw "An Error";
+</script>
+</body>
+</html>

LayoutTests/fast/events/window-onerror15-expected.txt
@@ -1,0 +1,5 @@
+Test that error is treated as handled when preventDefault is called on the error event.Bug 67119.
+
+Error event listener invoked. Error should *not* be printed in console.
+DONE.
+

LayoutTests/fast/events/window-onerror15.html
@@ -1,0 +1,27 @@
+<html>
+<head>
+<script src="resources/window-onerror.js">
+</script>
+</head>
+<body>
+<p>Test that error is treated as handled when preventDefault is called on the
+error event.<a href="https://bugs.webkit.org/show_bug.cgi?id=67119">Bug 67119</a>.</p>
+<div id="console"></div>
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+function log(msg) {
+    document.getElementById("console").innerHTML += msg + "<br>";
+}
+
+window.addEventListener("error", function(e) {
+  log("Error event listener invoked. Error should *not* be printed in console.");
+  log("DONE.");
+  e.preventDefault();
+}, true);
+
+throw "An Error";
+</script>
+</body>
+</html>

LayoutTests/fast/events/window-onerror16-expected.txt
@@ -1,0 +1,6 @@
+CONSOLE MESSAGE: line 0: An Error
+Test that error is treated as not handled when window.onerror return value is 'false'. Bug 67119.
+
+window.onerror invoked. Uncaught error should be printed in console
+DONE.
+

LayoutTests/fast/events/window-onerror16.html
@@ -1,0 +1,28 @@
+<html>
+<head>
+<script src="resources/window-onerror.js">
+</script>
+</head>
+<body>
+<p>Test that error is treated as not handled when window.onerror return
+value is 'false'. <a href="https://bugs.webkit.org/show_bug.cgi?id=67119">Bug 67119</a>.</p>
+<div id="console"></div>
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+function log(msg) {
+    document.getElementById("console").innerHTML += msg + "<br>";
+}
+
+window.onerror = function(msg, url, line)
+{
+  log("window.onerror invoked. Uncaught error should be printed in console");
+  log("DONE.");
+  return false;
+}
+
+throw "An Error";
+</script>
+</body>
+</html>

LayoutTests/platform/chromium/fast/events/window-onerror16-expected.txt
@@ -1,0 +1,6 @@
+CONSOLE MESSAGE: line 25: Uncaught An Error
+Test that error is treated as not handled when window.onerror return value is 'false'. Bug 67119.
+
+window.onerror invoked. Uncaught error should be printed in console
+DONE.
+
nwjs/blink

commit aacd7fff2f5d9fc1475e3172be445e88696a8638

Source/WebCore: Reuse CachedRawResources (e.g., XHRs) that are stored in the MemoryCache when appropriate. https://bugs.webkit.org/show_bug.cgi?id=76564
Reviewed by Antti Koivisto.

No new tests, expected behavior covered by existing tests.

* html/DOMURL.cpp:
(WebCore::DOMURL::revokeObjectURL): Objects shouldn't remain in the
    MemoryCache if revokeObjectURL is called on them.
* inspector/InspectorPageAgent.cpp:
(WebCore::InspectorPageAgent::cachedResourceContent): Add CachedRawResource support.
* inspector/InspectorResourceAgent.cpp:
(WebCore::InspectorResourceAgent::setCacheDisabled): Immediately
    evict resources, rather than waiting for navigation, since XHRs
    should hit the cache if it has been disabled.
* loader/cache/CachedRawResource.cpp:
(CachedRawResourceCallback): Encapsulates the async callback for
    a cache hit for CachedRawResources.
(WebCore::CachedRawResource::sendCallbacks): Do the work defered in didAddClient.
(WebCore::CachedRawResource::didAddClient): Scheduled a CachedRawResourceCallback if
    we already have a response, since async XHRs may not play nicely with receiving
    their data synchronously.
(WebCore::CachedRawResource::removeClient): Ensure we cancel a callback to a client if
    it removes itself.
(WebCore::CachedRawResource::canReuse): Provide some basic rules for when a
    CachedRawResource can be reused.
* loader/cache/CachedRawResource.h:
* loader/cache/CachedResource.h:
* loader/cache/CachedResourceLoader.cpp:
(WebCore::CachedResourceLoader::determineRevalidationPolicy): Don't automatically reload
    CachedRawResources, and add a check for whether this request has already been
    made conditional.
* xml/XMLHttpRequest.cpp:

LayoutTests: Test update for https://bugs.webkit.org/show_bug.cgi?id=76564.

Use sync XHRs instead of async in network-content-replacement-xhr.html,
since the inspector pulls async XHR data off of its CachedResource, instead
of a buffer it controls.

Reviewed by Antti Koivisto.

Source/WebCore/inspector/InspectorPageAgent.cpp
@@ -121,7 +121,7 @@ static bool prepareCachedResourceBuffer(CachedResource* cachedResource, bool* ha
 static bool hasTextContent(CachedResource* cachedResource)
 {
     InspectorPageAgent::ResourceType type = InspectorPageAgent::cachedResourceType(*cachedResource);
-    return type == InspectorPageAgent::StylesheetResource || type == InspectorPageAgent::ScriptResource;
+    return type == InspectorPageAgent::StylesheetResource || type == InspectorPageAgent::ScriptResource || type == InspectorPageAgent::XHRResource;
 }
 
 bool InspectorPageAgent::cachedResourceContent(CachedResource* cachedResource, String* result, bool* base64Encoded)
@@ -143,12 +143,16 @@ bool InspectorPageAgent::cachedResourceContent(CachedResource* cachedResource, S
     }
 
     if (cachedResource) {
+        SharedBuffer* buffer = cachedResource->data();
         switch (cachedResource->type()) {
         case CachedResource::CSSStyleSheet:
             *result = static_cast<CachedCSSStyleSheet*>(cachedResource)->sheetText();
             return true;
         case CachedResource::Script:
             *result = static_cast<CachedScript*>(cachedResource)->script();
+            return true;
+        case CachedResource::RawResource:
+            *result = String(buffer->data(), buffer->size());
             return true;
         default:
             if (hasZeroSize) {
@@ -254,6 +258,8 @@ InspectorPageAgent::ResourceType InspectorPageAgent::cachedResourceType(const Ca
         return InspectorPageAgent::StylesheetResource;
     case CachedResource::Script:
         return InspectorPageAgent::ScriptResource;
+    case CachedResource::RawResource:
+        return InspectorPageAgent::XHRResource;
     default:
         break;
     }

LayoutTests/http/tests/inspector/network/network-content-replacement-xhr-expected.txt
@@ -2,11 +2,11 @@ CONSOLE MESSAGE: line 39: Done.
 Tests NetworkResourcesData logic for XHR content replacement.
 
 http://127.0.0.1:8000/inspector/network/resources/resource.php?size=200
-resource.content: 
+resource.content: null
 http://127.0.0.1:8000/inspector/network/resources/resource.php?size=100
 resource.content: ****************************************************************************************************
 http://127.0.0.1:8000/inspector/network/resources/resource.php?size=201
-resource.content: 
+resource.content: null
 http://127.0.0.1:8000/inspector/network/resources/resource.php?size=100
 resource.content: ****************************************************************************************************
 

LayoutTests/http/tests/inspector/network/network-content-replacement-xhr.html
@@ -16,22 +16,22 @@ function loadData()
     // discard third resource content once we see its size exceeds limit,
     // and finally replace first resource content with the last resource content.
 
-    doXHR("GET", "resources/resource.php?size=200", true, xhrLoaded1);
+    doXHR("GET", "resources/resource.php?size=200", false, xhrLoaded1);
 }
 
 function xhrLoaded1()
 {
-    doXHR("GET", "resources/resource.php?size=100", true, xhrLoaded2);
+    doXHR("GET", "resources/resource.php?size=100", false, xhrLoaded2);
 }
 
 function xhrLoaded2()
 {
-    doXHR("GET", "resources/resource.php?size=201", true, xhrLoaded3);
+    doXHR("GET", "resources/resource.php?size=201", false, xhrLoaded3);
 }
 
 function xhrLoaded3()
 {
-    doXHR("GET", "resources/resource.php?size=100", true, allXHRsLoaded);
+    doXHR("GET", "resources/resource.php?size=100", false, allXHRsLoaded);
 }
 
 function allXHRsLoaded()

Source/WebCore/html/DOMURL.cpp
@@ -34,6 +34,7 @@
 #include "Blob.h"
 #include "BlobURL.h"
 #include "KURL.h"
+#include "MemoryCache.h"
 #include "PublicURLManager.h"
 #include "ScriptExecutionContext.h"
 #include "SecurityOrigin.h"
@@ -89,6 +90,8 @@ void DOMURL::revokeObjectURL(ScriptExecutionContext* scriptExecutionContext, con
         return;
 
     KURL url(KURL(), urlString);
+    if (CachedResource* resource = memoryCache()->resourceForURL(url))
+        memoryCache()->remove(resource);
 
     HashSet<String>& blobURLs = scriptExecutionContext->publicURLManager().blobURLs();
     if (blobURLs.contains(url.string())) {

Source/WebCore/inspector/InspectorResourceAgent.cpp
@@ -253,7 +253,7 @@ void InspectorResourceAgent::didReceiveResponse(unsigned long identifier, Docume
             type = InspectorPageAgent::ScriptResource;
         else if (equalIgnoringFragmentIdentifier(response.url(), loader->frameLoader()->icon()->url()))
             type = InspectorPageAgent::ImageResource;
-        else if (equalIgnoringFragmentIdentifier(response.url(), loader->url()) && type == InspectorPageAgent::OtherResource)
+        else if (equalIgnoringFragmentIdentifier(response.url(), loader->url()) && !loader->isCommitted())
             type = InspectorPageAgent::DocumentResource;
 
         m_resourcesData->responseReceived(requestId, m_pageAgent->frameId(loader->frame()), response);
@@ -529,6 +529,8 @@ void InspectorResourceAgent::clearBrowserCookies(ErrorString*)
 void InspectorResourceAgent::setCacheDisabled(ErrorString*, bool cacheDisabled)
 {
     m_state->setBoolean(ResourceAgentState::cacheDisabled, cacheDisabled);
+    if (cacheDisabled)
+        memoryCache()->evictResources();
 }
 
 void InspectorResourceAgent::mainFrameNavigated(DocumentLoader* loader)

Source/WebCore/loader/cache/CachedRawResource.cpp
@@ -37,18 +37,21 @@ namespace WebCore {
 
 CachedRawResource::CachedRawResource(ResourceRequest& resourceRequest)
     : CachedResource(resourceRequest, RawResource)
-    , m_dataLength(0)
+    , m_identifier(0)
 {
 }
 
 void CachedRawResource::data(PassRefPtr<SharedBuffer> data, bool allDataReceived)
 {
     CachedResourceHandle<CachedRawResource> protect(this);
+    if (!m_identifier)
+        m_identifier = m_loader->identifier();
+
     if (data) {
         // If we are buffering data, then we are saving the buffer in m_data and need to manually
         // calculate the incremental data. If we are not buffering, then m_data will be null and
         // the buffer contains only the incremental data.
-        size_t previousDataLength = (m_options.shouldBufferData == BufferData) ? m_dataLength : 0;
+        size_t previousDataLength = (m_options.shouldBufferData == BufferData) ? encodedSize() : 0;
         ASSERT(data->size() >= previousDataLength);
         const char* incrementalData = data->data() + previousDataLength;
         size_t incrementalDataLength = data->size() - previousDataLength;
@@ -61,19 +64,82 @@ void CachedRawResource::data(PassRefPtr<SharedBuffer> data, bool allDataReceived
     }
     
     if (m_options.shouldBufferData == BufferData) {
-        m_dataLength = data ? data->size() : 0;
+        if (data)
+            setEncodedSize(data->size());
         m_data = data;
     }
     CachedResource::data(m_data, allDataReceived);
 }
 
+class CachedRawResourceCallback {
+public:    
+    static CachedRawResourceCallback* schedule(CachedRawResource* resource, CachedRawResourceClient* client)
+    {
+        return new CachedRawResourceCallback(resource, client);
+    }
+
+    void cancel()
+    {
+        if (m_callbackTimer.isActive())
+            m_callbackTimer.stop();
+    }
+
+private:
+    CachedRawResourceCallback(CachedRawResource* resource, CachedRawResourceClient* client)
+        : m_resource(resource)
+        , m_client(client)
+        , m_callbackTimer(this, &CachedRawResourceCallback::timerFired)
+    {
+        m_callbackTimer.startOneShot(0);
+    }
+
+    void timerFired(Timer<CachedRawResourceCallback>*)
+    {
+        m_resource->sendCallbacks(m_client);
+    }
+    CachedResourceHandle<CachedRawResource> m_resource;
+    CachedRawResourceClient* m_client;
+    Timer<CachedRawResourceCallback> m_callbackTimer;
+};
+
+void CachedRawResource::sendCallbacks(CachedRawResourceClient* c)
+{
+    if (!m_clientsAwaitingCallback.contains(c))
+        return;
+    m_clientsAwaitingCallback.remove(c);
+    c->responseReceived(this, m_response);
+    if (!m_clients.contains(c) || !m_data)
+        return;
+    c->dataReceived(this, m_data->data(), m_data->size());
+    if (!m_clients.contains(c) || isLoading())
+       return;
+    c->notifyFinished(this);
+}
+
 void CachedRawResource::didAddClient(CachedResourceClient* c)
 {
-    if (m_data) {
-        static_cast<CachedRawResourceClient*>(c)->responseReceived(this, m_response);
-        static_cast<CachedRawResourceClient*>(c)->dataReceived(this, m_data->data(), m_data->size());
-    }
-    CachedResource::didAddClient(c);
+    if (m_response.isNull())
+        return;
+
+    // CachedRawResourceClients (especially XHRs) do crazy things if an asynchronous load returns
+    // synchronously (e.g., scripts may not have set all the state they need to handle the load).
+    // Therefore, rather than immediately sending callbacks on a cache hit like other CachedResources,
+    // we schedule the callbacks and ensure we never finish synchronously.
+    // FIXME: We also use an async callback on 304 because we don't have sufficient information to
+    // know whether we are receiving new clients because of revalidation or pure reuse. Perhaps move
+    // this logic to CachedResource?
+    CachedRawResourceClient* client = static_cast<CachedRawResourceClient*>(c);
+    ASSERT(!m_clientsAwaitingCallback.contains(client));
+    m_clientsAwaitingCallback.add(client, adoptPtr(CachedRawResourceCallback::schedule(this, client)));
+}
+ 
+void CachedRawResource::removeClient(CachedResourceClient* c)
+{
+    OwnPtr<CachedRawResourceCallback> callback = m_clientsAwaitingCallback.take(static_cast<CachedRawResourceClient*>(c));
+    if (callback)
+        callback->cancel();
+    callback.clear();
+    CachedResource::removeClient(c);
 }
 
 void CachedRawResource::allClientsRemoved()
@@ -113,9 +179,15 @@ void CachedRawResource::setDefersLoading(bool defers)
         m_loader->setDefersLoading(defers);
 }
 
-unsigned long CachedRawResource::identifier() const
+bool CachedRawResource::canReuse() const
 {
-    return m_loader ? m_loader->identifier() : 0;
+    if (m_options.shouldBufferData == DoNotBufferData)
+        return false;
+
+    if (m_resourceRequest.httpMethod() != "GET")
+        return false;
+
+    return true;
 }
 
 } // namespace WebCore

Source/WebCore/loader/cache/CachedRawResource.h
@@ -27,6 +27,8 @@
 #include "CachedResourceClient.h"
 
 namespace WebCore {
+class CachedRawResourceCallback;
+class CachedRawResourceClient;
 
 class CachedRawResource : public CachedResource {
 public:
@@ -38,7 +40,12 @@ public:
     virtual void setDefersLoading(bool);
     
     // FIXME: This is exposed for the InpsectorInstrumentation for preflights in DocumentThreadableLoader. It's also really lame.
-    unsigned long identifier() const;
+    unsigned long identifier() const { return m_identifier; }
+
+    bool canReuse() const;
+    void sendCallbacks(CachedRawResourceClient*);
+
+    virtual void removeClient(CachedResourceClient*);
 
 private:
     virtual void didAddClient(CachedResourceClient*);
@@ -54,7 +61,8 @@ private:
     virtual void didDownloadData(int);
 #endif
 
-    size_t m_dataLength;
+    unsigned long m_identifier;
+    HashMap<CachedRawResourceClient*, OwnPtr<CachedRawResourceCallback> > m_clientsAwaitingCallback;
 };
 
 

Source/WebCore/loader/cache/CachedResource.h
@@ -110,7 +110,7 @@ public:
     void setLoadPriority(ResourceLoadPriority);
 
     void addClient(CachedResourceClient*);
-    void removeClient(CachedResourceClient*);
+    virtual void removeClient(CachedResourceClient*);
     bool hasClients() const { return !m_clients.isEmpty(); }
     void deleteIfPossible();
 

commit 0876a4290e8129a08efb348eed3efc3914f3bbea

Web Inspector: retrieving content for some XHR requests crashes inspected page renderer https://bugs.webkit.org/show_bug.cgi?id=79026
Reviewed by Pavel Feldman.

Source/WebCore:

Fixed loading empty xhr content and xhr content decoding that was
broken in r107672.

Tests: http/tests/inspector/network/network-cyrillic-xhr.html
       http/tests/inspector/network/network-empty-xhr.html

* inspector/InspectorPageAgent.cpp:
(WebCore):
(WebCore::InspectorPageAgent::createDecoder):
(WebCore::InspectorPageAgent::cachedResourceContent):
* inspector/InspectorPageAgent.h:
(WebCore):
* inspector/NetworkResourcesData.cpp:
(WebCore::NetworkResourcesData::ResourceData::decodeDataToContent):
(WebCore::NetworkResourcesData::responseReceived):
* inspector/NetworkResourcesData.h:
(WebCore::NetworkResourcesData::ResourceData::decoder):
(WebCore::NetworkResourcesData::ResourceData::setDecoder):

LayoutTests:

* http/tests/inspector/network/network-cyrillic-xhr-expected.txt: Added.
* http/tests/inspector/network/network-cyrillic-xhr.html: Added.
* http/tests/inspector/network/network-empty-xhr-expected.txt: Added.
* http/tests/inspector/network/network-empty-xhr.html: Added.
* http/tests/inspector/network/resources/cyrillic.html: Added.
* http/tests/inspector/network/resources/empty.html: Added.

git-svn-id: svn://svn.chromium.org/blink/trunk@108497 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/inspector/InspectorPageAgent.cpp
@@ -44,6 +44,7 @@
 #include "ContentSearchUtils.h"
 #include "Cookie.h"
 #include "CookieJar.h"
+#include "DOMImplementation.h"
 #include "DOMNodeHighlighter.h"
 #include "DOMPatchSupport.h"
 #include "Document.h"
@@ -67,6 +68,7 @@
 #include "SecurityOrigin.h"
 #include "SharedBuffer.h"
 #include "TextEncoding.h"
+#include "TextResourceDecoder.h"
 #include "UserGestureIndicator.h"
 
 #include <wtf/CurrentTime.h>
@@ -129,6 +131,23 @@ static bool hasTextContent(CachedResource* cachedResource)
     return type == InspectorPageAgent::StylesheetResource || type == InspectorPageAgent::ScriptResource || type == InspectorPageAgent::XHRResource;
 }
 
+// static
+PassRefPtr<TextResourceDecoder> InspectorPageAgent::createDecoder(const String& mimeType, const String& textEncodingName)
+{
+    RefPtr<TextResourceDecoder> decoder;
+    if (!textEncodingName.isEmpty())
+        decoder = TextResourceDecoder::create("text/plain", textEncodingName);
+    else if (mimeType == "text/plain")
+        decoder = TextResourceDecoder::create("text/plain", "ISO-8859-1");
+    else if (mimeType == "text/html")
+        decoder = TextResourceDecoder::create("text/html", "UTF-8");
+    else if (DOMImplementation::isXMLMIMEType(mimeType)) {
+        decoder = TextResourceDecoder::create("application/xml");
+        decoder->useLenientXMLDecoding();
+    }
+    return decoder;
+}
+
 bool InspectorPageAgent::cachedResourceContent(CachedResource* cachedResource, String* result, bool* base64Encoded)
 {
     bool hasZeroSize;
@@ -147,8 +166,12 @@ bool InspectorPageAgent::cachedResourceContent(CachedResource* cachedResource, S
         return true;
     }
 
+    if (hasZeroSize) {
+        *result = "";
+        return true;
+    }
+
     if (cachedResource) {
-        SharedBuffer* buffer = cachedResource->data();
         switch (cachedResource->type()) {
         case CachedResource::CSSStyleSheet:
             *result = static_cast<CachedCSSStyleSheet*>(cachedResource)->sheetText();
@@ -156,14 +179,15 @@ bool InspectorPageAgent::cachedResourceContent(CachedResource* cachedResource, S
         case CachedResource::Script:
             *result = static_cast<CachedScript*>(cachedResource)->script();
             return true;
-        case CachedResource::RawResource:
-            *result = String(buffer->data(), buffer->size());
+        case CachedResource::RawResource: {
+            SharedBuffer* buffer = cachedResource->data();
+            RefPtr<TextResourceDecoder> decoder = InspectorPageAgent::createDecoder(cachedResource->response().mimeType(), cachedResource->response().textEncodingName());
+            String content = decoder->decode(buffer->data(), buffer->size());
+            content += decoder->flush();
+            *result = content;
             return true;
+        }
         default:
-            if (hasZeroSize) {
-                *result = "";
-                return true;
-            }
             return decodeSharedBuffer(cachedResource->data(), cachedResource->encoding(), result);
         }
     }

Source/WebCore/inspector/InspectorPageAgent.h
@@ -58,6 +58,7 @@ class KURL;
 class Page;
 class RegularExpression;
 class SharedBuffer;
+class TextResourceDecoder;
 
 typedef String ErrorString;
 
@@ -77,6 +78,7 @@ public:
 
     static PassOwnPtr<InspectorPageAgent> create(InstrumentingAgents*, Page*, InspectorState*, InjectedScriptManager*, InspectorClient*);
 
+    static PassRefPtr<TextResourceDecoder> createDecoder(const String& mimeType, const String& textEncodingName);
     static bool cachedResourceContent(CachedResource*, String* result, bool* base64Encoded);
     static bool sharedBufferContent(PassRefPtr<SharedBuffer>, const String& textEncodingName, bool withBase64Encode, String* result);
     static void resourceContent(ErrorString*, Frame*, const KURL&, String* result, bool* base64Encoded);

Source/WebCore/inspector/NetworkResourcesData.cpp
@@ -29,7 +29,6 @@
 #include "config.h"
 #include "NetworkResourcesData.h"
 
-#include "DOMImplementation.h"
 #include "SharedBuffer.h"
 #include "TextResourceDecoder.h"
 
@@ -80,20 +79,6 @@ unsigned NetworkResourcesData::ResourceData::purgeContent()
     return result;
 }
 
-void NetworkResourcesData::ResourceData::createDecoder(const String& mimeType, const String& textEncodingName)
-{
-    if (!textEncodingName.isEmpty())
-        m_decoder = TextResourceDecoder::create("text/plain", textEncodingName);
-    else if (mimeType == "text/plain")
-        m_decoder = TextResourceDecoder::create("text/plain", "ISO-8859-1");
-    else if (mimeType == "text/html")
-        m_decoder = TextResourceDecoder::create("text/html", "UTF-8");
-    else if (DOMImplementation::isXMLMIMEType(mimeType)) {
-        m_decoder = TextResourceDecoder::create("application/xml");
-        m_decoder->useLenientXMLDecoding();
-    }
-}
-
 int NetworkResourcesData::ResourceData::dataLength() const
 {
     return m_dataBuffer ? m_dataBuffer->size() : 0;
@@ -113,6 +98,7 @@ int NetworkResourcesData::ResourceData::decodeDataToContent()
     ASSERT(!hasContent());
     int dataLength = m_dataBuffer->size();
     m_content = m_decoder->decode(m_dataBuffer->data(), m_dataBuffer->size());
+    m_content += m_decoder->flush();
     m_dataBuffer = nullptr;
     return 2 * m_content.length() - dataLength;
 }
@@ -143,7 +129,7 @@ void NetworkResourcesData::responseReceived(const String& requestId, const Strin
         return;
     resourceData->setFrameId(frameId);
     resourceData->setUrl(response.url());
-    resourceData->createDecoder(response.mimeType(), response.textEncodingName());
+    resourceData->setDecoder(InspectorPageAgent::createDecoder(response.mimeType(), response.textEncodingName()));
 }
 
 void NetworkResourcesData::setResourceType(const String& requestId, InspectorPageAgent::ResourceType type)

Source/WebCore/inspector/NetworkResourcesData.h
@@ -31,6 +31,7 @@
 
 #include "CachedResourceHandle.h"
 #include "InspectorPageAgent.h"
+#include "TextResourceDecoder.h"
 
 #include <wtf/Deque.h>
 #include <wtf/HashMap.h>
@@ -75,8 +76,8 @@ public:
         String textEncodingName() const { return m_textEncodingName; }
         void setTextEncodingName(const String& textEncodingName) { m_textEncodingName = textEncodingName; }
 
-        TextResourceDecoder* decoder() const { return m_decoder.get(); }
-        void createDecoder(const String& mimeType, const String& textEncodingName);
+        PassRefPtr<TextResourceDecoder> decoder() const { return m_decoder; }
+        void setDecoder(PassRefPtr<TextResourceDecoder> decoder) { m_decoder = decoder; }
 
         PassRefPtr<SharedBuffer> buffer() const { return m_buffer; }
         void setBuffer(PassRefPtr<SharedBuffer> buffer) { m_buffer = buffer; }

LayoutTests/http/tests/inspector/network/network-cyrillic-xhr-expected.txt
@@ -1,0 +1,13 @@
+CONSOLE MESSAGE: line 18: Done.
+Tests cyrillic xhr content is correctly loaded in inspector.
+
+Bug 79026
+http://127.0.0.1:8000/inspector/network/resources/cyrillic.html
+resource.content: The following line contains a word "success" written with cyrillic letters.
+SUССЕSS
+
+http://127.0.0.1:8000/inspector/network/resources/cyrillic.html
+resource.content: The following line contains a word "success" written with cyrillic letters.
+SUССЕSS
+
+

LayoutTests/http/tests/inspector/network/network-cyrillic-xhr.html
@@ -1,0 +1,72 @@
+<html>
+<head>
+<script src="../inspector-test.js"></script>
+<script src="../network-test.js"></script>
+<script>
+function loadData()
+{
+    doXHR("GET", "resources/cyrillic.html", false, xhrLoaded1);
+}
+
+function xhrLoaded1()
+{
+    doXHR("GET", "resources/cyrillic.html", true, allXHRsLoaded);
+}
+
+function allXHRsLoaded()
+{
+    console.log("Done.");
+}
+
+function test()
+{
+    // Since this test could be run together with other inspector backend cache
+    // tests, we need to reset size limits to default ones.
+    InspectorTest.resetInspectorResourcesData(step1);
+
+    function dumpResource(resource, callback)
+    {
+        if (!resource)
+            return callback();
+        InspectorTest.addResult(resource.url);
+
+        function contentLoaded()
+        {
+            InspectorTest.addResult("resource.content: " + resource.content);
+            callback();
+        }
+
+        resource.requestContent(contentLoaded);
+    }
+
+    function step1()
+    {
+        InspectorTest.addConsoleSniffer(step2);
+        InspectorTest.evaluateInPage("loadData()");
+    }
+
+    function step2()
+    {
+        var resource = WebInspector.panels.network.resources[WebInspector.panels.network.resources.length - 2];
+        dumpResource(resource, step3);
+    }
+
+    function step3()
+    {
+        var resource = WebInspector.panels.network.resources[WebInspector.panels.network.resources.length - 1];
+        dumpResource(resource, step4);
+    }
+
+    function step4()
+    {
+        InspectorTest.completeTest();
+    }
+}
+</script>
+</head>
+<body onload="runTest()">
+<p> Tests cyrillic xhr content is correctly loaded in inspector.</p>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=79026">Bug 79026</a>
+</body>
+</html>
+

LayoutTests/http/tests/inspector/network/network-empty-xhr-expected.txt
@@ -1,0 +1,9 @@
+CONSOLE MESSAGE: line 18: Done.
+Tests empty xhr content is correctly loaded in inspector.
+
+Bug 79026
+http://127.0.0.1:8000/inspector/network/resources/empty.html
+resource.content: 
+http://127.0.0.1:8000/inspector/network/resources/empty.html
+resource.content: 
+

LayoutTests/http/tests/inspector/network/network-empty-xhr.html
@@ -1,0 +1,72 @@
+<html>
+<head>
+<script src="../inspector-test.js"></script>
+<script src="../network-test.js"></script>
+<script>
+function loadData()
+{
+    doXHR("GET", "resources/empty.html", false, xhrLoaded1);
+}
+
+function xhrLoaded1()
+{
+    doXHR("GET", "resources/empty.html", true, allXHRsLoaded);
+}
+
+function allXHRsLoaded()
+{
+    console.log("Done.");
+}
+
+function test()
+{
+    // Since this test could be run together with other inspector backend cache
+    // tests, we need to reset size limits to default ones.
+    InspectorTest.resetInspectorResourcesData(step1);
+
+    function dumpResource(resource, callback)
+    {
+        if (!resource)
+            return callback();
+        InspectorTest.addResult(resource.url);
+
+        function contentLoaded()
+        {
+            InspectorTest.addResult("resource.content: " + resource.content);
+            callback();
+        }
+
+        resource.requestContent(contentLoaded);
+    }
+
+    function step1()
+    {
+        InspectorTest.addConsoleSniffer(step2);
+        InspectorTest.evaluateInPage("loadData()");
+    }
+
+    function step2()
+    {
+        var resource = WebInspector.panels.network.resources[WebInspector.panels.network.resources.length - 2];
+        dumpResource(resource, step3);
+    }
+
+    function step3()
+    {
+        var resource = WebInspector.panels.network.resources[WebInspector.panels.network.resources.length - 1];
+        dumpResource(resource, step4);
+    }
+
+    function step4()
+    {
+        InspectorTest.completeTest();
+    }
+}
+</script>
+</head>
+<body onload="runTest()">
+<p> Tests empty xhr content is correctly loaded in inspector.</p>
+<a href="https://bugs.webkit.org/show_bug.cgi?id=79026">Bug 79026</a>
+</body>
+</html>
+

LayoutTests/http/tests/inspector/network/resources/cyrillic.html
@@ -1,0 +1,2 @@
+The following line contains a word "success" written with cyrillic letters.
+SUССЕSS
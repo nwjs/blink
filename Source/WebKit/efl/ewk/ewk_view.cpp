nwjs/blink

commit c1a3088b92a9e6a404c5f94c646ef3dc0de1f557

[EFL] Implement the Web Audio API feature. https://bugs.webkit.org/show_bug.cgi?id=78688
Patch by Dongwoo Im <dw.im@samsung.com> on 2012-05-02
Reviewed by Philippe Normand.

Implement the Web Audio API feature on the EFL port.
https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html

.:

* Source/cmake/FindGStreamer-Audio.cmake: Added. Find the pkgconfig of the GStreamer-audio.
* Source/cmake/FindGStreamer-FFT.cmake: Added. Find the pkgconfig of the GStreamer-fft.
* Source/cmake/OptionsEfl.cmake: Add the ENABLE_WEB_AUDIO option.
* Source/cmakeconfig.h.cmake: Add the ENABLE_WEB_AUDIO option.

Source/WebCore:

* CMakeLists.txt: Add the list of the files which are needed for the Web Audio APi.
* PlatformEfl.cmake: Add the list of the files which are needed for the Web Audio APi.
* UseJSC.cmake: Add the list of the files which are needed for the Web Audio APi.
* platform/audio/HRTFElevation.cpp: Enable the USE_CONCATENATED_IMPULSE_RESPONSES macro.
(WebCore):
* platform/audio/efl/AudioBusEfl.cpp: Added.
(WebCore):
(WebCore::AudioBus::loadPlatformResource): Create the absolute path of the audio resource.

Source/WebKit/efl:

* ewk/ewk_view.cpp: Add the setter/getter of the webaudio feature.
(_Ewk_View_Private_Data):
(_ewk_view_priv_new):
(ewk_view_setting_web_audio_get): Enable/disable the Web Audio API feature.
(ewk_view_setting_web_audio_set): Query if the Web Audio API feature is enabled.
* ewk/ewk_view.h: Add the prototypes.

LayoutTests:

* platform/efl/Skipped: Change the comment of webaudio.

git-svn-id: svn://svn.chromium.org/blink/trunk@115800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebKit/efl/ewk/ewk_view.cpp
@@ -224,6 +224,9 @@ struct _Ewk_View_Private_Data {
         bool offlineAppCache : 1;
         bool pageCache : 1;
         bool enableXSSAuditor : 1;
+#if ENABLE(WEB_AUDIO)
+        bool webAudio : 0;
+#endif
         bool webGLEnabled : 1;
         bool tabsToLinks : 1;
         struct {
@@ -683,6 +686,9 @@ static Ewk_View_Private_Data* _ewk_view_priv_new(Ewk_View_Smart_Data* smartData)
     priv->pageSettings->setOfflineWebApplicationCacheEnabled(true);
     priv->pageSettings->setUsesPageCache(true);
     priv->pageSettings->setUsesEncodingDetector(false);
+#if ENABLE(WEB_AUDIO)
+    priv->pageSettings->setWebAudioEnabled(false);
+#endif
     priv->pageSettings->setWebGLEnabled(true);
     priv->pageSettings->setXSSAuditorEnabled(true);
 
@@ -741,6 +747,9 @@ static Ewk_View_Private_Data* _ewk_view_priv_new(Ewk_View_Smart_Data* smartData)
     priv->settings.tabsToLinks = true;
 
     priv->settings.userAgent = ewk_settings_default_user_agent_get();
+#if ENABLE(WEB_AUDIO)
+    priv->settings.webAudio = priv->pageSettings->webAudioEnabled();
+#endif
 
     // Since there's no scale separated from zooming in webkit-efl, this functionality of
     // viewport meta tag is implemented using zoom. When scale zoom is supported by webkit-efl,
@@ -4202,6 +4211,32 @@ WebCore::GraphicsContext3D* ewk_view_accelerated_compositing_context_get(Evas_Ob
     return 0;
 }
 #endif
+
+Eina_Bool ewk_view_setting_web_audio_get(const Evas_Object* ewkView)
+{
+#if ENABLE(WEB_AUDIO)
+    EWK_VIEW_SD_GET_OR_RETURN(ewkView, smartData, false);
+    EWK_VIEW_PRIV_GET_OR_RETURN(smartData, priv, false);
+    return priv->settings.webAudio;
+#else
+    return false;
+#endif
+}
+
+Eina_Bool ewk_view_setting_web_audio_set(Evas_Object* ewkView, Eina_Bool enable)
+{
+#if ENABLE(WEB_AUDIO)
+    EWK_VIEW_SD_GET_OR_RETURN(ewkView, smartData, false);
+    EWK_VIEW_PRIV_GET_OR_RETURN(smartData, priv, false);
+    if (priv->settings.webAudio != enable) {
+        priv->pageSettings->setWebAudioEnabled(enable);
+        priv->settings.webAudio = enable;
+    }
+    return true;
+#else
+    return false;
+#endif
+}
 
 namespace EWKPrivate {
 

LayoutTests/platform/efl/Skipped
@@ -710,7 +710,7 @@ svg/webarchive
 svg/custom/image-with-prefix-in-webarchive.svg
 webarchive
 
-# The EFL port has no support for webaudio
+# For now, Web Audio API is disabled
 webaudio
 
 # The EFL port has no support for loading a PDF as an image

Source/WebCore/CMakeLists.txt
@@ -123,6 +123,7 @@ SET(WebCore_IDL_FILES
     Modules/geolocation/PositionErrorCallback.idl
 
     Modules/webaudio/AudioBuffer.idl
+    Modules/webaudio/AudioBufferCallback.idl
     Modules/webaudio/AudioBufferSourceNode.idl
     Modules/webaudio/AudioChannelSplitter.idl
     Modules/webaudio/AudioChannelMerger.idl
@@ -136,11 +137,18 @@ SET(WebCore_IDL_FILES
     Modules/webaudio/AudioParam.idl
     Modules/webaudio/AudioProcessingEvent.idl
     Modules/webaudio/AudioSourceNode.idl
+    Modules/webaudio/BiquadFilterNode.idl
     Modules/webaudio/ConvolverNode.idl
     Modules/webaudio/DOMWindowWebAudio.idl
     Modules/webaudio/DelayNode.idl
+    Modules/webaudio/DynamicsCompressorNode.idl
     Modules/webaudio/JavaScriptAudioNode.idl
+    Modules/webaudio/MediaElementAudioSourceNode.idl
+    Modules/webaudio/OfflineAudioCompletionEvent.idl
+    Modules/webaudio/Oscillator.idl
     Modules/webaudio/RealtimeAnalyserNode.idl
+    Modules/webaudio/WaveShaperNode.idl
+    Modules/webaudio/WaveTable.idl
 
     Modules/websockets/CloseEvent.idl
     Modules/websockets/DOMWindowWebSocket.idl
@@ -2416,6 +2424,80 @@ IF (ENABLE_BATTERY_STATUS)
         Modules/battery/BatteryController.cpp
         Modules/battery/BatteryManager.cpp
         Modules/battery/BatteryStatus.cpp
+    )
+ENDIF ()
+
+IF (ENABLE_WEB_AUDIO)
+    LIST(APPEND WebCore_SOURCES
+        Modules/webaudio/AsyncAudioDecoder.cpp
+        Modules/webaudio/AudioBasicProcessorNode.cpp
+        Modules/webaudio/AudioBuffer.cpp
+        Modules/webaudio/AudioBufferSourceNode.cpp
+        Modules/webaudio/AudioChannelMerger.cpp
+        Modules/webaudio/AudioChannelSplitter.cpp
+        Modules/webaudio/AudioContext.cpp
+        Modules/webaudio/AudioDestinationNode.cpp
+        Modules/webaudio/AudioGainNode.cpp
+        Modules/webaudio/AudioListener.cpp
+        Modules/webaudio/AudioNode.cpp
+        Modules/webaudio/AudioNodeInput.cpp
+        Modules/webaudio/AudioNodeOutput.cpp
+        Modules/webaudio/AudioPannerNode.cpp
+        Modules/webaudio/AudioParam.cpp
+        Modules/webaudio/AudioParamTimeline.cpp
+        Modules/webaudio/AudioProcessingEvent.cpp
+        Modules/webaudio/BiquadDSPKernel.cpp
+        Modules/webaudio/BiquadFilterNode.cpp
+        Modules/webaudio/BiquadProcessor.cpp
+        Modules/webaudio/ConvolverNode.cpp
+        Modules/webaudio/DefaultAudioDestinationNode.cpp
+        Modules/webaudio/DelayDSPKernel.cpp
+        Modules/webaudio/DelayNode.cpp
+        Modules/webaudio/DelayProcessor.cpp
+        Modules/webaudio/DynamicsCompressorNode.cpp
+        Modules/webaudio/JavaScriptAudioNode.cpp
+        Modules/webaudio/MediaElementAudioSourceNode.cpp
+        Modules/webaudio/OfflineAudioCompletionEvent.cpp
+        Modules/webaudio/OfflineAudioDestinationNode.cpp
+        Modules/webaudio/Oscillator.cpp
+        Modules/webaudio/RealtimeAnalyser.cpp
+        Modules/webaudio/RealtimeAnalyserNode.cpp
+        Modules/webaudio/WaveShaperDSPKernel.cpp
+        Modules/webaudio/WaveShaperNode.cpp
+        Modules/webaudio/WaveShaperProcessor.cpp
+        Modules/webaudio/WaveTable.cpp
+
+        platform/audio/AudioBus.cpp
+        platform/audio/AudioChannel.cpp
+        platform/audio/AudioDSPKernelProcessor.cpp
+        platform/audio/AudioResampler.cpp
+        platform/audio/AudioResamplerKernel.cpp
+        platform/audio/AudioUtilities.cpp
+        platform/audio/Biquad.cpp
+        platform/audio/Cone.cpp
+        platform/audio/DirectConvolver.cpp
+        platform/audio/Distance.cpp
+        platform/audio/DynamicsCompressor.cpp
+        platform/audio/DynamicsCompressorKernel.cpp
+        platform/audio/EqualPowerPanner.cpp
+        platform/audio/FFTConvolver.cpp
+        platform/audio/FFTFrame.cpp
+        platform/audio/FFTFrameStub.cpp
+        platform/audio/HRTFDatabase.cpp
+        platform/audio/HRTFDatabaseLoader.cpp
+        platform/audio/HRTFElevation.cpp
+        platform/audio/HRTFKernel.cpp
+        platform/audio/HRTFPanner.cpp
+        platform/audio/MultiChannelResampler.cpp
+        platform/audio/Panner.cpp
+        platform/audio/ReverbAccumulationBuffer.cpp
+        platform/audio/ReverbConvolver.cpp
+        platform/audio/ReverbConvolverStage.cpp
+        platform/audio/Reverb.cpp
+        platform/audio/ReverbInputBuffer.cpp
+        platform/audio/SincResampler.cpp
+        platform/audio/VectorMath.cpp
+        platform/audio/ZeroPole.cpp
     )
 ENDIF ()
 

Source/WebCore/PlatformEfl.cmake
@@ -183,23 +183,6 @@ IF (WTF_USE_ICU_UNICODE)
   )
 ENDIF ()
 
-IF (ENABLE_VIDEO)
-  LIST(APPEND WebCore_INCLUDE_DIRECTORIES
-    "${WEBCORE_DIR}/platform/graphics/gstreamer"
-  )
-  LIST(APPEND WebCore_SOURCES
-    platform/graphics/gstreamer/GRefPtrGStreamer.cpp
-    platform/graphics/gstreamer/GStreamerGWorld.cpp
-    platform/graphics/gstreamer/GStreamerUtilities.cpp
-    platform/graphics/gstreamer/GStreamerVersioning.cpp
-    platform/graphics/gstreamer/ImageGStreamerCairo.cpp
-    platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
-    platform/graphics/gstreamer/PlatformVideoWindowEfl.cpp
-    platform/graphics/gstreamer/VideoSinkGStreamer.cpp
-    platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
-  )
-ENDIF ()
-
 LIST(APPEND WebCore_LIBRARIES
   ${Cairo_LIBRARIES}
   ${ECORE_X_LIBRARIES}
@@ -217,15 +200,6 @@ LIST(APPEND WebCore_LIBRARIES
   ${ZLIB_LIBRARIES}
 )
 
-IF (ENABLE_VIDEO)
-  LIST(APPEND WebCore_LIBRARIES
-    ${GStreamer-App_LIBRARIES}
-    ${GStreamer-Interfaces_LIBRARIES}
-    ${GStreamer-Pbutils_LIBRARIES}
-    ${GStreamer-Video_LIBRARIES}
-  )
-ENDIF ()
-
 LIST(APPEND WebCore_INCLUDE_DIRECTORIES
   ${Cairo_INCLUDE_DIRS}
   ${ECORE_X_INCLUDE_DIRS}
@@ -241,12 +215,40 @@ LIST(APPEND WebCore_INCLUDE_DIRECTORIES
   ${ZLIB_INCLUDE_DIRS}
 )
 
-IF (ENABLE_VIDEO)
+IF (ENABLE_VIDEO OR ENABLE_WEB_AUDIO)
   LIST(APPEND WebCore_INCLUDE_DIRECTORIES
+    "${WEBCORE_DIR}/platform/graphics/gstreamer"
+
     ${GStreamer-App_INCLUDE_DIRS}
     ${GStreamer-Interfaces_INCLUDE_DIRS}
     ${GStreamer-Pbutils_INCLUDE_DIRS}
+  )
+  LIST(APPEND WebCore_SOURCES
+    platform/graphics/gstreamer/GRefPtrGStreamer.cpp
+    platform/graphics/gstreamer/GStreamerUtilities.cpp
+    platform/graphics/gstreamer/GStreamerVersioning.cpp
+  )
+  LIST(APPEND WebCore_LIBRARIES
+    ${GStreamer-App_LIBRARIES}
+    ${GStreamer-Interfaces_LIBRARIES}
+    ${GStreamer-Pbutils_LIBRARIES}
+  )
+ENDIF ()
+
+IF (ENABLE_VIDEO)
+  LIST(APPEND WebCore_INCLUDE_DIRECTORIES
     ${GStreamer-Video_INCLUDE_DIRS}
+  )
+  LIST(APPEND WebCore_SOURCES
+    platform/graphics/gstreamer/GStreamerGWorld.cpp
+    platform/graphics/gstreamer/ImageGStreamerCairo.cpp
+    platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+    platform/graphics/gstreamer/PlatformVideoWindowEfl.cpp
+    platform/graphics/gstreamer/VideoSinkGStreamer.cpp
+    platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
+  )
+  LIST(APPEND WebCore_LIBRARIES
+    ${GStreamer-Video_LIBRARIES}
   )
 ENDIF ()
 
@@ -273,3 +275,28 @@ ENDIF ()
 
 ADD_DEFINITIONS(-DWTF_USE_CROSS_PLATFORM_CONTEXT_MENUS=1
                 -DDATA_DIR="${CMAKE_INSTALL_PREFIX}/${DATA_INSTALL_DIR}")
+
+IF (ENABLE_WEB_AUDIO)
+  LIST(APPEND WebCore_INCLUDE_DIRECTORIES
+    "${WEBCORE_DIR}/platform/audio/gstreamer"
+
+    ${GStreamer-Audio_INCLUDE_DIRS}
+    ${GStreamer-FFT_INCLUDE_DIRS}
+  )
+  LIST(APPEND WebCore_SOURCES
+    platform/audio/efl/AudioBusEfl.cpp
+    platform/audio/gstreamer/AudioDestinationGStreamer.cpp
+    platform/audio/gstreamer/AudioFileReaderGStreamer.cpp
+    platform/audio/gstreamer/FFTFrameGStreamer.cpp
+    platform/audio/gstreamer/WebKitWebAudioSourceGStreamer.cpp
+  )
+  LIST(APPEND WebCore_LIBRARIES
+    ${GStreamer-Audio_LIBRARIES}
+    ${GStreamer-FFT_LIBRARIES}
+  )
+  SET(WEB_AUDIO_DIR ${CMAKE_INSTALL_PREFIX}/${DATA_INSTALL_DIR}/webaudio/resources)
+  FILE(GLOB WEB_AUDIO_DATA "${WEBCORE_DIR}/platform/audio/resources/*.wav")
+  INSTALL(FILES ${WEB_AUDIO_DATA} DESTINATION ${WEB_AUDIO_DIR})
+  ADD_DEFINITIONS(-DUNINSTALLED_AUDIO_RESOURCES_DIR="${WEBCORE_DIR}/platform/audio/resources")
+ENDIF ()
+

Source/WebCore/UseJSC.cmake
@@ -275,6 +275,16 @@ IF (ENABLE_WEBGL)
     )
 ENDIF ()
 
+IF (ENABLE_WEB_AUDIO)
+    LIST(APPEND WebCore_SOURCES
+        bindings/js/JSAudioBufferSourceNodeCustom.cpp
+        bindings/js/JSAudioContextCustom.cpp
+        bindings/js/JSConvolverNodeCustom.cpp
+        bindings/js/JSJavaScriptAudioNodeCustom.cpp
+        bindings/js/JSWaveShaperNodeCustom.cpp
+    )
+ENDIF ()
+
 LIST(APPEND SCRIPTS_BINDINGS
     ${WEBCORE_DIR}/bindings/scripts/CodeGenerator.pm
 )

Source/WebCore/platform/audio/HRTFElevation.cpp
@@ -60,7 +60,7 @@ const size_t ResponseFrameSize = 256;
 // The impulse responses may be resampled to a different sample-rate (depending on the audio hardware) when they are loaded.
 const float ResponseSampleRate = 44100;
 
-#if PLATFORM(GTK) || PLATFORM(MAC)
+#if PLATFORM(GTK) || PLATFORM(MAC) || PLATFORM(EFL)
 #define USE_CONCATENATED_IMPULSE_RESPONSES
 #endif
 

Source/WebCore/platform/audio/efl/AudioBusEfl.cpp
@@ -1,0 +1,42 @@
+/*
+    Copyright (C) 2012 Samsung Electronics
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License 2.1 as published by the Free Software Foundation.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "config.h"
+
+#if ENABLE(WEB_AUDIO)
+#include "AudioBus.h"
+
+#include "AudioFileReader.h"
+#include "FileSystem.h"
+#include <wtf/text/CString.h>
+#include <wtf/text/StringConcatenate.h>
+
+namespace WebCore {
+
+PassOwnPtr<AudioBus> AudioBus::loadPlatformResource(const char* name, float sampleRate)
+{
+    String absoluteFilename(makeString(DATA_DIR, "/webaudio/resources/", name, ".wav"));
+    if (!fileExists(absoluteFilename))
+        absoluteFilename = makeString(UNINSTALLED_AUDIO_RESOURCES_DIR, "/", name, ".wav");
+
+    return createBusFromAudioFile(absoluteFilename.utf8().data(), false, sampleRate);
+}
+
+} // namespace WebCore
+
+#endif // ENABLE(WEB_AUDIO)

Source/WebKit/efl/ewk/ewk_view.h
@@ -1,6 +1,6 @@
 /*
     Copyright (C) 2009-2010 ProFUSION embedded systems
-    Copyright (C) 2009-2010 Samsung Electronics
+    Copyright (C) 2009-2012 Samsung Electronics
     Copyright (C) 2012 Intel Corporation
 
     This library is free software; you can redistribute it and/or
@@ -2559,6 +2559,27 @@ EAPI Eina_Bool ewk_view_setting_should_display_text_descriptions_get(const Evas_
  * @c EINA_FALSE to disable.
  */
 EAPI void ewk_view_setting_should_display_text_descriptions_set(Evas_Object *o, Eina_Bool enable);
+
+/**
+ * Queries if the web audio feature of HTML5 is enabled.
+ *
+ * @param o view object to query if the web audio feature is enabled
+ *
+ * @return @c EINA_TRUE if web audio is enabled,
+ *         @c EINA_FALSE if not or on failure
+ */
+EAPI Eina_Bool    ewk_view_setting_web_audio_get(const Evas_Object *o);
+
+/**
+ * Enables/disables the web audio feature of HTML5.
+ *
+ * @param o view object to set the web audio
+ * @param enable @c EINA_TRUE to enable the web audio feature,
+ *        @c EINA_FALSE to disable
+ *
+ * @return @c EINA_TRUE on success or @c EINA_FALSE on failure
+ */
+EAPI Eina_Bool    ewk_view_setting_web_audio_set(Evas_Object *o, Eina_Bool enable);
 
 #ifdef __cplusplus
 }

commit 216c920c1980e0441ccaaf5ea2d752cb24338ba7

[EFL] Fix typo introduced in r115800. https://bugs.webkit.org/show_bug.cgi?id=85354
Patch by Dongwoo Im <dw.im@samsung.com> on 2012-05-02
Reviewed by Nikolas Zimmermann.

* ewk/ewk_view.cpp:
(_Ewk_View_Private_Data): Assign 1 bit to the boolean attribute, webAudio.

git-svn-id: svn://svn.chromium.org/blink/trunk@115809 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebKit/efl/ewk/ewk_view.cpp
@@ -225,7 +225,7 @@ struct _Ewk_View_Private_Data {
         bool pageCache : 1;
         bool enableXSSAuditor : 1;
 #if ENABLE(WEB_AUDIO)
-        bool webAudio : 0;
+        bool webAudio : 1;
 #endif
         bool webGLEnabled : 1;
         bool tabsToLinks : 1;
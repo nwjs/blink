nwjs/blink

commit 39b2585ffe744d0bb2ddb672ca299cb41e4a1169

Source/WebCore: Fix for a crasher when generating MHTML for a page containing SVG images. https://bugs.webkit.org/show_bug.cgi?id=99105
Reviewed by Adam Barth.

* page/PageSerializer.cpp:
(WebCore::PageSerializer::serializeCSSStyleSheet):
(WebCore::PageSerializer::addImageToResources):

Source/WebKit/chromium: Test for a fix for a crasher when generating MHTML for a page containing SVG images.
https://bugs.webkit.org/show_bug.cgi?id=99105

Reviewed by Adam Barth.

* page/PageSerializer.cpp:
(WebCore::PageSerializer::serializeCSSStyleSheet):
(WebCore::PageSerializer::addImageToResources):


git-svn-id: svn://svn.chromium.org/blink/trunk@131125 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/page/PageSerializer.cpp
@@ -267,7 +267,7 @@ void PageSerializer::serializeCSSStyleSheet(CSSStyleSheet* styleSheet, const KUR
         Document* document = styleSheet->ownerDocument();
         // Some rules have resources associated with them that we need to retrieve.
         if (rule->isImportRule()) {
-            CSSImportRule* importRule = static_cast<CSSImportRule*>(rule);            
+            CSSImportRule* importRule = static_cast<CSSImportRule*>(rule);
             KURL importURL = document->completeURL(importRule->href());
             if (m_resourceURLs.contains(importURL))
                 continue;
@@ -298,8 +298,14 @@ void PageSerializer::addImageToResources(CachedImage* image, RenderObject* image
     if (!image || image->image() == Image::nullImage())
         return;
 
+    RefPtr<SharedBuffer> data = imageRenderer ? image->imageForRenderer(imageRenderer)->data() : image->image()->data();
+    if (!data) {
+        // SVG images don't return data at this point. Bug 99102.
+        LOG_ERROR("No data for image %s", url.utf8String().data());
+        return;
+    }
     String mimeType = image->response().mimeType();
-    m_resources->append(Resource(url, mimeType, imageRenderer ? image->imageForRenderer(imageRenderer)->data() : image->image()->data()));
+    m_resources->append(Resource(url, mimeType, data));
     m_resourceURLs.add(url);
 }
 

Source/WebKit/chromium/tests/WebPageNewSerializerTest.cpp
@@ -88,6 +88,7 @@ public:
         , m_xhtmlMimeType(WebString::fromUTF8("application/xhtml+xml"))
         , m_cssMimeType(WebString::fromUTF8("text/css"))
         , m_pngMimeType(WebString::fromUTF8("image/png"))
+        , m_svgMimeType(WebString::fromUTF8("image/svg+xml"))
     {
     }
 
@@ -148,6 +149,7 @@ protected:
     const WebString& xhtmlMimeType() const { return m_xhtmlMimeType; }
     const WebString& cssMimeType() const { return m_cssMimeType; }
     const WebString& pngMimeType() const { return m_pngMimeType; }
+    const WebString& svgMimeType() const { return m_svgMimeType; }
 
     static bool resourceVectorContains(const WebVector<WebPageSerializer::Resource>& resources, const char* url, const char* mimeType)
     {
@@ -167,6 +169,7 @@ private:
     WebString m_xhtmlMimeType;
     WebString m_cssMimeType;
     WebString m_pngMimeType;
+    WebString m_svgMimeType;
     TestWebFrameClient m_webFrameClient;
 };
 
@@ -327,6 +330,22 @@ TEST_F(WebPageNewSerializeTest, FAILS_TestMHTMLEncoding)
         }
     }
     EXPECT_EQ(12, sectionCheckedCount);
+}
+
+// Test that we don't regress https://bugs.webkit.org/show_bug.cgi?id=99105
+TEST_F(WebPageNewSerializeTest, SVGImageDontCrash)
+{
+    WebURL pageUrl = toKURL("http://www.test.com");
+    WebURL imageUrl = toKURL("http://www.test.com/green_rectangle.svg");
+
+    registerMockedURLLoad(pageUrl, WebString::fromUTF8("page_with_svg_image.html"), WebString::fromUTF8("pageserializer/"), htmlMimeType());
+    registerMockedURLLoad(imageUrl, WebString::fromUTF8("green_rectangle.svg"), WebString::fromUTF8("pageserializer/"), svgMimeType());
+
+    loadURLInTopFrame(pageUrl);
+
+    WebCString mhtml = WebPageSerializer::serializeToMHTML(m_webView);
+    // We expect some data to be generated.
+    EXPECT_GT(mhtml.length(), 50U);
 }
 
 }

Source/WebKit/chromium/tests/data/pageserializer/green_rectangle.svg
@@ -1,0 +1,11 @@
+<?xml version="1.0"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1"
+      width="120" height="120" viewBox="0 0 236 120">
+  <rect x="14" y="23" width="250" height="50" fill="green"
+      stroke="black" stroke-width="1" />
+  <rect x="14" y="23" width="250" height="50" fill="green"
+      stroke="black" stroke-width="1" />
+</svg>

Source/WebKit/chromium/tests/data/pageserializer/page_with_svg_image.html
@@ -1,0 +1,6 @@
+<html>
+<body>
+SVG to the max!<br>
+<img src="green_rectangle.svg"/>
+</body>
+</html>

commit 0b0d99a5c2763f616b56966808ff6513367be038

Attempt to fix build broken by r131125.
* page/PageSerializer.cpp:
(WebCore::PageSerializer::addImageToResources):
KURL::utf8String() is apparently a USE(GOOGLEURL) feature. Call url.string().utf8() instead.

git-svn-id: svn://svn.chromium.org/blink/trunk@131135 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/page/PageSerializer.cpp
@@ -301,7 +301,7 @@ void PageSerializer::addImageToResources(CachedImage* image, RenderObject* image
     RefPtr<SharedBuffer> data = imageRenderer ? image->imageForRenderer(imageRenderer)->data() : image->image()->data();
     if (!data) {
         // SVG images don't return data at this point. Bug 99102.
-        LOG_ERROR("No data for image %s", url.utf8String().data());
+        LOG_ERROR("No data for image %s", url.string().utf8().data());
         return;
     }
     String mimeType = image->response().mimeType();
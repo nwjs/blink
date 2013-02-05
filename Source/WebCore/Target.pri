nwjs/blink

commit 26c3b091c4eacde133cde4e8becc4fff76eea020

Coordinated Graphics : Move CoordinatedGraphics related files to WebCore https://bugs.webkit.org/show_bug.cgi?id=108149
Patch by Jae Hyun Park <jae.park@company100.net> on 2013-01-31
Reviewed by Noam Rosenthal.

This patch moves Coordinated Graphics related code to WebCore. To
implement Threaded Coordinated Graphics, most of Coordianted Graphics
code should be shared. Therefore, they should reside in WebCore instead of
WebKit2.

When moving to WebCore, two renamings have been done.

1. Rename LayerTreeRenderer to CoordinatedGraphicsScene.

2. Rename WebCustomFilterProgram and WebCustomFilterOperation to
CoordinatedCustomFilterProgram and CoordinatedCustomFilterOperation,
respectively.

No new tests, covered by existing tests.

Source/WebCore:

* CMakeLists.txt:
* Target.pri:
* platform/graphics/texmap/GraphicsLayerTextureMapper.h:
* platform/graphics/texmap/coordinated/AreaAllocator.cpp: Renamed from Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/AreaAllocator.cpp.
* platform/graphics/texmap/coordinated/AreaAllocator.h: Renamed from Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/AreaAllocator.h.
* platform/graphics/texmap/coordinated/CoordinatedBackingStore.cpp: Renamed from Source/WebKit2/UIProcess/CoordinatedGraphics/CoordinatedBackingStore.cpp.
* platform/graphics/texmap/coordinated/CoordinatedBackingStore.h: Renamed from Source/WebKit2/UIProcess/CoordinatedGraphics/CoordinatedBackingStore.h.
* platform/graphics/texmap/coordinated/CoordinatedCustomFilterOperation.h: Renamed from Source/WebKit2/Shared/CoordinatedGraphics/WebCustomFilterOperation.h.
* platform/graphics/texmap/coordinated/CoordinatedCustomFilterProgram.h: Renamed from Source/WebKit2/Shared/CoordinatedGraphics/WebCustomFilterProgram.h.
* platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp: Renamed from Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp.

Source/WebCore/Target.pri
@@ -2241,6 +2241,18 @@ HEADERS += \
     platform/graphics/surfaces/GraphicsSurface.h \
     platform/graphics/surfaces/GraphicsSurfaceToken.h \
     platform/graphics/SurrogatePairAwareTextIterator.h \
+    platform/graphics/texmap/coordinated/AreaAllocator.h \
+    platform/graphics/texmap/coordinated/CoordinatedBackingStore.h \
+    platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.h \
+    platform/graphics/texmap/coordinated/CoordinatedGraphicsScene.h \
+    platform/graphics/texmap/coordinated/CoordinatedImageBacking.h \
+    platform/graphics/texmap/coordinated/CoordinatedLayerInfo.h \
+    platform/graphics/texmap/coordinated/CoordinatedSurface.h \
+    platform/graphics/texmap/coordinated/CoordinatedTile.h \
+    platform/graphics/texmap/coordinated/SurfaceUpdateInfo.h \
+    platform/graphics/texmap/coordinated/UpdateAtlas.h \
+    platform/graphics/texmap/coordinated/CoordinatedCustomFilterOperation.h \
+    platform/graphics/texmap/coordinated/CoordinatedCustomFilterProgram.h \
     platform/graphics/texmap/GraphicsLayerTextureMapper.h \
     platform/graphics/texmap/TextureMapper.h \
     platform/graphics/texmap/TextureMapperBackingStore.h \
@@ -2876,6 +2888,14 @@ SOURCES += \
     platform/graphics/qt/PathQt.cpp \
     platform/graphics/qt/PatternQt.cpp \
     platform/graphics/qt/StillImageQt.cpp \
+    platform/graphics/texmap/coordinated/AreaAllocator.cpp \
+    platform/graphics/texmap/coordinated/CoordinatedBackingStore.cpp \
+    platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp \
+    platform/graphics/texmap/coordinated/CoordinatedGraphicsScene.cpp \
+    platform/graphics/texmap/coordinated/CoordinatedImageBacking.cpp \
+    platform/graphics/texmap/coordinated/CoordinatedSurface.cpp \
+    platform/graphics/texmap/coordinated/CoordinatedTile.cpp \
+    platform/graphics/texmap/coordinated/UpdateAtlas.cpp \
     platform/graphics/texmap/GraphicsLayerTextureMapper.cpp \
     platform/graphics/texmap/TextureMapper.cpp \
     platform/graphics/texmap/TextureMapperBackingStore.cpp \

Source/WebKit2/Target.pri
@@ -135,11 +135,7 @@ HEADERS += \
     Shared/WebURLResponse.h \
     Shared/WebUserContentURLPattern.h \
     Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.h \
-    Shared/CoordinatedGraphics/CoordinatedLayerInfo.h \
-    Shared/CoordinatedGraphics/CoordinatedSurface.h \
-    Shared/CoordinatedGraphics/SurfaceUpdateInfo.h \
     Shared/CoordinatedGraphics/WebCoordinatedSurface.h \
-    Shared/CoordinatedGraphics/WebCustomFilterProgram.h \
     Shared/Plugins/Netscape/NetscapePluginModule.h \
     Shared/Plugins/NPRemoteObjectMap.h \
     Shared/Plugins/NPIdentifierData.h \
@@ -210,9 +206,7 @@ HEADERS += \
     UIProcess/Authentication/WebProtectionSpace.h \
     UIProcess/BackingStore.h \
     UIProcess/Downloads/DownloadProxy.h \
-    UIProcess/CoordinatedGraphics/CoordinatedBackingStore.h \
     UIProcess/CoordinatedGraphics/CoordinatedLayerTreeHostProxy.h \
-    UIProcess/CoordinatedGraphics/LayerTreeRenderer.h \
     UIProcess/DefaultUndoController.h \
     UIProcess/DrawingAreaProxy.h \
     UIProcess/DrawingAreaProxyImpl.h \
@@ -363,12 +357,7 @@ HEADERS += \
     WebProcess/WebPage/DrawingAreaImpl.h \
     WebProcess/WebPage/EventDispatcher.h \
     WebProcess/WebPage/FindController.h \
-    WebProcess/WebPage/CoordinatedGraphics/AreaAllocator.h \
-    WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.h \
-    WebProcess/WebPage/CoordinatedGraphics/CoordinatedImageBacking.h \
     WebProcess/WebPage/CoordinatedGraphics/CoordinatedLayerTreeHost.h \
-    WebProcess/WebPage/CoordinatedGraphics/CoordinatedTile.h \
-    WebProcess/WebPage/CoordinatedGraphics/UpdateAtlas.h \
     WebProcess/WebPage/TapHighlightController.h \
     WebProcess/WebPage/PageOverlay.h \
     WebProcess/WebPage/WebContextMenu.h \
@@ -503,7 +492,6 @@ SOURCES += \
     Shared/WebURLResponse.cpp \
     Shared/WebWheelEvent.cpp \
     Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.cpp \
-    Shared/CoordinatedGraphics/CoordinatedSurface.cpp \
     Shared/CoordinatedGraphics/WebCoordinatedSurface.cpp \
     Shared/CoordinatedGraphics/WebCustomFilterProgramProxy.cpp \
     Shared/qt/ArgumentCodersQt.cpp \
@@ -565,9 +553,7 @@ SOURCES += \
     UIProcess/Authentication/WebProtectionSpace.cpp \
     UIProcess/BackingStore.cpp \
     UIProcess/qt/BackingStoreQt.cpp \
-    UIProcess/CoordinatedGraphics/CoordinatedBackingStore.cpp \
     UIProcess/CoordinatedGraphics/CoordinatedLayerTreeHostProxy.cpp \
-    UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp \
     UIProcess/DefaultUndoController.cpp \
     UIProcess/Downloads/DownloadProxy.cpp \
     UIProcess/Downloads/DownloadProxyMap.cpp \
@@ -744,12 +730,7 @@ SOURCES += \
     WebProcess/WebPage/EncoderAdapter.cpp \
     WebProcess/WebPage/EventDispatcher.cpp \
     WebProcess/WebPage/FindController.cpp \
-    WebProcess/WebPage/CoordinatedGraphics/AreaAllocator.cpp \
-    WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp \
-    WebProcess/WebPage/CoordinatedGraphics/CoordinatedImageBacking.cpp \
     WebProcess/WebPage/CoordinatedGraphics/CoordinatedLayerTreeHost.cpp \
-    WebProcess/WebPage/CoordinatedGraphics/CoordinatedTile.cpp \
-    WebProcess/WebPage/CoordinatedGraphics/UpdateAtlas.cpp \
     WebProcess/WebPage/TapHighlightController.cpp \
     WebProcess/WebPage/LayerTreeHost.cpp \
     WebProcess/WebPage/PageOverlay.cpp \

Source/WebCore/CMakeLists.txt
@@ -55,6 +55,8 @@ set(WebCore_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/platform/graphics/harfbuzz"
     "${WEBCORE_DIR}/platform/graphics/harfbuzz/ng"
     "${WEBCORE_DIR}/platform/graphics/opentype"
+    "${WEBCORE_DIR}/platform/graphics/texmap"
+    "${WEBCORE_DIR}/platform/graphics/texmap/coordinated"
     "${WEBCORE_DIR}/platform/graphics/transforms"
     "${WEBCORE_DIR}/platform/image-decoders"
     "${WEBCORE_DIR}/platform/image-decoders/bmp"
@@ -1988,6 +1990,14 @@ set(WebCore_SOURCES
     platform/graphics/filters/SourceGraphic.cpp
     platform/graphics/filters/SpotLightSource.cpp
 
+    platform/graphics/texmap/coordinated/AreaAllocator.cpp
+    platform/graphics/texmap/coordinated/CoordinatedBackingStore.cpp
+    platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp
+    platform/graphics/texmap/coordinated/CoordinatedGraphicsScene.cpp
+    platform/graphics/texmap/coordinated/CoordinatedImageBacking.cpp
+    platform/graphics/texmap/coordinated/CoordinatedSurface.cpp
+    platform/graphics/texmap/coordinated/CoordinatedTile.cpp
+    platform/graphics/texmap/coordinated/UpdateAtlas.cpp
     platform/graphics/texmap/TextureMapper.cpp
     platform/graphics/texmap/TextureMapperBackingStore.cpp
     platform/graphics/texmap/TextureMapperImageBuffer.cpp

Source/WebCore/platform/graphics/texmap/GraphicsLayerTextureMapper.h
@@ -84,7 +84,7 @@ public:
     virtual bool setFilters(const FilterOperations&);
 #endif
 
-    // FIXME: It will be removed after removing dependency of LayerTreeRenderer on GraphicsLayerTextureMapper.
+    // FIXME: It will be removed after removing dependency of CoordinatedGraphicsScene on GraphicsLayerTextureMapper.
     void setHasOwnBackingStore(bool b) { m_hasOwnBackingStore = b; }
     void setBackingStore(PassRefPtr<TextureMapperBackingStore>);
 

Source/WebCore/platform/graphics/texmap/coordinated/AreaAllocator.cpp
@@ -23,9 +23,9 @@
 
 #include "AreaAllocator.h"
 
-namespace WebKit {
+namespace WebCore {
 
-AreaAllocator::AreaAllocator(const WebCore::IntSize& size)
+AreaAllocator::AreaAllocator(const IntSize& size)
     : m_size(size)
     , m_minAlloc(1, 1)
     , m_margin(0, 0)
@@ -36,17 +36,17 @@ AreaAllocator::~AreaAllocator()
 {
 }
 
-void AreaAllocator::expand(const WebCore::IntSize& size)
+void AreaAllocator::expand(const IntSize& size)
 {
     m_size = m_size.expandedTo(size);
 }
 
-void AreaAllocator::expandBy(const WebCore::IntSize& size)
+void AreaAllocator::expandBy(const IntSize& size)
 {
     m_size += size;
 }
 
-void AreaAllocator::release(const WebCore::IntRect&)
+void AreaAllocator::release(const IntRect&)
 {
 }
 
@@ -55,7 +55,7 @@ int AreaAllocator::overhead() const
     return 0;
 }
 
-WebCore::IntSize AreaAllocator::roundAllocation(const WebCore::IntSize& size) const
+IntSize AreaAllocator::roundAllocation(const IntSize& size) const
 {
     int width = size.width() + m_margin.width();
     int height = size.height() + m_margin.height();
@@ -66,20 +66,20 @@ WebCore::IntSize AreaAllocator::roundAllocation(const WebCore::IntSize& size) co
     if (extra)
         height += m_minAlloc.height() - extra;
 
-    return WebCore::IntSize(width, height);
+    return IntSize(width, height);
 }
 
-GeneralAreaAllocator::GeneralAreaAllocator(const WebCore::IntSize& size)
-    : AreaAllocator(WebCore::nextPowerOfTwo(size))
+GeneralAreaAllocator::GeneralAreaAllocator(const IntSize& size)
+    : AreaAllocator(nextPowerOfTwo(size))
 {
     m_root = new Node();
-    m_root->rect = WebCore::IntRect(0, 0, m_size.width(), m_size.height());
+    m_root->rect = IntRect(0, 0, m_size.width(), m_size.height());
     m_root->largestFree = m_size;
     m_root->parent = 0;
     m_root->left = 0;
     m_root->right = 0;
     m_nodeCount = 1;
-    setMinimumAllocation(WebCore::IntSize(8, 8));
+    setMinimumAllocation(IntSize(8, 8));
 }
 
 GeneralAreaAllocator::~GeneralAreaAllocator()
@@ -96,16 +96,16 @@ void GeneralAreaAllocator::freeNode(Node* node)
     delete node;
 }
 
-void GeneralAreaAllocator::expand(const WebCore::IntSize& size)
+void GeneralAreaAllocator::expand(const IntSize& size)
 {
-    AreaAllocator::expand(WebCore::nextPowerOfTwo(size));
+    AreaAllocator::expand(nextPowerOfTwo(size));
 
     if (m_root->rect.size() == m_size)
         return; // No change.
 
     if (!m_root->left && m_root->largestFree.width() > 0) {
         // No allocations have occurred, so just adjust the root size.
-        m_root->rect = WebCore::IntRect(0, 0, m_size.width(), m_size.height());
+        m_root->rect = IntRect(0, 0, m_size.width(), m_size.height());
         m_root->largestFree = m_size;
         return;
     }
@@ -136,14 +136,14 @@ void GeneralAreaAllocator::expand(const WebCore::IntSize& size)
         right->right = 0;
         right->largestFree = m_root->rect.size();
         if (split == SplitOnX) {
-            parent->rect = WebCore::IntRect(m_root->rect.x(), m_root->rect.y(),
+            parent->rect = IntRect(m_root->rect.x(), m_root->rect.y(),
                 m_root->rect.width() * 2, m_root->rect.height());
-            right->rect = WebCore::IntRect(m_root->rect.x() + m_root->rect.width(), m_root->rect.y(),
+            right->rect = IntRect(m_root->rect.x() + m_root->rect.width(), m_root->rect.y(),
                 m_root->rect.width(), m_root->rect.height());
         } else {
-            parent->rect = WebCore::IntRect(m_root->rect.x(), m_root->rect.y(),
+            parent->rect = IntRect(m_root->rect.x(), m_root->rect.y(),
                 m_root->rect.width(), m_root->rect.height() * 2);
-            right->rect = WebCore::IntRect(m_root->rect.x(), m_root->rect.y() + m_root->rect.width(),
+            right->rect = IntRect(m_root->rect.x(), m_root->rect.y() + m_root->rect.width(),
                 m_root->rect.width(), m_root->rect.height());
         }
         split = (split == SplitOnX ? SplitOnY : SplitOnX);
@@ -152,26 +152,26 @@ void GeneralAreaAllocator::expand(const WebCore::IntSize& size)
     updateLargestFree(oldRoot);
 }
 
-static inline bool fitsWithin(const WebCore::IntSize& size1, const WebCore::IntSize& size2)
+static inline bool fitsWithin(const IntSize& size1, const IntSize& size2)
 {
     return size1.width() <= size2.width() && size1.height() <= size2.height();
 }
 
-WebCore::IntRect GeneralAreaAllocator::allocate(const WebCore::IntSize& size)
+IntRect GeneralAreaAllocator::allocate(const IntSize& size)
 {
-    WebCore::IntSize rounded = roundAllocation(size);
-    rounded = WebCore::nextPowerOfTwo(rounded);
+    IntSize rounded = roundAllocation(size);
+    rounded = nextPowerOfTwo(rounded);
     if (rounded.width() <= 0 || rounded.width() > m_size.width()
         || rounded.height() <= 0 || rounded.height() > m_size.height())
-        return WebCore::IntRect();
+        return IntRect();
 
-    WebCore::IntPoint point = allocateFromNode(rounded, m_root);
+    IntPoint point = allocateFromNode(rounded, m_root);
     if (point.x() >= 0)
-        return WebCore::IntRect(point, size);
-    return WebCore::IntRect();
+        return IntRect(point, size);
+    return IntRect();
 }
 
-WebCore::IntPoint GeneralAreaAllocator::allocateFromNode(const WebCore::IntSize& size, Node* node)
+IntPoint GeneralAreaAllocator::allocateFromNode(const IntSize& size, Node* node)
 {
     // Find the best node to insert into, which should be
     // a node with the least amount of unused space that is
@@ -187,7 +187,7 @@ WebCore::IntPoint GeneralAreaAllocator::allocateFromNode(const WebCore::IntSize&
                     || left->largestFree.height() < right->largestFree.height()) {
                     // The largestFree values may be a little oversized,
                     // so try the left sub-tree and then the right sub-tree.
-                    WebCore::IntPoint point = allocateFromNode(size, left);
+                    IntPoint point = allocateFromNode(size, left);
                     if (point.x() >= 0)
                         return point;
                     return allocateFromNode(size, right);
@@ -199,11 +199,11 @@ WebCore::IntPoint GeneralAreaAllocator::allocateFromNode(const WebCore::IntSize&
             node = right;
         else if (left || right) {
             // Neither sub-node has enough space to allocate from.
-            return WebCore::IntPoint(-1, -1);
+            return IntPoint(-1, -1);
         } else if (fitsWithin(size, node->largestFree)) {
             // Do we need to split this node into smaller pieces?
             Split split;
-            if (fitsWithin(WebCore::IntSize(size.width() * 2, size.height() * 2), node->largestFree)) {
+            if (fitsWithin(IntSize(size.width() * 2, size.height() * 2), node->largestFree)) {
                 // Split in either direction: choose the inverse of
                 // the parent node's split direction to try to balance
                 // out the wasted space as further subdivisions happen.
@@ -216,15 +216,15 @@ WebCore::IntPoint GeneralAreaAllocator::allocateFromNode(const WebCore::IntSize&
                     split = SplitOnX;
                 else
                     split = SplitOnY;
-            } else if (fitsWithin(WebCore::IntSize(size.width() * 2, size.height()), node->largestFree)) {
+            } else if (fitsWithin(IntSize(size.width() * 2, size.height()), node->largestFree)) {
                 // Split along the X direction.
                 split = SplitOnX;
-            } else if (fitsWithin(WebCore::IntSize(size.width(), size.height() * 2), node->largestFree)) {
+            } else if (fitsWithin(IntSize(size.width(), size.height() * 2), node->largestFree)) {
                 // Split along the Y direction.
                 split = SplitOnY;
             } else {
                 // Cannot split further - allocate this node.
-                node->largestFree = WebCore::IntSize(0, 0);
+                node->largestFree = IntSize(0, 0);
                 updateLargestFree(node);
                 return node->rect.location();
             }
@@ -236,7 +236,7 @@ WebCore::IntPoint GeneralAreaAllocator::allocateFromNode(const WebCore::IntSize&
             break;
         }
     }
-    return WebCore::IntPoint(-1, -1);
+    return IntPoint(-1, -1);
 }
 
 GeneralAreaAllocator::Node* GeneralAreaAllocator::splitNode
@@ -255,14 +255,14 @@ GeneralAreaAllocator::Node* GeneralAreaAllocator::splitNode
     node->right = right;
 
     if (split == SplitOnX) {
-        left->rect = WebCore::IntRect(node->rect.x(), node->rect.y(),
+        left->rect = IntRect(node->rect.x(), node->rect.y(),
             node->rect.width() / 2, node->rect.height());
-        right->rect = WebCore::IntRect(left->rect.maxX(), node->rect.y(),
+        right->rect = IntRect(left->rect.maxX(), node->rect.y(),
             node->rect.width() / 2, node->rect.height());
     } else {
-        left->rect = WebCore::IntRect(node->rect.x(), node->rect.y(),
+        left->rect = IntRect(node->rect.x(), node->rect.y(),
             node->rect.width(), node->rect.height() / 2);
-        right->rect = WebCore::IntRect(node->rect.x(), left->rect.maxY(),
+        right->rect = IntRect(node->rect.x(), left->rect.maxY(),
             node->rect.width(), node->rect.height() / 2);
     }
 
@@ -275,18 +275,18 @@ GeneralAreaAllocator::Node* GeneralAreaAllocator::splitNode
 void GeneralAreaAllocator::updateLargestFree(Node* node)
 {
     while ((node = node->parent)) {
-        node->largestFree = WebCore::IntSize(
+        node->largestFree = IntSize(
             std::max(node->left->largestFree.width(), node->right->largestFree.width()),
             std::max(node->left->largestFree.height(), node->right->largestFree.height())
             );
     }
 }
 
-void GeneralAreaAllocator::release(const WebCore::IntRect& rect)
+void GeneralAreaAllocator::release(const IntRect& rect)
 {
     // Locate the node that contains the allocated region.
     Node* node = m_root;
-    WebCore::IntPoint point = rect.location();
+    IntPoint point = rect.location();
     while (node) {
         if (node->left && node->left->rect.contains(point))
             node = node->left;
@@ -330,6 +330,6 @@ int GeneralAreaAllocator::overhead() const
     return m_nodeCount * sizeof(Node);
 }
 
-} // namespace
+} // namespace WebCore
 
 #endif // USE(COORDINATED_GRAPHICS)

Source/WebCore/platform/graphics/texmap/coordinated/AreaAllocator.h
@@ -20,13 +20,14 @@
 #ifndef AreaAllocator_h
 #define AreaAllocator_h
 
-#include <IntPoint.h>
-#include <IntRect.h>
-#include <IntSize.h>
+#include "IntPoint.h"
+#include "IntRect.h"
+#include "IntSize.h"
 
 #if USE(COORDINATED_GRAPHICS)
 
 namespace WebCore {
+
 inline int nextPowerOfTwo(int number)
 {
     // This is a fast trick to get nextPowerOfTwo for an integer.
@@ -44,55 +45,52 @@ inline IntSize nextPowerOfTwo(const IntSize& size)
 {
     return IntSize(nextPowerOfTwo(size.width()), nextPowerOfTwo(size.height()));
 }
-} // namespace WebCore
-
-namespace WebKit {
 
 class AreaAllocator {
 public:
-    explicit AreaAllocator(const WebCore::IntSize&);
+    explicit AreaAllocator(const IntSize&);
     virtual ~AreaAllocator();
 
-    WebCore::IntSize size() const { return m_size; }
+    IntSize size() const { return m_size; }
 
-    WebCore::IntSize minimumAllocation() const { return m_minAlloc; }
-    void setMinimumAllocation(const WebCore::IntSize& size) { m_minAlloc = size; }
+    IntSize minimumAllocation() const { return m_minAlloc; }
+    void setMinimumAllocation(const IntSize& size) { m_minAlloc = size; }
 
-    WebCore::IntSize margin() const { return m_margin; }
-    void setMargin(const WebCore::IntSize &margin) { m_margin = margin; }
+    IntSize margin() const { return m_margin; }
+    void setMargin(const IntSize &margin) { m_margin = margin; }
 
-    virtual void expand(const WebCore::IntSize&);
-    void expandBy(const WebCore::IntSize&);
+    virtual void expand(const IntSize&);
+    void expandBy(const IntSize&);
 
-    virtual WebCore::IntRect allocate(const WebCore::IntSize&) = 0;
-    virtual void release(const WebCore::IntRect&);
+    virtual IntRect allocate(const IntSize&) = 0;
+    virtual void release(const IntRect&);
 
     virtual int overhead() const;
 
 protected:
-    WebCore::IntSize m_size;
-    WebCore::IntSize m_minAlloc;
-    WebCore::IntSize m_margin;
+    IntSize m_size;
+    IntSize m_minAlloc;
+    IntSize m_margin;
 
-    WebCore::IntSize roundAllocation(const WebCore::IntSize&) const;
+    IntSize roundAllocation(const IntSize&) const;
 };
 
 class GeneralAreaAllocator : public AreaAllocator {
 public:
-    explicit GeneralAreaAllocator(const WebCore::IntSize&);
+    explicit GeneralAreaAllocator(const IntSize&);
     virtual ~GeneralAreaAllocator();
 
-    void expand(const WebCore::IntSize&);
-    WebCore::IntRect allocate(const WebCore::IntSize&);
-    void release(const WebCore::IntRect&);
+    void expand(const IntSize&);
+    IntRect allocate(const IntSize&);
+    void release(const IntRect&);
     int overhead() const;
 
 private:
     enum Split { SplitOnX, SplitOnY };
 
     struct Node {
-        WebCore::IntRect rect;
-        WebCore::IntSize largestFree;
+        IntRect rect;
+        IntSize largestFree;
         Node* parent;
         Node* left;
         Node* right;
@@ -102,12 +100,12 @@ private:
     int m_nodeCount;
 
     static void freeNode(Node*);
-    WebCore::IntPoint allocateFromNode(const WebCore::IntSize&, Node*);
+    IntPoint allocateFromNode(const IntSize&, Node*);
     Node* splitNode(Node*, Split);
     static void updateLargestFree(Node*);
 };
 
-} // namespace WebKit
+} // namespace WebCore
 
 #endif // USE(COORDINATED_GRAPHICS)
 

Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedBackingStore.cpp
@@ -26,11 +26,9 @@
 #include "TextureMapper.h"
 #include "TextureMapperGL.h"
 
-using namespace WebCore;
+namespace WebCore {
 
-namespace WebKit {
-
-void CoordinatedBackingStoreTile::swapBuffers(WebCore::TextureMapper* textureMapper)
+void CoordinatedBackingStoreTile::swapBuffers(TextureMapper* textureMapper)
 {
     if (!m_surface)
         return;
@@ -103,7 +101,7 @@ PassRefPtr<BitmapTexture> CoordinatedBackingStore::texture() const
     return PassRefPtr<BitmapTexture>();
 }
 
-void CoordinatedBackingStore::setSize(const WebCore::FloatSize& size)
+void CoordinatedBackingStore::setSize(const FloatSize& size)
 {
     m_size = size;
 }
@@ -186,5 +184,5 @@ void CoordinatedBackingStore::commitTileOperations(TextureMapper* textureMapper)
         it->value.swapBuffers(textureMapper);
 }
 
-} // namespace WebKit
-#endif
+} // namespace WebCore
+#endif // USE(COORDINATED_GRAPHICS)

commit 20ce762267cdc263e7ca164fef49183e6863bbd4

Build is broken since r141543 for platforms without OpenGL https://bugs.webkit.org/show_bug.cgi?id=108862
Patch by Julien Brianceau <jbrianceau@nds.com> on 2013-02-05
Reviewed by Simon Hausmann.

* Target.pri:

git-svn-id: svn://svn.chromium.org/blink/trunk@141880 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/Target.pri
@@ -2243,18 +2243,6 @@ HEADERS += \
     platform/graphics/surfaces/GraphicsSurface.h \
     platform/graphics/surfaces/GraphicsSurfaceToken.h \
     platform/graphics/SurrogatePairAwareTextIterator.h \
-    platform/graphics/texmap/coordinated/AreaAllocator.h \
-    platform/graphics/texmap/coordinated/CoordinatedBackingStore.h \
-    platform/graphics/texmap/coordinated/CoordinatedCustomFilterOperation.h \
-    platform/graphics/texmap/coordinated/CoordinatedCustomFilterProgram.h \
-    platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.h \
-    platform/graphics/texmap/coordinated/CoordinatedGraphicsScene.h \
-    platform/graphics/texmap/coordinated/CoordinatedImageBacking.h \
-    platform/graphics/texmap/coordinated/CoordinatedLayerInfo.h \
-    platform/graphics/texmap/coordinated/CoordinatedSurface.h \
-    platform/graphics/texmap/coordinated/CoordinatedTile.h \
-    platform/graphics/texmap/coordinated/SurfaceUpdateInfo.h \
-    platform/graphics/texmap/coordinated/UpdateAtlas.h \
     platform/graphics/texmap/GraphicsLayerTextureMapper.h \
     platform/graphics/texmap/TextureMapper.h \
     platform/graphics/texmap/TextureMapperBackingStore.h \
@@ -2890,14 +2878,6 @@ SOURCES += \
     platform/graphics/qt/PathQt.cpp \
     platform/graphics/qt/PatternQt.cpp \
     platform/graphics/qt/StillImageQt.cpp \
-    platform/graphics/texmap/coordinated/AreaAllocator.cpp \
-    platform/graphics/texmap/coordinated/CoordinatedBackingStore.cpp \
-    platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp \
-    platform/graphics/texmap/coordinated/CoordinatedGraphicsScene.cpp \
-    platform/graphics/texmap/coordinated/CoordinatedImageBacking.cpp \
-    platform/graphics/texmap/coordinated/CoordinatedSurface.cpp \
-    platform/graphics/texmap/coordinated/CoordinatedTile.cpp \
-    platform/graphics/texmap/coordinated/UpdateAtlas.cpp \
     platform/graphics/texmap/GraphicsLayerTextureMapper.cpp \
     platform/graphics/texmap/TextureMapper.cpp \
     platform/graphics/texmap/TextureMapperBackingStore.cpp \
@@ -4070,7 +4050,19 @@ use?(3D_GRAPHICS) {
         platform/graphics/gpu/TilingData.h \
         platform/graphics/opengl/Extensions3DOpenGL.h \
         platform/graphics/texmap/TextureMapperGL.h \
-        platform/graphics/texmap/TextureMapperShaderProgram.h
+        platform/graphics/texmap/TextureMapperShaderProgram.h \
+        platform/graphics/texmap/coordinated/AreaAllocator.h \
+        platform/graphics/texmap/coordinated/CoordinatedBackingStore.h \
+        platform/graphics/texmap/coordinated/CoordinatedCustomFilterOperation.h \
+        platform/graphics/texmap/coordinated/CoordinatedCustomFilterProgram.h \
+        platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.h \
+        platform/graphics/texmap/coordinated/CoordinatedGraphicsScene.h \
+        platform/graphics/texmap/coordinated/CoordinatedImageBacking.h \
+        platform/graphics/texmap/coordinated/CoordinatedLayerInfo.h \
+        platform/graphics/texmap/coordinated/CoordinatedSurface.h \
+        platform/graphics/texmap/coordinated/CoordinatedTile.h \
+        platform/graphics/texmap/coordinated/SurfaceUpdateInfo.h \
+        platform/graphics/texmap/coordinated/UpdateAtlas.h
 
     SOURCES += \
         platform/graphics/ANGLEWebKitBridge.cpp \
@@ -4083,7 +4075,15 @@ use?(3D_GRAPHICS) {
         platform/graphics/opengl/Extensions3DOpenGLCommon.cpp \
         platform/graphics/qt/GraphicsContext3DQt.cpp \
         platform/graphics/texmap/TextureMapperGL.cpp \
-        platform/graphics/texmap/TextureMapperShaderProgram.cpp
+        platform/graphics/texmap/TextureMapperShaderProgram.cpp \
+        platform/graphics/texmap/coordinated/AreaAllocator.cpp \
+        platform/graphics/texmap/coordinated/CoordinatedBackingStore.cpp \
+        platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp \
+        platform/graphics/texmap/coordinated/CoordinatedGraphicsScene.cpp \
+        platform/graphics/texmap/coordinated/CoordinatedImageBacking.cpp \
+        platform/graphics/texmap/coordinated/CoordinatedSurface.cpp \
+        platform/graphics/texmap/coordinated/CoordinatedTile.cpp \
+        platform/graphics/texmap/coordinated/UpdateAtlas.cpp
 
     INCLUDEPATH += $$PWD/platform/graphics/gpu
 
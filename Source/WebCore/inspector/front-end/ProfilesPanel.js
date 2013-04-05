nwjs/blink

commit 98fea400e63485a9fbe63a3ccf22cf8166a38599

DevTools: get rid of obsolete capability checks.
Review URL: https://codereview.chromium.org/13679008

git-svn-id: svn://svn.chromium.org/blink/trunk@147610 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/inspector/front-end/ProfilesPanel.js
@@ -357,7 +357,6 @@ WebInspector.ProfilesPanel = function(name, type)
     var singleProfileMode = typeof name !== "undefined";
     name = name || "profiles";
     WebInspector.Panel.call(this, name);
-    this.registerRequiredCSS("panelEnablerView.css");
     this.registerRequiredCSS("heapProfiler.css");
     this.registerRequiredCSS("profilesPanel.css");
 
@@ -369,23 +368,12 @@ WebInspector.ProfilesPanel = function(name, type)
     this._singleProfileMode = singleProfileMode;
     this._profileTypesByIdMap = {};
 
-    var panelEnablerHeading = WebInspector.UIString("You need to enable profiling before you can use the Profiles panel.");
-    var panelEnablerDisclaimer = WebInspector.UIString("Enabling profiling will make scripts run slower.");
-    var panelEnablerButton = WebInspector.UIString("Enable Profiling");
-    this.panelEnablerView = new WebInspector.PanelEnablerView(name, panelEnablerHeading, panelEnablerDisclaimer, panelEnablerButton);
-    this.panelEnablerView.addEventListener("enable clicked", this.enableProfiler, this);
-
     this.profileViews = document.createElement("div");
     this.profileViews.id = "profile-views";
     this.splitView.mainElement.appendChild(this.profileViews);
 
     this._statusBarButtons = [];
 
-    this.enableToggleButton = new WebInspector.StatusBarButton("", "enable-toggle-status-bar-item");
-    if (Capabilities.profilerCausesRecompilation) {
-        this._statusBarButtons.push(this.enableToggleButton);
-        this.enableToggleButton.addEventListener("click", this._onToggleProfiling, this);
-    }
     this.recordButton = new WebInspector.StatusBarButton("", "record-profile-status-bar-item");
     this.recordButton.addEventListener("click", this.toggleRecordButton, this);
     this._statusBarButtons.push(this.recordButton);
@@ -400,8 +388,6 @@ WebInspector.ProfilesPanel = function(name, type)
     this._profileViewStatusBarItemsContainer = document.createElement("div");
     this._profileViewStatusBarItemsContainer.className = "status-bar-items";
 
-    this._profilerEnabled = !Capabilities.profilerCausesRecompilation;
-
     if (singleProfileMode) {
         this._launcherView = this._createLauncherView();
         this._registerProfileType(/** @type {!WebInspector.ProfileType} */ (type));
@@ -414,8 +400,7 @@ WebInspector.ProfilesPanel = function(name, type)
         this._registerProfileType(new WebInspector.CPUProfileType());
         if (!WebInspector.WorkerManager.isWorkerFrontend())
             this._registerProfileType(new WebInspector.CSSSelectorProfileType());
-        if (Capabilities.heapProfilerPresent)
-            this._registerProfileType(new WebInspector.HeapSnapshotProfileType());
+        this._registerProfileType(new WebInspector.HeapSnapshotProfileType());
         if (!WebInspector.WorkerManager.isWorkerFrontend() && WebInspector.experimentsSettings.nativeMemorySnapshots.isEnabled()) {
             this._registerProfileType(new WebInspector.NativeSnapshotProfileType());
             this._registerProfileType(new WebInspector.NativeMemoryProfileType());
@@ -506,7 +491,7 @@ WebInspector.ProfilesPanel.prototype = {
 
     _populateAllProfiles: function()
     {
-        if (!this._profilerEnabled || this._profilesWereRequested)
+        if (this._profilesWereRequested)
             return;
         this._profilesWereRequested = true;
         for (var typeId in this._profileTypesByIdMap)
@@ -517,27 +502,6 @@ WebInspector.ProfilesPanel.prototype = {
     {
         WebInspector.Panel.prototype.wasShown.call(this);
         this._populateAllProfiles();
-    },
-
-    _profilerWasEnabled: function()
-    {
-        if (this._profilerEnabled)
-            return;
-
-        this._profilerEnabled = true;
-
-        this._reset();
-        if (this.isShowing())
-            this._populateAllProfiles();
-    },
-
-    _profilerWasDisabled: function()
-    {
-        if (!this._profilerEnabled)
-            return;
-
-        this._profilerEnabled = false;
-        this._reset();
     },
 
     /**
@@ -587,7 +551,9 @@ WebInspector.ProfilesPanel.prototype = {
 
         this.removeAllListeners();
 
-        this._updateInterface();
+        this.recordButton.visible = true;
+        this._profileViewStatusBarItemsContainer.removeStyleClass("hidden");
+        this.clearResultsButton.element.removeStyleClass("hidden");
         this.profilesItemTreeElement.select();
         this._showLauncherView();
     },
@@ -1102,66 +1068,6 @@ WebInspector.ProfilesPanel.prototype = {
         var profiles = this._getAllProfiles();
         for (var i = 0; i < profiles.length; ++i)
             profiles[i]._profilesTreeElement.searchMatches = 0;
-    },
-
-    _updateInterface: function()
-    {
-        // FIXME: Replace ProfileType-specific button visibility changes by a single ProfileType-agnostic "combo-button" visibility change.
-        if (this._profilerEnabled) {
-            this.enableToggleButton.title = WebInspector.UIString("Profiling enabled. Click to disable.");
-            this.enableToggleButton.toggled = true;
-            this.recordButton.visible = true;
-            this._profileViewStatusBarItemsContainer.removeStyleClass("hidden");
-            this.clearResultsButton.element.removeStyleClass("hidden");
-            this.panelEnablerView.detach();
-        } else {
-            this.enableToggleButton.title = WebInspector.UIString("Profiling disabled. Click to enable.");
-            this.enableToggleButton.toggled = false;
-            this.recordButton.visible = false;
-            this._profileViewStatusBarItemsContainer.addStyleClass("hidden");
-            this.clearResultsButton.element.addStyleClass("hidden");
-            this.panelEnablerView.show(this.element);
-        }
-    },
-
-    get profilerEnabled()
-    {
-        return this._profilerEnabled;
-    },
-
-    enableProfiler: function()
-    {
-        if (this._profilerEnabled)
-            return;
-        this._toggleProfiling(this.panelEnablerView.alwaysEnabled);
-    },
-
-    disableProfiler: function()
-    {
-        if (!this._profilerEnabled)
-            return;
-        this._toggleProfiling(this.panelEnablerView.alwaysEnabled);
-    },
-
-    /**
-     * @param {WebInspector.Event} event
-     */
-    _onToggleProfiling: function(event) {
-        this._toggleProfiling(true);
-    },
-
-    /**
-     * @param {boolean} always
-     */
-    _toggleProfiling: function(always)
-    {
-        if (this._profilerEnabled) {
-            WebInspector.settings.profilerEnabled.set(false);
-            ProfilerAgent.disable(this._profilerWasDisabled.bind(this));
-        } else {
-            WebInspector.settings.profilerEnabled.set(always);
-            ProfilerAgent.enable(this._profilerWasEnabled.bind(this));
-        }
     },
 
     /**

LayoutTests/inspector/profiler/heap-snapshot-test.js
@@ -169,65 +169,32 @@ InspectorTest.startProfilerTest = function(callback)
     WebInspector.showPanel("profiles");
     WebInspector.settings.showHeapSnapshotObjectsHiddenProperties.set(true);
 
-    function profilerEnabled()
-    {
-        InspectorTest.addResult("Profiler was enabled.");
-        // We mock out HeapProfilerAgent -- as DRT runs in single-process mode, Inspector
-        // and test share the same heap. Taking a snapshot takes too long for a test,
-        // so we provide synthetic snapshots.
-        InspectorTest._panelReset = InspectorTest.override(WebInspector.panels.profiles, "_reset", function(){}, true);
-        InspectorTest.addSniffer(WebInspector.HeapSnapshotView.prototype, "show", InspectorTest._snapshotViewShown, true);
+    InspectorTest.addResult("Profiler was enabled.");
+    // We mock out HeapProfilerAgent -- as DRT runs in single-process mode, Inspector
+    // and test share the same heap. Taking a snapshot takes too long for a test,
+    // so we provide synthetic snapshots.
+    InspectorTest._panelReset = InspectorTest.override(WebInspector.panels.profiles, "_reset", function(){}, true);
+    InspectorTest.addSniffer(WebInspector.HeapSnapshotView.prototype, "show", InspectorTest._snapshotViewShown, true);
 
-        detailedHeapProfilesEnabled();
-    }
-
-    function detailedHeapProfilesEnabled()
-    {
-        // Reduce the number of populated nodes to speed up testing.
-        WebInspector.HeapSnapshotContainmentDataGrid.prototype.defaultPopulateCount = function() { return 10; };
-        WebInspector.HeapSnapshotConstructorsDataGrid.prototype.defaultPopulateCount = function() { return 10; };
-        WebInspector.HeapSnapshotDiffDataGrid.prototype.defaultPopulateCount = function() { return 5; };
-        WebInspector.HeapSnapshotDominatorsDataGrid.prototype.defaultPopulateCount = function() { return 3; }
-        InspectorTest.addResult("Detailed heap profiles were enabled.");
-        InspectorTest.safeWrap(callback)();
-    }
-
-    if (WebInspector.panels.profiles._profilerEnabled)
-        profilerEnabled();
-    else {
-        InspectorTest.addSniffer(WebInspector.panels.profiles, "_profilerWasEnabled", profilerEnabled);
-        WebInspector.panels.profiles._toggleProfiling(false);
-    }
+    // Reduce the number of populated nodes to speed up testing.
+    WebInspector.HeapSnapshotContainmentDataGrid.prototype.defaultPopulateCount = function() { return 10; };
+    WebInspector.HeapSnapshotConstructorsDataGrid.prototype.defaultPopulateCount = function() { return 10; };
+    WebInspector.HeapSnapshotDiffDataGrid.prototype.defaultPopulateCount = function() { return 5; };
+    WebInspector.HeapSnapshotDominatorsDataGrid.prototype.defaultPopulateCount = function() { return 3; }
+    InspectorTest.addResult("Detailed heap profiles were enabled.");
+    InspectorTest.safeWrap(callback)();
 };
 
 InspectorTest.completeProfilerTest = function()
 {
     // There is no way to disable detailed heap profiles.
-
-    function completeTest()
-    {
-        InspectorTest.addResult("");
-        InspectorTest.addResult("Profiler was disabled.");
-        InspectorTest.completeTest();
-    }
-
-    var profilesPanel = WebInspector.panels.profiles;
-    if (!profilesPanel._profilerEnabled)
-        completeTest();
-    else {
-        InspectorTest.addSniffer(WebInspector.panels.profiles, "_profilerWasDisabled", completeTest);
-        profilesPanel._toggleProfiling(false);
-    }
+    InspectorTest.addResult("");
+    InspectorTest.addResult("Profiler was disabled.");
+    InspectorTest.completeTest();
 };
 
 InspectorTest.runHeapSnapshotTestSuite = function(testSuite)
 {
-    if (!Capabilities.heapProfilerPresent) {
-        InspectorTest.addResult("Heap profiler is disabled");
-        InspectorTest.completeTest();
-        return;
-    }
-
     InspectorTest._nextUid = 1;
     var testSuiteTests = testSuite.slice();
 

LayoutTests/inspector/profiler/profiler-test.js
@@ -4,38 +4,17 @@ InspectorTest.startProfilerTest = function(callback)
 {
     WebInspector.showPanel("profiles");
 
-    function startTest()
-    {
-        InspectorTest.addResult("Profiler was enabled.");
-        InspectorTest.addSniffer(WebInspector.panels.profiles, "_addProfileHeader", InspectorTest._profileHeaderAdded, true);
-        InspectorTest.addSniffer(WebInspector.CPUProfileView.prototype, "refresh", InspectorTest._profileViewRefresh, true);
-        InspectorTest.safeWrap(callback)();
-    }
-
-    if (WebInspector.panels.profiles._profilerEnabled)
-        startTest();
-    else {
-        InspectorTest.addSniffer(WebInspector.panels.profiles, "_profilerWasEnabled", startTest);
-        WebInspector.panels.profiles._toggleProfiling(false);
-    }
+    InspectorTest.addResult("Profiler was enabled.");
+    InspectorTest.addSniffer(WebInspector.panels.profiles, "_addProfileHeader", InspectorTest._profileHeaderAdded, true);
+    InspectorTest.addSniffer(WebInspector.CPUProfileView.prototype, "refresh", InspectorTest._profileViewRefresh, true);
+    InspectorTest.safeWrap(callback)();
 };
 
 InspectorTest.completeProfilerTest = function()
 {
-    function completeTest()
-    {
-        InspectorTest.addResult("");
-        InspectorTest.addResult("Profiler was disabled.");
-        InspectorTest.completeTest();
-    }
-
-    var profilesPanel = WebInspector.panels.profiles;
-    if (!profilesPanel._profilerEnabled)
-        completeTest();
-    else {
-        InspectorTest.addSniffer(WebInspector.panels.profiles, "_profilerWasDisabled", completeTest);
-        profilesPanel._toggleProfiling(false);
-    }
+    InspectorTest.addResult("");
+    InspectorTest.addResult("Profiler was disabled.");
+    InspectorTest.completeTest();
 };
 
 InspectorTest.runProfilerTestSuite = function(testSuite)

Source/WebCore/WebCore.gypi
@@ -4962,7 +4962,6 @@
             'inspector/front-end/OverridesView.js',
             'inspector/front-end/OverviewGrid.js',
             'inspector/front-end/Panel.js',
-            'inspector/front-end/PanelEnablerView.js',
             'inspector/front-end/ParsedURL.js',
             'inspector/front-end/Placard.js',
             'inspector/front-end/Popover.js',

Source/WebCore/inspector/InspectorClient.h
@@ -53,18 +53,13 @@ public:
     virtual void highlight() = 0;
     virtual void hideHighlight() = 0;
 
-    virtual bool canClearBrowserCache() { return false; }
     virtual void clearBrowserCache() { }
-    virtual bool canClearBrowserCookies() { return false; }
     virtual void clearBrowserCookies() { }
-    virtual bool canMonitorMainThread() { return false; }
 
     typedef void (*TraceEventCallback)(char phase, const unsigned char*, const char* name, unsigned long long id,
         int numArgs, const char* const* argNames, const unsigned char* argTypes, const unsigned long long* argValues,
         unsigned char flags);
     virtual void setTraceEventCallback(TraceEventCallback) { }
-
-    virtual bool canOverrideDeviceMetrics() { return false; }
 
     virtual void overrideDeviceMetrics(int /*width*/, int /*height*/, float /*fontScaleFactor*/, bool /*fitWindow*/)
     {
@@ -78,7 +73,6 @@ public:
     virtual bool overridesShowPaintRects() { return false; }
     virtual void setShowPaintRects(bool) { }
 
-    virtual bool canShowDebugBorders() { return false; }
     virtual void setShowDebugBorders(bool) { }
 
     virtual bool canShowFPSCounter() { return false; }
@@ -87,16 +81,8 @@ public:
     virtual bool canContinuouslyPaint() { return false; }
     virtual void setContinuousPaintingEnabled(bool) { }
 
-    virtual bool supportsFrameInstrumentation() { return false; }
-
     virtual void getAllocatedObjects(HashSet<const void*>&) { }
     virtual void dumpUncountedAllocatedObjects(const HashMap<const void*, size_t>&) { }
-
-    virtual bool captureScreenshot(String*) { return false; }
-
-    virtual bool handleJavaScriptDialog(bool, const String*) { return false; }
-
-    virtual bool canSetFileInputFiles() { return false; }
 
     static bool doDispatchMessageOnFrontendPage(Page* frontendPage, const String& message);
 };

Source/WebCore/inspector/InspectorDOMAgent.cpp
@@ -1283,11 +1283,6 @@ void InspectorDOMAgent::focus(ErrorString* errorString, int nodeId)
 
 void InspectorDOMAgent::setFileInputFiles(ErrorString* errorString, int nodeId, const RefPtr<InspectorArray>& files)
 {
-    if (!m_client->canSetFileInputFiles()) {
-        *errorString = "Cannot set file input files";
-        return;
-    }
-
     Node* node = assertNode(errorString, nodeId);
     if (!node)
         return;

Source/WebCore/inspector/InspectorPageAgent.cpp
@@ -703,11 +703,6 @@ void InspectorPageAgent::setDocumentContent(ErrorString* errorString, const Stri
     DOMPatchSupport::patchDocument(document, html);
 }
 
-void InspectorPageAgent::canOverrideDeviceMetrics(ErrorString*, bool* result)
-{
-    *result = m_client->canOverrideDeviceMetrics();
-}
-
 void InspectorPageAgent::setDeviceMetricsOverride(ErrorString* errorString, int width, int height, double fontScaleFactor, bool fitWindow)
 {
     const static long maxDimension = 10000000;
@@ -756,11 +751,6 @@ void InspectorPageAgent::setShowPaintRects(ErrorString*, bool show)
 
     if (!show && mainFrame() && mainFrame()->view())
         mainFrame()->view()->invalidate();
-}
-
-void InspectorPageAgent::canShowDebugBorders(ErrorString*, bool* outParam)
-{
-    *outParam = m_client->canShowDebugBorders();
 }
 
 void InspectorPageAgent::setShowDebugBorders(ErrorString*, bool show)
@@ -1296,16 +1286,14 @@ void InspectorPageAgent::setCompositingBordersVisible(ErrorString*, bool visible
     settings->setShowRepaintCounter(visible);
 }
 
-void InspectorPageAgent::captureScreenshot(ErrorString* errorString, String* data)
+void InspectorPageAgent::captureScreenshot(ErrorString*, String*)
 {
-    if (!m_client->captureScreenshot(data))
-        *errorString = "Could not capture screenshot";
+    // Handled on the browser level.
 }
 
 void InspectorPageAgent::handleJavaScriptDialog(ErrorString* errorString, bool accept, const String* promptText)
 {
-    if (!m_client->handleJavaScriptDialog(accept, promptText))
-        *errorString = "Could not handle JavaScript dialog";
+    // Handled on the browser level.
 }
 
 } // namespace WebCore

Source/WebCore/inspector/InspectorPageAgent.h
@@ -106,10 +106,8 @@ public:
     virtual void searchInResource(ErrorString*, const String& frameId, const String& url, const String& query, const bool* optionalCaseSensitive, const bool* optionalIsRegex, RefPtr<TypeBuilder::Array<TypeBuilder::Page::SearchMatch> >&);
     virtual void searchInResources(ErrorString*, const String&, const bool* caseSensitive, const bool* isRegex, RefPtr<TypeBuilder::Array<TypeBuilder::Page::SearchResult> >&);
     virtual void setDocumentContent(ErrorString*, const String& frameId, const String& html);
-    virtual void canOverrideDeviceMetrics(ErrorString*, bool*);
     virtual void setDeviceMetricsOverride(ErrorString*, int width, int height, double fontScaleFactor, bool fitWindow);
     virtual void setShowPaintRects(ErrorString*, bool show);
-    virtual void canShowDebugBorders(ErrorString*, bool*);
     virtual void setShowDebugBorders(ErrorString*, bool show);
     virtual void canShowFPSCounter(ErrorString*, bool*);
     virtual void setShowFPSCounter(ErrorString*, bool show);

commit 0d116ff8414b3dd592f1923d25320aa5145f2a3f

DevTools: Restore profiles panel style (regressed in r147610) Original code review: https://codereview.chromium.org/13685016/
git-svn-id: svn://svn.chromium.org/blink/trunk@147645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/inspector/front-end/ProfilesPanel.js
@@ -357,6 +357,7 @@ WebInspector.ProfilesPanel = function(name, type)
     var singleProfileMode = typeof name !== "undefined";
     name = name || "profiles";
     WebInspector.Panel.call(this, name);
+    this.registerRequiredCSS("panelEnablerView.css");
     this.registerRequiredCSS("heapProfiler.css");
     this.registerRequiredCSS("profilesPanel.css");
 
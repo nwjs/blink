fb3c29ee31fe5b886d82fd8eadaee987daddbcaa
Roger WANG <roger.wang@intel.com>
2013-05-15 23:45:27 +0800

    nwfaketop

--- Source/core/page/Frame.h
+++ Source/core/page/Frame.h
@@ -169,6 +169,7 @@ namespace WebCore {
         void setNodeJS(bool node) { m_nodejs = node; }
         bool isNodeJS() const;
         bool isNwDisabledChildFrame() const;
+        bool isNwFakeTop() const;
 
     // ========
 

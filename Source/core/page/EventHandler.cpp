nwjs/blink

commit 71e323a2278b27db7b771a55d240b48f11c02067

Update mouse cursor on style changes without emitting fake mousemove event
BUG=228496

Review URL: https://codereview.chromium.org/25872002

git-svn-id: svn://svn.chromium.org/blink/trunk@159019 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/page/EventHandler.cpp
@@ -121,6 +121,10 @@ using namespace SVGNames;
 const double fakeMouseMoveShortInterval = 0.1;
 const double fakeMouseMoveLongInterval = 0.250;
 
+// The amount of time to wait for a cursor update on style and layout changes
+// Set to 50Hz, no need to be faster than common screen refresh rate
+const double cursorUpdateInterval = 0.02;
+
 const int maximumCursorSize = 128;
 
 // It's pretty unlikely that a scale of less than one would ever be used. But all we really
@@ -288,7 +292,9 @@ EventHandler::EventHandler(Frame* frame)
     , m_dragMayStartSelectionInstead(false)
     , m_mouseDownWasSingleClickInSelection(false)
     , m_selectionInitiationState(HaveNotStartedSelection)
+    , m_panScrollButtonPressed(false)
     , m_hoverTimer(this, &EventHandler::hoverTimerFired)
+    , m_cursorUpdateTimer(this, &EventHandler::cursorUpdateTimerFired)
     , m_mouseDownMayStartAutoscroll(false)
     , m_mouseDownWasInSubframe(false)
     , m_fakeMouseMoveEventTimer(this, &EventHandler::fakeMouseMoveEventTimerFired)
@@ -296,6 +302,7 @@ EventHandler::EventHandler(Frame* frame)
     , m_resizeLayer(0)
     , m_eventHandlerWillResetCapturingMouseEventsNode(0)
     , m_clickCount(0)
+    , m_shouldOnlyFireDragOverEvent(false)
     , m_mousePositionIsUnknown(true)
     , m_mouseDownTimestamp(0)
     , m_widgetIsLatched(false)
@@ -324,6 +331,7 @@ DragState& EventHandler::dragState()
 void EventHandler::clear()
 {
     m_hoverTimer.stop();
+    m_cursorUpdateTimer.stop();
     m_fakeMouseMoveEventTimer.stop();
     m_resizeLayer = 0;
     m_nodeUnderMouse = 0;
@@ -1084,7 +1092,47 @@ bool EventHandler::useHandCursor(Node* node, bool isOverLink, bool shiftKey)
     return ((isOverLink || isSubmitImage(node)) && (!editable || editableLinkEnabled));
 }
 
-OptionalCursor EventHandler::selectCursor(const MouseEventWithHitTestResults& event, Scrollbar* scrollbar)
+void EventHandler::cursorUpdateTimerFired(Timer<EventHandler>*)
+{
+    ASSERT(m_frame);
+    ASSERT(m_frame->document());
+
+    updateCursor();
+}
+
+void EventHandler::updateCursor()
+{
+    if (m_mousePositionIsUnknown)
+        return;
+
+    FrameView* view = m_frame->view();
+    if (!view || !view->shouldSetCursor())
+        return;
+
+    RenderView* renderView = view->renderView();
+    if (!renderView)
+        return;
+
+    bool shiftKey;
+    bool ctrlKey;
+    bool altKey;
+    bool metaKey;
+    PlatformKeyboardEvent::getCurrentModifierState(shiftKey, ctrlKey, altKey, metaKey);
+
+    m_frame->document()->updateLayout();
+
+    HitTestRequest request(HitTestRequest::ReadOnly);
+    HitTestResult result(view->windowToContents(m_lastKnownMousePosition));
+    renderView->hitTest(request, result);
+
+    OptionalCursor optionalCursor = selectCursor(result, shiftKey);
+    if (optionalCursor.isCursorChange()) {
+        m_currentMouseCursor = optionalCursor.cursor();
+        view->setCursor(m_currentMouseCursor);
+    }
+}
+
+OptionalCursor EventHandler::selectCursor(const HitTestResult& result, bool shiftKey)
 {
     if (m_resizeLayer && m_resizeLayer->inResizeMode())
         return NoCursorChange;
@@ -1097,15 +1145,18 @@ OptionalCursor EventHandler::selectCursor(const MouseEventWithHitTestResults& ev
         return NoCursorChange;
 #endif
 
-    Node* node = event.targetNode();
-    RenderObject* renderer = node ? node->renderer() : 0;
+    Node* node = result.targetNode();
+    if (!node)
+        return NoCursorChange;
+
+    RenderObject* renderer = node->renderer();
     RenderStyle* style = renderer ? renderer->style() : 0;
     bool horizontalText = !style || style->isHorizontalWritingMode();
     const Cursor& iBeam = horizontalText ? iBeamCursor() : verticalTextCursor();
 
     if (renderer) {
         Cursor overrideCursor;
-        switch (renderer->getCursor(roundedIntPoint(event.localPoint()), overrideCursor)) {
+        switch (renderer->getCursor(roundedIntPoint(result.localPoint()), overrideCursor)) {
         case SetCursorBasedOnStyle:
             break;
         case SetCursor:
@@ -1147,16 +1198,16 @@ OptionalCursor EventHandler::selectCursor(const MouseEventWithHitTestResults& ev
 
     switch (style ? style->cursor() : CURSOR_AUTO) {
     case CURSOR_AUTO: {
-        bool editable = (node && node->rendererIsEditable());
+        bool editable = node->rendererIsEditable();
 
-        if (useHandCursor(node, event.isOverLink(), event.event().shiftKey()))
+        if (useHandCursor(node, result.isOverLink(), shiftKey))
             return handCursor();
 
         bool inResizer = false;
         if (renderer) {
             if (RenderLayer* layer = renderer->enclosingLayer()) {
                 if (FrameView* view = m_frame->view())
-                    inResizer = layer->isPointInResizeControl(view->windowToContents(event.event().position()), ResizerForPointer);
+                    inResizer = layer->isPointInResizeControl(view->windowToContents(roundedIntPoint(result.localPoint())), ResizerForPointer);
             }
         }
 
@@ -1169,7 +1220,7 @@ OptionalCursor EventHandler::selectCursor(const MouseEventWithHitTestResults& ev
             return iBeam;
         }
 
-        if ((editable || (renderer && renderer->isText() && node->canStartSelection())) && !inResizer && !scrollbar)
+        if ((editable || (renderer && renderer->isText() && node->canStartSelection())) && !inResizer && !result.scrollbar())
             return iBeam;
         return pointerCursor();
     }
@@ -1468,6 +1519,8 @@ bool EventHandler::handleMouseMoveOrLeaveEvent(const PlatformMouseEvent& mouseEv
     if (m_hoverTimer.isActive())
         m_hoverTimer.stop();
 
+    m_cursorUpdateTimer.stop();
+
     cancelFakeMouseMoveEvent();
 
     if (m_svgPan) {
@@ -1538,7 +1591,7 @@ bool EventHandler::handleMouseMoveOrLeaveEvent(const PlatformMouseEvent& mouseEv
         if (scrollbar && !m_mousePressed)
             scrollbar->mouseMoved(mouseEvent); // Handle hover effects on platforms that support visual feedback on scrollbar hovering.
         if (FrameView* view = m_frame->view()) {
-            OptionalCursor optionalCursor = selectCursor(mev, scrollbar);
+            OptionalCursor optionalCursor = selectCursor(mev.hitTestResult(), mouseEvent.shiftKey());
             if (optionalCursor.isCursorChange()) {
                 m_currentMouseCursor = optionalCursor.cursor();
                 view->setCursor(m_currentMouseCursor);
@@ -2810,6 +2863,12 @@ void EventHandler::scheduleHoverStateUpdate()
 {
     if (!m_hoverTimer.isActive())
         m_hoverTimer.startOneShot(0);
+}
+
+void EventHandler::scheduleCursorUpdate()
+{
+    if (!m_cursorUpdateTimer.isActive())
+        m_cursorUpdateTimer.startOneShot(cursorUpdateInterval);
 }
 
 void EventHandler::dispatchFakeMouseMoveEventSoon()

Source/core/page/EventHandler.h
@@ -124,6 +124,7 @@ public:
     void updateDragStateAfterEditDragIfNeeded(Element* rootEditableElement);
 
     void scheduleHoverStateUpdate();
+    void scheduleCursorUpdate();
 
     void setResizingFrameSet(HTMLFrameSetElement*);
 
@@ -195,6 +196,7 @@ public:
     bool handleTouchEvent(const PlatformTouchEvent&);
 
     bool useHandCursor(Node*, bool isOverLink, bool shiftKey);
+    void updateCursor();
 
 private:
     static DragState& dragState();
@@ -217,8 +219,10 @@ private:
     bool handleMouseDraggedEvent(const MouseEventWithHitTestResults&);
     bool handleMouseReleaseEvent(const MouseEventWithHitTestResults&);
 
-    OptionalCursor selectCursor(const MouseEventWithHitTestResults&, Scrollbar*);
+    OptionalCursor selectCursor(const HitTestResult&, bool shiftKey);
+
     void hoverTimerFired(Timer<EventHandler>*);
+    void cursorUpdateTimerFired(Timer<EventHandler>*);
 
     bool logicalScrollOverflow(ScrollLogicalDirection, ScrollGranularity, Node* startingNode = 0);
 
@@ -321,6 +325,7 @@ private:
     bool m_panScrollButtonPressed;
 
     Timer<EventHandler> m_hoverTimer;
+    Timer<EventHandler> m_cursorUpdateTimer;
 
     bool m_mouseDownMayStartAutoscroll;
     bool m_mouseDownWasInSubframe;

LayoutTests/fast/events/mouse-cursor-change-expected.txt
@@ -1,0 +1,24 @@
+Test that mouse cursors are changed correctly on mouse events.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+Bug 53341
+
+
+Mouse move
+Cursor Info: type=Hand hotSpot=0,0
+
+Mouse down
+Cursor Info: type=Progress hotSpot=0,0
+
+Mouse hold down, move
+Cursor Info: type=Hand hotSpot=0,0
+
+Mouse up
+Cursor Info: type=Help hotSpot=0,0
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/events/mouse-cursor-change.html
@@ -1,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="../js/resources/js-test-pre.js"></script>
+<style type="text/css">
+</style>
+</head>
+<body>
+<p id="description"></p>
+<p><a href="https://bugs.webkit.org/show_bug.cgi?id=53341">Bug 53341</a></p>
+<div id="test-container">
+    <div id="target" onMouseDown="style.cursor='progress';event.preventDefault();" onMouseMove="style.cursor='pointer';" onMouseUp="style.cursor='help';" style="cursor:pointer;">Play with mouse on this element. Cursors change on events - mousemove: pointer(hand), mousedown: progress, mouseup: help.</div>
+</div>
+<br/>
+<div id="console"></div>
+<script>
+var CURSOR_UPDATE_DELAY = 50;
+
+description("Test that mouse cursors are changed correctly on mouse events.");
+
+if (!window.eventSender) {
+    testFailed('This test requires DumpRenderTree');
+}
+
+if (window.testRunner) {
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+    window.jsTestIsAsync = true;
+}
+
+function runTest(prepare, next) {
+    prepare();
+    setTimeout(function() {
+        debug('Cursor Info: ' + window.internals.getCurrentCursorInfo(document));
+        debug('');
+        next();
+    }, CURSOR_UPDATE_DELAY);
+}
+
+function testsDone() {
+    // This text is redundant with the test output - hide it
+    document.getElementById('test-container').style.display = 'none';
+    finishJSTest();
+}
+
+// Can't do anything useful here without eventSender
+if (window.eventSender) {
+    var target = document.getElementById('target');
+    eventSender.dragMode = false;
+    var tests = [
+        function() {
+            debug('Mouse move');
+            eventSender.mouseMoveTo(target.offsetLeft + 3, target.offsetTop + 3);
+        }, function() {
+            debug('Mouse down');
+            eventSender.mouseDown();
+        }, function() {
+            debug('Mouse hold down, move');
+            eventSender.mouseMoveTo(target.offsetLeft + 13, target.offsetTop + 3);
+        }, function() {
+            debug('Mouse up');
+            eventSender.mouseUp();
+        }
+    ];
+
+    var i = 0;
+    function nextTest() {
+        if (i < tests.length) {
+            runTest(tests[i++], nextTest);
+        } else {
+            testsDone();
+        }
+    }
+    nextTest();
+}
+
+</script>
+<script src="../../fast/js/resources/js-test-post.js"></script>
+</body>
+</html>

LayoutTests/fast/events/mouse-cursor-no-mousemove-expected.txt
@@ -1,0 +1,16 @@
+Test that there is no mousemove event fired when changing cursor.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+Bug 85343
+
+
+TEST CASE: Mouse idle, change cursor should not fire mousemove event
+Cursor Info: type=Pointer hotSpot=0,0
+Cursor Info: type=Help hotSpot=0,0
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/events/mouse-cursor-no-mousemove.html
@@ -1,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="../js/resources/js-test-pre.js"></script>
+<style type="text/css">
+</style>
+</head>
+<body>
+<p id="description"></p>
+<p><a href="https://bugs.webkit.org/show_bug.cgi?id=85343">Bug 85343</a></p>
+<div id="test-container">
+    <div id="target" style="cursor:default">Mouse idle, change cursor should not fire mousemove event</div>
+</div>
+<br/>
+<div id="console"></div>
+<script>
+var CURSOR_UPDATE_DELAY = 50;
+
+description("Test that there is no mousemove event fired when changing cursor.");
+
+if (!window.eventSender) {
+    testFailed('This test requires DumpRenderTree');
+}
+
+if (window.testRunner) {
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+    window.jsTestIsAsync = true;
+}
+
+// Can't do anything useful here without eventSender
+if (window.eventSender) {
+    var node = document.getElementById('target');
+    debug('TEST CASE: ' + node.textContent);
+    eventSender.mouseMoveTo(node.offsetLeft + 3, node.offsetTop + 3);
+    debug('Cursor Info: ' + window.internals.getCurrentCursorInfo(document));
+    node.addEventListener('mousemove', function() {
+        testFailed('Mousemove event should not be fired when changing cursor');
+        finishJSTest();
+    });
+    node.style.cursor = 'help';
+    setTimeout(function() {
+        debug('Cursor Info: ' + window.internals.getCurrentCursorInfo(document));
+        debug('');
+    }, CURSOR_UPDATE_DELAY);
+
+    // Give some time for the change to resolve. Fake mousemove event that caused bug, is using a timer
+    setTimeout(function() {
+        document.getElementById('test-container').style.display = 'none';
+        finishJSTest();
+    }, 150);
+}
+
+</script>
+<script src="../../fast/js/resources/js-test-post.js"></script>
+</body>
+</html>

Source/core/page/FrameView.cpp
@@ -1344,6 +1344,12 @@ IntPoint FrameView::lastKnownMousePosition() const
     return m_frame->eventHandler()->lastKnownMousePosition();
 }
 
+bool FrameView::shouldSetCursor() const
+{
+    Page* page = frame().page();
+    return page && page->visibilityState() != PageVisibilityStateHidden && page->focusController().isActive();
+}
+
 bool FrameView::scrollContentsFastPath(const IntSize& scrollDelta, const IntRect& rectToScroll, const IntRect& clipRect)
 {
     if (!m_viewportConstrainedObjects || m_viewportConstrainedObjects->isEmpty()) {

Source/core/page/FrameView.h
@@ -264,6 +264,7 @@ public:
     static void setRepaintThrottlingDeferredRepaintDelayIncrementDuringLoading(double p);
 
     virtual IntPoint lastKnownMousePosition() const;
+    bool shouldSetCursor() const;
 
     virtual bool scrollbarsCanBeActive() const OVERRIDE;
 

commit 07e71764c3e2a8441a245645b38f7143de035d97

Handle the case of no underlying node when computing cursor change.
This fixes a regression introduced by r159019.

R=dglazkov
BUG=306919

Review URL: https://codereview.chromium.org/27489002

git-svn-id: svn://svn.chromium.org/blink/trunk@159996 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/page/EventHandler.cpp
@@ -1133,12 +1133,10 @@ OptionalCursor EventHandler::selectCursor(const HitTestResult& result, bool shif
 
     Node* node = result.targetNode();
     if (!node)
-        return NoCursorChange;
+        return selectAutoCursor(result, node, iBeamCursor(), shiftKey);
 
     RenderObject* renderer = node->renderer();
     RenderStyle* style = renderer ? renderer->style() : 0;
-    bool horizontalText = !style || style->isHorizontalWritingMode();
-    const Cursor& iBeam = horizontalText ? iBeamCursor() : verticalTextCursor();
 
     if (renderer) {
         Cursor overrideCursor;
@@ -1184,31 +1182,9 @@ OptionalCursor EventHandler::selectCursor(const HitTestResult& result, bool shif
 
     switch (style ? style->cursor() : CURSOR_AUTO) {
     case CURSOR_AUTO: {
-        bool editable = node->rendererIsEditable();
-
-        if (useHandCursor(node, result.isOverLink(), shiftKey))
-            return handCursor();
-
-        bool inResizer = false;
-        if (renderer) {
-            if (RenderLayer* layer = renderer->enclosingLayer()) {
-                if (FrameView* view = m_frame->view())
-                    inResizer = layer->isPointInResizeControl(view->windowToContents(roundedIntPoint(result.localPoint())), ResizerForPointer);
-            }
-        }
-
-        // During selection, use an I-beam no matter what we're over.
-        // If a drag may be starting or we're capturing mouse events for a particular node, don't treat this as a selection.
-        if (m_mousePressed && m_mouseDownMayStartSelect
-            && !m_mouseDownMayStartDrag
-            && m_frame->selection().isCaretOrRange()
-            && !m_capturingMouseEventsNode) {
-            return iBeam;
-        }
-
-        if ((editable || (renderer && renderer->isText() && node->canStartSelection())) && !inResizer && !result.scrollbar())
-            return iBeam;
-        return pointerCursor();
+        bool horizontalText = !style || style->isHorizontalWritingMode();
+        const Cursor& iBeam = horizontalText ? iBeamCursor() : verticalTextCursor();
+        return selectAutoCursor(result, node, iBeam, shiftKey);
     }
     case CURSOR_CROSS:
         return crossCursor();
@@ -1281,6 +1257,36 @@ OptionalCursor EventHandler::selectCursor(const HitTestResult& result, bool shif
     case CURSOR_WEBKIT_GRABBING:
         return grabbingCursor();
     }
+    return pointerCursor();
+}
+
+OptionalCursor EventHandler::selectAutoCursor(const HitTestResult& result, Node* node, const Cursor& iBeam, bool shiftKey)
+{
+    bool editable = (node && node->rendererIsEditable());
+
+    if (useHandCursor(node, result.isOverLink(), shiftKey))
+        return handCursor();
+
+    bool inResizer = false;
+    RenderObject* renderer = node ? node->renderer() : 0;
+    if (renderer) {
+        if (RenderLayer* layer = renderer->enclosingLayer()) {
+            if (FrameView* view = m_frame->view())
+                inResizer = layer->isPointInResizeControl(view->windowToContents(roundedIntPoint(result.localPoint())), ResizerForPointer);
+        }
+    }
+
+    // During selection, use an I-beam no matter what we're over.
+    // If a drag may be starting or we're capturing mouse events for a particular node, don't treat this as a selection.
+    if (m_mousePressed && m_mouseDownMayStartSelect
+        && !m_mouseDownMayStartDrag
+        && m_frame->selection().isCaretOrRange()
+        && !m_capturingMouseEventsNode) {
+        return iBeam;
+    }
+
+    if ((editable || (renderer && renderer->isText() && node->canStartSelection())) && !inResizer && !result.scrollbar())
+        return iBeam;
     return pointerCursor();
 }
 

Source/core/page/EventHandler.h
@@ -208,6 +208,7 @@ private:
     bool shouldApplyTouchAdjustment(const PlatformGestureEvent&) const;
 
     OptionalCursor selectCursor(const HitTestResult&, bool shiftKey);
+    OptionalCursor selectAutoCursor(const HitTestResult&, Node*, const Cursor& iBeam, bool shiftKey);
 
     void hoverTimerFired(Timer<EventHandler>*);
     void cursorUpdateTimerFired(Timer<EventHandler>*);

LayoutTests/fast/events/mousemove-to-scrollbar-changes-cursor-expected.txt
@@ -1,0 +1,3 @@
+This tests that hovering over a scrollbar resets the mouse cursor to the default pointer.
+Hovered pointer: type=Hand hotSpot=0,0
+Scrollbar pointer: type=Pointer hotSpot=0,0
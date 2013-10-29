nwjs/blink

commit eb86bb4125b6063202536b960da0f013d1d0d1a5

Remove accesskey caching in Document.
This change simplifies Document by removing its ability to cache the mapping
between accesskeys and elements. Now getElementByAccessKey() member function
traverses all elements under the document every time it is called.

See bug 226839 for further details about motivation of this change.

Document's member function getElementByAccessKey() is moved to TreeScope,
because it needs to recurse on the shadow roots.

The behavior of accesskey should not change.

BUG=226839
R=jamesr@chromium.org,morrita@chromium.org

Review URL: https://chromiumcodereview.appspot.com/18684003

git-svn-id: svn://svn.chromium.org/blink/trunk@153757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/TreeScope.cpp
@@ -35,6 +35,7 @@
 #include "core/dom/IdTargetObserverRegistry.h"
 #include "core/dom/NodeTraversal.h"
 #include "core/dom/TreeScopeAdopter.h"
+#include "core/dom/shadow/ElementShadow.h"
 #include "core/dom/shadow/ShadowRoot.h"
 #include "core/html/HTMLAnchorElement.h"
 #include "core/html/HTMLFrameOwnerElement.h"
@@ -492,6 +493,23 @@ bool TreeScope::isInclusiveAncestorOf(const TreeScope* scope) const
             return true;
     }
     return false;
+}
+
+Element* TreeScope::getElementByAccessKey(const String& key) const
+{
+    if (key.isEmpty())
+        return 0;
+    Element* result = 0;
+    Node* root = rootNode();
+    for (Element* element = ElementTraversal::firstWithin(root); element; element = ElementTraversal::next(element, root)) {
+        if (element->fastGetAttribute(accesskeyAttr) == key)
+            result = element;
+        for (ShadowRoot* shadowRoot = element->youngestShadowRoot(); shadowRoot; shadowRoot = shadowRoot->olderShadowRoot()) {
+            if (Element* shadowResult = shadowRoot->getElementByAccessKey(key))
+                result = shadowResult;
+        }
+    }
+    return result;
 }
 
 } // namespace WebCore

LayoutTests/fast/forms/access-key-mutated-expected.txt
@@ -1,0 +1,12 @@
+Access key should work after accesskey attribute is mutated. To test this manually, press <alt>+a, <alt>+b and <alt>+c keys in this order (on Mac OS X, press <Ctrl>+<Opt> instead of <alt>).
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS Pressing the "a" access key triggered a focus event.
+PASS Pressing the "b" access key triggered a focus event.
+PASS Pressing the "c" access key triggered a focus event.
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/forms/access-key-mutated.html
@@ -1,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mutating accesskey attribute</title>
+<script src="../js/resources/js-test-pre.js"></script>
+</head>
+</head>
+<body>
+<script>
+description('Access key should work after accesskey attribute is mutated. To test this manually, press &lt;alt&gt;+a, &lt;alt&gt;+b and &lt;alt&gt;+c keys in this order (on Mac OS X, press &lt;Ctrl&gt;+&lt;Opt&gt; instead of &lt;alt&gt;).');
+
+window.jsTestIsAsync = true;
+
+function pressKey(key)
+{
+    if (navigator.userAgent.search(/\bMac OS X\b/) !== -1)
+        modifiers = ["ctrlKey", "altKey"];
+    else
+        modifiers = ["altKey"];
+
+    if (window.eventSender)
+        eventSender.keyDown(key, modifiers);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var input = document.createElement('input');
+    input.accessKey = 'a';
+    input.onfocus = function () {
+        testPassed('Pressing the "a" access key triggered a focus event.');
+        input.blur();
+        input.accessKey = 'b';
+        input.onfocus = function () {
+            testPassed('Pressing the "b" access key triggered a focus event.');
+            input.blur();
+            input.setAttribute('accesskey', 'c');
+            input.onfocus = function () {
+                testPassed('Pressing the "c" access key triggered a focus event.');
+                document.body.removeChild(input);
+                finishJSTest();
+            };
+            pressKey('c');
+        };
+        pressKey('b');
+    };
+    document.body.appendChild(input);
+    pressKey('a');
+});
+</script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>

Source/core/dom/Document.cpp
@@ -423,7 +423,6 @@ Document::Document(Frame* frame, const KURL& url, DocumentClassFlags documentCla
     , m_designMode(inherit)
     , m_hasAnnotatedRegions(false)
     , m_annotatedRegionsDirty(false)
-    , m_accessKeyMapValid(false)
     , m_useSecureKeyboardEntryWhenActive(false)
     , m_documentClasses(documentClasses)
     , m_isViewSource(false)
@@ -614,37 +613,6 @@ void Document::dispose()
 Element* Document::getElementById(const AtomicString& id) const
 {
     return TreeScope::getElementById(id);
-}
-
-Element* Document::getElementByAccessKey(const String& key)
-{
-    if (key.isEmpty())
-        return 0;
-    if (!m_accessKeyMapValid) {
-        buildAccessKeyMap(this);
-        m_accessKeyMapValid = true;
-    }
-    return m_elementsByAccessKey.get(key.impl());
-}
-
-void Document::buildAccessKeyMap(TreeScope* scope)
-{
-    ASSERT(scope);
-    Node* rootNode = scope->rootNode();
-    for (Element* element = ElementTraversal::firstWithin(rootNode); element; element = ElementTraversal::next(element, rootNode)) {
-        const AtomicString& accessKey = element->getAttribute(accesskeyAttr);
-        if (!accessKey.isEmpty())
-            m_elementsByAccessKey.set(accessKey.impl(), element);
-
-        for (ShadowRoot* root = element->youngestShadowRoot(); root; root = root->olderShadowRoot())
-            buildAccessKeyMap(root);
-    }
-}
-
-void Document::invalidateAccessKeyMap()
-{
-    m_accessKeyMapValid = false;
-    m_elementsByAccessKey.clear();
 }
 
 SelectorQueryCache* Document::selectorQueryCache()
@@ -1581,9 +1549,6 @@ void Document::scheduleStyleRecalc()
         return;
 
     ASSERT(childNeedsStyleRecalc() || m_pendingStyleRecalcShouldForce);
-
-    // FIXME: Why on earth is this here? This is clearly misplaced.
-    invalidateAccessKeyMap();
 
     m_styleRecalcTimer.startOneShot(0);
 
@@ -5060,7 +5025,6 @@ void Document::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
     info.addMember(m_annotatedRegions, "annotatedRegions");
     info.addMember(m_cssCanvasElements, "cssCanvasElements");
     info.addMember(m_iconURLs, "iconURLs");
-    info.addMember(m_elementsByAccessKey, "elementsByAccessKey");
     info.addMember(m_eventQueue, "eventQueue");
     info.addMember(m_pendingTasks, "pendingTasks");
     info.addMember(m_prerenderer, "prerenderer");

Source/core/dom/Document.h
@@ -229,9 +229,6 @@ public:
 
     virtual bool canContainRangeEndPoint() const { return true; }
 
-    Element* getElementByAccessKey(const String& key);
-    void invalidateAccessKeyMap();
-
     SelectorQueryCache* selectorQueryCache();
 
     // DOM methods & attributes for Document
@@ -1100,8 +1097,6 @@ private:
     void updateFocusAppearanceTimerFired(Timer<Document>*);
     void updateBaseURL();
 
-    void buildAccessKeyMap(TreeScope* root);
-
     void createStyleResolver();
 
     void executeScriptsWaitingForResourcesIfNeeded();
@@ -1291,9 +1286,6 @@ private:
     HashMap<String, RefPtr<HTMLCanvasElement> > m_cssCanvasElements;
 
     Vector<IconURL> m_iconURLs;
-
-    HashMap<StringImpl*, Element*, CaseFoldingHash> m_elementsByAccessKey;
-    bool m_accessKeyMapValid;
 
     OwnPtr<SelectorQueryCache> m_selectorQueryCache;
 

Source/core/dom/TreeScope.h
@@ -130,6 +130,8 @@ public:
     bool isInclusiveAncestorOf(const TreeScope*) const;
     unsigned short comparePosition(const TreeScope*) const;
 
+    Element* getElementByAccessKey(const String& key) const;
+
 protected:
     TreeScope(ContainerNode*, Document*);
     TreeScope(Document*);

commit 329851a67dbac8dfba61c14cb508ff6b7f8cee2d

accessKey should be compared case-insensitively.
This fixes a regression from r153757. I overlooked CaseFoldingHash specified to
HashMap<> included in the previous implementation, which caused capital
accessKeys to be totally ignored.

BUG=303204
R=jamesr@chromium.org

Review URL: https://codereview.chromium.org/48433002

git-svn-id: svn://svn.chromium.org/blink/trunk@160771 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/TreeScope.cpp
@@ -487,7 +487,7 @@ Element* TreeScope::getElementByAccessKey(const String& key) const
     Element* result = 0;
     Node* root = rootNode();
     for (Element* element = ElementTraversal::firstWithin(root); element; element = ElementTraversal::next(element, root)) {
-        if (element->fastGetAttribute(accesskeyAttr) == key)
+        if (equalIgnoringCase(element->fastGetAttribute(accesskeyAttr), key))
             result = element;
         for (ShadowRoot* shadowRoot = element->youngestShadowRoot(); shadowRoot; shadowRoot = shadowRoot->olderShadowRoot()) {
             if (Element* shadowResult = shadowRoot->getElementByAccessKey(key))

LayoutTests/fast/forms/access-key-case-insensitive-expected.txt
@@ -1,0 +1,12 @@
+Access key should work case-insensitively. To test this manually, press <alt>+a, <alt>+<shift>+b and <alt>+<shift>+c keys in this order (on Mac OS X, press <ctrl>+<opt> instead of <alt>).
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS Pressing the "a" access key triggered a focus event.
+PASS Pressing the "B" access key triggered a focus event.
+PASS Pressing the "C" access key triggered a focus event.
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/forms/access-key-case-insensitive.html
@@ -1,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Case insensitiveness of accesskey attribute</title>
+<script src="../js/resources/js-test-pre.js"></script>
+</head>
+</head>
+<body>
+<script>
+description('Access key should work case-insensitively. To test this manually, press &lt;alt&gt;+a, &lt;alt&gt;+&lt;shift&gt;+b and &lt;alt&gt;+&lt;shift&gt;+c keys in this order (on Mac OS X, press &lt;ctrl&gt;+&lt;opt&gt; instead of &lt;alt&gt;).');
+
+window.jsTestIsAsync = true;
+
+function isUpperCase(string)
+{
+    return string === string.toUpperCase();
+}
+
+function pressKey(key)
+{
+    var modifiers;
+    if (navigator.userAgent.search(/\bMac OS X\b/) !== -1)
+        modifiers = ["ctrlKey", "altKey"];
+    else
+        modifiers = ["altKey"];
+
+    if (isUpperCase(key)) {
+        modifiers.push("shiftKey");
+        key = key.toLowerCase();
+    }
+
+    if (window.eventSender)
+        eventSender.keyDown(key, modifiers);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var input = document.createElement('input');
+    input.accessKey = 'A';
+    input.onfocus = function () {
+        testPassed('Pressing the "a" access key triggered a focus event.');
+        input.blur();
+        input.accessKey = 'b';
+        input.onfocus = function () {
+            testPassed('Pressing the "B" access key triggered a focus event.');
+            input.blur();
+            input.setAttribute('accesskey', 'C');
+            input.onfocus = function () {
+                testPassed('Pressing the "C" access key triggered a focus event.');
+                document.body.removeChild(input);
+                finishJSTest();
+            };
+            pressKey('C');
+        };
+        pressKey('B');
+    };
+    document.body.appendChild(input);
+    pressKey('a');
+});
+</script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>
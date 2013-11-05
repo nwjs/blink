nwjs/blink

commit ff7f362f96a8de4c6a222305e9bef983844c691b

setAttributeNode() does not set the new value to an existing attribute if specified attribute is in a different case.
setAttributeNode() now performs a case-insensitive search for an existing attribute
and if found, retrieves the index of such an attribute. For setting the value we
call upon setAttributeInternal() to which we pass both the index as well as the name.
The name passed to this method is the same as the one passed to the setAttributeNode() API from the webpage and thus can be in any case.

However, setAttributeInternal() uses this name to get the corresponding existing node.
Since this retrieval is not case-insensitive, the existing node is not found and thus
the new value is not set on the existing node.
We should instead use the passed index and use that to retrieve the existing node.

Note that obtaining the attribute's value using getAttributeNode() would still
return the correct value, i.e. the new one.

Also, this now makes our behavior similar to that of FF and IE.

BUG= 279193, 282922
R= esprehn@chromium.org,eseidel@chromium.org,ojan@chromium.org

Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=157015

Review URL: https://chromiumcodereview.appspot.com/22842013

git-svn-id: svn://svn.chromium.org/blink/trunk@157508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/Element.cpp
@@ -869,21 +869,23 @@ inline void Element::setAttributeInternal(size_t index, const QualifiedName& nam
         return;
     }
 
+    QualifiedName existingAttributeName = attributeItem(index)->name();
+
     if (!inSynchronizationOfLazyAttribute)
-        willModifyAttribute(name, attributeItem(index)->value(), newValue);
+        willModifyAttribute(existingAttributeName, attributeItem(index)->value(), newValue);
 
     if (newValue != attributeItem(index)->value()) {
         // If there is an Attr node hooked to this attribute, the Attr::setValue() call below
         // will write into the ElementData.
         // FIXME: Refactor this so it makes some sense.
-        if (RefPtr<Attr> attrNode = inSynchronizationOfLazyAttribute ? 0 : attrIfExists(name))
+        if (RefPtr<Attr> attrNode = inSynchronizationOfLazyAttribute ? 0 : attrIfExists(existingAttributeName))
             attrNode->setValue(newValue);
         else
             ensureUniqueElementData()->attributeItem(index)->setValue(newValue);
     }
 
     if (!inSynchronizationOfLazyAttribute)
-        didModifyAttribute(name, newValue);
+        didModifyAttribute(existingAttributeName, newValue);
 }
 
 static inline AtomicString makeIdForStyleResolution(const AtomicString& value, bool inQuirksMode)

LayoutTests/fast/dom/Element/setAttributeNode-for-existing-attribute-expected.txt
@@ -1,0 +1,3 @@
+Testcase for bug 279193: setAttributeNode() does not set the new value to an existing attribute if specified attribute is in a different case.\nThe issue can be manually verified by ascertaining that a green colored block is displayed instead of a red one.
+ 
+background-color specified with lowercase style attribute was: red and specified with uppercase style attribute is: green

LayoutTests/fast/dom/Element/setAttributeNode-for-existing-attribute.html
@@ -1,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function runTest() {
+    if (window.testRunner)
+        testRunner.dumpAsText();
+        
+    var testElement = document.getElementById('test');
+    var testElementBgColor = testElement.style.background;
+    
+    var newAttr = document.createAttribute("STYLE");
+    newAttr.value = "background-color: green";
+    testElement.setAttributeNode(newAttr);
+    var testElementBgColorAfterReset = testElement.style.background;
+    
+    document.getElementById('result').innerHTML = "background-color specified with lowercase style attribute was: "+testElementBgColor+" and specified with uppercase style attribute is: "+testElementBgColorAfterReset;
+}
+</script>
+</head>
+<body onload="runTest();">
+<div>Testcase for bug <a href="http://crbug.com/279193">279193</a>: setAttributeNode() does not set the new value to an existing attribute if specified attribute is in a different case.\nThe issue can be manually verified by ascertaining that a green colored block is displayed instead of a red one.</div>
+<div id="test" style="background-color: red;"/>&nbsp;</div>
+<div id="result"></div>
+</body>
+</html>

commit 181dde44f8a4cc2ef754d5376b88a100bf6169b4

Minimize performance regression caused by r157508
Minimize performance regression caused by r157508:
- Stop copying the value returned by Attribute::name()
- Cache the value returned by Element::attributeItem() as it hides
  an if branch in ElementData::attributeBase().

I get a 8.77% improvement locally on Bindings/id-setter.html 
performance test.

R=haraken
BUG=293663

Review URL: https://codereview.chromium.org/60143002

git-svn-id: svn://svn.chromium.org/blink/trunk@161383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/Element.cpp
@@ -924,12 +924,14 @@ inline void Element::setAttributeInternal(size_t index, const QualifiedName& nam
         return;
     }
 
-    QualifiedName existingAttributeName = attributeItem(index)->name();
+    const Attribute* existingAttribute = attributeItem(index);
+    ASSERT(existingAttribute);
+    const QualifiedName& existingAttributeName = existingAttribute->name();
 
     if (!inSynchronizationOfLazyAttribute)
-        willModifyAttribute(existingAttributeName, attributeItem(index)->value(), newValue);
+        willModifyAttribute(existingAttributeName, existingAttribute->value(), newValue);
 
-    if (newValue != attributeItem(index)->value()) {
+    if (newValue != existingAttribute->value()) {
         // If there is an Attr node hooked to this attribute, the Attr::setValue() call below
         // will write into the ElementData.
         // FIXME: Refactor this so it makes some sense.
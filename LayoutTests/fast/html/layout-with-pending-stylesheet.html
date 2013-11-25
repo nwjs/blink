nwjs/blink

commit 50887b823be2c3eedcb138d5d4aec2131af6e019

Only do partial layouts when possible.
This patch changes offsetWidth, offsetHeight, offsetLeft, and offsetTop to only
layout a portion of the render tree, up to the target renderer. Partial layout
leaves the root needing layout so a full layout must happen before painting.

An example of the benefit of this patch is wikipedia where the following
pattern is common:
1) page load
2) javascript calls offsetWidth, forcing a full layout
3) javascript injects style into the page, invalidating layout
4) a full layout is performed for painting

With this patch, the offsetWidth call in #2 is significantly improved.

Text autosizing currently removes most of the benefit of this patch, this will
be addressed in a followup patch. Similarly, non-overlay scrollbars reduce
the benefit from this patch.

This is currently implemented behind the PartialLayout
runtime enabled feature flag.

BUG=283623

Review URL: https://chromiumcodereview.appspot.com/18601002

git-svn-id: svn://svn.chromium.org/blink/trunk@157252 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/Document.cpp
@@ -156,6 +156,7 @@
 #include "core/platform/DateComponents.h"
 #include "core/platform/HistogramSupport.h"
 #include "core/platform/Language.h"
+#include "core/platform/ScrollbarTheme.h"
 #include "core/platform/Timer.h"
 #include "core/platform/chromium/TraceEvent.h"
 #include "core/platform/network/HTTPParsers.h"
@@ -1822,6 +1823,9 @@ void Document::updateLayout()
     if (frameView && renderer() && (frameView->layoutPending() || renderer()->needsLayout()))
         frameView->layout();
 
+    if (frameView)
+        frameView->partialLayout().reset();
+
     setNeedsFocusedElementCheck();
 }
 
@@ -1834,6 +1838,31 @@ void Document::setNeedsFocusedElementCheck()
     m_didPostCheckFocusedElementTask = true;
 }
 
+void Document::recalcStyleForLayoutIgnoringPendingStylesheets()
+{
+    TemporaryChange<bool> ignorePendingStylesheets(m_ignorePendingStylesheets, m_ignorePendingStylesheets);
+    if (!haveStylesheetsLoaded()) {
+        m_ignorePendingStylesheets = true;
+        // FIXME: We are willing to attempt to suppress painting with outdated style info only once.
+        // Our assumption is that it would be dangerous to try to stop it a second time, after page
+        // content has already been loaded and displayed with accurate style information. (Our
+        // suppression involves blanking the whole page at the moment. If it were more refined, we
+        // might be able to do something better.) It's worth noting though that this entire method
+        // is a hack, since what we really want to do is suspend JS instead of doing a layout with
+        // inaccurate information.
+        HTMLElement* bodyElement = body();
+        if (bodyElement && !bodyElement->renderer() && m_pendingSheetLayout == NoLayoutWithPendingSheets) {
+            m_pendingSheetLayout = DidLayoutWithPendingSheets;
+            styleResolverChanged(RecalcStyleImmediately);
+        } else if (m_hasNodesWithPlaceholderStyle) {
+            // If new nodes have been added or style recalc has been done with style sheets still
+            // pending, some nodes may not have had their real style calculated yet. Normally this
+            // gets cleaned when style sheets arrive but here we need up-to-date style immediately.
+            recalcStyle(Force);
+        }
+    }
+}
+
 // FIXME: This is a bad idea and needs to be removed eventually.
 // Other browsers load stylesheets before they continue parsing the web page.
 // Since we don't, we can run JavaScript code that needs answers before the
@@ -1842,30 +1871,40 @@ void Document::setNeedsFocusedElementCheck()
 // to instead suspend JavaScript execution.
 void Document::updateLayoutIgnorePendingStylesheets()
 {
-    bool oldIgnore = m_ignorePendingStylesheets;
+    recalcStyleForLayoutIgnoringPendingStylesheets();
+    updateLayout();
+}
 
-    if (!haveStylesheetsLoaded()) {
-        m_ignorePendingStylesheets = true;
-        // FIXME: We are willing to attempt to suppress painting with outdated style info only once.  Our assumption is that it would be
-        // dangerous to try to stop it a second time, after page content has already been loaded and displayed
-        // with accurate style information.  (Our suppression involves blanking the whole page at the
-        // moment.  If it were more refined, we might be able to do something better.)
-        // It's worth noting though that this entire method is a hack, since what we really want to do is
-        // suspend JS instead of doing a layout with inaccurate information.
-        HTMLElement* bodyElement = body();
-        if (bodyElement && !bodyElement->renderer() && m_pendingSheetLayout == NoLayoutWithPendingSheets) {
-            m_pendingSheetLayout = DidLayoutWithPendingSheets;
-            styleResolverChanged(RecalcStyleImmediately);
-        } else if (m_hasNodesWithPlaceholderStyle)
-            // If new nodes have been added or style recalc has been done with style sheets still pending, some nodes
-            // may not have had their real style calculated yet. Normally this gets cleaned when style sheets arrive
-            // but here we need up-to-date style immediately.
-            recalcStyle(Force);
+void Document::partialUpdateLayoutIgnorePendingStylesheets(Node* stopLayoutAtNode)
+{
+    // Non-overlay scrollbars can cause a second layout that is dependent
+    // on a first layout. This is disabled for partial layout for now.
+    if (!RuntimeEnabledFeatures::partialLayoutEnabled() || !ScrollbarTheme::theme()->usesOverlayScrollbars()) {
+        updateLayoutIgnorePendingStylesheets();
+        return;
+    }
+
+    TemporaryChange<bool> ignorePendingStylesheets(m_ignorePendingStylesheets, m_ignorePendingStylesheets);
+    recalcStyleForLayoutIgnoringPendingStylesheets();
+
+    if (stopLayoutAtNode) {
+        RenderObject* renderer = stopLayoutAtNode->renderer();
+        bool canPartialLayout = renderer;
+        while (renderer) {
+            if (!renderer->supportsPartialLayout()) {
+                canPartialLayout = false;
+                break;
+            }
+            renderer = renderer->parent();
+        }
+        if (canPartialLayout && view())
+            view()->partialLayout().setStopAtRenderer(stopLayoutAtNode->renderer());
     }
 
     updateLayout();
 
-    m_ignorePendingStylesheets = oldIgnore;
+    if (view())
+        view()->partialLayout().reset();
 }
 
 PassRefPtr<RenderStyle> Document::styleForElementIgnoringPendingStylesheets(Element* element)

Source/core/dom/Document.h
@@ -491,6 +491,7 @@ public:
     void updateStyleForNodeIfNeeded(Node*);
     void updateLayout();
     void updateLayoutIgnorePendingStylesheets();
+    void partialUpdateLayoutIgnorePendingStylesheets(Node*);
     PassRefPtr<RenderStyle> styleForElementIgnoringPendingStylesheets(Element*);
     PassRefPtr<RenderStyle> styleForPage(int pageIndex);
 
@@ -1120,6 +1121,8 @@ private:
     void executeScriptsWaitingForResourcesIfNeeded();
 
     void seamlessParentUpdatedStylesheets();
+
+    void recalcStyleForLayoutIgnoringPendingStylesheets();
 
     PassRefPtr<NodeList> handleZeroPadding(const HitTestRequest&, HitTestResult&) const;
 

LayoutTests/NeverFixTests
@@ -30,6 +30,9 @@ fast/harness/sample-fail-mismatch-reftest.html [ WontFix ]
 # this test will always fail.
 [ Linux Win ] fast/events/option-tab.html [ WontFix ]
 
+# This test only applies to overlay scrollbar platforms.
+[ SnowLeopard Win Linux ] fast/dom/partial-layout-overlay-scrollbars.html [ WontFix ]
+
 # <progress> on Mac is always animated. So it's hard to get a reliable pixel result.
 [ Mac ] fast/dom/HTMLProgressElement/progress-element.html [ WontFix ]
 

LayoutTests/fast/dom/partial-layout-block-expected.txt
@@ -1,0 +1,3 @@
+
+PASS Test that partial layout works for offset{width, height, left, top} methods. 
+

LayoutTests/fast/dom/partial-layout-block.html
@@ -1,0 +1,58 @@
+<!DOCTYPE HTML>
+<html>
+<script src="../../resources/testharness.js"></script>
+<script src="../../resources/testharnessreport.js"></script>
+<link rel="stylesheet" href="../../resources/testharness.css">
+<head>
+<style>
+html, body {
+    margin: 0;
+    padding: 0;
+}
+
+#measure {
+    margin: 10px;
+    padding: 10px;
+}
+
+#fixedsize {
+    width: 100px;
+    height: 100px;
+}
+</style>
+</head>
+<body>
+<div id="measure"><div id="fixedsize"></div></div>
+<script>
+    if (window.testRunner)
+        testRunner.dumpAsText();
+
+    if (window.internals)
+        window.internals.setUsesOverlayScrollbars(true);
+
+    test(function() {
+        var measure = document.getElementById('measure');
+
+        // Record partial layout values for offset*.
+        var measureWidth = measure.offsetWidth;
+        var measureHeight = measure.offsetHeight;
+        var measureTop = measure.offsetTop;
+        var measureLeft = measure.offsetLeft;
+
+        // Invalidate measure and force a full layout.
+        var child = measure.firstChild;
+        measure.removeChild(child);
+        document.body.clientHeight;
+        measure.appendChild(child);
+        var forceLayout = document.body.clientHeight;
+
+        var childOffsetTop = child.offsetTop;
+
+        assert_equals(measureWidth, document.body.offsetWidth - 20);
+        assert_equals(measureHeight, measure.offsetHeight);
+        assert_equals(measureTop, childOffsetTop - 10);
+        assert_equals(measureLeft, measure.offsetLeft);
+    }, 'Test that partial layout works for offset{width, height, left, top} methods.');
+</script>
+</body>
+</html>

LayoutTests/fast/dom/partial-layout-non-overlay-scrollbars-expected.txt
@@ -1,0 +1,3 @@
+
+PASS Test that partial layout works with non-overlay scrollbars. 
+

LayoutTests/fast/dom/partial-layout-non-overlay-scrollbars.html
@@ -1,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+<script src="../../resources/testharness.js"></script>
+<script src="../../resources/testharnessreport.js"></script>
+<link rel="stylesheet" href="../../resources/testharness.css">
+<head>
+<style>
+::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 8px;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+#makespace {
+    height: 5000px;
+}
+</style>
+</head>
+<body>
+<div id="makespace"></div>
+<div id="measure"></div>
+<script>
+    if (window.testRunner)
+        testRunner.dumpAsText();
+
+    if (window.internals)
+        window.internals.setUsesOverlayScrollbars(true); // Note: We force non-overlay scrollbars with CSS.
+
+    var test = async_test("Test that partial layout works with non-overlay scrollbars.");
+    setTimeout(function() {
+        test.step(function() {
+            var measure = document.getElementById('measure');
+            var measureWidth = measure.offsetWidth; // Partial layout occurs here!
+
+            assert_equals(measureWidth, document.body.offsetWidth);
+            assert_equals(measureWidth, window.innerWidth - 8);
+        });
+
+        test.done();
+    }, 0);
+</script>
+</body>
+</html>

LayoutTests/fast/dom/partial-layout-overlay-scrollbars-expected.txt
@@ -1,0 +1,3 @@
+
+PASS Test that partial layout works with overlay scrollbars. 
+

commit 03fc0f82399f5a9449ac2d699f5653b0345ec789

Fix layout regression when updating layout with a pending stylesheet
This patch fixes a regression from r157252[1] where updateLayout() would
not have ignorePendingStylesheets set. This regresison was caused when
recalcStyleForLayoutIgnoringPendingStylesheets was introduced which sets
ignorePendingStylesheets. After this patch, changes to ignore pending
stylesheets are retained while updateLayout is called. The partial
layout codepath has been modified to use IgnoringPendingStylesheet as well,
and an assert has been added to recalcStyleForLayoutIgnoringPendingStylesheets
ensuring stylesheets are ignored.

This change also removes m_ignorePendingStylesheets from Document.
This state has been moved onto StyleEngine but someone forgot to
remove it from Document. Free bits!

[1] https://src.chromium.org/viewvc/blink?view=rev&revision=157252

BUG=322705

Review URL: https://codereview.chromium.org/84873002

git-svn-id: svn://svn.chromium.org/blink/trunk@162638 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/Document.cpp
@@ -1809,10 +1809,11 @@ void Document::setNeedsFocusedElementCheck()
 
 void Document::recalcStyleForLayoutIgnoringPendingStylesheets()
 {
-    if (haveStylesheetsLoaded())
+    ASSERT(m_styleEngine->ignoringPendingStylesheets());
+
+    if (!m_styleEngine->hasPendingSheets())
         return;
 
-    StyleEngine::IgnoringPendingStylesheet ignoring(m_styleEngine.get());
     // FIXME: We are willing to attempt to suppress painting with outdated style info only once.
     // Our assumption is that it would be dangerous to try to stop it a second time, after page
     // content has already been loaded and displayed with accurate style information. (Our
@@ -1840,6 +1841,7 @@ void Document::recalcStyleForLayoutIgnoringPendingStylesheets()
 // to instead suspend JavaScript execution.
 void Document::updateLayoutIgnorePendingStylesheets(Document::RunPostLayoutTasks runPostLayoutTasks)
 {
+    StyleEngine::IgnoringPendingStylesheet ignoring(m_styleEngine.get());
     recalcStyleForLayoutIgnoringPendingStylesheets();
     updateLayout();
     if (runPostLayoutTasks == RunPostLayoutTasksSynchronously && view())
@@ -1855,7 +1857,7 @@ void Document::partialUpdateLayoutIgnorePendingStylesheets(Node* stopLayoutAtNod
         return;
     }
 
-    StyleEngine::ProtectingPendingStylesheet protecting(m_styleEngine.get());
+    StyleEngine::IgnoringPendingStylesheet ignoring(m_styleEngine.get());
     recalcStyleForLayoutIgnoringPendingStylesheets();
 
     if (stopLayoutAtNode) {

Source/core/dom/Document.h
@@ -1093,9 +1093,6 @@ private:
 
     bool m_hasNodesWithPlaceholderStyle;
     bool m_needsNotifyRemoveAllPendingStylesheet;
-    // But sometimes you need to ignore pending stylesheet count to
-    // force an immediate layout when requested by JS.
-    bool m_ignorePendingStylesheets;
     bool m_evaluateMediaQueriesOnStyleRecalc;
 
     // If we do ignore the pending stylesheet count, then we need to add a boolean

Source/core/dom/StyleEngine.h
@@ -83,16 +83,7 @@ public:
         }
     };
 
-    class ProtectingPendingStylesheet : public TemporaryChange<bool> {
-    public:
-        ProtectingPendingStylesheet(StyleEngine* engine)
-            : TemporaryChange<bool>(engine->m_ignorePendingStylesheets, engine->m_ignorePendingStylesheets)
-        {
-        }
-    };
-
     friend class IgnoringPendingStylesheet;
-    friend class ProtectingPendingStylesheet;
 
     static PassOwnPtr<StyleEngine> create(Document& document) { return adoptPtr(new StyleEngine(document)); }
 
@@ -133,6 +124,7 @@ public:
 
     bool hasPendingSheets() const { return m_pendingStylesheets > 0; }
     bool haveStylesheetsLoaded() const { return !hasPendingSheets() || m_ignorePendingStylesheets; }
+    bool ignoringPendingStylesheets() const { return m_ignorePendingStylesheets; }
 
     unsigned maxDirectAdjacentSelectors() const { return m_maxDirectAdjacentSelectors; }
     bool usesSiblingRules() const { return m_usesSiblingRules || m_usesSiblingRulesOverride; }

LayoutTests/fast/html/layout-with-pending-stylesheet-expected.txt
@@ -1,0 +1,5 @@
+PASS successfullyParsed is true
+
+TEST COMPLETE
+PASS document.getElementById("text").clientWidth is document.getElementById("text").clientWidth
+test

LayoutTests/fast/html/layout-with-pending-stylesheet.html
@@ -1,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="../../resources/js-test.js"></script>
+<script>
+    if (window.testRunner)
+        testRunner.dumpAsText();
+
+    function test() {
+        // Create a pending stylesheet, this href intentionally 404s.
+        var linkTag = document.createElement('link');
+        linkTag.href = 'does-not-exist.css';
+        linkTag.rel = 'stylesheet';
+        document.head.appendChild(linkTag);
+
+        var textDiv = document.createElement('div');
+        textDiv.setAttribute('id', 'text');
+        textDiv.appendChild(document.createTextNode('test'));
+        document.getElementById('testDiv').appendChild(textDiv);
+
+        shouldBe('document.getElementById("text").clientWidth', 'document.getElementById("text").clientWidth');
+    }
+</script>
+</head>
+<body onload="test()">
+    <div id="testDiv"></div>
+</body>
+</html>
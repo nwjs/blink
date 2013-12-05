nwjs/blink

commit bc57a502e2cf203fa84bc7f6bece6d478857476d

RenderTable should clip rounded rect in the same way as RenderBox does.
RenderBox::paintBoxDecorations renders background and border into a transparency layer and then clip that in one go.
So RenderTable should do the same way for border-radius.

TESTS=fast/table/border-radius-with-image.html
BUG=243774

Review URL: https://chromiumcodereview.appspot.com/18541007

git-svn-id: svn://svn.chromium.org/blink/trunk@158295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/rendering/RenderBox.cpp
@@ -1107,7 +1107,11 @@ void RenderBox::paintBoxDecorations(PaintInfo& paintInfo, const LayoutPoint& pai
 
     LayoutRect paintRect = borderBoxRectInRegion(paintInfo.renderRegion);
     paintRect.moveBy(paintOffset);
+    paintBoxDecorationsWithRect(paintInfo, paintOffset, paintRect);
+}
 
+void RenderBox::paintBoxDecorationsWithRect(PaintInfo& paintInfo, const LayoutPoint& paintOffset, const LayoutRect& paintRect)
+{
     BackgroundBleedAvoidance bleedAvoidance = determineBackgroundBleedAvoidance(paintInfo.context);
 
     // FIXME: Should eventually give the theme control over whether the box shadow should paint, since controls could have
@@ -1126,6 +1130,14 @@ void RenderBox::paintBoxDecorations(PaintInfo& paintInfo, const LayoutPoint& pai
         paintInfo.context->beginTransparencyLayer(1);
     }
 
+    paintBackgroundWithBorderAndBoxShadow(paintInfo, paintRect, bleedAvoidance);
+
+    if (bleedAvoidance == BackgroundBleedUseTransparencyLayer)
+        paintInfo.context->endLayer();
+}
+
+void RenderBox::paintBackgroundWithBorderAndBoxShadow(PaintInfo& paintInfo, const LayoutRect& paintRect, BackgroundBleedAvoidance bleedAvoidance)
+{
     // If we have a native theme appearance, paint that before painting our background.
     // The theme will tell us whether or not we should also paint the CSS background.
     IntRect snappedPaintRect(pixelSnappedIntRect(paintRect));
@@ -1144,9 +1156,6 @@ void RenderBox::paintBoxDecorations(PaintInfo& paintInfo, const LayoutPoint& pai
     // The theme will tell us whether or not we should also paint the CSS border.
     if (bleedAvoidance != BackgroundBleedBackgroundOverBorder && (!style()->hasAppearance() || (!themePainted && RenderTheme::theme().paintBorderOnly(this, paintInfo, snappedPaintRect))) && style()->hasBorder())
         paintBorder(paintInfo, paintRect, style(), bleedAvoidance);
-
-    if (bleedAvoidance == BackgroundBleedUseTransparencyLayer)
-        paintInfo.context->endLayer();
 }
 
 void RenderBox::paintBackground(const PaintInfo& paintInfo, const LayoutRect& paintRect, BackgroundBleedAvoidance bleedAvoidance)

Source/core/rendering/RenderTable.cpp
@@ -30,6 +30,7 @@
 #include "core/dom/Document.h"
 #include "core/html/HTMLTableElement.h"
 #include "core/page/FrameView.h"
+#include "core/platform/graphics/GraphicsContextStateSaver.h"
 #include "core/rendering/AutoTableLayout.h"
 #include "core/rendering/FixedTableLayout.h"
 #include "core/rendering/HitTestResult.h"
@@ -718,14 +719,15 @@ void RenderTable::paintBoxDecorations(PaintInfo& paintInfo, const LayoutPoint& p
 
     LayoutRect rect(paintOffset, size());
     subtractCaptionRect(rect);
+    paintBoxDecorationsWithRect(paintInfo, paintOffset, rect);
+}
 
-    BackgroundBleedAvoidance bleedAvoidance = determineBackgroundBleedAvoidance(paintInfo.context);
-    if (!boxShadowShouldBeAppliedToBackground(bleedAvoidance))
-        paintBoxShadow(paintInfo, rect, style(), Normal);
+void RenderTable::paintBackgroundWithBorderAndBoxShadow(PaintInfo& paintInfo, const LayoutRect& rect, BackgroundBleedAvoidance bleedAvoidance)
+{
     paintBackground(paintInfo, rect, bleedAvoidance);
     paintBoxShadow(paintInfo, rect, style(), Inset);
 
-    if (style()->hasBorder() && !collapseBorders())
+    if (bleedAvoidance != BackgroundBleedBackgroundOverBorder && !style()->hasAppearance() && style()->hasBorder() && !collapseBorders())
         paintBorder(paintInfo, rect, style());
 }
 

Source/core/rendering/RenderTable.h
@@ -276,6 +276,7 @@ private:
     virtual void paint(PaintInfo&, const LayoutPoint&);
     virtual void paintObject(PaintInfo&, const LayoutPoint&);
     virtual void paintBoxDecorations(PaintInfo&, const LayoutPoint&);
+    virtual void paintBackgroundWithBorderAndBoxShadow(PaintInfo&, const LayoutRect&, BackgroundBleedAvoidance) OVERRIDE;
     virtual void paintMask(PaintInfo&, const LayoutPoint&);
     virtual void layout();
     virtual bool supportsPartialLayout() const OVERRIDE { return false; }

LayoutTests/TestExpectations
@@ -1325,3 +1325,5 @@ crbug.com/246077 virtual/android/fullscreen/full-screen-render-inline.html [ Ima
 
 # Transient. Removes as https://codereview.chromium.org/23597036/ lands.
 crbug.com/254989 virtual/android/fullscreen/full-screen-stacking-context.html [ ImageOnlyFailure ]
+
+crbug.com/243774 fast/table/border-radius-with-image.html [ NeedsRebaseline ]

LayoutTests/fast/table/border-radius-with-image-expected.txt
@@ -1,0 +1,1 @@
+Test crbug.com/243774: background image from rounded corner element bleeds out. If this test passes, the background image will be clipped according to border-radius.

LayoutTests/fast/table/border-radius-with-image.html
@@ -1,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<script>
+if (window.testRunner)
+    testRunner.dumpAsText(true);
+</script>
+<style>
+table {
+    width: 225px;
+    height: 225px;
+    background: url('../backgrounds/resources/bg_position_larger.gif') repeat;
+    border-radius: 50%;
+    border: solid 1px #aaa;
+}
+#description {
+    opacity: 0;
+}
+</style>
+</head>
+<body>
+<div>
+    <table></table>
+</div>
+<div id='description'>
+Test crbug.com/243774: background image from rounded corner element bleeds out.
+If this test passes, the background image will be clipped according to border-radius.
+</div>
+</body>
+

Source/core/rendering/RenderBox.h
@@ -609,12 +609,14 @@ protected:
     virtual bool foregroundIsKnownToBeOpaqueInRect(const LayoutRect& localRect, unsigned maxDepthToTest) const;
     virtual bool computeBackgroundIsKnownToBeObscured() OVERRIDE;
 
+    virtual void paintBackgroundWithBorderAndBoxShadow(PaintInfo&, const LayoutRect&, BackgroundBleedAvoidance);
     void paintBackground(const PaintInfo&, const LayoutRect&, BackgroundBleedAvoidance = BackgroundBleedNone);
 
     void paintFillLayer(const PaintInfo&, const Color&, const FillLayer*, const LayoutRect&, BackgroundBleedAvoidance, CompositeOperator, RenderObject* backgroundObject);
     void paintFillLayers(const PaintInfo&, const Color&, const FillLayer*, const LayoutRect&, BackgroundBleedAvoidance = BackgroundBleedNone, CompositeOperator = CompositeSourceOver, RenderObject* backgroundObject = 0);
 
     void paintMaskImages(const PaintInfo&, const LayoutRect&);
+    void paintBoxDecorationsWithRect(PaintInfo&, const LayoutPoint&, const LayoutRect&);
 
     BackgroundBleedAvoidance determineBackgroundBleedAvoidance(GraphicsContext*) const;
     bool backgroundHasOpaqueTopLayer() const;

commit 5bdce8fd122b4289eec0c219be54dbef411cee35

Should paint border before background when bleedAvoidance is  BackgroundBleedBackgroundOverBorder.
This is a fixing patch for a regression caused by r158295:
http://src.chromium.org/viewvc/blink?revision=158295&view=revision

BUG=305216
TEST=fast/table/table-with-border-radius.html

Review URL: https://codereview.chromium.org/26997002

git-svn-id: svn://svn.chromium.org/blink/trunk@163220 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/rendering/RenderBox.cpp
@@ -1346,7 +1346,7 @@ void RenderBox::paintBackgroundWithBorderAndBoxShadow(PaintInfo& paintInfo, cons
     paintBoxShadow(paintInfo, paintRect, style(), Inset);
 
     // The theme will tell us whether or not we should also paint the CSS border.
-    if (bleedAvoidance != BackgroundBleedBackgroundOverBorder && (!style()->hasAppearance() || (!themePainted && RenderTheme::theme().paintBorderOnly(this, paintInfo, snappedPaintRect))) && style()->hasBorder())
+    if (bleedAvoidance != BackgroundBleedBackgroundOverBorder && (!style()->hasAppearance() || (!themePainted && RenderTheme::theme().paintBorderOnly(this, paintInfo, snappedPaintRect))) && style()->hasBorder() && !(isTable() && toRenderTable(this)->collapseBorders()))
         paintBorder(paintInfo, paintRect, style(), bleedAvoidance);
 }
 

Source/core/rendering/RenderTable.cpp
@@ -733,15 +733,6 @@ void RenderTable::paintBoxDecorations(PaintInfo& paintInfo, const LayoutPoint& p
     paintBoxDecorationsWithRect(paintInfo, paintOffset, rect);
 }
 
-void RenderTable::paintBackgroundWithBorderAndBoxShadow(PaintInfo& paintInfo, const LayoutRect& rect, BackgroundBleedAvoidance bleedAvoidance)
-{
-    paintBackground(paintInfo, rect, bleedAvoidance);
-    paintBoxShadow(paintInfo, rect, style(), Inset);
-
-    if (bleedAvoidance != BackgroundBleedBackgroundOverBorder && !style()->hasAppearance() && style()->hasBorder() && !collapseBorders())
-        paintBorder(paintInfo, rect, style());
-}
-
 void RenderTable::paintMask(PaintInfo& paintInfo, const LayoutPoint& paintOffset)
 {
     if (style()->visibility() != VISIBLE || paintInfo.phase != PaintPhaseMask)

Source/core/rendering/RenderTable.h
@@ -276,7 +276,6 @@ private:
     virtual void paint(PaintInfo&, const LayoutPoint&);
     virtual void paintObject(PaintInfo&, const LayoutPoint&);
     virtual void paintBoxDecorations(PaintInfo&, const LayoutPoint&);
-    virtual void paintBackgroundWithBorderAndBoxShadow(PaintInfo&, const LayoutRect&, BackgroundBleedAvoidance) OVERRIDE;
     virtual void paintMask(PaintInfo&, const LayoutPoint&);
     virtual void layout();
     virtual bool supportsPartialLayout() const OVERRIDE { return false; }

LayoutTests/TestExpectations
@@ -969,3 +969,5 @@ crbug.com/324369 virtual/softwarecompositing/video/video-reflection.html [ Failu
 
 # This test will pass once we implement SVG animation events using SVGDocumentExtensions
 crbug.com/323915 svg/animations/repeatn-remove-add-animation.html [ Failure ]
+crbug.com/305216 fast/table/table-with-border-radius.html [ NeedsRebaseline ]
+crbug.com/305216 fast/table/border-radius-with-image.html [ NeedsRebaseline ]

LayoutTests/fast/table/table-with-border-radius-expected.txt
@@ -1,0 +1,1 @@
+Test for crbug.com/305216: Borders for CSS tables don't display when border-radius is used. If this test passes, rounded black borders will be shown. Otherwise, no black borders will be shown.

LayoutTests/fast/table/table-with-border-radius.html
@@ -1,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<script>
+if (window.testRunner)
+    testRunner.dumpAsTextWithPixelResults();
+</script>
+<style>
+.tableWithBorderRadius {
+    background-color: lightgrey;
+    border: 1px solid black;
+    border-radius: 5px;
+    display: table;
+    width: 100px;
+    height: 100px;
+}
+
+.description {
+    font-size: 0px;
+}
+</style>
+</head>
+<body>
+<div class="tableWithBorderRadius"></div>
+<div class="description">Test for crbug.com/305216: Borders for CSS tables don't display when border-radius is used.<br>
+If this test passes, rounded black borders will be shown.
+</div>
+</body>
+</html>
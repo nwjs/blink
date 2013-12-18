nwjs/blink

commit b394ae9b176062df4958e867f18decdaf36f2b50

Dispatch 'click' event even if a mousedown event target and a mouseup event target are different.
We dispatched 'click' event only if a mousedown event target and a mouseup event
target were identical. It's not practical because a click event was not
dispatched in the following case:

<button>Click me</button>
<style>
button {
    padding: 10px;
}
button:active {
    top: 5px;
    position: relative;
}
</style>
- Click on the top of an inner element.

<div id=outer><div id=inner>Click me</div></div>
- mousedown on the inner element, remove the inner element from
the tree, mouseup on the outer element.

The latest draft of DOM Events specification [1] says:
> should fire click and dblclick events on the nearest common ancestor when the
> event targets of the associated mousedown and mouseup events are different.

We follow it, and the new behavior matches to IE11.

[1] https://dvcs.w3.org/hg/dom3events/raw-file/tip/html/DOM3-Events.html#events-mouseevents

BUG=163854, 310263

Review URL: https://codereview.chromium.org/71293002

git-svn-id: svn://svn.chromium.org/blink/trunk@162081 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/Node.cpp
@@ -920,6 +920,43 @@ bool Node::containsIncludingHostElements(const Node& node) const
     return false;
 }
 
+Node* Node::commonAncestorOverShadowBoundary(const Node& other)
+{
+    if (this == other)
+        return this;
+    if (document() != other.document())
+        return 0;
+    int thisDepth = 0;
+    for (Node* node = this; node; node = node->parentOrShadowHostNode()) {
+        if (node == &other)
+            return node;
+        thisDepth++;
+    }
+    int otherDepth = 0;
+    for (const Node* node = &other; node; node = node->parentOrShadowHostNode()) {
+        if (node == this)
+            return this;
+        otherDepth++;
+    }
+    Node* thisIterator = this;
+    const Node* otherIterator = &other;
+    if (thisDepth > otherDepth) {
+        for (int i = thisDepth; i > otherDepth; --i)
+            thisIterator = thisIterator->parentOrShadowHostNode();
+    } else if (otherDepth > thisDepth) {
+        for (int i = otherDepth; i > thisDepth; --i)
+            otherIterator = otherIterator->parentOrShadowHostNode();
+    }
+    while (thisIterator) {
+        if (thisIterator == otherIterator)
+            return thisIterator;
+        thisIterator = thisIterator->parentOrShadowHostNode();
+        otherIterator = otherIterator->parentOrShadowHostNode();
+    }
+    ASSERT(!otherIterator);
+    return 0;
+}
+
 void Node::reattach(const AttachContext& context)
 {
     AttachContext reattachContext(context);

Source/core/dom/Node.h
@@ -498,6 +498,7 @@ public:
     bool contains(const Node*) const;
     bool containsIncludingShadowDOM(const Node*) const;
     bool containsIncludingHostElements(const Node&) const;
+    Node* commonAncestorOverShadowBoundary(const Node&);
 
     // FIXME: Remove this when crbug.com/265716 cleans up contains semantics.
     bool bindingsContains(const Node* node) const { return containsIncludingShadowDOM(node); }

Source/core/page/EventHandler.cpp
@@ -359,8 +359,15 @@ void EventHandler::clear()
 
 void EventHandler::nodeWillBeRemoved(Node& nodeToBeRemoved)
 {
-    if (nodeToBeRemoved.contains(m_clickNode.get()))
+    if (!nodeToBeRemoved.containsIncludingShadowDOM(m_clickNode.get()))
+        return;
+    if (nodeToBeRemoved.isInShadowTree()) {
+        m_clickNode = nodeToBeRemoved.parentOrShadowHostNode();
+    } else {
+        // We don't dispatch click events if the mousedown node is removed
+        // before a mouseup event. It is compatible with IE and Firefox.
         m_clickNode = 0;
+    }
 }
 
 static void setSelectionIfNeeded(FrameSelection& selection, const VisibleSelection& newSelection)
@@ -1364,10 +1371,10 @@ bool EventHandler::handleMousePressEvent(const PlatformMouseEvent& mouseEvent)
 #endif
 
     m_clickCount = mouseEvent.clickCount();
-    m_clickNode = mev.targetNode();
+    m_clickNode = mev.targetNode()->isTextNode() ?  mev.targetNode()->parentOrShadowHostNode() : mev.targetNode();
 
     if (FrameView* view = m_frame->view()) {
-        RenderLayer* layer = m_clickNode->renderer() ? m_clickNode->renderer()->enclosingLayer() : 0;
+        RenderLayer* layer = mev.targetNode()->renderer() ? mev.targetNode()->renderer()->enclosingLayer() : 0;
         IntPoint p = view->windowToContents(mouseEvent.position());
         if (layer && layer->scrollableArea() && layer->scrollableArea()->isPointInResizeControl(p, ResizerForPointer)) {
             m_resizeScrollableArea = layer->scrollableArea();
@@ -1606,30 +1613,6 @@ void EventHandler::invalidateClick()
     m_clickNode = 0;
 }
 
-inline static bool mouseIsReleasedOnPressedElement(Node* targetNode, Node* clickNode)
-{
-    if (targetNode == clickNode)
-        return true;
-
-    if (!targetNode)
-        return false;
-
-    ShadowRoot* containingShadowRoot = targetNode->containingShadowRoot();
-    if (!containingShadowRoot)
-        return false;
-
-    // FIXME: When an element in UA ShadowDOM (e.g. inner element in <input>) is clicked,
-    // we assume that the host element is clicked. This is necessary for implementing <input type="range"> etc.
-    // However, we should not check ShadowRoot type basically.
-    // https://bugs.webkit.org/show_bug.cgi?id=108047
-    if (containingShadowRoot->type() != ShadowRoot::UserAgentShadowRoot)
-        return false;
-
-    Node* adjustedTargetNode = targetNode->shadowHost();
-    Node* adjustedClickNode = clickNode ? clickNode->shadowHost() : 0;
-    return adjustedTargetNode == adjustedClickNode;
-}
-
 bool EventHandler::handleMouseReleaseEvent(const PlatformMouseEvent& mouseEvent)
 {
     RefPtr<FrameView> protector(m_frame->view());
@@ -1689,7 +1672,11 @@ bool EventHandler::handleMouseReleaseEvent(const PlatformMouseEvent& mouseEvent)
         contextMenuEvent = true;
 #endif
 
-    bool swallowClickEvent = m_clickCount > 0 && !contextMenuEvent && mouseIsReleasedOnPressedElement(mev.targetNode(), m_clickNode.get()) && !dispatchMouseEvent(EventTypeNames::click, mev.targetNode(), true, m_clickCount, mouseEvent, true);
+    bool swallowClickEvent = false;
+    if (m_clickCount > 0 && !contextMenuEvent && mev.targetNode() && m_clickNode) {
+        if (Node* clickTargetNode = mev.targetNode()->commonAncestorOverShadowBoundary(*m_clickNode))
+            swallowClickEvent = !dispatchMouseEvent(EventTypeNames::click, clickTargetNode, true, m_clickCount, mouseEvent, true);
+    }
 
     if (m_resizeScrollableArea) {
         m_resizeScrollableArea->setInResizeMode(false);

LayoutTests/fast/dom/shadow/select-in-shadowdom-expected.txt
@@ -1,9 +1,10 @@
 When selecting from non-anchor Node to anchor node in ShadowDOM, client should not cause page jump.
 
-Selecting from a node to another node in ShadowDOM. This should not fire click event
+Selecting from a node to another node in ShadowDOM. This should fire click event on the host.
+PASS lastClickTarget is host
 
 Clicking a node in ShadowDOM.
-PASS click should be fired.
+PASS lastClickTarget is host
 
 PASS successfullyParsed is true
 

LayoutTests/fast/dom/shadow/select-in-shadowdom.html
@@ -16,29 +16,30 @@ function mouseMoveTo(element)
 }
 
 var shadowRoot = host.createShadowRoot();
-shadowRoot.innerHTML = '<span>select form here</span><span>select to here</span>';
+shadowRoot.innerHTML = '<span>select form here</span><a href="javascript:alert(\'FAIL\');">select to here</a>';
 
-var clickEventShouldNotBeFired = true;
-
+var lastClickTarget = null;
 host.addEventListener('click', function(e) {
-    if (clickEventShouldNotBeFired)
-        debug('FAIL click should not be fired.');
-    else
-        debug('PASS click should be fired.');
+    lastClickTarget = e.target;
 });
+shadowRoot.lastChild.addEventListener('click', function(e) {
+    testFailed('Click event on an anchor element should not be fired.');
+}, false);
 
-debug('Selecting from a node to another node in ShadowDOM. This should not fire click event');
+debug('Selecting from a node to another node in ShadowDOM. This should fire click event on the host.');
 mouseMoveTo(shadowRoot.firstChild);
 eventSender.mouseDown();
 mouseMoveTo(shadowRoot.firstChild.nextSibling);
 eventSender.mouseUp();
+shouldBe('lastClickTarget', 'host');
 debug('');
 
+lastClickTarget = null;
 debug('Clicking a node in ShadowDOM.');
-clickEventShouldNotBeFired = false;
 mouseMoveTo(shadowRoot.firstChild);
 eventSender.mouseDown();
 eventSender.mouseUp();
+shouldBe('lastClickTarget', 'host');
 debug('');
 
 </script>

LayoutTests/fast/events/click-over-descendant-elements-expected.txt
@@ -1,0 +1,28 @@
+Check if any mousedown-mouseup pairs in one element dispatch click events.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+This test needs to run in a test environment.
+Very normal click on container:
+PASS lastClickTarget is container
+Move inside container:
+PASS lastClickTarget is container
+Move from container to a child:
+PASS lastClickTarget is container
+Move from a child to container:
+PASS lastClickTarget is container
+Move from a child to another child:
+PASS lastClickTarget is container
+Move out from a child:
+PASS lastClickTarget is document.body
+Removing a child element in the light DOM during a click:
+PASS lastClickTarget is null
+Click on escaping button content:
+PASS lastClickTarget is button1
+Click on disappearing INPUT value:
+PASS lastClickTarget is input1
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/events/click-over-descendant-elements.html
@@ -1,0 +1,134 @@
+<!DOCTYPE html>
+<body>
+<script src="../../resources/js-test.js"></script>
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    -webkit-user-select: none;
+}
+
+#container {
+    padding: 40px;
+}
+
+#inner1, #inner2 {
+    height: 40px;
+}
+
+#button1 {
+    padding-top: 4px;
+}
+#button1:active {
+    padding-top: 40px;
+}
+</style>
+<div id="container">
+<div id="inner1"></div>
+<div id="inner2"></div>
+</div>
+<button id="button1"><span id="button-span">Click me</span></button>
+<input id="input1" value="A quick brown fox jumps over the lazy dog. A quick brown fox jumps over the lazy dog.">
+<!-- Avoid to create a console element before the above elements -->
+<div id="console"></div>
+<script>
+description('Check if any mousedown-mouseup pairs in one element dispatch click events.');
+if (window.eventSender)
+    debug('This test needs to run in a test environment.');
+
+var container = document.getElementById('container');
+var containerRect = container.getBoundingClientRect();
+var inner1 = document.getElementById('inner1');
+var inner1Rect = inner1.getBoundingClientRect();
+var inner2 = document.getElementById('inner2');
+var inner2Rect = inner2.getBoundingClientRect();
+var x = (containerRect.left + containerRect.right) / 2;
+var inContainer = (containerRect.top + inner1Rect.top) / 2;
+var inInner1 = (inner1Rect.top + inner1Rect.bottom) / 2;
+var inInner2 = (inner2Rect.top + inner2Rect.bottom) / 2;
+
+var lastClickTarget = null;
+document.addEventListener('click', function(event) {
+    lastClickTarget = event.target;
+}, false);
+
+debug('Very normal click on container:');
+eventSender.mouseMoveTo(x, inContainer);
+eventSender.mouseDown();
+eventSender.mouseUp();
+shouldBe('lastClickTarget', 'container');
+lastClickTarget = null;
+
+debug('Move inside container:');
+eventSender.mouseMoveTo(x, inContainer);
+eventSender.mouseDown();
+eventSender.mouseMoveTo(x + 10, inContainer);
+eventSender.mouseUp();
+shouldBe('lastClickTarget', 'container');
+lastClickTarget = null;
+
+debug('Move from container to a child:');
+eventSender.mouseMoveTo(x, inContainer);
+eventSender.mouseDown();
+eventSender.mouseMoveTo(x, inInner1);
+eventSender.mouseUp();
+shouldBe('lastClickTarget', 'container');
+lastClickTarget = null;
+
+debug('Move from a child to container:');
+eventSender.mouseMoveTo(x, inInner1);
+eventSender.mouseDown();
+eventSender.mouseMoveTo(x, inContainer);
+eventSender.mouseUp();
+shouldBe('lastClickTarget', 'container');
+lastClickTarget = null;
+
+debug('Move from a child to another child:');
+eventSender.mouseMoveTo(x, inInner1);
+eventSender.mouseDown();
+eventSender.mouseMoveTo(x, inInner2);
+eventSender.mouseUp();
+shouldBe('lastClickTarget', 'container');
+lastClickTarget = null;
+
+debug('Move out from a child:');
+eventSender.mouseMoveTo(x, inInner1);
+eventSender.mouseDown();
+eventSender.mouseMoveTo(x, containerRect.bottom + 10);
+eventSender.mouseUp();
+shouldBe('lastClickTarget', 'document.body');
+lastClickTarget = null;
+
+debug('Removing a child element in the light DOM during a click:');
+eventSender.mouseMoveTo(x, inInner2);
+eventSender.mouseDown();
+inner2.remove();
+eventSender.mouseUp();
+shouldBeNull('lastClickTarget');
+lastClickTarget = null;
+
+debug('Click on escaping button content:');
+var button1 = document.getElementById("button1");
+var spanRect = document.getElementById('button-span').getBoundingClientRect();
+eventSender.mouseMoveTo((spanRect.left + spanRect.right) / 2, spanRect.top + 1);
+eventSender.mouseDown();
+eventSender.mouseUp();
+shouldBe('lastClickTarget', 'button1');
+lastClickTarget = null;
+
+debug('Click on disappearing INPUT value:');
+var input1 = document.getElementById('input1');
+input1.addEventListener('focus', function() { input1.value = ""; }, false);
+var inputRect = input1.getBoundingClientRect();
+eventSender.mouseMoveTo(inputRect.left + 8, (inputRect.top + inputRect.bottom) / 2);
+eventSender.mouseDown();
+eventSender.mouseUp();
+shouldBe('lastClickTarget', 'input1');
+lastClickTarget = null;
+
+container.remove();
+button1.remove();
+input1.remove();
+
+</script>
+</body>

commit 7d8ae686afae441a21c293d4d18d15a56a77f23c

Don't dispatch a 'click' event for mousedown/mouseup across form controls.
This CL fixes a regression by r162081 [1].

The behavior change by r162081 was correct according to the standard, however it
caused unexpected behavior on some sites.  IE11 doesn't dispatch a click event
if a mousedown event happened on a form control, and a mouseup event happened on
anoter element. We follow it.

[1] http://src.chromium.org/viewvc/blink?revision=162081&view=revision

BUG=328304, 329136
TEST=automated

Review URL: https://codereview.chromium.org/110173005

git-svn-id: svn://svn.chromium.org/blink/trunk@164073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/Node.cpp
@@ -995,20 +995,20 @@ bool Node::containsIncludingHostElements(const Node& node) const
     return false;
 }
 
-Node* Node::commonAncestorOverShadowBoundary(const Node& other)
+Node* Node::commonAncestor(const Node& other, Node* (*parent)(const Node&))
 {
     if (this == other)
         return this;
     if (document() != other.document())
         return 0;
     int thisDepth = 0;
-    for (Node* node = this; node; node = node->parentOrShadowHostNode()) {
+    for (Node* node = this; node; node = parent(*node)) {
         if (node == &other)
             return node;
         thisDepth++;
     }
     int otherDepth = 0;
-    for (const Node* node = &other; node; node = node->parentOrShadowHostNode()) {
+    for (const Node* node = &other; node; node = parent(*node)) {
         if (node == this)
             return this;
         otherDepth++;
@@ -1017,16 +1017,16 @@ Node* Node::commonAncestorOverShadowBoundary(const Node& other)
     const Node* otherIterator = &other;
     if (thisDepth > otherDepth) {
         for (int i = thisDepth; i > otherDepth; --i)
-            thisIterator = thisIterator->parentOrShadowHostNode();
+            thisIterator = parent(*thisIterator);
     } else if (otherDepth > thisDepth) {
         for (int i = otherDepth; i > thisDepth; --i)
-            otherIterator = otherIterator->parentOrShadowHostNode();
+            otherIterator = parent(*otherIterator);
     }
     while (thisIterator) {
         if (thisIterator == otherIterator)
             return thisIterator;
-        thisIterator = thisIterator->parentOrShadowHostNode();
-        otherIterator = otherIterator->parentOrShadowHostNode();
+        thisIterator = parent(*thisIterator);
+        otherIterator = parent(*otherIterator);
     }
     ASSERT(!otherIterator);
     return 0;

Source/core/dom/Node.h
@@ -499,7 +499,7 @@ public:
     bool contains(const Node*) const;
     bool containsIncludingShadowDOM(const Node*) const;
     bool containsIncludingHostElements(const Node&) const;
-    Node* commonAncestorOverShadowBoundary(const Node&);
+    Node* commonAncestor(const Node&, Node* (*parent)(const Node&));
 
     // FIXME: Remove this when crbug.com/265716 cleans up contains semantics.
     bool bindingsContains(const Node* node) const { return containsIncludingShadowDOM(node); }

Source/core/page/EventHandler.cpp
@@ -1612,6 +1612,15 @@ void EventHandler::invalidateClick()
     m_clickNode = 0;
 }
 
+static Node* parentForClickEvent(const Node& node)
+{
+    // IE doesn't dispatch click events for mousedown/mouseup events across form
+    // controls.
+    if (node.isHTMLElement() && toHTMLElement(node).isInteractiveContent())
+        return 0;
+    return node.parentOrShadowHostNode();
+}
+
 bool EventHandler::handleMouseReleaseEvent(const PlatformMouseEvent& mouseEvent)
 {
     RefPtr<FrameView> protector(m_frame->view());
@@ -1676,7 +1685,7 @@ bool EventHandler::handleMouseReleaseEvent(const PlatformMouseEvent& mouseEvent)
 
     bool swallowClickEvent = false;
     if (m_clickCount > 0 && !contextMenuEvent && mev.targetNode() && m_clickNode) {
-        if (Node* clickTargetNode = mev.targetNode()->commonAncestorOverShadowBoundary(*m_clickNode))
+        if (Node* clickTargetNode = mev.targetNode()->commonAncestor(*m_clickNode, parentForClickEvent))
             swallowClickEvent = !dispatchMouseEvent(EventTypeNames::click, clickTargetNode, true, m_clickCount, mouseEvent, true);
     }
 

LayoutTests/fast/dom/shadow/select-in-shadowdom-expected.txt
@@ -1,7 +1,7 @@
 When selecting from non-anchor Node to anchor node in ShadowDOM, client should not cause page jump.
 
-Selecting from a node to another node in ShadowDOM. This should fire click event on the host.
-PASS lastClickTarget is host
+Selecting from a node to another node in ShadowDOM. This should not start page navigation.
+PASS lastClickTarget is null
 
 Clicking a node in ShadowDOM.
 PASS lastClickTarget is host

LayoutTests/fast/dom/shadow/select-in-shadowdom.html
@@ -26,12 +26,12 @@ shadowRoot.lastChild.addEventListener('click', function(e) {
     testFailed('Click event on an anchor element should not be fired.');
 }, false);
 
-debug('Selecting from a node to another node in ShadowDOM. This should fire click event on the host.');
+debug('Selecting from a node to another node in ShadowDOM. This should not start page navigation.');
 mouseMoveTo(shadowRoot.firstChild);
 eventSender.mouseDown();
 mouseMoveTo(shadowRoot.firstChild.nextSibling);
 eventSender.mouseUp();
-shouldBe('lastClickTarget', 'host');
+shouldBeNull('lastClickTarget');
 debug('');
 
 lastClickTarget = null;

LayoutTests/fast/events/click-over-descendant-elements-expected.txt
@@ -22,6 +22,8 @@ Click on escaping button content:
 PASS lastClickTarget is button1
 Click on disappearing INPUT value:
 PASS lastClickTarget is input1
+Mousedown on a form control, and mouseup on an element outside:
+PASS lastClickTarget is null
 PASS successfullyParsed is true
 
 TEST COMPLETE

LayoutTests/fast/events/click-over-descendant-elements.html
@@ -126,6 +126,14 @@ eventSender.mouseUp();
 shouldBe('lastClickTarget', 'input1');
 lastClickTarget = null;
 
+debug('Mousedown on a form control, and mouseup on an element outside:');
+eventSender.mouseMoveTo(inputRect.left + 8, (inputRect.top + inputRect.bottom) / 2);
+eventSender.mouseDown();
+eventSender.mouseMoveTo((spanRect.left + spanRect.right) / 2, spanRect.top + 1);
+eventSender.mouseUp();
+shouldBeNull('lastClickTarget');
+lastClickTarget = null;
+
 container.remove();
 button1.remove();
 input1.remove();
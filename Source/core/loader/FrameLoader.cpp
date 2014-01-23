4bf338cd7018e5bc2ed28581c4f087d1b4995197
Roger <wenrui@gmail.com>
2014-01-23 15:39:44 +0800

    inherit parent iframe's nwuseragent attribute

--- Source/core/loader/FrameLoader.cpp
+++ Source/core/loader/FrameLoader.cpp
@@ -1072,8 +1072,12 @@ String FrameLoader::userAgentOverride() const
 
 String FrameLoader::userAgent(const KURL& url) const
 {
-    if (!m_userAgentOverride.isEmpty())
-        return m_userAgentOverride;
+    Frame* frame = m_frame;
+    for (; frame; frame = frame->tree().parent()) {
+        if (!frame->loader().m_userAgentOverride.isEmpty())
+            return frame->loader().m_userAgentOverride;
+    }
+
     String userAgent = m_client->userAgent(url);
     InspectorInstrumentation::applyUserAgentOverride(m_frame, &userAgent);
     return userAgent;

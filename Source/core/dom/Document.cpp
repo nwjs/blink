nwjs/blink

commit ad7a4934ed5a678790120b9aa6067d64c576985c

Have getElementsByClassName() / getElementsByTagName*() return an HTMLCollection
Have getElementsByClassName() / getElementsByTagName*() return an HTMLCollection
instead of a NodeList, as per the specification:
http://dom.spec.whatwg.org/#document
http://dom.spec.whatwg.org/#element

This behavior is consistent with Firefox 26 and IE 11.

R=arv, haraken, tkent
BUG=235008
BUG=339442

Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=166088

Review URL: https://codereview.chromium.org/143453010

git-svn-id: svn://svn.chromium.org/blink/trunk@166263 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/NodeRareData.h
@@ -27,7 +27,7 @@
 #include "core/dom/LiveNodeList.h"
 #include "core/dom/MutationObserverRegistration.h"
 #include "core/dom/QualifiedName.h"
-#include "core/dom/TagNodeList.h"
+#include "core/dom/TagCollection.h"
 #include "core/page/Page.h"
 #include "wtf/HashSet.h"
 #include "wtf/OwnPtr.h"
@@ -96,7 +96,7 @@ public:
 
     typedef HashMap<std::pair<unsigned char, AtomicString>, LiveNodeListBase*, NodeListCacheMapEntryHash<AtomicString> > NodeListAtomicNameCacheMap;
     typedef HashMap<std::pair<unsigned char, String>, LiveNodeListBase*, NodeListCacheMapEntryHash<String> > NodeListNameCacheMap;
-    typedef HashMap<QualifiedName, TagNodeList*> TagNodeListCacheNS;
+    typedef HashMap<QualifiedName, TagCollection*> TagCollectionCacheNS;
 
     template<typename T>
     PassRefPtr<T> addCacheWithAtomicName(ContainerNode* node, CollectionType collectionType, const AtomicString& name)
@@ -141,14 +141,14 @@ public:
         return list.release();
     }
 
-    PassRefPtr<TagNodeList> addCacheWithQualifiedName(ContainerNode* node, const AtomicString& namespaceURI, const AtomicString& localName)
+    PassRefPtr<TagCollection> addCacheWithQualifiedName(ContainerNode* node, const AtomicString& namespaceURI, const AtomicString& localName)
     {
         QualifiedName name(nullAtom, localName, namespaceURI);
-        TagNodeListCacheNS::AddResult result = m_tagNodeListCacheNS.add(name, 0);
+        TagCollectionCacheNS::AddResult result = m_tagCollectionCacheNS.add(name, 0);
         if (!result.isNewEntry)
             return result.iterator->value;
 
-        RefPtr<TagNodeList> list = TagNodeList::create(node, namespaceURI, localName);
+        RefPtr<TagCollection> list = TagCollection::create(node, namespaceURI, localName);
         result.iterator->value = list.get();
         return list.release();
     }
@@ -169,13 +169,13 @@ public:
         m_nameCaches.remove(namedNodeListKey(collectionType, name));
     }
 
-    void removeCacheWithQualifiedName(LiveNodeList* list, const AtomicString& namespaceURI, const AtomicString& localName)
+    void removeCacheWithQualifiedName(LiveNodeListBase* list, const AtomicString& namespaceURI, const AtomicString& localName)
     {
         QualifiedName name(nullAtom, localName, namespaceURI);
-        ASSERT(list == m_tagNodeListCacheNS.get(name));
+        ASSERT(list == m_tagCollectionCacheNS.get(name));
         if (deleteThisAndUpdateNodeRareDataIfAboutToRemoveLastList(list->ownerNode()))
             return;
-        m_tagNodeListCacheNS.remove(name);
+        m_tagCollectionCacheNS.remove(name);
     }
 
     static PassOwnPtr<NodeListsNodeData> create()
@@ -186,7 +186,7 @@ public:
     void invalidateCaches(const QualifiedName* attrName = 0);
     bool isEmpty() const
     {
-        return m_atomicNameCaches.isEmpty() && m_nameCaches.isEmpty() && m_tagNodeListCacheNS.isEmpty();
+        return m_atomicNameCaches.isEmpty() && m_nameCaches.isEmpty() && m_tagCollectionCacheNS.isEmpty();
     }
 
     void adoptTreeScope()
@@ -213,8 +213,8 @@ public:
                 newDocument.registerNodeList(list);
             }
 
-            TagNodeListCacheNS::const_iterator tagEnd = m_tagNodeListCacheNS.end();
-            for (TagNodeListCacheNS::const_iterator it = m_tagNodeListCacheNS.begin(); it != tagEnd; ++it) {
+            TagCollectionCacheNS::const_iterator tagEnd = m_tagCollectionCacheNS.end();
+            for (TagCollectionCacheNS::const_iterator it = m_tagCollectionCacheNS.begin(); it != tagEnd; ++it) {
                 LiveNodeListBase* list = it->value;
                 ASSERT(!list->isRootedAtDocument());
                 oldDocument.unregisterNodeList(list);
@@ -244,7 +244,7 @@ private:
     NodeList* m_childNodeList;
     NodeListAtomicNameCacheMap m_atomicNameCaches;
     NodeListNameCacheMap m_nameCaches;
-    TagNodeListCacheNS m_tagNodeListCacheNS;
+    TagCollectionCacheNS m_tagCollectionCacheNS;
 };
 
 class NodeMutationObserverData {
@@ -310,7 +310,7 @@ inline bool NodeListsNodeData::deleteThisAndUpdateNodeRareDataIfAboutToRemoveLas
 {
     ASSERT(ownerNode);
     ASSERT(ownerNode->nodeLists() == this);
-    if ((m_childNodeList ? 1 : 0) + m_atomicNameCaches.size() + m_nameCaches.size() + m_tagNodeListCacheNS.size() != 1)
+    if ((m_childNodeList ? 1 : 0) + m_atomicNameCaches.size() + m_nameCaches.size() + m_tagCollectionCacheNS.size() != 1)
         return false;
     ownerNode->clearNodeLists();
     return true;

Source/core/html/HTMLAllCollection.cpp
@@ -84,7 +84,7 @@ void HTMLAllCollection::anonymousNamedGetter(const AtomicString& name, bool& ret
     returnValue0 = NamedNodesCollection::create(namedItems);
 }
 
-PassRefPtr<NodeList> HTMLAllCollection::tags(const AtomicString& name)
+PassRefPtr<HTMLCollection> HTMLAllCollection::tags(const AtomicString& name)
 {
     return ownerNode()->getElementsByTagName(name);
 }

Source/core/html/HTMLCollection.cpp
@@ -24,7 +24,7 @@
 #include "core/html/HTMLCollection.h"
 
 #include "HTMLNames.h"
-#include "core/dom/ClassNodeList.h"
+#include "core/dom/ClassCollection.h"
 #include "core/dom/ElementTraversal.h"
 #include "core/dom/NodeList.h"
 #include "core/dom/NodeRareData.h"
@@ -40,6 +40,9 @@ using namespace HTMLNames;
 static bool shouldOnlyIncludeDirectChildren(CollectionType type)
 {
     switch (type) {
+    case ClassCollectionType:
+    case TagCollectionType:
+    case HTMLTagCollectionType:
     case DocAll:
     case DocAnchors:
     case DocApplets:
@@ -63,10 +66,7 @@ static bool shouldOnlyIncludeDirectChildren(CollectionType type)
     case TableTBodies:
         return true;
     case ChildNodeListType:
-    case ClassNodeListType:
     case NameNodeListType:
-    case TagNodeListType:
-    case HTMLTagNodeListType:
     case RadioNodeListType:
     case RadioImgNodeListType:
     case LabelsNodeListType:
@@ -91,6 +91,9 @@ static NodeListRootType rootTypeFromCollectionType(CollectionType type)
     case DocumentNamedItems:
     case FormControls:
         return NodeListIsRootedAtDocument;
+    case ClassCollectionType:
+    case TagCollectionType:
+    case HTMLTagCollectionType:
     case NodeChildren:
     case TableTBodies:
     case TSectionRows:
@@ -102,10 +105,7 @@ static NodeListRootType rootTypeFromCollectionType(CollectionType type)
     case MapAreas:
         return NodeListIsRootedAtNode;
     case ChildNodeListType:
-    case ClassNodeListType:
     case NameNodeListType:
-    case TagNodeListType:
-    case HTMLTagNodeListType:
     case RadioNodeListType:
     case RadioImgNodeListType:
     case LabelsNodeListType:
@@ -118,6 +118,8 @@ static NodeListRootType rootTypeFromCollectionType(CollectionType type)
 static NodeListInvalidationType invalidationTypeExcludingIdAndNameAttributes(CollectionType type)
 {
     switch (type) {
+    case TagCollectionType:
+    case HTMLTagCollectionType:
     case DocImages:
     case DocEmbeds:
     case DocForms:
@@ -146,11 +148,10 @@ static NodeListInvalidationType invalidationTypeExcludingIdAndNameAttributes(Col
         return InvalidateOnIdNameAttrChange;
     case FormControls:
         return InvalidateForFormControls;
+    case ClassCollectionType:
+        return InvalidateOnClassAttrChange;
     case ChildNodeListType:
-    case ClassNodeListType:
     case NameNodeListType:
-    case TagNodeListType:
-    case HTMLTagNodeListType:
     case RadioNodeListType:
     case RadioImgNodeListType:
     case LabelsNodeListType:
@@ -175,9 +176,11 @@ PassRefPtr<HTMLCollection> HTMLCollection::create(ContainerNode* base, Collectio
 
 HTMLCollection::~HTMLCollection()
 {
-    // HTMLNameCollection removes cache by itself.
-    if (type() != WindowNamedItems && type() != DocumentNamedItems)
+    // HTMLNameCollection, ClassCollection and TagCollection remove cache by themselves.
+    if (type() != WindowNamedItems && type() != DocumentNamedItems && type() != ClassCollectionType
+        && type() != HTMLTagCollectionType && type() != TagCollectionType) {
         ownerNode()->nodeLists()->removeCacheWithAtomicName(this, type());
+    }
 }
 
 void HTMLCollection::invalidateCache() const
@@ -192,7 +195,24 @@ inline bool isMatchingElement(const NodeListType&, const Element&);
 template <> inline bool isMatchingElement(const HTMLCollection& htmlCollection, const Element& element)
 {
     CollectionType type = htmlCollection.type();
-    if (!element.isHTMLElement() && !(type == DocAll || type == NodeChildren || type == WindowNamedItems))
+
+    // These collections apply to any kind of Elements, not just HTMLElements.
+    switch (type) {
+    case DocAll:
+    case NodeChildren:
+        return true;
+    case ClassCollectionType:
+        return static_cast<const ClassCollection&>(htmlCollection).elementMatches(element);
+    case TagCollectionType:
+        return static_cast<const TagCollection&>(htmlCollection).elementMatches(element);
+    case HTMLTagCollectionType:
+        return static_cast<const HTMLTagCollection&>(htmlCollection).elementMatches(element);
+    default:
+        break;
+    }
+
+    // The following only applies to HTMLElements.
+    if (!element.isHTMLElement())
         return false;
 
     switch (type) {
@@ -229,18 +249,17 @@ template <> inline bool isMatchingElement(const HTMLCollection& htmlCollection, 
         return (element.hasLocalName(aTag) || element.hasLocalName(areaTag)) && element.fastHasAttribute(hrefAttr);
     case DocAnchors:
         return element.hasLocalName(aTag) && element.fastHasAttribute(nameAttr);
+    case ClassCollectionType:
+    case TagCollectionType:
+    case HTMLTagCollectionType:
     case DocAll:
     case NodeChildren:
-        return true;
     case FormControls:
     case DocumentNamedItems:
     case TableRows:
     case WindowNamedItems:
     case ChildNodeListType:
-    case ClassNodeListType:
     case NameNodeListType:
-    case TagNodeListType:
-    case HTMLTagNodeListType:
     case RadioNodeListType:
     case RadioImgNodeListType:
     case LabelsNodeListType:
@@ -252,16 +271,6 @@ template <> inline bool isMatchingElement(const HTMLCollection& htmlCollection, 
 template <> inline bool isMatchingElement(const LiveNodeList& nodeList, const Element& element)
 {
     return nodeList.nodeMatches(element);
-}
-
-template <> inline bool isMatchingElement(const HTMLTagNodeList& nodeList, const Element& element)
-{
-    return nodeList.nodeMatchesInlined(element);
-}
-
-template <> inline bool isMatchingElement(const ClassNodeList& nodeList, const Element& element)
-{
-    return nodeList.nodeMatchesInlined(element);
 }
 
 static Node* previousNode(const Node& base, const Node& previous, bool onlyIncludeDirectChildren)
@@ -362,10 +371,6 @@ Node* LiveNodeList::traverseToFirstElement(const ContainerNode& root) const
     switch (type()) {
     case ChildNodeListType:
         return root.firstChild();
-    case HTMLTagNodeListType:
-        return firstMatchingElement(static_cast<const HTMLTagNodeList&>(*this), root);
-    case ClassNodeListType:
-        return firstMatchingElement(static_cast<const ClassNodeList&>(*this), root);
     default:
         return firstMatchingElement(static_cast<const LiveNodeList&>(*this), root);
     }
@@ -378,10 +383,6 @@ Node* LiveNodeList::traverseForwardToOffset(unsigned offset, Node& currentNode, 
     switch (type()) {
     case ChildNodeListType:
         return traverseSiblingsForwardToOffset(offset, currentNode, currentOffset);
-    case HTMLTagNodeListType:
-        return traverseMatchingElementsForwardToOffset(static_cast<const HTMLTagNodeList&>(*this), offset, toElement(currentNode), currentOffset, root);
-    case ClassNodeListType:
-        return traverseMatchingElementsForwardToOffset(static_cast<const ClassNodeList&>(*this), offset, toElement(currentNode), currentOffset, root);
     default:
         return traverseMatchingElementsForwardToOffset(*this, offset, toElement(currentNode), currentOffset, root);
     }

LayoutTests/fast/dom/getElementsByClassName/dumpNodeList.html
@@ -14,7 +14,7 @@
         document.getElementById("console").appendChild(item);
     }
     
-    NodeList.prototype.dump = function()
+    HTMLCollection.prototype.dump = function()
     {
         var result = "";
         var i = 0;

LayoutTests/fast/dom/non-numeric-values-numeric-parameters-expected.txt
@@ -51,7 +51,7 @@ PASS nonNumericPolicy('document.createEvent("MouseEvent").initMouseEvent("a", fa
 PASS nonNumericPolicy('document.createEvent("MouseEvent").initMouseEvent("a", false, false, null, 0, 0, 0, 0, 0, false, false, false, false, x, null)') is 'any type allowed'
 PASS nonNumericPolicy('document.body.attributes.item(x)') is 'any type allowed'
 PASS nonNumericPolicy('document.createNodeIterator(document, x, null, false)') is 'any type allowed'
-PASS nonNumericPolicy('document.getElementsByTagName("div").item(x)') is 'any type allowed (but not omitted)'
+PASS nonNumericPolicy('document.getElementsByTagName("div").item(x)') is 'any type allowed'
 PASS nonNumericPolicy('document.createRange().setStart(document, x)') is 'any type allowed (but not omitted)'
 PASS nonNumericPolicy('document.createRange().setEnd(document, x)') is 'any type allowed (but not omitted)'
 PASS nonNumericPolicy('document.createRange().comparePoint(document, x)') is 'any type allowed (but not omitted)'

LayoutTests/fast/dom/script-tests/domListEnumeration.js
@@ -94,7 +94,7 @@ setup();
 var resultArray = new Array();
 
 // NodeList
-var nodeList = document.getElementsByTagName('ol');
+var nodeList = document.querySelectorAll('ol');
 resultArray = iterateList(nodeList);
 
 shouldBe("resultArray.length", "5");

LayoutTests/fast/dom/script-tests/non-numeric-values-numeric-parameters.js
@@ -294,7 +294,7 @@ shouldBe("nonNumericPolicy('document.createNodeIterator(document, x, null, false
 
 // NodeList
 
-shouldBe("nonNumericPolicy('document.getElementsByTagName(\"div\").item(x)')", "'any type allowed (but not omitted)'");
+shouldBe("nonNumericPolicy('document.getElementsByTagName(\"div\").item(x)')", "'any type allowed'");
 
 // Range
 

LayoutTests/fast/dom/wrapper-classes-expected.txt
@@ -32,10 +32,10 @@ PASS classString(document.createNodeIterator(root, 0, null, false)) is "NodeIter
 FAIL classString(document.createNodeIterator(root, 0, null, false).__proto__) should be NodeIteratorPrototype. Was Object.
 PASS classString(document.createNodeIterator(root, 0, null, false).constructor) is "Function"
 PASS document.createNodeIterator(root, 0, null, false).constructor.name is "NodeIterator"
-PASS classString(document.getElementsByTagName('div')) is "NodeList"
-FAIL classString(document.getElementsByTagName('div').__proto__) should be NodeListPrototype. Was Object.
+PASS classString(document.getElementsByTagName('div')) is "HTMLCollection"
+FAIL classString(document.getElementsByTagName('div').__proto__) should be HTMLCollectionPrototype. Was Object.
 PASS classString(document.getElementsByTagName('div').constructor) is "Function"
-PASS document.getElementsByTagName('div').constructor.name is "NodeList"
+PASS document.getElementsByTagName('div').constructor.name is "HTMLCollection"
 PASS classString(document.createRange()) is "Range"
 FAIL classString(document.createRange().__proto__) should be RangePrototype. Was Object.
 PASS classString(document.createRange().constructor) is "Function"

commit 5b06b91b79098f7d42e480f85be32198315d2440

Do not cause unnecessary node lists invalidation on id/name attribute change
Do not cause unnecessary node lists invalidation on id/name attribute change.
Invalidation now occurs only if there are node lists with a *valid* id / name
cache.

I see a 12% speed up on Dromaeo's dom-attr test. This patch should fix the
performance regression by r166263, which made getElementsByTagName() return an
HTMLCollection instead of a NodeList. According to the profiler, the
performance impact is mainly due to node lists cache invalidation. This is
because HTMLCollection has an id / name cache, while NodeList doesn't.

This CL also improves the id / name cache API naming on HTMLCollection for
clarity.

R=abarth, adamk
BUG=340325

Review URL: https://codereview.chromium.org/159503003

git-svn-id: svn://svn.chromium.org/blink/trunk@167115 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/Document.cpp
@@ -3559,8 +3559,6 @@ void Document::setCSSTarget(Element* n)
 
 void Document::registerNodeList(LiveNodeListBase* list)
 {
-    if (list->hasIdNameCache())
-        m_nodeListCounts[InvalidateOnIdNameAttrChange]++;
     m_nodeListCounts[list->invalidationType()]++;
     if (list->isRootedAtDocument())
         m_listsInvalidatedAtDocument.add(list);
@@ -3568,8 +3566,6 @@ void Document::registerNodeList(LiveNodeListBase* list)
 
 void Document::unregisterNodeList(LiveNodeListBase* list)
 {
-    if (list->hasIdNameCache())
-        m_nodeListCounts[InvalidateOnIdNameAttrChange]--;
     m_nodeListCounts[list->invalidationType()]--;
     if (list->isRootedAtDocument()) {
         ASSERT(m_listsInvalidatedAtDocument.contains(list));

Source/core/dom/Document.h
@@ -649,6 +649,8 @@ public:
 
     void registerNodeList(LiveNodeListBase*);
     void unregisterNodeList(LiveNodeListBase*);
+    void incrementNodeListWithIdNameCacheCount();
+    void decrementNodeListWithIdNameCacheCount();
     bool shouldInvalidateNodeListCaches(const QualifiedName* attrName = 0) const;
     void invalidateNodeListCaches(const QualifiedName* attrName);
 
@@ -1365,6 +1367,17 @@ inline bool Document::shouldOverrideLegacyDescription(ViewportDescription::Type 
     // regardless of which order they appear in the DOM. The priority is given by the
     // ViewportDescription::Type enum.
     return origin >= m_legacyViewportDescription.type;
+}
+
+inline void Document::incrementNodeListWithIdNameCacheCount()
+{
+    m_nodeListCounts[InvalidateOnIdNameAttrChange]++;
+}
+
+inline void Document::decrementNodeListWithIdNameCacheCount()
+{
+    ASSERT(m_nodeListCounts[InvalidateOnIdNameAttrChange] > 0);
+    m_nodeListCounts[InvalidateOnIdNameAttrChange]--;
 }
 
 DEFINE_TYPE_CASTS(Document, ExecutionContextClient, client, client->isDocument(), client.isDocument());

Source/core/dom/LiveNodeList.cpp
@@ -35,6 +35,13 @@ ContainerNode& LiveNodeListBase::rootNode() const
     return *m_ownerNode;
 }
 
+void LiveNodeListBase::didMoveToDocument(Document& oldDocument, Document& newDocument)
+{
+    invalidateCache(&oldDocument);
+    oldDocument.unregisterNodeList(this);
+    newDocument.registerNodeList(this);
+}
+
 void LiveNodeListBase::invalidateIdNameCacheMaps() const
 {
     ASSERT(hasIdNameCache());
@@ -46,7 +53,7 @@ Node* LiveNodeList::virtualOwnerNode() const
     return ownerNode();
 }
 
-void LiveNodeList::invalidateCache() const
+void LiveNodeList::invalidateCache(Document*) const
 {
     m_collectionIndexCache.invalidate();
 }

Source/core/dom/LiveNodeList.h
@@ -66,6 +66,7 @@ public:
 
     ContainerNode& rootNode() const;
 
+    void didMoveToDocument(Document& oldDocument, Document& newDocument);
     ALWAYS_INLINE bool hasIdNameCache() const { return !isLiveNodeListType(type()); }
     ALWAYS_INLINE bool isRootedAtDocument() const { return m_rootType == NodeListIsRootedAtDocument || m_rootType == NodeListIsRootedAtDocumentIfOwnerHasItemrefAttr; }
     ALWAYS_INLINE NodeListInvalidationType invalidationType() const { return static_cast<NodeListInvalidationType>(m_invalidationType); }
@@ -78,7 +79,7 @@ public:
         else if (hasIdNameCache() && (*attrName == HTMLNames::idAttr || *attrName == HTMLNames::nameAttr))
             invalidateIdNameCacheMaps();
     }
-    virtual void invalidateCache() const = 0;
+    virtual void invalidateCache(Document* oldDocument = 0) const = 0;
 
     static bool shouldInvalidateTypeOnAttributeChange(NodeListInvalidationType, const QualifiedName&);
 
@@ -136,7 +137,7 @@ public:
     virtual Node* item(unsigned offset) const OVERRIDE FINAL { return m_collectionIndexCache.nodeAt(*this, offset); }
     virtual bool nodeMatches(const Element&) const = 0;
 
-    virtual void invalidateCache() const OVERRIDE FINAL;
+    virtual void invalidateCache(Document* oldDocument) const OVERRIDE FINAL;
     bool shouldOnlyIncludeDirectChildren() const { return false; }
 
     // Collection IndexCache API.

Source/core/dom/NodeRareData.h
@@ -174,21 +174,18 @@ public:
     void adoptDocument(Document& oldDocument, Document& newDocument)
     {
         ASSERT(oldDocument != newDocument);
-        invalidateCaches();
 
         NodeListAtomicNameCacheMap::const_iterator atomicNameCacheEnd = m_atomicNameCaches.end();
         for (NodeListAtomicNameCacheMap::const_iterator it = m_atomicNameCaches.begin(); it != atomicNameCacheEnd; ++it) {
             LiveNodeListBase* list = it->value;
-            oldDocument.unregisterNodeList(list);
-            newDocument.registerNodeList(list);
+            list->didMoveToDocument(oldDocument, newDocument);
         }
 
         TagCollectionCacheNS::const_iterator tagEnd = m_tagCollectionCacheNS.end();
         for (TagCollectionCacheNS::const_iterator it = m_tagCollectionCacheNS.begin(); it != tagEnd; ++it) {
             LiveNodeListBase* list = it->value;
             ASSERT(!list->isRootedAtDocument());
-            oldDocument.unregisterNodeList(list);
-            newDocument.registerNodeList(list);
+            list->didMoveToDocument(oldDocument, newDocument);
         }
     }
 

Source/core/html/HTMLAllCollection.cpp
@@ -48,7 +48,7 @@ HTMLAllCollection::~HTMLAllCollection()
 
 Element* HTMLAllCollection::namedItemWithIndex(const AtomicString& name, unsigned index) const
 {
-    updateNameCache();
+    updateIdNameCache();
 
     if (Vector<Element*>* cache = idCache(name)) {
         if (index < cache->size())

Source/core/html/HTMLCollection.cpp
@@ -164,7 +164,7 @@ HTMLCollection::HTMLCollection(ContainerNode* ownerNode, CollectionType type, It
     : LiveNodeListBase(ownerNode, rootTypeFromCollectionType(type), invalidationTypeExcludingIdAndNameAttributes(type), type)
     , m_overridesItemAfter(itemAfterOverrideType == OverridesItemAfter)
     , m_shouldOnlyIncludeDirectChildren(shouldTypeOnlyIncludeDirectChildren(type))
-    , m_isNameCacheValid(false)
+    , m_hasValidIdNameCache(false)
 {
     ScriptWrappable::init(this);
 }
@@ -176,6 +176,8 @@ PassRefPtr<HTMLCollection> HTMLCollection::create(ContainerNode* base, Collectio
 
 HTMLCollection::~HTMLCollection()
 {
+    if (hasValidIdNameCache())
+        unregisterIdNameCacheFromDocument(document());
     // HTMLNameCollection, ClassCollection and TagCollection remove cache by themselves.
     if (type() != WindowNamedItems && type() != DocumentNamedItems && type() != ClassCollectionType
         && type() != HTMLTagCollectionType && type() != TagCollectionType) {
@@ -183,10 +185,10 @@ HTMLCollection::~HTMLCollection()
     }
 }
 
-void HTMLCollection::invalidateCache() const
+void HTMLCollection::invalidateCache(Document* oldDocument) const
 {
     m_collectionIndexCache.invalidate();
-    invalidateIdNameCacheMaps();
+    invalidateIdNameCacheMaps(oldDocument);
 }
 
 template <class NodeListType>
@@ -476,7 +478,7 @@ Element* HTMLCollection::namedItem(const AtomicString& name) const
     // attribute. If a match is not found, the method then searches for an
     // object with a matching name attribute, but only on those elements
     // that are allowed a name attribute.
-    updateNameCache();
+    updateIdNameCache();
 
     Vector<Element*>* idResults = idCache(name);
     if (idResults && !idResults->isEmpty())
@@ -529,9 +531,9 @@ void HTMLCollection::namedPropertyEnumerator(Vector<String>& names, ExceptionSta
     supportedPropertyNames(names);
 }
 
-void HTMLCollection::updateNameCache() const
+void HTMLCollection::updateIdNameCache() const
 {
-    if (hasNameCache())
+    if (hasValidIdNameCache())
         return;
 
     ContainerNode& root = rootNode();
@@ -546,7 +548,7 @@ void HTMLCollection::updateNameCache() const
             appendNameCache(nameAttrVal, element);
     }
 
-    setHasNameCache();
+    setHasValidIdNameCache();
 }
 
 void HTMLCollection::namedItems(const AtomicString& name, Vector<RefPtr<Element> >& result) const
@@ -555,7 +557,7 @@ void HTMLCollection::namedItems(const AtomicString& name, Vector<RefPtr<Element>
     if (name.isEmpty())
         return;
 
-    updateNameCache();
+    updateIdNameCache();
 
     Vector<Element*>* idResults = idCache(name);
     Vector<Element*>* nameResults = nameCache(name);

Source/core/html/HTMLCollection.h
@@ -41,7 +41,7 @@ public:
 
     static PassRefPtr<HTMLCollection> create(ContainerNode* base, CollectionType);
     virtual ~HTMLCollection();
-    virtual void invalidateCache() const OVERRIDE;
+    virtual void invalidateCache(Document* oldDocument = 0) const OVERRIDE;
 
     // DOM API
     unsigned length() const { return m_collectionIndexCache.nodeCount(*this); }
@@ -69,9 +69,14 @@ protected:
     bool shouldOnlyIncludeDirectChildren() const { return m_shouldOnlyIncludeDirectChildren; }
     virtual void supportedPropertyNames(Vector<String>& names);
 
-    virtual void updateNameCache() const;
-    bool hasNameCache() const { return m_isNameCacheValid; }
-    void setHasNameCache() const { m_isNameCacheValid = true; }
+    virtual void updateIdNameCache() const;
+    bool hasValidIdNameCache() const { return m_hasValidIdNameCache; }
+    void setHasValidIdNameCache() const
+    {
+        ASSERT(!m_hasValidIdNameCache);
+        m_hasValidIdNameCache = true;
+        document().incrementNodeListWithIdNameCacheCount();
+    }
 
     typedef HashMap<StringImpl*, OwnPtr<Vector<Element*> > > NodeCacheMap;
     Vector<Element*>* idCache(const AtomicString& name) const { return m_idCache.get(name.impl()); }
@@ -83,16 +88,30 @@ private:
     Element* traverseNextElement(Element& previous, const ContainerNode& root) const;
 
     static void append(NodeCacheMap&, const AtomicString&, Element*);
-    void invalidateIdNameCacheMaps() const
+    void invalidateIdNameCacheMaps(Document* oldDocument = 0) const
     {
+        if (!m_hasValidIdNameCache)
+            return;
+
+        // Make sure we decrement the NodeListWithIdNameCache count from
+        // the old document instead of the new one in the case the collection
+        // is moved to a new document.
+        unregisterIdNameCacheFromDocument(oldDocument ? *oldDocument : document());
+
         m_idCache.clear();
         m_nameCache.clear();
-        m_isNameCacheValid = false;
+        m_hasValidIdNameCache = false;
+    }
+
+    void unregisterIdNameCacheFromDocument(Document& document) const
+    {
+        ASSERT(m_hasValidIdNameCache);
+        document.decrementNodeListWithIdNameCacheCount();
     }
 
     const unsigned m_overridesItemAfter : 1;
     const unsigned m_shouldOnlyIncludeDirectChildren : 1;
-    mutable unsigned m_isNameCacheValid : 1;
+    mutable unsigned m_hasValidIdNameCache : 1;
     mutable NodeCacheMap m_idCache;
     mutable NodeCacheMap m_nameCache;
     mutable CollectionIndexCache<HTMLCollection, Element> m_collectionIndexCache;

Source/core/html/HTMLFormControlsCollection.cpp
@@ -104,9 +104,9 @@ Element* HTMLFormControlsCollection::virtualItemAfter(Element* previous) const
     return 0;
 }
 
-void HTMLFormControlsCollection::invalidateCache() const
+void HTMLFormControlsCollection::invalidateCache(Document* oldDocument) const
 {
-    HTMLCollection::invalidateCache();
+    HTMLCollection::invalidateCache(oldDocument);
     m_cachedElement = 0;
     m_cachedElementOffsetInArray = 0;
 }
@@ -150,9 +150,9 @@ Element* HTMLFormControlsCollection::namedItem(const AtomicString& name) const
     return firstNamedItem(formControlElements(), imagesElements, nameAttr, name);
 }
 
-void HTMLFormControlsCollection::updateNameCache() const
+void HTMLFormControlsCollection::updateIdNameCache() const
 {
-    if (hasNameCache())
+    if (hasValidIdNameCache())
         return;
 
     HashSet<StringImpl*> foundInputElements;
@@ -189,7 +189,7 @@ void HTMLFormControlsCollection::updateNameCache() const
         }
     }
 
-    setHasNameCache();
+    setHasValidIdNameCache();
 }
 
 void HTMLFormControlsCollection::namedGetter(const AtomicString& name, bool& radioNodeListEnabled, RefPtr<RadioNodeList>& radioNodeList, bool& elementEnabled, RefPtr<Element>& element)

Source/core/html/HTMLFormControlsCollection.h
@@ -49,13 +49,13 @@ public:
 private:
     explicit HTMLFormControlsCollection(ContainerNode*);
 
-    virtual void updateNameCache() const OVERRIDE;
+    virtual void updateIdNameCache() const OVERRIDE;
     virtual void supportedPropertyNames(Vector<String>& names) OVERRIDE;
 
     const Vector<FormAssociatedElement*>& formControlElements() const;
     const Vector<HTMLImageElement*>& formImageElements() const;
     virtual Element* virtualItemAfter(Element*) const OVERRIDE;
-    virtual void invalidateCache() const OVERRIDE;
+    virtual void invalidateCache(Document* oldDocument = 0) const OVERRIDE;
 
     mutable Element* m_cachedElement;
     mutable unsigned m_cachedElementOffsetInArray;
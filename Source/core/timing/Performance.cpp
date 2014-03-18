nwjs/blink

commit c53a45484bd36c0005b7a1d6bf89637f506bdee5

Fix DOMWindow::isCurrentlyDisplayedInFrame to return false when detached
Right now, this can return true even if the frame is detached, since it
doesn't check that the frame has a non-null host.

The remaining changes in DOMWindow are to preserve current web visible
behavior, as well as fixing a null pointer dereferences that this exposes
in Performance and ScreenOrientation.

BUG=347331
R=abarth@chromium.org

Review URL: https://codereview.chromium.org/131113003

git-svn-id: svn://svn.chromium.org/blink/trunk@168256 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/timing/Performance.cpp
@@ -48,10 +48,9 @@ static const size_t defaultResourceTimingBufferSize = 150;
 Performance::Performance(LocalFrame* frame)
     : DOMWindowProperty(frame)
     , m_resourceTimingBufferSize(defaultResourceTimingBufferSize)
-    , m_referenceTime(frame->document()->loader()->timing()->referenceMonotonicTime())
+    , m_referenceTime(frame->host() ? frame->document()->loader()->timing()->referenceMonotonicTime() : 0.0)
     , m_userTiming(nullptr)
 {
-    ASSERT(m_referenceTime);
     ScriptWrappable::init(this);
 }
 

LayoutTests/fast/dom/Window/property-access-on-cached-properties-after-frame-removed-expected.txt
@@ -1,0 +1,50 @@
+CONSOLE WARNING: 'window.webkitStorageInfo' is deprecated. Please use 'navigator.webkitTemporaryStorage' or 'navigator.webkitPersistentStorage' instead.
+Tests access of cached DOMWindow properties after the associated frame is no longer in a web page. Test should not crash and properties should be set to sane defaults.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS window.cached_history.length is 0
+PASS window.cached_location.href is 'about:blank'
+PASS window.cached_location.origin is 'null'
+PASS window.cached_location.pathname is 'blank'
+PASS window.cached_location.protocol is 'about:'
+PASS window.cached_locationbar.visible is false
+PASS window.cached_menubar.visible is false
+PASS window.cached_navigator.appCodeName is window.navigator.appCodeName
+PASS window.cached_navigator.appName is window.navigator.appName
+PASS window.cached_navigator.appVersion is ''
+PASS window.cached_navigator.cookieEnabled is false
+PASS window.cached_navigator.language is window.navigator.language
+PASS window.cached_navigator.onLine is window.navigator.onLine
+PASS window.cached_navigator.platform is window.navigator.platform
+PASS window.cached_navigator.product is window.navigator.product
+PASS window.cached_navigator.productSub is window.navigator.productSub
+PASS window.cached_navigator.userAgent is ''
+PASS window.cached_navigator.vendor is window.navigator.vendor
+PASS window.cached_performance_timing.connectEnd is 0
+PASS window.cached_performance_timing.connectStart is 0
+PASS window.cached_performance_timing.domLoading is 0
+PASS window.cached_performance_timing.domainLookupEnd is 0
+PASS window.cached_performance_timing.domainLookupStart is 0
+PASS window.cached_performance_timing.fetchStart is 0
+PASS window.cached_performance_timing.navigationStart is 0
+PASS window.cached_performance_timing.requestStart is 0
+PASS window.cached_performance_timing.responseEnd is 0
+PASS window.cached_performance_timing.responseStart is 0
+PASS window.cached_personalbar.visible is false
+PASS window.cached_screen.availHeight is 0
+PASS window.cached_screen.availWidth is 0
+PASS window.cached_screen.colorDepth is 0
+PASS window.cached_screen.height is 0
+PASS window.cached_screen.orientation is ''
+PASS window.cached_screen.pixelDepth is 0
+PASS window.cached_screen.width is 0
+PASS window.cached_scrollbars.visible is false
+PASS window.cached_statusbar.visible is false
+PASS window.cached_styleMedia.type is ''
+PASS window.cached_toolbar.visible is false
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/dom/Window/resources/window-property-collector.js
@@ -1,0 +1,88 @@
+function collectProperties()
+{
+    // Collect properties of the top-level window, since touching the properties
+    // of a DOMWindow affects its internal C++ state.
+    collectPropertiesHelper(window, []);
+
+    propertiesToVerify.sort(function (a, b)
+    {
+        if (a.property < b.property)
+            return -1
+        if (a.property > b.property)
+            return 1;
+        return 0;
+    });
+}
+
+function emitExpectedResult(path, expected)
+{
+    if (path[0] == 'internals' // Skip internals properties, since they aren't web accessible.
+        || path[0] == 'propertiesToVerify' // Skip the list we're building...
+        || path[0] == 'clientInformation') // Just an alias for navigator.
+        return;
+    // FIXME: Skip MemoryInfo for now, since it's not implemented as a DOMWindowProperty, and has
+    // no way of knowing when it's detached. Eventually this should have the same behavior.
+    if (path.length >= 2 && (path[0] == 'console' || path[0] == 'performance') && path[1] == 'memory')
+        return;
+    // Skip things that are assumed to be constants.
+    if (path[path.length - 1].toUpperCase() == path[path.length - 1])
+        return;
+
+    // Various special cases...
+    var propertyPath = path.join('.');
+    switch (propertyPath) {
+    case "location.href":
+        expected = "'about:blank'";
+        break;
+    case "location.origin":
+        expected = "'null'";
+        break;
+    case "location.pathname":
+        expected = "'blank'";
+        break;
+    case "location.protocol":
+        expected = "'about:'";
+        break;
+    case "navigator.appCodeName":
+    case "navigator.appName":
+    case "navigator.language":
+    case "navigator.onLine":
+    case "navigator.platform":
+    case "navigator.product":
+    case "navigator.productSub":
+    case "navigator.vendor":
+        expected = "window." + propertyPath;
+        break;
+    }
+
+    insertExpectedResult(path, expected);
+}
+
+function collectPropertiesHelper(object, path)
+{
+    if (path.length > 20)
+        throw 'Error: probably looping';
+    for (var property in object) {
+        if (!object[property])
+            continue;
+        path.push(property);
+        var type = typeof(object[property]);
+        if (type == "object") {
+            // Skip some traversing through types that will end up in cycles...
+            if (!object[property].Window
+                && !(object[property] instanceof Node)
+                && !(object[property] instanceof MimeTypeArray)
+                && !(object[property] instanceof PluginArray)) {
+                collectPropertiesHelper(object[property], path);
+            }
+        } else if (type == "string") {
+            emitExpectedResult(path, "''");
+        } else if (type == "number") {
+            emitExpectedResult(path, "0");
+        } else if (type == "boolean") {
+            emitExpectedResult(path, "false");
+        }
+        path.pop();
+    }
+}
+

LayoutTests/fast/dom/Window/BarInfo-after-frame-removed-expected.txt
@@ -1,3 +1,0 @@
-This tests accessing the visible property of a BarInfo after the relevant frame is no longer in a web page. If the test did not crash, it passed.
-
-TEST RAN

LayoutTests/fast/dom/Window/BarInfo-after-frame-removed.html
@@ -1,35 +1,0 @@
-<html>
-<head>
-<script>
-
-    if (window.testRunner) {
-        testRunner.dumpAsText();
-        testRunner.waitUntilDone();
-    }
-
-    function runTest()
-    {
-        if (window.testRunner)
-            testRunner.notifyDone();
-
-        var frame = document.getElementById("frame");
-        var childWindow = frame.contentWindow;
-        frame.parentNode.removeChild(frame);
-        childWindow.toolbar.visible;
-        
-        document.getElementById("console").firstChild.data = "TEST RAN";
-    }
-
-</script>
-</head>
-<body>
-
-<p>This tests accessing the visible property of a BarInfo after the relevant frame is no longer in a web page.
-If the test did not crash, it passed.<p>
-
-<pre id="console">TEST DID NOT RUN</pre>
-
-<iframe id="frame" src="about:blank" onload="runTest()">
-
-</body>
-</html>

LayoutTests/fast/dom/Window/open-after-frame-detached-expected.txt
@@ -1,0 +1,10 @@
+Tests that calling window.open on the contentWindow of a detached frame does not crash or open a new window. You may need to disable popup blocking for this test case.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+FAIL childWindow.open('about:blank') should be null (of type object). Was undefined (of type undefined).
+

LayoutTests/fast/dom/Window/open-after-frame-detached.html
@@ -1,0 +1,24 @@
+<html>
+<head>
+<script src="../../../resources/js-test.js"></script>
+<script>
+var childWindow;
+function runTest()
+{
+    description("Tests that calling window.open on the contentWindow of a detached frame does not crash or open a new window. You may need to disable popup blocking for this test case.");
+    if (window.testRunner)
+        testRunner.setCanOpenWindows();
+    var ifr = document.createElement("iframe");
+    ifr.onload = function ()
+    {
+        childWindow = ifr.contentWindow;
+        ifr.parentNode.removeChild(ifr);
+        shouldBeNull("childWindow.open('about:blank')");
+    }
+    document.body.appendChild(ifr);
+}
+</script>
+</head>
+<body onload="runTest()">
+</body>
+</html>

LayoutTests/fast/dom/Window/property-access-on-cached-properties-after-frame-navigated-expected.txt
@@ -1,0 +1,53 @@
+CONSOLE WARNING: 'window.webkitStorageInfo' is deprecated. Please use 'navigator.webkitTemporaryStorage' or 'navigator.webkitPersistentStorage' instead.
+Tests access of cached DOMWindow properties after the associated frame is navigated. Test should not crash and properties should be set to sane defaults.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS window.cached_history.length is 0
+FAIL window.cached_location.href should be about:blank (of type string). Was undefined (of type undefined).
+FAIL window.cached_location.origin should be null (of type string). Was undefined (of type undefined).
+FAIL window.cached_location.pathname should be blank (of type string). Was undefined (of type undefined).
+FAIL window.cached_location.protocol should be about: (of type string). Was undefined (of type undefined).
+PASS window.cached_locationbar.visible is false
+PASS window.cached_menubar.visible is false
+PASS window.cached_navigator.appCodeName is window.navigator.appCodeName
+PASS window.cached_navigator.appName is window.navigator.appName
+PASS window.cached_navigator.appVersion is ''
+PASS window.cached_navigator.cookieEnabled is false
+PASS window.cached_navigator.language is window.navigator.language
+PASS window.cached_navigator.onLine is window.navigator.onLine
+PASS window.cached_navigator.platform is window.navigator.platform
+PASS window.cached_navigator.product is window.navigator.product
+PASS window.cached_navigator.productSub is window.navigator.productSub
+PASS window.cached_navigator.userAgent is ''
+PASS window.cached_navigator.vendor is window.navigator.vendor
+PASS window.cached_performance_timing.connectEnd is 0
+PASS window.cached_performance_timing.connectStart is 0
+PASS window.cached_performance_timing.domContentLoadedEventEnd is 0
+PASS window.cached_performance_timing.domContentLoadedEventStart is 0
+PASS window.cached_performance_timing.domInteractive is 0
+PASS window.cached_performance_timing.domLoading is 0
+PASS window.cached_performance_timing.domainLookupEnd is 0
+PASS window.cached_performance_timing.domainLookupStart is 0
+PASS window.cached_performance_timing.fetchStart is 0
+PASS window.cached_performance_timing.navigationStart is 0
+PASS window.cached_performance_timing.requestStart is 0
+PASS window.cached_performance_timing.responseEnd is 0
+PASS window.cached_performance_timing.responseStart is 0
+PASS window.cached_personalbar.visible is false
+PASS window.cached_screen.availHeight is 0
+PASS window.cached_screen.availWidth is 0
+PASS window.cached_screen.colorDepth is 0
+PASS window.cached_screen.height is 0
+PASS window.cached_screen.orientation is ''
+PASS window.cached_screen.pixelDepth is 0
+PASS window.cached_screen.width is 0
+PASS window.cached_scrollbars.visible is false
+PASS window.cached_statusbar.visible is false
+PASS window.cached_styleMedia.type is ''
+PASS window.cached_toolbar.visible is false
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

commit 78a29bd8cf65c0a53a69e189035d30f2a0f145f3

Add null check for frame when creating WebCore::Performance.
This regression was introduced in r168256. The previous behavior
depended on whether Frame had already been garbage collected after being
removed from the DOM. If it had not been garbage collected yet, then it
would return a Performance object, but if it had, then it would return
null.

This patch adds the now-necessary check that frame is non-null and makes
the behavior consistent whether or not the underlying Frame object has
already been garbage collected, which makes Firefox's behavior.

BUG=350978
R=abarth@chromium.org

Review URL: https://codereview.chromium.org/203053002

git-svn-id: svn://svn.chromium.org/blink/trunk@169467 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/timing/Performance.cpp
@@ -48,7 +48,7 @@ static const size_t defaultResourceTimingBufferSize = 150;
 Performance::Performance(LocalFrame* frame)
     : DOMWindowProperty(frame)
     , m_resourceTimingBufferSize(defaultResourceTimingBufferSize)
-    , m_referenceTime(frame->host() ? frame->document()->loader()->timing()->referenceMonotonicTime() : 0.0)
+    , m_referenceTime(frame && frame->host() ? frame->document()->loader()->timing()->referenceMonotonicTime() : 0.0)
     , m_userTiming(nullptr)
 {
     ScriptWrappable::init(this);

LayoutTests/TestExpectations
@@ -898,6 +898,12 @@ crbug.com/350829 [ Win Debug ] virtual/threaded/animations/missing-keyframe-prop
 crbug.com/350829 [ Linux ] virtual/threaded/animations/missing-keyframe-properties-timing-function.html [ Timeout Pass ]
 crbug.com/350829 [ Win Debug ] virtual/threaded/animations/missing-keyframe-properties-timing-function.html [ Timeout Pass ]
 
+crbug.com/350978 [ Mac ] fast/dom/Window/property-access-on-cached-properties-after-frame-navigated.html [ NeedsRebaseline ]
+crbug.com/350978 [ Mac ] fast/dom/Window/property-access-on-cached-properties-after-frame-removed-and-gced.html [ NeedsRebaseline ]
+crbug.com/350978 [ Mac ] fast/dom/Window/property-access-on-cached-properties-after-frame-removed.html [ NeedsRebaseline ]
+crbug.com/350978 [ Mac ] fast/dom/Window/property-access-on-cached-window-after-frame-removed-and-gced.html [ NeedsRebaseline ]
+crbug.com/350978 [ Mac ] fast/dom/Window/property-access-on-cached-window-after-frame-removed.html [ NeedsRebaseline ]
+
 crbug.com/352377 [ Debug ] svg/animations/repeatn-event-1c.svg [ ImageOnlyFailure Pass ]
 crbug.com/352385 [ Win Debug ] virtual/threaded/animations/3d/transform-perspective.html [ Timeout Pass ]
 crbug.com/352703 [ Win Debug ] virtual/serviceworker/http/tests/serviceworker/registration.html [ Crash Pass ]

LayoutTests/fast/dom/Window/property-access-on-cached-properties-after-frame-removed-and-gced-expected.txt
@@ -1,5 +1,5 @@
 CONSOLE WARNING: 'window.webkitStorageInfo' is deprecated. Please use 'navigator.webkitTemporaryStorage' or 'navigator.webkitPersistentStorage' instead.
-Tests access of cached DOMWindow properties after the associated frame is navigated. Test should not crash and properties should be set to sane defaults.
+Tests access of cached DOMWindow properties after the associated frame is removed from a web page and garbage collected. Test should not crash and properties should be set to sane defaults.
 
 On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
 
@@ -28,9 +28,6 @@ FAIL window.cached_navigator_battery.dischargingTime should be 0. Was Infinity.
 FAIL window.cached_navigator_battery.level should be 0. Was 1.
 PASS window.cached_performance_timing.connectEnd is 0
 PASS window.cached_performance_timing.connectStart is 0
-PASS window.cached_performance_timing.domContentLoadedEventEnd is 0
-PASS window.cached_performance_timing.domContentLoadedEventStart is 0
-PASS window.cached_performance_timing.domInteractive is 0
 PASS window.cached_performance_timing.domLoading is 0
 PASS window.cached_performance_timing.domainLookupEnd is 0
 PASS window.cached_performance_timing.domainLookupStart is 0

LayoutTests/fast/dom/Window/property-access-on-cached-properties-after-frame-removed-and-gced.html
@@ -1,0 +1,47 @@
+<html>
+<head>
+<script src="../../../resources/js-test.js"></script>
+<script src="resources/window-property-collector.js"></script>
+<script>
+var jsTestIsAsync = true;
+
+var childWindow;
+var propertiesToVerify = [];
+
+function insertExpectedResult(path, expected)
+{
+    if (path.length < 2)
+        return;
+    var propertyDir = path.slice(0, -1).join('.');
+    var cachedPropertyDir = "cached_" + propertyDir.replace('.', '_');
+    window[cachedPropertyDir] = eval("childWindow." + propertyDir);
+    propertiesToVerify.push({
+        'property': ["window", cachedPropertyDir, path[path.length - 1]].join("."),
+        'expected': expected,
+    });
+}
+
+function runTest()
+{
+    description("Tests access of cached DOMWindow properties after the associated frame is removed from a web page and garbage collected. Test should not crash and properties should be set to sane defaults.");
+    var frame = document.getElementById("frame");
+    childWindow = frame.contentWindow;
+    collectProperties();
+    frame.parentNode.removeChild(frame);
+    window.setTimeout(verifyResults, 0);
+}
+
+function verifyResults()
+{
+    if (window.gc)
+        gc();
+    for (var i = 0; i < propertiesToVerify.length; ++i)
+        shouldBe(propertiesToVerify[i].property, propertiesToVerify[i].expected);
+    finishJSTest();
+}
+</script>
+</head>
+<body>
+<iframe id="frame" src="about:blank" onload="runTest()"></iframe>
+</body>
+</html>

LayoutTests/fast/dom/Window/property-access-on-cached-properties-after-frame-removed-expected.txt
@@ -22,10 +22,10 @@ PASS window.cached_navigator.product is window.navigator.product
 PASS window.cached_navigator.productSub is window.navigator.productSub
 PASS window.cached_navigator.userAgent is ''
 PASS window.cached_navigator.vendor is window.navigator.vendor
-PASS window.cached_navigator_battery.charging is true
-PASS window.cached_navigator_battery.chargingTime is Infinity
-PASS window.cached_navigator_battery.dischargingTime is Infinity
-PASS window.cached_navigator_battery.level is 1
+FAIL window.cached_navigator_battery.charging should be false. Was true.
+FAIL window.cached_navigator_battery.chargingTime should be 0. Was Infinity.
+FAIL window.cached_navigator_battery.dischargingTime should be 0. Was Infinity.
+FAIL window.cached_navigator_battery.level should be 0. Was 1.
 PASS window.cached_performance_timing.connectEnd is 0
 PASS window.cached_performance_timing.connectStart is 0
 PASS window.cached_performance_timing.domLoading is 0

LayoutTests/fast/dom/Window/property-access-on-cached-window-after-frame-removed-and-gced-expected.txt
@@ -1,5 +1,5 @@
 CONSOLE WARNING: 'window.webkitStorageInfo' is deprecated. Please use 'navigator.webkitTemporaryStorage' or 'navigator.webkitPersistentStorage' instead.
-Tests property access on a cached DOMWindow after the associated frame is no longer in a web page. Test should not crash and properties should be set to sane defaults.
+Tests property access on a cached DOMWindow after the associated frame is removed from a web page and garbage collected. Test should not crash and properties should be set to sane defaults.
 
 On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
 
@@ -8,20 +8,21 @@ PASS childWindow.devicePixelRatio is 0
 PASS childWindow.history.length is 0
 PASS childWindow.innerHeight is 0
 PASS childWindow.innerWidth is 0
+FAIL childWindow.jsTestIsAsync should be false (of type boolean). Was undefined (of type undefined).
 PASS childWindow.length is 0
-PASS childWindow.location.href is 'about:blank'
-PASS childWindow.location.origin is 'null'
-PASS childWindow.location.pathname is 'blank'
-PASS childWindow.location.protocol is 'about:'
+FAIL childWindow.location.href should be about:blank (of type string). Was undefined (of type undefined).
+FAIL childWindow.location.origin should be null (of type string). Was undefined (of type undefined).
+FAIL childWindow.location.pathname should be blank (of type string). Was undefined (of type undefined).
+FAIL childWindow.location.protocol should be about: (of type string). Was undefined (of type undefined).
 PASS childWindow.locationbar.visible is false
 PASS childWindow.menubar.visible is false
 PASS childWindow.navigator.appCodeName is window.navigator.appCodeName
 PASS childWindow.navigator.appName is window.navigator.appName
 PASS childWindow.navigator.appVersion is ''
-FAIL childWindow.navigator.battery.charging should be false. Was true.
-FAIL childWindow.navigator.battery.chargingTime should be 0. Was Infinity.
-FAIL childWindow.navigator.battery.dischargingTime should be 0. Was Infinity.
-FAIL childWindow.navigator.battery.level should be 0. Was 1.
+FAIL childWindow.navigator.battery.charging should be false. Threw exception TypeError: Cannot read property 'charging' of null
+FAIL childWindow.navigator.battery.chargingTime should be 0. Threw exception TypeError: Cannot read property 'chargingTime' of null
+FAIL childWindow.navigator.battery.dischargingTime should be 0. Threw exception TypeError: Cannot read property 'dischargingTime' of null
+FAIL childWindow.navigator.battery.level should be 0. Threw exception TypeError: Cannot read property 'level' of null
 PASS childWindow.navigator.cookieEnabled is false
 PASS childWindow.navigator.language is window.navigator.language
 PASS childWindow.navigator.onLine is window.navigator.onLine

LayoutTests/fast/dom/Window/property-access-on-cached-window-after-frame-removed-and-gced.html
@@ -1,0 +1,40 @@
+<html>
+<head>
+<script src="../../../resources/js-test.js"></script>
+<script src="resources/window-property-collector.js"></script>
+<script>
+var jsTestIsAsync = true;
+
+var childWindow;
+var propertiesToVerify = [];
+
+function insertExpectedResult(path, expected)
+{
+    var propertyPath = path.join('.');
+    propertiesToVerify.push({'property': "childWindow." + propertyPath, 'expected': expected});
+}
+
+function runTest()
+{
+    description("Tests property access on a cached DOMWindow after the associated frame is removed from a web page and garbage collected. Test should not crash and properties should be set to sane defaults.");
+    var frame = document.getElementById("frame");
+    childWindow = frame.contentWindow;
+    collectProperties();
+    frame.parentNode.removeChild(frame);
+    window.setTimeout(verifyResults, 0);
+}
+
+function verifyResults()
+{
+    if (window.gc)
+        gc();
+    for (var i = 0; i < propertiesToVerify.length; ++i)
+        shouldBe(propertiesToVerify[i].property, propertiesToVerify[i].expected);
+    finishJSTest();
+}
+</script>
+</head>
+<body>
+<iframe id="frame" src="about:blank" onload="runTest()"></iframe>
+</body>
+</html>

LayoutTests/fast/dom/Window/resources/window-property-collector.js
@@ -35,7 +35,7 @@ function emitExpectedResult(path, expected)
     if (path[path.length - 1].toUpperCase() == path[path.length - 1])
         return;
 
-    // Various special cases...
+    // Various special cases for legacy reasons. Please do not add entries to this list.
     var propertyPath = path.join('.');
     switch (propertyPath) {
     case "location.href":
@@ -59,18 +59,6 @@ function emitExpectedResult(path, expected)
     case "navigator.productSub":
     case "navigator.vendor":
         expected = "window." + propertyPath;
-        break;
-    case "navigator.battery.charging":
-        expected = "true";
-        break;
-    case "navigator.battery.chargingTime":
-        expected = "Infinity";
-        break;
-    case "navigator.battery.dischargingTime":
-        expected = "Infinity";
-        break;
-    case "navigator.battery.level":
-        expected = "1";
         break;
     }
 
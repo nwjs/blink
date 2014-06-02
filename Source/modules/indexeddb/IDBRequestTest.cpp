nwjs/blink

commit 05f1d3989956fb83a6005181cf9484753f86822d

Remove ScriptState::current() from IDBRequest
- ScriptState::current() should be called only from the binding layer. This CL removes ScriptState::current() from IDBRequest. For that goal, this CL passes ScriptState around in a lot of places in IDB code.

- This CL also introduces ScriptStateForTesting. It's hard to use ScriptState for testing purposes because webkit_unit_tests doesn't create DOMWindow nor ExecutionContext, and thus ScriptState cannot be associated with a valid ExecutionContext. On the other hand, ScriptStateForTesting allows us to mock the ExecutionContext for testing purpose.

- This CL renames V8ExecutionScope to V8TestingScope. This is because V8TestingScope uses ScriptStateForTesting, and thus it's only for testing.

- This CL stops using V8ExecutionScope in V8GCController, because V8GCController is not for testing.

BUG=357144
TBR=adamk

Review URL: https://codereview.chromium.org/295163005

git-svn-id: svn://svn.chromium.org/blink/trunk@174937 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/bindings/v8/ScriptState.cpp
@@ -6,6 +6,7 @@
 #include "bindings/v8/ScriptState.h"
 
 #include "bindings/v8/V8Binding.h"
+#include "core/dom/ExecutionContext.h"
 #include "core/frame/LocalFrame.h"
 
 namespace WebCore {
@@ -68,6 +69,11 @@ ExecutionContext* ScriptState::executionContext() const
     return toExecutionContext(context());
 }
 
+void ScriptState::setExecutionContext(PassRefPtr<ExecutionContext>)
+{
+    ASSERT_NOT_REACHED();
+}
+
 DOMWindow* ScriptState::domWindow() const
 {
     v8::HandleScope scope(m_isolate);
@@ -79,6 +85,30 @@ ScriptState* ScriptState::forMainWorld(LocalFrame* frame)
     v8::Isolate* isolate = toIsolate(frame);
     v8::HandleScope handleScope(isolate);
     return ScriptState::from(toV8Context(isolate, frame, DOMWrapperWorld::mainWorld()));
+}
+
+PassRefPtr<ScriptStateForTesting> ScriptStateForTesting::create(v8::Handle<v8::Context> context, PassRefPtr<DOMWrapperWorld> world)
+{
+    RefPtr<ScriptStateForTesting> scriptState = adoptRef(new ScriptStateForTesting(context, world));
+    // This ref() is for keeping this ScriptState alive as long as the v8::Context is alive.
+    // This is deref()ed in the weak callback of the v8::Context.
+    scriptState->ref();
+    return scriptState;
+}
+
+ScriptStateForTesting::ScriptStateForTesting(v8::Handle<v8::Context> context, PassRefPtr<DOMWrapperWorld> world)
+    : ScriptState(context, world)
+{
+}
+
+ExecutionContext* ScriptStateForTesting::executionContext() const
+{
+    return m_executionContext.get();
+}
+
+void ScriptStateForTesting::setExecutionContext(PassRefPtr<ExecutionContext> executionContext)
+{
+    m_executionContext = executionContext;
 }
 
 }

Source/bindings/v8/ScriptState.h
@@ -46,7 +46,7 @@ public:
     };
 
     static PassRefPtr<ScriptState> create(v8::Handle<v8::Context>, PassRefPtr<DOMWrapperWorld>);
-    ~ScriptState();
+    virtual ~ScriptState();
 
     static ScriptState* current(v8::Isolate* isolate)
     {
@@ -68,8 +68,9 @@ public:
 
     v8::Isolate* isolate() const { return m_isolate; }
     DOMWrapperWorld& world() const { return *m_world; }
-    ExecutionContext* executionContext() const;
     DOMWindow* domWindow() const;
+    virtual ExecutionContext* executionContext() const;
+    virtual void setExecutionContext(PassRefPtr<ExecutionContext>);
 
     // This can return an empty handle if the v8::Context is gone.
     v8::Handle<v8::Context> context() const { return m_context.newLocal(m_isolate); }
@@ -83,9 +84,10 @@ public:
     void setEvalEnabled(bool);
     ScriptValue getFromGlobalObject(const char* name);
 
-private:
+protected:
     ScriptState(v8::Handle<v8::Context>, PassRefPtr<DOMWrapperWorld>);
 
+private:
     v8::Isolate* m_isolate;
     // This persistent handle is weak.
     ScopedPersistent<v8::Context> m_context;
@@ -98,6 +100,19 @@ private:
     // So you must explicitly clear the OwnPtr by calling disposePerContextData()
     // once you no longer need V8PerContextData. Otherwise, the v8::Context will leak.
     OwnPtr<V8PerContextData> m_perContextData;
+};
+
+class ScriptStateForTesting : public ScriptState {
+public:
+    static PassRefPtr<ScriptStateForTesting> create(v8::Handle<v8::Context>, PassRefPtr<DOMWrapperWorld>);
+
+    virtual ExecutionContext* executionContext() const OVERRIDE;
+    virtual void setExecutionContext(PassRefPtr<ExecutionContext>) OVERRIDE;
+
+private:
+    ScriptStateForTesting(v8::Handle<v8::Context>, PassRefPtr<DOMWrapperWorld>);
+
+    RefPtr<ExecutionContext> m_executionContext;
 };
 
 // ScriptStateProtectingContext keeps the context associated with the ScriptState alive.

Source/modules/indexeddb/IDBRequestTest.cpp
@@ -74,25 +74,23 @@ NullExecutionContext::NullExecutionContext()
 class IDBRequestTest : public testing::Test {
 public:
     IDBRequestTest()
-        : m_scope(V8ExecutionScope::create(v8::Isolate::GetCurrent()))
-        , m_context(adoptRef(new NullExecutionContext()))
+        : m_scope(V8TestingScope::create(v8::Isolate::GetCurrent()))
     {
+        m_scope->scriptState()->setExecutionContext(adoptRef(new NullExecutionContext()));
     }
 
-    ExecutionContext* executionContext()
-    {
-        return m_context.get();
-    }
+    v8::Isolate* isolate() const { return m_scope->isolate(); }
+    ScriptState* scriptState() const { return m_scope->scriptState(); }
+    ExecutionContext* executionContext() const { return m_scope->scriptState()->executionContext(); }
 
 private:
-    OwnPtr<V8ExecutionScope> m_scope;
-    RefPtr<ExecutionContext> m_context;
+    OwnPtr<V8TestingScope> m_scope;
 };
 
 TEST_F(IDBRequestTest, EventsAfterStopping)
 {
     IDBTransaction* transaction = 0;
-    IDBRequest* request = IDBRequest::create(executionContext(), IDBAny::createUndefined(), transaction);
+    IDBRequest* request = IDBRequest::create(scriptState(), IDBAny::createUndefined(), transaction);
     EXPECT_EQ(request->readyState(), "pending");
     executionContext()->stopActiveDOMObjects();
 
@@ -111,7 +109,7 @@ TEST_F(IDBRequestTest, EventsAfterStopping)
 TEST_F(IDBRequestTest, AbortErrorAfterAbort)
 {
     IDBTransaction* transaction = 0;
-    IDBRequest* request = IDBRequest::create(executionContext(), IDBAny::createUndefined(), transaction);
+    IDBRequest* request = IDBRequest::create(scriptState(), IDBAny::createUndefined(), transaction);
     EXPECT_EQ(request->readyState(), "pending");
 
     // Simulate the IDBTransaction having received onAbort from back end and aborting the request:
@@ -158,7 +156,7 @@ TEST_F(IDBRequestTest, ConnectionsAfterStopping)
 
     {
         OwnPtr<MockWebIDBDatabase> backend = MockWebIDBDatabase::create();
-        IDBOpenDBRequest* request = IDBOpenDBRequest::create(executionContext(), callbacks, transactionId, version);
+        IDBOpenDBRequest* request = IDBOpenDBRequest::create(scriptState(), callbacks, transactionId, version);
         EXPECT_EQ(request->readyState(), "pending");
 
         executionContext()->stopActiveDOMObjects();
@@ -167,7 +165,7 @@ TEST_F(IDBRequestTest, ConnectionsAfterStopping)
 
     {
         OwnPtr<MockWebIDBDatabase> backend = MockWebIDBDatabase::create();
-        IDBOpenDBRequest* request = IDBOpenDBRequest::create(executionContext(), callbacks, transactionId, version);
+        IDBOpenDBRequest* request = IDBOpenDBRequest::create(scriptState(), callbacks, transactionId, version);
         EXPECT_EQ(request->readyState(), "pending");
 
         executionContext()->stopActiveDOMObjects();

Source/modules/indexeddb/IDBTransactionTest.cpp
@@ -50,19 +50,17 @@ namespace {
 class IDBTransactionTest : public testing::Test {
 public:
     IDBTransactionTest()
-        : m_scope(V8ExecutionScope::create(v8::Isolate::GetCurrent()))
-        , m_document(Document::create())
+        : m_scope(V8TestingScope::create(v8::Isolate::GetCurrent()))
     {
+        m_scope->scriptState()->setExecutionContext(Document::create());
     }
 
-    ExecutionContext* executionContext()
-    {
-        return m_document.get();
-    }
+    v8::Isolate* isolate() const { return m_scope->isolate(); }
+    ScriptState* scriptState() const { return m_scope->scriptState(); }
+    ExecutionContext* executionContext() { return m_scope->scriptState()->executionContext(); }
 
 private:
-    OwnPtr<V8ExecutionScope> m_scope;
-    RefPtr<Document> m_document;
+    OwnPtr<V8TestingScope> m_scope;
 };
 
 class FakeWebIDBDatabase FINAL : public blink::WebIDBDatabase {
@@ -102,7 +100,7 @@ TEST_F(IDBTransactionTest, EnsureLifetime)
     Heap::collectAllGarbage();
     EXPECT_EQ(1u, set.size());
 
-    Persistent<IDBRequest> request = IDBRequest::create(executionContext(), IDBAny::createUndefined(), transaction);
+    Persistent<IDBRequest> request = IDBRequest::create(scriptState(), IDBAny::createUndefined(), transaction.get());
     IDBPendingTransactionMonitor::from(*executionContext()).deactivateNewTransactions();
 
     Heap::collectAllGarbage();

Source/bindings/v8/IDBBindingUtilitiesTest.cpp
@@ -90,11 +90,11 @@ void checkKeyPathNumberValue(v8::Isolate* isolate, const ScriptValue& value, con
 class IDBKeyFromValueAndKeyPathTest : public testing::Test {
 public:
     IDBKeyFromValueAndKeyPathTest()
-        : m_scope(V8ExecutionScope::create(v8::Isolate::GetCurrent()))
+        : m_scope(V8TestingScope::create(v8::Isolate::GetCurrent()))
     {
     }
 private:
-    OwnPtr<V8ExecutionScope> m_scope;
+    OwnPtr<V8TestingScope> m_scope;
 };
 
 TEST_F(IDBKeyFromValueAndKeyPathTest, TopLevelPropertyStringValue)

Source/bindings/v8/ScriptPromiseResolverTest.cpp
@@ -64,7 +64,7 @@ private:
 class ScriptPromiseResolverTest : public testing::Test {
 public:
     ScriptPromiseResolverTest()
-        : m_scope(V8ExecutionScope::create(v8::Isolate::GetCurrent()))
+        : m_scope(V8TestingScope::create(v8::Isolate::GetCurrent()))
     {
         m_resolver = ScriptPromiseResolver::create(m_scope->scriptState());
     }
@@ -79,7 +79,7 @@ public:
 
 protected:
     RefPtr<ScriptPromiseResolver> m_resolver;
-    OwnPtr<V8ExecutionScope> m_scope;
+    OwnPtr<V8TestingScope> m_scope;
 };
 
 TEST_F(ScriptPromiseResolverTest, initialState)

Source/bindings/v8/ScriptPromiseTest.cpp
@@ -70,7 +70,7 @@ private:
 class ScriptPromiseTest : public testing::Test {
 public:
     ScriptPromiseTest()
-        : m_scope(V8ExecutionScope::create(v8::Isolate::GetCurrent()))
+        : m_scope(V8TestingScope::create(v8::Isolate::GetCurrent()))
     {
     }
 
@@ -87,7 +87,7 @@ public:
     v8::Isolate* isolate() const { return m_scope->isolate(); }
 
 protected:
-    OwnPtr<V8ExecutionScope> m_scope;
+    OwnPtr<V8TestingScope> m_scope;
 };
 
 TEST_F(ScriptPromiseTest, constructFromNonPromise)

Source/bindings/v8/V8Binding.cpp
@@ -745,21 +745,31 @@ PassRefPtr<JSONValue> v8ToJSONValue(v8::Isolate* isolate, v8::Handle<v8::Value> 
     return nullptr;
 }
 
-PassOwnPtr<V8ExecutionScope> V8ExecutionScope::create(v8::Isolate* isolate)
+PassOwnPtr<V8TestingScope> V8TestingScope::create(v8::Isolate* isolate)
 {
-    return adoptPtr(new V8ExecutionScope(isolate));
+    return adoptPtr(new V8TestingScope(isolate));
 }
 
-V8ExecutionScope::V8ExecutionScope(v8::Isolate* isolate)
+V8TestingScope::V8TestingScope(v8::Isolate* isolate)
     : m_handleScope(isolate)
     , m_contextScope(v8::Context::New(isolate))
-    , m_scriptState(ScriptState::create(isolate->GetCurrentContext(), DOMWrapperWorld::create()))
+    , m_scriptState(ScriptStateForTesting::create(isolate->GetCurrentContext(), DOMWrapperWorld::create()))
 {
 }
 
-V8ExecutionScope::~V8ExecutionScope()
+V8TestingScope::~V8TestingScope()
 {
     m_scriptState->disposePerContextData();
+}
+
+ScriptState* V8TestingScope::scriptState() const
+{
+    return m_scriptState.get();
+}
+
+v8::Isolate* V8TestingScope::isolate() const
+{
+    return m_scriptState->isolate();
 }
 
 void GetDevToolsFunctionInfo(v8::Handle<v8::Function> function, v8::Isolate* isolate, int& scriptId, String& resourceName, int& lineNumber)
@@ -784,16 +794,6 @@ PassRefPtr<TraceEvent::ConvertableToTraceFormat> devToolsTraceEventData(Executio
     int lineNumber = 1;
     GetDevToolsFunctionInfo(function, isolate, scriptId, resourceName, lineNumber);
     return InspectorFunctionCallEvent::data(context, scriptId, resourceName, lineNumber);
-}
-
-ScriptState* V8ExecutionScope::scriptState() const
-{
-    return m_scriptState.get();
-}
-
-v8::Isolate* V8ExecutionScope::isolate() const
-{
-    return m_scriptState->isolate();
 }
 
 } // namespace WebCore

commit 4c5462d08d858f793aa448a6d6ae962f45bafdad

Fix test failures of IDBTransactionTest.EnsureLifetime with ENABLE(OILPAN)
This is a regression caused by r174937.
After r174937, ExecutionContexts created for IDBRequestTest and IDBTransactionTest
outlive the tests (because the lifetime of the ExecutionContexts is bound to a ScriptState
associated with the tests). This is wrong, and we should clear the ExecutionContexts
when shutting down the tests.

BUG=379616

Review URL: https://codereview.chromium.org/307933011

git-svn-id: svn://svn.chromium.org/blink/trunk@175259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/bindings/v8/ScriptState.cpp
@@ -69,7 +69,7 @@ ExecutionContext* ScriptState::executionContext() const
     return toExecutionContext(context());
 }
 
-void ScriptState::setExecutionContext(PassRefPtr<ExecutionContext>)
+void ScriptState::setExecutionContext(ExecutionContext*)
 {
     ASSERT_NOT_REACHED();
 }
@@ -103,10 +103,10 @@ ScriptStateForTesting::ScriptStateForTesting(v8::Handle<v8::Context> context, Pa
 
 ExecutionContext* ScriptStateForTesting::executionContext() const
 {
-    return m_executionContext.get();
+    return m_executionContext;
 }
 
-void ScriptStateForTesting::setExecutionContext(PassRefPtr<ExecutionContext> executionContext)
+void ScriptStateForTesting::setExecutionContext(ExecutionContext* executionContext)
 {
     m_executionContext = executionContext;
 }

Source/bindings/v8/ScriptState.h
@@ -70,7 +70,7 @@ public:
     DOMWrapperWorld& world() const { return *m_world; }
     DOMWindow* domWindow() const;
     virtual ExecutionContext* executionContext() const;
-    virtual void setExecutionContext(PassRefPtr<ExecutionContext>);
+    virtual void setExecutionContext(ExecutionContext*);
 
     // This can return an empty handle if the v8::Context is gone.
     v8::Handle<v8::Context> context() const { return m_context.newLocal(m_isolate); }
@@ -107,12 +107,12 @@ public:
     static PassRefPtr<ScriptStateForTesting> create(v8::Handle<v8::Context>, PassRefPtr<DOMWrapperWorld>);
 
     virtual ExecutionContext* executionContext() const OVERRIDE;
-    virtual void setExecutionContext(PassRefPtr<ExecutionContext>) OVERRIDE;
+    virtual void setExecutionContext(ExecutionContext*) OVERRIDE;
 
 private:
     ScriptStateForTesting(v8::Handle<v8::Context>, PassRefPtr<DOMWrapperWorld>);
 
-    RefPtr<ExecutionContext> m_executionContext;
+    ExecutionContext* m_executionContext;
 };
 
 // ScriptStateProtectingContext keeps the context associated with the ScriptState alive.

Source/modules/indexeddb/IDBRequestTest.cpp
@@ -75,8 +75,14 @@ class IDBRequestTest : public testing::Test {
 public:
     IDBRequestTest()
         : m_scope(v8::Isolate::GetCurrent())
+        , m_executionContext(adoptRef(new NullExecutionContext()))
     {
-        m_scope.scriptState()->setExecutionContext(adoptRef(new NullExecutionContext()));
+        m_scope.scriptState()->setExecutionContext(m_executionContext.get());
+    }
+
+    ~IDBRequestTest()
+    {
+        m_scope.scriptState()->setExecutionContext(0);
     }
 
     v8::Isolate* isolate() const { return m_scope.isolate(); }
@@ -85,6 +91,7 @@ public:
 
 private:
     V8TestingScope m_scope;
+    RefPtr<ExecutionContext> m_executionContext;
 };
 
 TEST_F(IDBRequestTest, EventsAfterStopping)

Source/modules/indexeddb/IDBTransactionTest.cpp
@@ -51,8 +51,14 @@ class IDBTransactionTest : public testing::Test {
 public:
     IDBTransactionTest()
         : m_scope(v8::Isolate::GetCurrent())
+        , m_executionContext(Document::create())
     {
-        m_scope.scriptState()->setExecutionContext(Document::create());
+        m_scope.scriptState()->setExecutionContext(m_executionContext.get());
+    }
+
+    ~IDBTransactionTest()
+    {
+        m_scope.scriptState()->setExecutionContext(0);
     }
 
     v8::Isolate* isolate() const { return m_scope.isolate(); }
@@ -61,6 +67,7 @@ public:
 
 private:
     V8TestingScope m_scope;
+    RefPtr<ExecutionContext> m_executionContext;
 };
 
 class FakeWebIDBDatabase FINAL : public blink::WebIDBDatabase {
@@ -86,13 +93,7 @@ private:
     FakeIDBDatabaseCallbacks() { }
 };
 
-// crbug.com/379616
-#if ENABLE(OILPAN)
-#define MAYBE_EnsureLifetime DISABLED_EnsureLifetime
-#else
-#define MAYBE_EnsureLifetime EnsureLifetime
-#endif
-TEST_F(IDBTransactionTest, MAYBE_EnsureLifetime)
+TEST_F(IDBTransactionTest, EnsureLifetime)
 {
     OwnPtr<FakeWebIDBDatabase> backend = FakeWebIDBDatabase::create();
     Persistent<IDBDatabase> db = IDBDatabase::create(executionContext(), backend.release(), FakeIDBDatabaseCallbacks::create());
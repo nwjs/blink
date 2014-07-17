nwjs/blink

commit fd276646fa47752ca24ea336d57acc91a0544467

Web Animations API: Add getAnimationPlayers()
Add getAnimationPlayers() to the AnimationTimeline and Element.

Spec:
http://dev.w3.org/fxtf/web-animations/#widl-AnimationTimeline-getAnimationPlayers-sequence-AnimationPlayer
http://dev.w3.org/fxtf/web-animations/#widl-Animatable-getAnimationPlayers-sequence-AnimationPlayer

BUG=391364

Review URL: https://codereview.chromium.org/369223002

git-svn-id: svn://svn.chromium.org/blink/trunk@178326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/animation/AnimationTimeline.h
@@ -67,6 +67,7 @@ public:
     // Creates a player attached to this timeline, but without a start time.
     AnimationPlayer* createAnimationPlayer(AnimationNode*);
     AnimationPlayer* play(AnimationNode*);
+    Vector<RefPtrWillBeRawPtr<AnimationPlayer> > getAnimationPlayers();
 
 #if !ENABLE(OILPAN)
     void playerDestroyed(AnimationPlayer* player)

LayoutTests/web-animations-api/w3c/getAnimationPlayers.html
@@ -1,0 +1,118 @@
+<!DOCTYPE html>
+<script src="../../resources/testharness.js"></script>
+<script src="../../resources/testharnessreport.js"></script>
+<style>
+@keyframes test {
+    from { opacity: 0; }
+    to { opacity: 1; }
+}
+.cssAnimation {
+    animation: test 2s;
+}
+</style>
+<div id='container'>
+    <div id='element'></div>
+</div>
+
+<script>
+
+var container = document.getElementById('container');
+var element = document.getElementById('element');
+
+test(function() {
+    assert_equals(document.timeline.getAnimationPlayers().length, 0);
+
+    var player = element.animate([], 1000);
+    assert_equals(document.timeline.getAnimationPlayers().length, 1);
+    assert_equals(document.timeline.getAnimationPlayers()[0], player);
+
+    var player2 = container.animate([], 1000);
+    assert_equals(document.timeline.getAnimationPlayers().length, 2);
+    assert_equals(document.timeline.getAnimationPlayers()[0], player);
+    assert_equals(document.timeline.getAnimationPlayers()[1], player2);
+
+    player.finish();
+    assert_equals(document.timeline.getAnimationPlayers().length, 1);
+    assert_equals(document.timeline.getAnimationPlayers()[0], player2);
+
+    player2.finish();
+    assert_equals(document.timeline.getAnimationPlayers().length, 0);
+}, 'Timeline getAnimationPlayers()');
+
+test(function() {
+    assert_equals(document.timeline.getAnimationPlayers().length, 0);
+    assert_equals(container.getAnimationPlayers().length, 0);
+    assert_equals(element.getAnimationPlayers().length, 0);
+
+    var player = element.animate([], 1000);
+    assert_equals(document.timeline.getAnimationPlayers().length, 1);
+    assert_equals(document.timeline.getAnimationPlayers()[0], player);
+    assert_equals(container.getAnimationPlayers().length, 0);
+    assert_equals(element.getAnimationPlayers().length, 1);
+    assert_equals(element.getAnimationPlayers()[0], player);
+
+    var player2 = container.animate([], 1000);
+    assert_equals(document.timeline.getAnimationPlayers().length, 2);
+    assert_equals(document.timeline.getAnimationPlayers()[0], player);
+    assert_equals(document.timeline.getAnimationPlayers()[1], player2);
+    assert_equals(container.getAnimationPlayers().length, 1);
+    assert_equals(container.getAnimationPlayers()[0], player2);
+    assert_equals(element.getAnimationPlayers().length, 1);
+    assert_equals(element.getAnimationPlayers()[0], player);
+
+    player.finish();
+    assert_equals(document.timeline.getAnimationPlayers().length, 1);
+    assert_equals(document.timeline.getAnimationPlayers()[0], player2);
+    assert_equals(container.getAnimationPlayers().length, 1);
+    assert_equals(container.getAnimationPlayers()[0], player2);
+    assert_equals(element.getAnimationPlayers().length, 0);
+
+    player2.finish();
+    assert_equals(document.timeline.getAnimationPlayers().length, 0);
+    assert_equals(container.getAnimationPlayers().length, 0);
+    assert_equals(element.getAnimationPlayers().length, 0);
+
+}, 'Animatable getAnimationPlayers()');
+
+test(function() {
+    assert_equals(document.timeline.getAnimationPlayers().length, 0);
+    assert_equals(container.getAnimationPlayers().length, 0);
+    assert_equals(element.getAnimationPlayers().length, 0);
+
+    var player = element.animate([], {duration: 1000, delay: 500});
+    assert_equals(document.timeline.getAnimationPlayers().length, 1);
+    assert_equals(document.timeline.getAnimationPlayers()[0], player);
+    assert_equals(container.getAnimationPlayers().length, 0);
+    assert_equals(element.getAnimationPlayers().length, 1);
+    assert_equals(element.getAnimationPlayers()[0], player);
+
+    player.finish();
+    assert_equals(document.timeline.getAnimationPlayers().length, 0);
+    assert_equals(container.getAnimationPlayers().length, 0);
+    assert_equals(element.getAnimationPlayers().length, 0);
+
+}, 'getAnimationPlayers() with delays');
+
+async_test(function(t) {
+    assert_equals(document.timeline.getAnimationPlayers().length, 0);
+    assert_equals(container.getAnimationPlayers().length, 0);
+    assert_equals(element.getAnimationPlayers().length, 0);
+
+    element.className = 'cssAnimation';
+    t.step(function() {
+        onload = function () {
+            var players = document.timeline.getAnimationPlayers();
+            assert_equals(players.length, 1);
+            assert_equals(container.getAnimationPlayers().length, 0);
+            assert_equals(element.getAnimationPlayers().length, 1);
+
+            players[0].finish();
+            assert_equals(document.timeline.getAnimationPlayers().length, 0);
+            assert_equals(container.getAnimationPlayers().length, 0);
+            assert_equals(element.getAnimationPlayers().length, 0);
+            t.done();
+        }
+    });
+}, 'getAnimationPlayers() with cssanimations');
+
+</script>

Source/core/animation/ActiveAnimations.h
@@ -44,6 +44,8 @@ class CSSAnimations;
 class RenderObject;
 class Element;
 
+typedef WillBeHeapHashMap<RawPtrWillBeWeakMember<AnimationPlayer>, int> AnimationPlayerCountedSet;
+
 class ActiveAnimations : public NoBaseWillBeGarbageCollectedFinalized<ActiveAnimations> {
     WTF_MAKE_NONCOPYABLE(ActiveAnimations);
 public:
@@ -64,7 +66,6 @@ public:
     CSSAnimations& cssAnimations() { return m_cssAnimations; }
     const CSSAnimations& cssAnimations() const { return m_cssAnimations; }
 
-    typedef WillBeHeapHashMap<RawPtrWillBeWeakMember<AnimationPlayer>, int> AnimationPlayerCountedSet;
     // AnimationPlayers which have animations targeting this element.
     const AnimationPlayerCountedSet& players() const { return m_players; }
     void addPlayer(AnimationPlayer*);

Source/core/animation/AnimationPlayer.h
@@ -128,6 +128,10 @@ public:
         friend class AnimationPlayer;
         bool operator<(const SortInfo& other) const;
         double startTime() const { return m_startTime; }
+        bool hasLowerSequenceNumber(const SortInfo& other) const
+        {
+            return m_sequenceNumber < other.m_sequenceNumber;
+        }
     private:
         SortInfo(unsigned sequenceNumber, double startTime)
             : m_sequenceNumber(sequenceNumber)

Source/core/animation/AnimationTimeline.cpp
@@ -40,6 +40,15 @@
 
 namespace WebCore {
 
+namespace {
+
+bool compareAnimationPlayers(const RefPtrWillBeMember<WebCore::AnimationPlayer>& left, const RefPtrWillBeMember<WebCore::AnimationPlayer>& right)
+{
+    return left->sortInfo().hasLowerSequenceNumber(right->sortInfo());
+}
+
+}
+
 // This value represents 1 frame at 30Hz plus a little bit of wiggle room.
 // TODO: Plumb a nominal framerate through and derive this value from that.
 const double AnimationTimeline::s_minimumDelay = 0.04;
@@ -85,6 +94,18 @@ AnimationPlayer* AnimationTimeline::play(AnimationNode* child)
     AnimationPlayer* player = createAnimationPlayer(child);
     m_document->compositorPendingAnimations().add(player);
     return player;
+}
+
+WillBeHeapVector<RefPtrWillBeMember<AnimationPlayer> > AnimationTimeline::getAnimationPlayers()
+{
+    WillBeHeapVector<RefPtrWillBeMember<AnimationPlayer> > animationPlayers;
+    for (WillBeHeapHashSet<RawPtrWillBeWeakMember<AnimationPlayer> >::iterator it = m_players.begin(); it != m_players.end(); ++it) {
+        if ((*it)->source() && (*it)->source()->isCurrent()) {
+            animationPlayers.append(*it);
+        }
+    }
+    std::sort(animationPlayers.begin(), animationPlayers.end(), compareAnimationPlayers);
+    return animationPlayers;
 }
 
 void AnimationTimeline::wake()

Source/core/animation/AnimationTimeline.idl
@@ -8,4 +8,5 @@
 ] interface AnimationTimeline {
     readonly attribute double? currentTime;
     AnimationPlayer play(AnimationNode source);
+    sequence<AnimationPlayer> getAnimationPlayers();
 };

Source/core/animation/ElementAnimation.h
@@ -31,6 +31,7 @@
 #ifndef ElementAnimation_h
 #define ElementAnimation_h
 
+#include "core/animation/ActiveAnimations.h"
 #include "core/animation/Animation.h"
 #include "core/animation/AnimationTimeline.h"
 #include "core/animation/EffectInput.h"
@@ -38,6 +39,7 @@
 #include "core/dom/Document.h"
 #include "core/dom/Element.h"
 #include "platform/RuntimeEnabledFeatures.h"
+
 
 namespace WebCore {
 
@@ -85,6 +87,19 @@ public:
             return 0;
         ASSERT(effect);
         return animateInternal(element, effect.release(), Timing());
+    }
+
+    static WillBeHeapVector<RefPtrWillBeMember<AnimationPlayer> > getAnimationPlayers(Element& element)
+    {
+        WillBeHeapVector<RefPtrWillBeMember<AnimationPlayer> > animationPlayers = WillBeHeapVector<RefPtrWillBeMember<AnimationPlayer> >();
+        const AnimationPlayerCountedSet& players = element.activeAnimations()->players();
+
+        for (AnimationPlayerCountedSet::const_iterator it = players.begin(); it != players.end(); ++it) {
+            ASSERT(it->key->source());
+            if (it->key->source()->isCurrent())
+                animationPlayers.append(it->key);
+        }
+        return animationPlayers;
     }
 
 private:

Source/core/animation/ElementAnimation.idl
@@ -36,4 +36,5 @@ partial interface Element {
     // FIXME: needs support for union types http://crbug.com/240176
     // AnimationPlayer animate((AnimationEffect or sequence<Dictionary>)? effect, optional (double or Dictionary) timing);
     [Custom, RaisesException] AnimationPlayer animate(object effect, optional object timing);
+    sequence<AnimationPlayer> getAnimationPlayers();
 };

commit 060ded8d96d7b8152dc3601670ee805e6c2cb6ff

Fix Oilpan build broken by r178326.
TBR=samli@chromium.org,oilpan-reviews@chromium.org
BUG=

Review URL: https://codereview.chromium.org/403463002

git-svn-id: svn://svn.chromium.org/blink/trunk@178329 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/animation/AnimationTimeline.h
@@ -67,7 +67,7 @@ public:
     // Creates a player attached to this timeline, but without a start time.
     AnimationPlayer* createAnimationPlayer(AnimationNode*);
     AnimationPlayer* play(AnimationNode*);
-    Vector<RefPtrWillBeRawPtr<AnimationPlayer> > getAnimationPlayers();
+    WillBeHeapVector<RefPtrWillBeMember<AnimationPlayer> > getAnimationPlayers();
 
 #if !ENABLE(OILPAN)
     void playerDestroyed(AnimationPlayer* player)
nwjs/blink

commit b734752d167b783a2437603cc4970073092648b4

DevTools: [tracing timeline] console.timeline based on tracing. Blink part.
The fix needs changes on the both sides, chromium and blink.
Looks like two patches, one the first for blink and the
second for chromium would be enough.

The chromium part could not be landed due to changes in protocol.

1) a new hidden command was added to the protocol
for managing tracing mode of console.timeline.
It should be removed with timeline agent.
2) two new events were introduced. Tracing.started and
Tracing.stopped so we can switch the ui into recording mode.
Unfortunately the backend part doesn't trigger stopped event
 yet because stop command to tracing agent is processing on the browser side.

The mechanics of TracingTimelineModel was switched from callbacks to the event based

the chromium side patch https://codereview.chromium.org/423283004

BUG=398787
R=yurys@chromium.org

Review URL: https://codereview.chromium.org/432433002

git-svn-id: svn://svn.chromium.org/blink/trunk@179290 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/inspector/InspectorConsoleAgent.cpp
@@ -35,6 +35,7 @@
 #include "core/inspector/InspectorConsoleMessage.h"
 #include "core/inspector/InspectorState.h"
 #include "core/inspector/InspectorTimelineAgent.h"
+#include "core/inspector/InspectorTracingAgent.h"
 #include "core/inspector/InstrumentingAgents.h"
 #include "core/inspector/ScriptArguments.h"
 #include "core/inspector/ScriptCallFrame.h"
@@ -57,13 +58,15 @@ static const int expireConsoleMessagesStep = 100;
 namespace ConsoleAgentState {
 static const char monitoringXHR[] = "monitoringXHR";
 static const char consoleMessagesEnabled[] = "consoleMessagesEnabled";
+static const char tracingBasedTimeline[] = "tracingBasedTimeline";
 }
 
 int InspectorConsoleAgent::s_enabledAgentCount = 0;
 
-InspectorConsoleAgent::InspectorConsoleAgent(InspectorTimelineAgent* timelineAgent, InjectedScriptManager* injectedScriptManager)
+InspectorConsoleAgent::InspectorConsoleAgent(InspectorTimelineAgent* timelineAgent, InspectorTracingAgent* tracingAgent, InjectedScriptManager* injectedScriptManager)
     : InspectorBaseAgent<InspectorConsoleAgent>("Console")
     , m_timelineAgent(timelineAgent)
+    , m_tracingAgent(tracingAgent)
     , m_injectedScriptManager(injectedScriptManager)
     , m_frontend(0)
     , m_expiredConsoleMessageCount(0)
@@ -228,14 +231,25 @@ void InspectorConsoleAgent::consoleTimeEnd(ExecutionContext*, const String& titl
     addConsoleAPIMessageToConsole(LogMessageType, DebugMessageLevel, message, scriptState, nullptr);
 }
 
+void InspectorConsoleAgent::setTracingBasedTimeline(ErrorString*, bool enabled)
+{
+    m_state->setBoolean(ConsoleAgentState::tracingBasedTimeline, enabled);
+}
+
 void InspectorConsoleAgent::consoleTimeline(ExecutionContext* context, const String& title, ScriptState* scriptState)
 {
-    m_timelineAgent->consoleTimeline(context, title, scriptState);
+    if (m_tracingAgent && m_state->getBoolean(ConsoleAgentState::tracingBasedTimeline))
+        m_tracingAgent->consoleTimeline(title);
+    else
+        m_timelineAgent->consoleTimeline(context, title, scriptState);
 }
 
 void InspectorConsoleAgent::consoleTimelineEnd(ExecutionContext* context, const String& title, ScriptState* scriptState)
 {
-    m_timelineAgent->consoleTimelineEnd(context, title, scriptState);
+    if (m_state->getBoolean(ConsoleAgentState::tracingBasedTimeline))
+        m_tracingAgent->consoleTimelineEnd(title);
+    else
+        m_timelineAgent->consoleTimelineEnd(context, title, scriptState);
 }
 
 void InspectorConsoleAgent::consoleCount(ScriptState* scriptState, PassRefPtrWillBeRawPtr<ScriptArguments> arguments)

LayoutTests/inspector/tracing.html
@@ -61,6 +61,7 @@ function test()
 
     function onTracingComplete()
     {
+        tracingModel.removeEventListener(WebInspector.TracingModel.Events.TracingComplete, onTracingComplete);
         InspectorTest.addResult("Tracing complete");
         runEventsSanityCheck();
         InspectorTest.completeTest();
@@ -72,6 +73,7 @@ function test()
     {
         InspectorTest.addResult("Tracing started (error: " + JSON.stringify(error) + ")");
         InspectorTest.evaluateInPage("doWork()", function() {
+            tracingModel.addEventListener(WebInspector.TracingModel.Events.TracingComplete, onTracingComplete);
             tracingModel.stop(onTracingComplete);
         });
     }

Source/core/inspector/InspectorConsoleAgent.h
@@ -47,6 +47,7 @@ class LocalFrame;
 class InspectorFrontend;
 class InjectedScriptManager;
 class InspectorTimelineAgent;
+class InspectorTracingAgent;
 class InstrumentingAgents;
 class ResourceError;
 class ResourceLoader;
@@ -62,7 +63,7 @@ typedef String ErrorString;
 class InspectorConsoleAgent : public InspectorBaseAgent<InspectorConsoleAgent>, public InspectorBackendDispatcher::ConsoleCommandHandler {
     WTF_MAKE_NONCOPYABLE(InspectorConsoleAgent);
 public:
-    InspectorConsoleAgent(InspectorTimelineAgent*, InjectedScriptManager*);
+    InspectorConsoleAgent(InspectorTimelineAgent*, InspectorTracingAgent*, InjectedScriptManager*);
     virtual ~InspectorConsoleAgent();
     virtual void trace(Visitor*) OVERRIDE;
 
@@ -87,6 +88,7 @@ public:
 
     void consoleTime(ExecutionContext*, const String& title);
     void consoleTimeEnd(ExecutionContext*, const String& title, ScriptState*);
+    void setTracingBasedTimeline(ErrorString*, bool enabled);
     void consoleTimeline(ExecutionContext*, const String& title, ScriptState*);
     void consoleTimelineEnd(ExecutionContext*, const String& title, ScriptState*);
 
@@ -110,6 +112,7 @@ protected:
     void addConsoleMessage(PassOwnPtr<InspectorConsoleMessage>);
 
     RawPtrWillBeMember<InspectorTimelineAgent> m_timelineAgent;
+    RawPtrWillBeMember<InspectorTracingAgent> m_tracingAgent;
     RawPtrWillBeMember<InjectedScriptManager> m_injectedScriptManager;
     InspectorFrontend::Console* m_frontend;
     Vector<OwnPtr<InspectorConsoleMessage> > m_consoleMessages;

Source/core/inspector/InspectorController.cpp
@@ -116,7 +116,7 @@ InspectorController::InspectorController(Page* page, InspectorClient* inspectorC
 
     m_agents.append(PageRuntimeAgent::create(injectedScriptManager, pageScriptDebugServer, m_page, m_pageAgent));
 
-    m_agents.append(PageConsoleAgent::create(injectedScriptManager, m_domAgent, m_timelineAgent));
+    m_agents.append(PageConsoleAgent::create(injectedScriptManager, m_domAgent, m_timelineAgent, m_tracingAgent));
 
     m_agents.append(InspectorWorkerAgent::create());
 

Source/core/inspector/InspectorTracingAgent.cpp
@@ -17,6 +17,8 @@ namespace blink {
 
 namespace TracingAgentState {
 const char sessionId[] = "sessionId";
+const char tracingStartedFromProtocol[] = "tracingStartedFromProtocol";
+const char tracingStarted[] = "tracingStarted";
 }
 
 namespace {
@@ -27,6 +29,7 @@ InspectorTracingAgent::InspectorTracingAgent(InspectorClient* client)
     : InspectorBaseAgent<InspectorTracingAgent>("Tracing")
     , m_layerTreeId(0)
     , m_client(client)
+    , m_frontend(0)
 {
 }
 
@@ -35,12 +38,26 @@ void InspectorTracingAgent::restore()
     emitMetadataEvents();
 }
 
-void InspectorTracingAgent::start(ErrorString*, const String& categoryFilter, const String&, const double*, String* outSessionId)
+void InspectorTracingAgent::start(ErrorString*, const String& categoryFilter, const String&, const double*)
+{
+    m_state->setBoolean(TracingAgentState::tracingStartedFromProtocol, true);
+    innerStart(categoryFilter, false);
+}
+
+void InspectorTracingAgent::end(ErrorString* errorString)
+{
+    m_state->setBoolean(TracingAgentState::tracingStarted, false);
+    m_consoleTimelines.clear();
+    m_frontend->stopped();
+}
+
+void InspectorTracingAgent::innerStart(const String& categoryFilter, bool fromConsole)
 {
     m_state->setString(TracingAgentState::sessionId, IdentifiersFactory::createIdentifier());
+    m_state->setBoolean(TracingAgentState::tracingStarted, true);
     m_client->enableTracing(categoryFilter);
     emitMetadataEvents();
-    *outSessionId = sessionId();
+    m_frontend->started(fromConsole, sessionId());
 }
 
 String InspectorTracingAgent::sessionId()
@@ -50,6 +67,8 @@ String InspectorTracingAgent::sessionId()
 
 void InspectorTracingAgent::emitMetadataEvents()
 {
+    if (!m_state->getBoolean(TracingAgentState::tracingStarted))
+        return;
     TRACE_EVENT_INSTANT1(devtoolsMetadataEventCategory, "TracingStartedInPage", "sessionId", sessionId().utf8());
     if (m_layerTreeId)
         setLayerTreeId(m_layerTreeId);
@@ -59,6 +78,33 @@ void InspectorTracingAgent::setLayerTreeId(int layerTreeId)
 {
     m_layerTreeId = layerTreeId;
     TRACE_EVENT_INSTANT2(devtoolsMetadataEventCategory, "SetLayerTreeId", "sessionId", sessionId().utf8(), "layerTreeId", m_layerTreeId);
+}
+
+void InspectorTracingAgent::consoleTimeline(const String& title)
+{
+    m_consoleTimelines.append(title);
+    if (m_state->getBoolean(TracingAgentState::tracingStarted))
+        return;
+    innerStart("-*,disabled-by-default-devtools.timeline", true);
+}
+
+void InspectorTracingAgent::consoleTimelineEnd(const String& title)
+{
+    size_t index = m_consoleTimelines.find(title);
+    if (index == kNotFound)
+        return;
+
+    m_consoleTimelines.remove(index);
+    if (!m_consoleTimelines.size()
+        && m_state->getBoolean(TracingAgentState::tracingStarted)
+        && !m_state->getBoolean(TracingAgentState::tracingStartedFromProtocol))
+        m_frontend->stopped();
+    m_state->setBoolean(TracingAgentState::tracingStarted, false);
+}
+
+void InspectorTracingAgent::setFrontend(InspectorFrontend* frontend)
+{
+    m_frontend = frontend->tracing();
 }
 
 }

Source/core/inspector/InspectorTracingAgent.h
@@ -28,9 +28,14 @@ public:
 
     // Base agent methods.
     virtual void restore() OVERRIDE;
+    virtual void setFrontend(InspectorFrontend*) OVERRIDE;
+
+    void consoleTimeline(const String& title);
+    void consoleTimelineEnd(const String& title);
 
     // Protocol method implementations.
-    virtual void start(ErrorString*, const String& categoryFilter, const String&, const double*, String* sessionId) OVERRIDE;
+    virtual void start(ErrorString*, const String& categoryFilter, const String&, const double*) OVERRIDE;
+    virtual void end(ErrorString*);
 
     // Methods for other agents to use.
     void setLayerTreeId(int);
@@ -39,10 +44,13 @@ private:
     explicit InspectorTracingAgent(InspectorClient*);
 
     void emitMetadataEvents();
+    void innerStart(const String& categoryFilter, bool fromConsole);
     String sessionId();
 
     int m_layerTreeId;
     InspectorClient* m_client;
+    Vector<String> m_consoleTimelines;
+    InspectorFrontend::Tracing* m_frontend;
 };
 
 }

Source/core/inspector/PageConsoleAgent.cpp
@@ -40,8 +40,8 @@
 
 namespace blink {
 
-PageConsoleAgent::PageConsoleAgent(InjectedScriptManager* injectedScriptManager, InspectorDOMAgent* domAgent, InspectorTimelineAgent* timelineAgent)
-    : InspectorConsoleAgent(timelineAgent, injectedScriptManager)
+PageConsoleAgent::PageConsoleAgent(InjectedScriptManager* injectedScriptManager, InspectorDOMAgent* domAgent, InspectorTimelineAgent* timelineAgent, InspectorTracingAgent* tracingAgent)
+    : InspectorConsoleAgent(timelineAgent, tracingAgent, injectedScriptManager)
     , m_inspectorDOMAgent(domAgent)
 {
 }

Source/core/inspector/PageConsoleAgent.h
@@ -41,9 +41,9 @@ class InspectorDOMAgent;
 class PageConsoleAgent FINAL : public InspectorConsoleAgent {
     WTF_MAKE_NONCOPYABLE(PageConsoleAgent);
 public:
-    static PassOwnPtrWillBeRawPtr<PageConsoleAgent> create(InjectedScriptManager* injectedScriptManager, InspectorDOMAgent* domAgent, InspectorTimelineAgent* timelineAgent)
+    static PassOwnPtrWillBeRawPtr<PageConsoleAgent> create(InjectedScriptManager* injectedScriptManager, InspectorDOMAgent* domAgent, InspectorTimelineAgent* timelineAgent, InspectorTracingAgent* tracingAgent)
     {
-        return adoptPtrWillBeNoop(new PageConsoleAgent(injectedScriptManager, domAgent, timelineAgent));
+        return adoptPtrWillBeNoop(new PageConsoleAgent(injectedScriptManager, domAgent, timelineAgent, tracingAgent));
     }
     virtual ~PageConsoleAgent();
     virtual void trace(Visitor*) OVERRIDE;
@@ -51,7 +51,7 @@ public:
     virtual bool isWorkerAgent() OVERRIDE { return false; }
 
 private:
-    PageConsoleAgent(InjectedScriptManager*, InspectorDOMAgent*, InspectorTimelineAgent*);
+    PageConsoleAgent(InjectedScriptManager*, InspectorDOMAgent*, InspectorTimelineAgent*, InspectorTracingAgent*);
     virtual void clearMessages(ErrorString*) OVERRIDE;
     virtual void addInspectedNode(ErrorString*, int nodeId) OVERRIDE;
 

commit 1deef674cf835d73c2c1d6fdf312cfcffa409cac

Fix Oilpan build broken by r179290.
TBR=loislo@chromium.org, oilpan-reviews@chromium.org
BUG=

Review URL: https://codereview.chromium.org/433773003

git-svn-id: svn://svn.chromium.org/blink/trunk@179294 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/inspector/InspectorConsoleAgent.cpp
@@ -84,6 +84,7 @@ InspectorConsoleAgent::~InspectorConsoleAgent()
 void InspectorConsoleAgent::trace(Visitor* visitor)
 {
     visitor->trace(m_timelineAgent);
+    visitor->trace(m_tracingAgent);
     visitor->trace(m_injectedScriptManager);
     InspectorBaseAgent::trace(visitor);
 }
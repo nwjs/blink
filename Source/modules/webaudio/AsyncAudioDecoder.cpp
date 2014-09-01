nwjs/blink

commit ddaaad897226b858acd9aaa9d316310482455efc

Oilpan: Prepare moving callbacks to oilpan
BUG=

Review URL: https://codereview.chromium.org/470823002

git-svn-id: svn://svn.chromium.org/blink/trunk@181160 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/modules/webaudio/AsyncAudioDecoder.cpp
@@ -49,7 +49,7 @@ AsyncAudioDecoder::~AsyncAudioDecoder()
 {
 }
 
-void AsyncAudioDecoder::decodeAsync(ArrayBuffer* audioData, float sampleRate, PassOwnPtr<AudioBufferCallback> successCallback, PassOwnPtr<AudioBufferCallback> errorCallback)
+void AsyncAudioDecoder::decodeAsync(ArrayBuffer* audioData, float sampleRate, PassOwnPtrWillBeRawPtr<AudioBufferCallback> successCallback, PassOwnPtrWillBeRawPtr<AudioBufferCallback> errorCallback)
 {
     ASSERT(isMainThread());
     ASSERT(audioData);
@@ -76,8 +76,6 @@ void AsyncAudioDecoder::notifyComplete(ArrayBuffer* audioData, AudioBufferCallba
 {
     // Adopt references, so everything gets correctly dereffed.
     RefPtr<ArrayBuffer> audioDataRef = adoptRef(audioData);
-    OwnPtr<AudioBufferCallback> successCallbackPtr = adoptPtr(successCallback);
-    OwnPtr<AudioBufferCallback> errorCallbackPtr = adoptPtr(errorCallback);
     RefPtr<AudioBus> audioBusRef = adoptRef(audioBus);
 
     AudioBuffer* audioBuffer = AudioBuffer::createFromAudioBus(audioBus);

commit 0f13ba4c4b4529d1118c049259c2fbbcaee50e07

Fix leak caused by r181160
Adding back lines that adopt the callbacks.

BUG=409585

Review URL: https://codereview.chromium.org/531533002

git-svn-id: svn://svn.chromium.org/blink/trunk@181185 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/modules/webaudio/AsyncAudioDecoder.cpp
@@ -76,6 +76,10 @@ void AsyncAudioDecoder::notifyComplete(ArrayBuffer* audioData, AudioBufferCallba
 {
     // Adopt references, so everything gets correctly dereffed.
     RefPtr<ArrayBuffer> audioDataRef = adoptRef(audioData);
+#if !ENABLE(OILPAN)
+    OwnPtr<AudioBufferCallback> successCallbackPtr = adoptPtr(successCallback);
+    OwnPtr<AudioBufferCallback> errorCallbackPtr = adoptPtr(errorCallback);
+#endif
     RefPtr<AudioBus> audioBusRef = adoptRef(audioBus);
 
     AudioBuffer* audioBuffer = AudioBuffer::createFromAudioBus(audioBus);

LayoutTests/LeakExpectations
@@ -77,12 +77,3 @@ crbug.com/366455 svg/custom/svg-fonts-fallback.xhtml [ Leak ]
 crbug.com/403987 fast/dom/element-attribute-js-null.html [ Leak ]
 crbug.com/403363 http/tests/security/mixedContent/insecure-audio-video-in-main-frame.html [ Leak ]
 crbug.com/405125 media/media-controls-clone-crash.html [ Leak Pass ]
-
-crbug.com/409585 webaudio/decode-audio-data-basic.html [ Leak ]
-crbug.com/409585 webaudio/codec-tests/wav/24bit-44khz.html [ Leak ]
-crbug.com/409585 webaudio/codec-tests/wav/24bit-22khz-resample.html [ Leak ]
-crbug.com/409585 webaudio/codec-tests/vorbis/vbr-96kbps-44khz.html [ Leak ]
-crbug.com/409585 webaudio/codec-tests/vorbis/vbr-128kbps-44khz.html [ Leak ]
-crbug.com/409585 webaudio/codec-tests/vorbis/vbr-70kbps-44khz.html [ Leak ]
-crbug.com/409585 webaudio/mixing.html [ Leak ]
-crbug.com/409585 webaudio/audiobuffersource.html [ Leak ]
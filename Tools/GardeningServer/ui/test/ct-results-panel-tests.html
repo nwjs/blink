nwjs/blink

commit 06c35afbbecaa4d9d62624363233d47b9f356330

Added CLs from the regression ranges to the results panel
BUG=404045
R=ojan@chromium.org

Review URL: https://codereview.chromium.org/559943002

git-svn-id: svn://svn.chromium.org/blink/trunk@182172 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Tools/GardeningServer/ui/ct-results-panel.html
@@ -8,8 +8,9 @@ found in the LICENSE file.
 <link rel="import" href="ct-results-by-builder.html">
 <link rel="import" href="ct-embedded-flakiness-dashboard.html">
 <link rel="import" href="ct-popout-iframe.html">
+<link rel="import" href="ct-commit-list.html">
 
-<polymer-element name="ct-results-panel" attributes="failures tree">
+<polymer-element name="ct-results-panel" attributes="failures commitList tree">
   <template>
     <style>
       :host {
@@ -71,6 +72,7 @@ found in the LICENSE file.
           <ct-embedded-flakiness-dashboard test="{{ failures[selected] }}" tree="{{ tree }}"></ct-embedded-flakiness-dashboard>
         </template>
         <ct-results-by-builder failure="{{ failures[selected] }}"></ct-results-by-builder>
+        <ct-commit-list commitList="{{ commitList }}" detailed="true"></ct-commit-list>
       </div>
     </template>
   </template>

Tools/GardeningServer/ui/ct-sheriff-o-matic.html
@@ -76,7 +76,7 @@ found in the LICENSE file.
 
     <core-animated-pages selected="{{ selected }}" transitions="slide-from-right">
       <ct-unexpected-failures id="unexpected" tree="{{ tree }}" commitLog="{{ commitLog }}" failures="{{ failures }}" on-ct-examine-failures="{{ onExamine }}"></ct-unexpected-failures>
-      <ct-results-panel failures="{{ currentFailures }}" tree="{{ tree }}"></ct-results-panel>
+      <ct-results-panel failures="{{ currentFailures }}" commitList="{{ currentCommitList }}" tree="{{ tree }}"></ct-results-panel>
     </core-animated-pages>
   </template>
   <script>
@@ -98,6 +98,15 @@ found in the LICENSE file.
               new CTFailure(failure.step, failure.testName, failure.resultNodesByBuilder));
           }, this);
         }
+        if ('currentCommitList' in state) {
+          var serializedRevisions = []
+          for (tree in state.currentCommitList.revisions) {
+            state.currentCommitList.revisions[tree].forEach(function(revision) {
+              serializedRevisions.push(tree + ":" + revision)
+            })
+          }
+          this.currentCommitList = new CTCommitList(this.commitLog, serializedRevisions)
+        }
       },
 
       onPopState: function(e) {
@@ -114,12 +123,14 @@ found in the LICENSE file.
       onExamine: function(event) {
         var dataToExamine = event.detail.dataToExamine();
         window.history.pushState({
-          currentFailures: dataToExamine,
+          currentFailures: dataToExamine.failures,
+          currentCommitList: dataToExamine.commitList,
           selected: 1,
         });
 
 
-        this.currentFailures = dataToExamine;
+        this.currentFailures = dataToExamine.failures;
+        this.currentCommitList = dataToExamine.commitList;
         this.selected = 1;
       },
 

Tools/GardeningServer/model/ct-sheriff-failure-group-data.html
@@ -22,7 +22,10 @@ CTSheriffFailureGroupData.prototype.getAnnotations = function() {
 };
 
 CTSheriffFailureGroupData.prototype.dataToExamine = function() {
-  return this.failures;
+  return {
+    'failures': this.failures,
+    'commitList': this.commitList
+  }
 };
 
 CTSheriffFailureGroupData.prototype.failureKeys = function() {

Tools/GardeningServer/ui/ct-button.html
@@ -1,0 +1,46 @@
+<!--
+Copyright 2014 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+-->
+
+<link rel="import" href="../bower_components/paper-button/paper-button.html">
+
+<polymer-element name="ct-button" attributes="label" noscript>
+  <template>
+    <style>
+      :host {
+        display: flex;
+        flex-direction: column;
+      }
+
+      paper-button {
+        -webkit-user-select: none;
+        background: #f5f5f5;
+        border-radius: 2px;
+        border: 1px solid #dcdcdc;
+        color: #444;
+        padding: 0 16px;
+        margin-bottom: 5px;
+        text-align: center;
+      }
+
+      paper-button:focus {
+        border: 1px solid #4d90fe;
+        outline: none;
+      }
+
+      paper-button:active {
+        box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.3);
+      }
+
+      paper-button:active, paper-button:hover {
+        background: #f8f8f8;
+        border-color: #c6c6c6;
+        box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
+        color: #222;
+      }
+    </style>
+    <paper-button label="{{ label }}"></paper-button>
+  </template>
+</polymer-element>

Tools/GardeningServer/ui/ct-commit-list.html
@@ -5,9 +5,10 @@ found in the LICENSE file.
 -->
 
 <link rel="import" href="ct-commit.html">
+<link rel="import" href="ct-detailed-commit.html">
 <link rel="import" href="../bower_components/paper-button/paper-button.html">
 
-<polymer-element name="ct-commit-list" attributes="commitList">
+<polymer-element name="ct-commit-list" attributes="commitList detailed">
   <template>
     <style>
       :host {
@@ -34,7 +35,12 @@ found in the LICENSE file.
             on-click="{{ _toggle }}" repository="{{ repository.name }}"></paper-icon-button>
         <template if="{{ repository.expanded }}">
           <template repeat="{{ commit in repository.commits }}">
-            <ct-commit data="{{ commit }}"></ct-commit>
+            <template if="{{ !detailed }}">
+              <ct-commit data="{{ commit }}"></ct-commit>
+            </template>
+            <template if="{{ detailed }}">
+              <ct-detailed-commit data="{{ commit }}"></ct-detailed-commit>
+            </template>
           </template>
         </template>
       </div>
@@ -42,6 +48,7 @@ found in the LICENSE file.
   </template>
   <script>
   Polymer({
+    detailed: false,
     _toggle: function(event, detail, sender, target) {
       var repo = sender.getAttribute('repository');
       var r = this.commitList.repositories.find(function(item) {

Tools/GardeningServer/ui/ct-detailed-commit.html
@@ -1,0 +1,29 @@
+<!--
+Copyright 2014 The Chromium Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+-->
+
+<link rel="import" href="ct-button.html">
+
+<polymer-element name="ct-detailed-commit" attributes="data" noscript>
+  <template>
+    <style>
+      .header {
+        text-align: center;
+        font-size: 20px;
+      }
+    </style>
+
+    <div class="header">
+      <a href="{{ data.url }}">{{ data.repository }}:{{ data.revision }}</a>
+    </div>
+
+    <pre>{{ data.message }}</pre>
+
+    <!-- TODO(sergiyb): Add modified files linked to their diffs from
+         http://src.chromium.org/viewvc/blink/path/to/file?pathrev=123456.  -->
+    <!-- <iframe src="{{ data.url }}"></iframe> -->
+    <hr/>
+  </template>
+</polymer-element>

Tools/GardeningServer/ui/ct-failure-card-buttons.html
@@ -5,55 +5,21 @@ found in the LICENSE file.
 -->
 
 <link rel="import" href="../model/ct-builder-list.html">
+<link rel="import" href="ct-button.html">
 <link rel="import" href="../bower_components/paper-dialog/paper-dialog.html">
 <link rel="import" href="../bower_components/paper-dialog/paper-dialog-transition.html">
 <link rel="import" href="../bower_components/paper-input/paper-input.html">
 
 <polymer-element name="ct-failure-card-buttons" attributes="group bug">
   <template>
-    <style>
-      :host {
-        display: flex;
-        flex-direction: column;
-      }
-
-      /* FIXME: All this paper-button styling should go in a cr-button component so that
-         we can use buttons in different places and have them all look the same. */
-      paper-button {
-        -webkit-user-select: none;
-        background: #f5f5f5;
-        border-radius: 2px;
-        border: 1px solid #dcdcdc;
-        color: #444;
-        padding: 0 16px;
-        margin-bottom: 5px;
-        text-align: center;
-      }
-
-      paper-button:focus {
-        border: 1px solid #4d90fe;
-        outline: none;
-      }
-
-      paper-button:active {
-        box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.3);
-      }
-
-      paper-button:active, paper-button:hover {
-        background: #f8f8f8;
-        border-color: #c6c6c6;
-        box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
-        color: #222;
-      }
-    </style>
-    <paper-button id="examine" on-tap="{{ examine }}" label="Examine"></paper-button>
+    <ct-button id="examine" on-tap="{{ examine }}" label="Examine"></ct-button>
     <template if="{{ !group.isSnoozed }}">
-      <paper-button id="snooze" on-tap="{{ snooze }}" label="Snooze"></paper-button>
+      <ct-button id="snooze" on-tap="{{ snooze }}" label="Snooze"></ct-button>
     </template>
     <template if="{{ group.isSnoozed }}">
-      <paper-button id="snooze" on-tap="{{ unsnooze }}" label="Unsnooze"></paper-button>
+      <ct-button id="snooze" on-tap="{{ unsnooze }}" label="Unsnooze"></ct-button>
     </template>
-    <paper-button id="link-bug" on-tap="{{ linkBug }}" label="Link Bug"></paper-button>
+    <ct-button id="link-bug" on-tap="{{ linkBug }}" label="Link Bug"></ct-button>
     <template if="{{ group.bug !== undefined }}">
       <div>
         <a href="{{ group.bug }}">
@@ -63,8 +29,8 @@ found in the LICENSE file.
 
     <paper-dialog heading="Enter bug number" transition="paper-transition-center" id="bugDialog">
       <paper-input label="Bug# or URL" floatingLabel autofocus id="bug"></paper-input>
-      <paper-button label="Remove bug link" on-tap="{{ removeBug }}" dismissive id="dialogRemoveBug"></paper-button>
-      <paper-button label="OK" on-tap="{{ saveBug }}" affirmative id="dialogOk"></paper-button>
+      <ct-button label="Remove bug link" on-tap="{{ removeBug }}" dismissive id="dialogRemoveBug"></ct-button>
+      <ct-button label="OK" on-tap="{{ saveBug }}" affirmative id="dialogOk"></ct-button>
     </paper-dialog>
   </template>
   <script>

commit 1cd62d80cf763cdad3596d08bc0a4669459d25e4

Get rid of dataToExamine from sheriff-o-matic.
It doesn't serve any purpose. We should just pass the CTFailureGroup
directly to the ct-results-panel. In addition to simplifying the code
this fixes a bug introduced in r182172 where examine pages wouldn't
work.

Store the failure group key in the pushState so that we have an easy
way to grab the key out and find the right CTFailureGroup on popState.

Added a new test to ct-results-panel-tests for the url results panel case.
Also, cleaned up this test to get rid of the unnecessary use of beforeEach.
beforeEach was making it hard to write the url test because it used global
state that didn't apply.

NOTRY=true
R=leviw@chromium.org, michaelpg@chromium.org

Review URL: https://codereview.chromium.org/590003002

git-svn-id: svn://svn.chromium.org/blink/trunk@182557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Tools/GardeningServer/model/ct-builder-failure-group-data.html
@@ -20,11 +20,6 @@ CTBuilderFailureGroupData.prototype.getAnnotations = function() {
   return [];
 };
 
-// FIXME: This function should be replaced and callers should just pass the original object.
-CTBuilderFailureGroupData.prototype.dataToExamine = function() {
-  return {url: this.url};
-};
-
 CTBuilderFailureGroupData.prototype.failureKeys = function() {
   return [this.failure.key];
 };

Tools/GardeningServer/model/ct-failure-group.html
@@ -15,10 +15,6 @@ function CTFailureGroup(key, data, category) {
   this._computeProperties();
 }
 
-CTFailureGroup.prototype.dataToExamine = function() {
-  return this.data.dataToExamine();
-}
-
 CTFailureGroup.prototype.snoozeUntil = function(time) {
   return this._annotate({
     snoozeTime: time,

Tools/GardeningServer/model/ct-step-failure-group-data.html
@@ -21,10 +21,6 @@ CTStepFailureGroupData.prototype.getAnnotations = function() {
   }).flatten();
 };
 
-CTStepFailureGroupData.prototype.dataToExamine = function() {
-  return this.failures;
-};
-
 CTStepFailureGroupData.prototype.failureKeys = function() {
   return this.failures.map(function(failure) {
     return failure.keys();

Tools/GardeningServer/model/ct-trooper-failure-group-data.html
@@ -19,10 +19,6 @@ CTTrooperFailureGroupData.prototype.getAnnotations = function() {
   return [];
 };
 
-CTTrooperFailureGroupData.prototype.dataToExamine = function() {
-  return {url: this.url};
-};
-
 CTTrooperFailureGroupData.prototype.failureKeys = function() {
   // FIXME: This should return a unique key for this CTTrooperFailureGroupData.
   return [];

Tools/GardeningServer/ui/ct-results-panel.html
@@ -10,7 +10,7 @@ found in the LICENSE file.
 <link rel="import" href="ct-popout-iframe.html">
 <link rel="import" href="ct-commit-list.html">
 
-<polymer-element name="ct-results-panel" attributes="failures commitList">
+<polymer-element name="ct-results-panel" attributes="group">
   <template>
     <style>
       :host {
@@ -53,36 +53,44 @@ found in the LICENSE file.
         padding-top: 6px;
       }
     </style>
-    <template if="{{ !failures.length && !failures.url }}">
-      <div class="message">No results to display.</div>
+
+    <template if="{{ !group.data.url && !group.data.failures.length }}">
+      <div class="message">{{ loading ? 'Loading...' : 'No results to display.' }}</div>
     </template>
-    <template if="{{ failures.url }}">
-      <ct-popout-iframe src="{{ failures.url }}"></ct-popout-iframe>
+
+    <template if="{{ group.data.url }}">
+      <ct-popout-iframe src="{{ group.data.url }}"></ct-popout-iframe>
     </template>
-    <template if="{{ failures.length }}">
-      <core-menu selected="{{ selected }}">
-        <template repeat="{{ failure in failures }}">
-          <template if="{{ failure.testName }}">
-            <div>{{ failure.testName }}</div>
+
+    <template if="{{ group.data.failures }}">
+      <template if="{{ group.data.failures.length }}">
+        <core-menu selected="{{ selected }}">
+          <template repeat="{{ failure in group.data.failures }}">
+            <template if="{{ failure.testName }}">
+              <div>{{ failure.testName }}</div>
+            </template>
           </template>
-        </template>
-      </core-menu>
+        </core-menu>
+      </template>
       <div class="results">
-        <template if="{{ failures[selected].testName }}">
-          <ct-embedded-flakiness-dashboard test="{{ failures[selected] }}"></ct-embedded-flakiness-dashboard>
+        <template if="{{ group.data.failures[selected].testName }}">
+          <ct-embedded-flakiness-dashboard test="{{ group.data.failures[selected] }}"></ct-embedded-flakiness-dashboard>
         </template>
-        <ct-results-by-builder failure="{{ failures[selected] }}"></ct-results-by-builder>
-        <ct-commit-list commitList="{{ commitList }}" detailed="true"></ct-commit-list>
+        <ct-results-by-builder failure="{{ group.data.failures[selected] }}"></ct-results-by-builder>
+        <ct-commit-list commitList="{{ group.data.commitList }}" detailed="true"></ct-commit-list>
       </div>
     </template>
+
   </template>
   <script>
     Polymer({
-      failures: [],
+      group: null,
       selected: 0,
-      tree: '',
+      loading: true,
 
-      failuresChanged: function() {
+      groupChanged: function() {
+        if (this.group)
+          this.loading = false;
         this.selected = 0;
       },
     });

Tools/GardeningServer/ui/ct-sheriff-o-matic.html
@@ -79,37 +79,23 @@ found in the LICENSE file.
 
     <core-animated-pages selected="{{ selected }}" transitions="slide-from-right">
       <ct-unexpected-failures id="unexpected" tree="{{ tree }}" commitLog="{{ commitLog }}" failures="{{ failures }}" on-ct-examine-failures="{{ onExamine }}"></ct-unexpected-failures>
-      <ct-results-panel failures="{{ currentFailures }}" commitList="{{ currentCommitList }}"></ct-results-panel>
+      <ct-results-panel group="{{ examinedFailureGroup }}"></ct-results-panel>
     </core-animated-pages>
   </template>
   <script>
     var kUpdateFrequency = 1000 * 30;
 
     Polymer({
-      tree: "",
+      tree: '',
       treeList: null,
       selected: 0,
-      currentFailures: [],
+      examinedFailureGroup: null,
+      _pendingFailureGroupKey: '',
 
       updateFromHistoryState: function(state) {
+        this._pendingFailureGroupKey = state.examinedFailureGroupKey;
         if ('selected' in state)
           this.selected = state.selected;
-        if ('currentFailures' in state) {
-          this.currentFailures = [];
-          state.currentFailures.forEach(function(failure) {
-            this.currentFailures.push(
-              new CTStepFailure(failure.step, failure.testName, failure.resultNodesByBuilder));
-          }, this);
-        }
-        if ('currentCommitList' in state) {
-          var serializedRevisions = []
-          for (tree in state.currentCommitList.revisions) {
-            state.currentCommitList.revisions[tree].forEach(function(revision) {
-              serializedRevisions.push(tree + ":" + revision)
-            })
-          }
-          this.currentCommitList = new CTCommitList(this.commitLog, serializedRevisions)
-        }
       },
 
       onPopState: function(e) {
@@ -124,16 +110,13 @@ found in the LICENSE file.
       },
 
       onExamine: function(event) {
-        var dataToExamine = event.detail.dataToExamine();
         window.history.pushState({
-          currentFailures: dataToExamine.failures,
-          currentCommitList: dataToExamine.commitList,
+          examinedFailureGroupKey: event.detail.key,
           selected: 1,
         });
 
 
-        this.currentFailures = dataToExamine.failures;
-        this.currentCommitList = dataToExamine.commitList;
+        this.examinedFailureGroup = event.detail;
         this.selected = 1;
       },
 
@@ -167,11 +150,24 @@ found in the LICENSE file.
       },
 
       update: function() {
-        // FIXME: These shouldn't update if there's already an update in
-        // progress.
-        this.commitLog.update();
-        this.failures.update();
+        if (this._promise)
+          return;
+        this._promise = Promise.all(
+          [this.commitLog.update(),
+           this.failures.update()]).then(this._updateCompleted.bind(this));
+      },
+
+      _updateCompleted: function() {
+        this._promise = null;
         this.$.unexpected.update();
+        if (!this.failures.failures)
+          return;
+        if (!this._pendingFailureGroupKey)
+          return;
+        this.examinedFailureGroup = this.failures.failures[this.tree].find(function(group) {
+          return group.key == this._pendingFailureGroupKey;
+        }.bind(this));
+        this._pendingFailureGroupKey = '';
       },
 
     });

Tools/GardeningServer/ui/test/ct-results-panel-tests.html
@@ -93,9 +93,8 @@ var kExampleFailures = [
 
 describe('ct-results-panel', function() {
   var panel;
-  var failures;
 
-  beforeEach(function(done) {
+  function createPanel() {
     panel = document.createElement('ct-results-panel');
     // FIXME: The results-panel needs to be appended to the DOM so that the flakiness
     // dashboard's iframe has a window when we grab location off of it, but that
@@ -103,34 +102,54 @@ describe('ct-results-panel', function() {
     // This results in some console spam from the 404'ed request.
     panel.hidden = true;
     document.body.appendChild(panel);
+  }
 
-    if (failures)
-      panel.failures = failures;
+  function assignFailures(failures) {
+    // FIXME: This should be a proper CTFailureGroup
+    panel.group = {
+      data: {
+        failures: failures
+      }
+    };
+  }
 
-    setTimeout(done);
-  });
+  function assignUrl(url) {
+    // FIXME: This should be a proper CTFailureGroup
+    panel.group = {
+      data: {
+        url: url
+      }
+    };
+  }
 
   afterEach(function() {
-    document.body.removeChild(panel);
+    panel.remove();
+    panel = undefined;
   });
 
-  describe('empty', function() {
-    before(function() {
-      failures = undefined;
-    });
-
-    it('should show no results', function() {
+  it('should show no results', function(done) {
+    createPanel();
+    setTimeout(function() {
       var message = panel.shadowRoot.querySelector('.message');
-      assert.equal(message.textContent, 'No results to display.');
+      assert.equal(message.textContent, 'Loading...');
+
+      panel.group = 'this is an invalid group';
+
+      setTimeout(function() {
+        var message = panel.shadowRoot.querySelector('.message');
+        assert.equal(message.textContent, 'No results to display.');
+        done();
+      });
     });
   });
 
-  describe('failing tests', function() {
-    before(function() {
-      failures = kExampleFailures;
-    });
+  it('should show tests and results for selected test', function(done) {
+    createPanel();
+    assignFailures(kExampleFailures);
 
-    it('should show tests and results for selected test', function(done) {
+    setTimeout(function() {
+      assert.notOk(panel.shadowRoot.querySelector('.message'));
+
       var items = panel.shadowRoot.querySelectorAll('core-menu > div');
       assert.lengthOf(items, 3);
       assert.equal(items[0].textContent, 'plugins/gesture-events-scrolled.html');
@@ -153,33 +172,56 @@ describe('ct-results-panel', function() {
     });
   });
 
-  describe('whole step failed', function() {
-    before(function() {
-      var failure = {
-        testName: null,
-        step: 'foo-step',
-        resultNodesByBuilder: {
-          'WebKit Win7 (dbg)': {
-            actual: 'UNKNOWN',
-          },
+  it('should show step failure', function(done) {
+    createPanel();
+    var failure = {
+      testName: null,
+      step: 'foo-step',
+      resultNodesByBuilder: {
+        'WebKit Win7 (dbg)': {
+          actual: 'UNKNOWN',
         },
-        oldestFailingRevision: 123,
-        newestPassingRevision: 124,
-      };
-      failures = [failure];
-    });
+      },
+      oldestFailingRevision: 123,
+      newestPassingRevision: 124,
+    };
+    assignFailures([failure]);
 
-    it('should show step failure', function() {
+    setTimeout(function() {
+      assert.notOk(panel.shadowRoot.querySelector('.message'));
+
       var items = panel.shadowRoot.querySelectorAll('core-menu > div');
       assert.lengthOf(items, 0);
 
       var results = panel.shadowRoot.querySelectorAll('ct-results-by-builder');
       assert.lengthOf(results, 1);
-      assert.equal(results[0].failure, failures[0]);
+      assert.equal(results[0].failure, failure);
 
       assert.lengthOf(panel.shadowRoot.querySelectorAll('ct-embedded-flakiness-dashboard'), 0);
+      done();
     });
   });
+
+  it('should show url failure', function(done) {
+    createPanel();
+    var url = 'http://url.com';
+    assignUrl(url);
+
+    setTimeout(function() {
+      assert.notOk(panel.shadowRoot.querySelector('.message'));
+
+      var items = panel.shadowRoot.querySelectorAll('core-menu > div');
+      assert.lengthOf(items, 0);
+
+      var results = panel.shadowRoot.querySelectorAll('ct-popout-iframe');
+      assert.lengthOf(results, 1);
+      assert.equal(results[0].src, url);
+
+      assert.lengthOf(panel.shadowRoot.querySelectorAll('ct-embedded-flakiness-dashboard'), 0);
+      done();
+    });
+  });
+
 });
 
 })();
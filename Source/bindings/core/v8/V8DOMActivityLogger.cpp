82e98d0e7da663b6f4794de09ec5703a38c43cab
Roger <wenrui@gmail.com>
2014-11-05 10:22:45 +0800

    guard more handle scope; Fix using V8DOMActivityLogger from Node context again
    Fix rogerwang/node-webkit#2549

--- Source/bindings/core/v8/V8DOMActivityLogger.cpp
+++ Source/bindings/core/v8/V8DOMActivityLogger.cpp
@@ -75,7 +75,10 @@ V8DOMActivityLogger* V8DOMActivityLogger::currentActivityLogger()
     if (!isolate->InContext())
         return 0;
 
+    v8::HandleScope handle_scope(isolate);
     v8::Handle<v8::Context> context = isolate->GetCurrentContext();
+    if (context == node::g_context)
+        return 0;
     if (context.IsEmpty() || !toDOMWindow(context))
         return 0;
 

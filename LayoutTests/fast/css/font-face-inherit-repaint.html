nwjs/blink

commit 27216e97748fefde08df63c77f1aa1788d123ed0

Don't return false for RenderStyle::operator== while fonts load.
This caused an assert for baseRenderStyle in ActiveAnimations to hit. When
a font finishes loading, we trigger a full recalc of Document. That recalc
relies on getting a visual diff to trigger re-layout and paint and was
triggered by the Font::operator==. Instead, we remove the false return from
that operator and explicitly check if font loading status changed in
RenderStyle::diffNeedsFullLayoutAndPaintInvalidation().

R=eae@chromium.org
BUG=433765

Review URL: https://codereview.chromium.org/754743004

git-svn-id: svn://svn.chromium.org/blink/trunk@186572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/rendering/style/RenderStyle.cpp
@@ -508,6 +508,9 @@ bool RenderStyle::diffNeedsFullLayoutAndPaintInvalidation(const RenderStyle& oth
     if (inherited->textAutosizingMultiplier != other.inherited->textAutosizingMultiplier)
         return true;
 
+    if (inherited->font.loadingCustomFonts() != other.inherited->font.loadingCustomFonts())
+        return true;
+
     if (inherited.get() != other.inherited.get()) {
         if (inherited->line_height != other.inherited->line_height
             || inherited->font != other.inherited->font

LayoutTests/http/tests/webfont/animation-assert-expected.html
@@ -1,0 +1,3 @@
+<!DOCTYPE html>
+<div style="line-height: 10px">&nbsp;</div>
+<div>PASS if no crash.</div>

LayoutTests/http/tests/webfont/animation-assert.html
@@ -1,0 +1,21 @@
+<!DOCTYPE html>
+<style>
+@font-face {
+    font-family: delayed;
+    src: url(slow-ahem-loading.cgi?delay=1000)
+}
+#outer {
+    line-height: 10px;
+}
+#inner {
+    font-family: delayed;
+    transition: transform .1s;
+}
+</style>
+<div id="outer" style="display: none"><div id="inner">&nbsp;</div></div>
+<script>
+outer.style.display = "";
+outer.offsetWidth;
+inner.style.transform = "translate(0)";
+</script>
+<div>PASS if no crash.</div>

Source/platform/fonts/Font.cpp
@@ -75,11 +75,6 @@ Font& Font::operator=(const Font& other)
 
 bool Font::operator==(const Font& other) const
 {
-    // Our FontData don't have to be checked, since checking the font description will be fine.
-    // FIXME: This does not work if the font was made with the FontPlatformData constructor.
-    if (loadingCustomFonts() || other.loadingCustomFonts())
-        return false;
-
     FontSelector* first = m_fontFallbackList ? m_fontFallbackList->fontSelector() : 0;
     FontSelector* second = other.m_fontFallbackList ? other.m_fontFallbackList->fontSelector() : 0;
 

Source/platform/fonts/Font.h
@@ -171,12 +171,12 @@ public:
 
     void willUseFontData(UChar32) const;
 
-private:
     bool loadingCustomFonts() const
     {
         return m_fontFallbackList && m_fontFallbackList->loadingCustomFonts();
     }
 
+private:
     bool shouldSkipDrawing() const
     {
         return m_fontFallbackList && m_fontFallbackList->shouldSkipDrawing();

Source/platform/fonts/shaping/HarfBuzzShaper.cpp
@@ -839,7 +839,8 @@ bool HarfBuzzShaper::shapeHarfBuzzRuns()
 
         CachedShapingResults* cachedResults = runCache.find(key);
         if (cachedResults) {
-            if (cachedResults->dir == currentRun->direction() && cachedResults->font == *m_font && cachedResults->locale == localeString) {
+            if (cachedResults->dir == currentRun->direction() && cachedResults->font == *m_font
+                && !cachedResults->font.loadingCustomFonts() && !m_font->loadingCustomFonts() && cachedResults->locale == localeString) {
                 currentRun->applyShapeResult(cachedResults->buffer);
                 setGlyphPositionsForHarfBuzzRun(currentRun, cachedResults->buffer);
 

commit 1521a28e7b3e17e66fa272db3ffb46a8ab27bb6d

Inherit propagation diff necessary for loaded web fonts.
As the loading status is not part of the RenderStyle equality operator
anymore, we need to explicitly check for the loading status to make the
style propagation diff be different from NoChange. That is necessary to
trigger a call to RenderObject::setStyle which in turn will call
visualInvalidationDiff that will trigger a layout/paint for the loaded
font.

This is a regression from r186572.

R=eae@chromium.org
BUG=440271

Review URL: https://codereview.chromium.org/790823003

git-svn-id: svn://svn.chromium.org/blink/trunk@186801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/rendering/style/RenderStyle.cpp
@@ -186,13 +186,13 @@ StyleRecalcChange RenderStyle::stylePropagationDiff(const RenderStyle* oldStyle,
         || oldStyle->alignItems() != newStyle->alignItems())
         return Reattach;
 
-    if (*oldStyle == *newStyle)
-        return diffPseudoStyles(oldStyle, newStyle);
-
     if (oldStyle->inheritedNotEqual(newStyle)
         || oldStyle->hasExplicitlyInheritedProperties()
         || newStyle->hasExplicitlyInheritedProperties())
         return Inherit;
+
+    if (*oldStyle == *newStyle)
+        return diffPseudoStyles(oldStyle, newStyle);
 
     return NoInherit;
 }
@@ -339,9 +339,10 @@ void RenderStyle::removeCachedPseudoStyle(PseudoId pid)
 bool RenderStyle::inheritedNotEqual(const RenderStyle* other) const
 {
     return inherited_flags != other->inherited_flags
-           || inherited != other->inherited
-           || m_svgStyle->inheritedNotEqual(other->m_svgStyle.get())
-           || rareInheritedData != other->rareInheritedData;
+        || inherited != other->inherited
+        || font().loadingCustomFonts() != other->font().loadingCustomFonts()
+        || m_svgStyle->inheritedNotEqual(other->m_svgStyle.get())
+        || rareInheritedData != other->rareInheritedData;
 }
 
 bool RenderStyle::inheritedDataShared(const RenderStyle* other) const

LayoutTests/fast/css/font-face-inherit-repaint-expected.html
@@ -1,0 +1,5 @@
+<!DOCTYPE html>
+<p>You should see the word PASS below.</p>
+<select style="font-family: sans-serif">
+    <option>PASS</option>
+</select>

LayoutTests/fast/css/font-face-inherit-repaint.html
@@ -1,0 +1,12 @@
+<!DOCTYPE html>
+<style>
+@font-face {
+    font-family: NotFound;
+    src: url(notfound.ttf);
+}
+select { font-family: NotFound, sans-serif; }
+</style>
+<p>You should see the word PASS below.</p>
+<select>
+    <option>PASS</option>
+</select>
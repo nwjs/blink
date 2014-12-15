nwjs/blink

commit bab429b2d7886cc21a45d74053aee38018ae289f

Implement select listbox using shadow DOM
User visible behavior changes.
- Old listbox could only scroll in itemHeight increments but now we have smooth scroll.
- Mouse events need to be dispatched on the option element, not select element.
- Select element height is determined based on the height of the first item, not the font size.
- Spatial navigation now draws a black dashed outline around the active selection instead of a focus ring.

We can no longer let RenderListBox draw the focus ring for spatial navigation mode because the items are drawn on another RenderLayer.
I added a user agent stylesheet only :-internal-spatial-navigation-focus pseudo type so a style can be applied to the spatial navigation active element.
If we add the default focus ring on the option element, it gets clipped to the select element so I changed it to a black dashed outline.

Option and optgroups were represented as AXListBoxOptions but now only options are AXListBoxOptions and optgroups are represented by the optgroup-label AXNodeObject. We let AXRenderObject handle hit testing so we can remove the custom hit testing.

BUG=234871

Review URL: https://codereview.chromium.org/347773002

git-svn-id: svn://svn.chromium.org/blink/trunk@178354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/css/html.css
@@ -641,6 +641,11 @@ input[type="button"]:active:disabled, input[type="submit"]:active:disabled, inpu
     border-style: outset
 }
 
+option:-internal-spatial-navigation-focus {
+    outline: black dashed 1px;
+    outline-offset: -1px;
+}
+
 datalist {
     display: none
 }
@@ -759,24 +764,54 @@ select[size][multiple] {
     align-items: flex-start;
     border: 1px inset gray;
     border-radius: initial;
-    white-space: initial;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    vertical-align: text-bottom;
+    -webkit-user-select: none;
+    white-space: nowrap;
 }
 
 select[size="0"],
 select[size="1"] {
     -webkit-appearance: menulist;
     align-items: center;
-    border: 1px solid;
+    border-width: 1px;
+    border-style: solid;
+    border-color: initial;
     border-radius: 5px;
+    overflow-x: initial;
+    overflow-y: initial;
+    vertical-align: initial;
+    -webkit-user-select: initial;
     white-space: pre;
 }
 
 optgroup {
     font-weight: bolder;
+    display: block;
 }
 
 option {
     font-weight: normal;
+    display: block;
+    padding: 0 2px 1px 2px;
+    white-space: pre;
+    min-height: 1.2em;
+}
+
+option:checked {
+    background-color: -internal-inactive-list-box-selection;
+    color: -internal-inactive-list-box-selection-text;
+}
+
+select:focus option:checked {
+    background-color: -internal-active-list-box-selection;
+    color: -internal-active-list-box-selection-text;
+}
+
+select:disabled option:checked,
+option:checked:disabled {
+    color: gray;
 }
 
 output {

commit a03d29236a80af5befacea4befe8bbd701a00137

Fix regression caused by r178354 where options in optgroup not indented
updateLabel wasn't always called when the parents changed so it wasn't getting indented even if it was inside an optgroup.

For example the problem happened when the label is provided through the label attribute or when the option is inserted from JS.

TEST=automated. listbox-group-indent.html
BUG=440253

Review URL: https://codereview.chromium.org/783983005

git-svn-id: svn://svn.chromium.org/blink/trunk@187110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/css/html.css
@@ -791,6 +791,10 @@ option {
     min-height: 1.2em;
 }
 
+select:-internal-list-box optgroup option:before {
+    content: "\00a0\00a0\00a0\00a0";;
+}
+
 select:-internal-list-box option,
 select:-internal-list-box optgroup {
     line-height: initial !important;

Source/core/html/HTMLOptionElement.cpp
@@ -330,20 +330,12 @@ bool HTMLOptionElement::isDisabledFormControl() const
 
 Node::InsertionNotificationRequest HTMLOptionElement::insertedInto(ContainerNode* insertionPoint)
 {
+    HTMLElement::insertedInto(insertionPoint);
     if (HTMLSelectElement* select = ownerSelectElement()) {
         select->setRecalcListItems();
-        // Do not call selected() since calling updateListItemSelectedStates()
-        // at this time won't do the right thing. (Why, exactly?)
-        if (m_isSelected) {
-            // FIXME: Might be better to call this unconditionally, always
-            // passing m_isSelected, rather than only calling it if we are
-            // selected.
-            select->optionSelectionStateChanged(this, true);
-            select->scrollToSelection();
-        }
+        select->optionInserted(*this, m_isSelected);
     }
-
-    return HTMLElement::insertedInto(insertionPoint);
+    return InsertionDone;
 }
 
 void HTMLOptionElement::removedFrom(ContainerNode* insertionPoint)
@@ -386,7 +378,7 @@ void HTMLOptionElement::didAddUserAgentShadowRoot(ShadowRoot& root)
 void HTMLOptionElement::updateLabel()
 {
     if (ShadowRoot* root = userAgentShadowRoot())
-        root->setTextContent(textIndentedToRespectGroupLabel());
+        root->setTextContent(text());
 }
 
 bool HTMLOptionElement::spatialNavigationFocused() const

Source/core/html/HTMLSelectElement.cpp
@@ -84,6 +84,7 @@ HTMLSelectElement::HTMLSelectElement(Document& document, HTMLFormElement* form)
     , m_shouldRecalcListItems(false)
     , m_suggestedIndex(-1)
     , m_isAutofilledByPreview(false)
+    , m_scrollToSelectionLater(false)
 {
 }
 
@@ -418,6 +419,11 @@ void HTMLSelectElement::childrenChanged(const ChildrenChange& change)
     m_lastOnChangeSelection.clear();
 
     HTMLFormControlElementWithState::childrenChanged(change);
+
+    if (m_scrollToSelectionLater) {
+        m_scrollToSelectionLater = false;
+        scrollToSelection();
+    }
 }
 
 void HTMLSelectElement::optionElementChildrenChanged()
@@ -918,6 +924,13 @@ void HTMLSelectElement::optionSelectionStateChanged(HTMLOptionElement* option, b
         selectOption(nextSelectableListIndex(-1));
 }
 
+void HTMLSelectElement::optionInserted(const HTMLOptionElement& option, bool optionIsSelected)
+{
+    ASSERT(option.ownerSelectElement() == this);
+    if (optionIsSelected)
+        selectOption(option.index(), DelayScrollToSelection);
+}
+
 void HTMLSelectElement::optionRemoved(const HTMLOptionElement& option)
 {
     if (m_activeSelectionAnchorIndex < 0 && m_activeSelectionEndIndex < 0)
@@ -960,7 +973,10 @@ void HTMLSelectElement::selectOption(int optionIndex, SelectOptionFlags flags)
     if (RenderObject* renderer = this->renderer())
         renderer->updateFromElement();
 
-    scrollToSelection();
+    if (flags & DelayScrollToSelection)
+        m_scrollToSelectionLater = true;
+    else
+        scrollToSelection();
 
     setNeedsValidityCheck();
 
@@ -1655,7 +1671,7 @@ String HTMLSelectElement::optionAtIndex(int index) const
     HTMLElement* element = items[index];
     if (!isHTMLOptionElement(*element) || toHTMLOptionElement(element)->isDisabledFormControl())
         return String();
-    return toHTMLOptionElement(element)->textIndentedToRespectGroupLabel();
+    return toHTMLOptionElement(element)->text();
 }
 
 void HTMLSelectElement::typeAheadFind(KeyboardEvent* event)

Source/core/html/HTMLSelectElement.h
@@ -117,6 +117,7 @@ public:
 
     // For use in the implementation of HTMLOptionElement.
     void optionSelectionStateChanged(HTMLOptionElement*, bool optionIsSelected);
+    void optionInserted(const HTMLOptionElement&, bool optionIsSelected);
     void optionRemoved(const HTMLOptionElement&);
     bool anonymousIndexedSetter(unsigned, PassRefPtrWillBeRawPtr<HTMLOptionElement>, ExceptionState&);
 
@@ -175,6 +176,7 @@ private:
         DeselectOtherOptions = 1 << 0,
         DispatchInputAndChangeEvent = 1 << 1,
         UserDriven = 1 << 2,
+        DelayScrollToSelection = 1 << 3,
     };
     typedef unsigned SelectOptionFlags;
     void selectOption(int optionIndex, SelectOptionFlags = 0);
@@ -229,6 +231,7 @@ private:
     mutable bool m_shouldRecalcListItems;
     int m_suggestedIndex;
     bool m_isAutofilledByPreview;
+    bool m_scrollToSelectionLater;
 };
 
 } // namespace blink

LayoutTests/TestExpectations
@@ -1407,6 +1407,11 @@ crbug.com/426543 fast/js/mozilla/strict/15.4.4.13.html [ Pass Failure ]
 
 crbug.com/401377 [ XP ] http/tests/serviceworker/fetch-mixed-content-to-inscope.html [ Pass Timeout ]
 
+crbug.com/440253 fast/forms/select/listbox-appearance-basic.html [ NeedsRebaseline ]
+crbug.com/440253 fast/forms/select/listbox-with-display-none-option.html [ NeedsRebaseline ]
+crbug.com/440253 fast/forms/select/menulist-appearance-basic.html [ NeedsRebaseline ]
+crbug.com/440253 fast/forms/select/optgroup-rendering.html [ NeedsRebaseline ]
+
 crbug.com/430557 [ Win Linux Mac ] inspector/layers/layer-canvas-log.html [ Failure ]
 
 crbug.com/429128 virtual/gpu/fast/canvas/yuv-video-on-accelerated-canvas.html [ Pass Timeout ]

LayoutTests/fast/forms/select/listbox-group-indent-expected.html
@@ -1,0 +1,12 @@
+<!DOCTYPE html>
+<select size=20>
+  <optgroup label=group>
+    <option>apple</option>
+    <option>banana</option>
+    <option>durian</option>
+  </optgroup>
+  <optgroup label=nested style="font-weight: 900">
+    <option>fig</option>
+  </optgroup>
+  <option>cherry</option>
+</select>

LayoutTests/fast/forms/select/listbox-group-indent.html
@@ -1,0 +1,17 @@
+<!DOCTYPE html>
+<select size=20>
+  <optgroup label=group id=group>
+    <option>apple</option>
+    <option label=banana></option>
+  </optgroup>
+  <option label=cherry></option>
+</select>
+
+<script>
+var group = document.getElementById('group');
+group.appendChild(new Option('durian'));
+var nestedGroup = document.createElement('optgroup');
+nestedGroup.label = 'nested';
+nestedGroup.appendChild(new Option('fig'));
+group.appendChild(nestedGroup);
+</script>

LayoutTests/platform/linux/fast/forms/select/listbox-appearance-basic-expected.txt
@@ -74,8 +74,11 @@ layer at (164,12) size 58x70 clip at (165,13) size 41x68
     RenderBlock {OPTGROUP} at (1,35) size 41x34
       RenderBlock {DIV} at (0,0) size 41x17
       RenderBlock {OPTION} at (0,17) size 41x17 [bgcolor=#999999]
-        RenderText {#text} at (2,0) size 37x16
-          text run at (2,0) width 37: "    baz"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 21x16
+          text run at (18,0) width 21: "baz"
 layer at (234,63) size 45x19 clip at (235,64) size 28x17 scrollHeight 68
   RenderListBox {SELECT} at (226,55) size 45x19 [bgcolor=#C0C0C0] [border: (1px solid #A9A9A9)]
     RenderBlock {OPTION} at (1,1) size 28x17

LayoutTests/platform/linux/fast/forms/select/listbox-with-display-none-option-expected.txt
@@ -55,8 +55,11 @@ layer at (68,69) size 65x121 clip at (69,70) size 48x119
         RenderText {#text} at (2,0) size 44x16
           text run at (2,0) width 44: "***45***"
       RenderBlock {OPTION} at (0,17) size 48x17
-        RenderText {#text} at (2,0) size 39x16
-          text run at (2,0) width 39: "    Five"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 23x16
+          text run at (18,0) width 23: "Five"
     RenderBlock {OPTION} at (1,86) size 48x17
       RenderText {#text} at (2,0) size 35x16
         text run at (2,0) width 35: "Seven"
@@ -73,8 +76,11 @@ layer at (137,69) size 65x121 clip at (138,70) size 48x119
         RenderText {#text} at (2,0) size 44x16
           text run at (2,0) width 44: "***45***"
       RenderBlock {OPTION} at (0,17) size 48x17
-        RenderText {#text} at (2,0) size 42x16
-          text run at (2,0) width 42: "    Four"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 26x16
+          text run at (18,0) width 26: "Four"
     RenderBlock {OPTION} at (1,69) size 48x17
       RenderText {#text} at (2,0) size 19x16
         text run at (2,0) width 19: "Six"
@@ -125,14 +131,23 @@ layer at (326,52) size 63x138 clip at (327,53) size 46x136
         RenderText {#text} at (2,0) size 41x16
           text run at (2,0) width 41: "**456**"
       RenderBlock {OPTION} at (0,17) size 46x17
-        RenderText {#text} at (2,0) size 42x16
-          text run at (2,0) width 42: "    Four"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 26x16
+          text run at (18,0) width 26: "Four"
       RenderBlock {OPTION} at (0,34) size 46x17
-        RenderText {#text} at (2,0) size 39x16
-          text run at (2,0) width 39: "    Five"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 23x16
+          text run at (18,0) width 23: "Five"
       RenderBlock {OPTION} at (0,51) size 46x17
-        RenderText {#text} at (2,0) size 35x16
-          text run at (2,0) width 35: "    Six"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 19x16
+          text run at (18,0) width 19: "Six"
     RenderBlock {OPTION} at (1,120) size 46x17
       RenderText {#text} at (2,0) size 35x16
         text run at (2,0) width 35: "Seven"

LayoutTests/platform/linux/fast/forms/select/menulist-appearance-basic-expected.txt
@@ -157,66 +157,111 @@ layer at (12,383) size 200x25 clip at (13,384) size 183x23 scrollHeight 374
         RenderText {#text} at (2,0) size 50x16
           text run at (2,0) width 50: "Group 1"
       RenderBlock {OPTION} at (0,17) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 1.1"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 1.1"
       RenderBlock {OPTION} at (0,34) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 1.2"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 1.2"
       RenderBlock {OPTION} at (0,51) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 1.3"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 1.3"
     RenderBlock {OPTGROUP} at (1,86) size 183x68
       RenderBlock {DIV} at (0,0) size 183x17
         RenderText {#text} at (2,0) size 50x16
           text run at (2,0) width 50: "Group 2"
       RenderBlock {OPTION} at (0,17) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 2.1"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 2.1"
       RenderBlock {OPTION} at (0,34) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 2.2"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 2.2"
       RenderBlock {OPTION} at (0,51) size 183x17 [color=#FFFFFF] [bgcolor=#999999]
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 2.3"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 2.3"
     RenderBlock {OPTGROUP} at (1,154) size 183x68
       RenderBlock {DIV} at (0,0) size 183x17
         RenderText {#text} at (2,0) size 50x16
           text run at (2,0) width 50: "Group 3"
       RenderBlock {OPTION} at (0,17) size 183x17 [color=#FFFFFF] [bgcolor=#999999]
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 3.1"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 3.1"
       RenderBlock {OPTION} at (0,34) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 3.2"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 3.2"
       RenderBlock {OPTION} at (0,51) size 183x17 [color=#FFFFFF] [bgcolor=#999999]
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 3.3"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 3.3"
     RenderBlock {OPTGROUP} at (1,222) size 183x68
       RenderBlock {DIV} at (0,0) size 183x17
         RenderText {#text} at (2,0) size 50x16
           text run at (2,0) width 50: "Group 4"
       RenderBlock {OPTION} at (0,17) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 4.1"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 4.1"
       RenderBlock {OPTION} at (0,34) size 183x17 [color=#FFFFFF] [bgcolor=#999999]
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 4.2"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 4.2"
       RenderBlock {OPTION} at (0,51) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 4.3"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 4.3"
     RenderBlock {OPTGROUP} at (1,290) size 183x68
       RenderBlock {DIV} at (0,0) size 183x17
         RenderText {#text} at (2,0) size 50x16
           text run at (2,0) width 50: "Group 5"
       RenderBlock {OPTION} at (0,17) size 183x17 [color=#FFFFFF] [bgcolor=#999999]
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 5.1"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 5.1"
       RenderBlock {OPTION} at (0,34) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 5.2"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 5.2"
       RenderBlock {OPTION} at (0,51) size 183x17
-        RenderText {#text} at (2,0) size 63x16
-          text run at (2,0) width 63: "    Item 5.3"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 47x16
+          text run at (18,0) width 47: "Item 5.3"
     RenderBlock {OPTION} at (1,358) size 183x17 [color=#FFFFFF] [bgcolor=#999999]
       RenderText {#text} at (2,0) size 47x16
         text run at (2,0) width 47: "Item 0.2"

LayoutTests/platform/linux/fast/forms/select/optgroup-rendering-expected.txt
@@ -19,46 +19,82 @@ layer at (8,8) size 80x342 clip at (9,9) size 63x340
         RenderText {#text} at (2,0) size 52x16
           text run at (2,0) width 52: "Enabled"
       RenderBlock {OPTION} at (0,17) size 63x17 [color=#0000FF]
-        RenderText {#text} at (2,0) size 40x16
-          text run at (2,0) width 40: "    One"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 24x16
+          text run at (18,0) width 24: "One"
       RenderBlock {OPTION} at (0,34) size 63x17 [color=#0000FF]
-        RenderText {#text} at (2,0) size 39x16
-          text run at (2,0) width 39: "    Two"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 23x16
+          text run at (18,0) width 23: "Two"
       RenderBlock {OPTION} at (0,51) size 63x17 [color=#0000FF]
-        RenderText {#text} at (2,0) size 48x16
-          text run at (2,0) width 48: "    Three"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 32x16
+          text run at (18,0) width 32: "Three"
       RenderBlock {OPTION} at (0,68) size 63x17 [color=#0000FF]
-        RenderText {#text} at (2,0) size 42x16
-          text run at (2,0) width 42: "    Four"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 26x16
+          text run at (18,0) width 26: "Four"
     RenderBlock {OPTGROUP} at (1,86) size 63x85 [color=#FFC0CB]
       RenderBlock {DIV} at (0,0) size 63x17
         RenderText {#text} at (2,0) size 55x16
           text run at (2,0) width 55: "Disabled"
       RenderBlock {OPTION} at (0,17) size 63x17 [color=#FF0000]
-        RenderText {#text} at (2,0) size 40x16
-          text run at (2,0) width 40: "    One"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 24x16
+          text run at (18,0) width 24: "One"
       RenderBlock {OPTION} at (0,34) size 63x17 [color=#FF0000]
-        RenderText {#text} at (2,0) size 39x16
-          text run at (2,0) width 39: "    Two"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 23x16
+          text run at (18,0) width 23: "Two"
       RenderBlock {OPTION} at (0,51) size 63x17 [color=#FF0000]
-        RenderText {#text} at (2,0) size 48x16
-          text run at (2,0) width 48: "    Three"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 32x16
+          text run at (18,0) width 32: "Three"
       RenderBlock {OPTION} at (0,68) size 63x17 [color=#FF0000]
-        RenderText {#text} at (2,0) size 42x16
-          text run at (2,0) width 42: "    Four"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 26x16
+          text run at (18,0) width 26: "Four"
     RenderBlock {OPTGROUP} at (1,171) size 63x85 [color=#008000]
       RenderBlock {DIV} at (0,0) size 63x17
         RenderText {#text} at (2,0) size 59x16
           text run at (2,0) width 59: "Enabled2"
       RenderBlock {OPTION} at (0,17) size 63x17 [color=#0000FF]
-        RenderText {#text} at (2,0) size 40x16
-          text run at (2,0) width 40: "    One"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 24x16
+          text run at (18,0) width 24: "One"
       RenderBlock {OPTION} at (0,34) size 63x17 [color=#FF0000]
-        RenderText {#text} at (2,0) size 39x16
-          text run at (2,0) width 39: "    Two"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 23x16
+          text run at (18,0) width 23: "Two"
       RenderBlock {OPTION} at (0,51) size 63x17 [color=#FFFFFF] [bgcolor=#999999]
-        RenderText {#text} at (2,0) size 48x16
-          text run at (2,0) width 48: "    Three"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 32x16
+          text run at (18,0) width 32: "Three"
       RenderBlock {OPTION} at (0,68) size 63x17 [color=#0000FF]
-        RenderText {#text} at (2,0) size 42x16
-          text run at (2,0) width 42: "    Four"
+        RenderInline {<pseudo:before>} at (0,0) size 16x16
+          RenderTextFragment at (2,0) size 16x16
+            text run at (2,0) width 16: "    "
+        RenderText {#text} at (18,0) size 26x16
+          text run at (18,0) width 26: "Four"
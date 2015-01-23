nwjs/blink

commit a14cbc6fae27bd24417dcfd6482ae776723464d9

DevTools: Auto switch to a paused target.
We do automatically switch to a paused target when debugger stops, if there
were no paused targets yet. If we have several paused targets already, resuming
one of them should eventually switch to the other paused target too, otherwise
the pause on the other target may stay unnoticed for the user.

R=sergeyv, pfeldman, vsevik

Review URL: https://codereview.chromium.org/822203003

git-svn-id: svn://svn.chromium.org/blink/trunk@188216 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/sources/SourcesPanel.js
@@ -451,6 +451,27 @@ WebInspector.SourcesPanel.prototype = {
 
         this._sourcesView.clearCurrentExecutionLine();
         this._updateDebuggerButtons();
+
+        if (this._switchToPausedTargetTimeout)
+            clearTimeout(this._switchToPausedTargetTimeout);
+        this._switchToPausedTargetTimeout = setTimeout(this._switchToPausedTarget.bind(this), 500);
+    },
+
+    _switchToPausedTarget: function()
+    {
+        delete this._switchToPausedTargetTimeout;
+        if (this._paused)
+            return;
+        var target = WebInspector.context.flavor(WebInspector.Target);
+        if (target && target.debuggerModel.isPaused())
+            return;
+        var targets = WebInspector.targetManager.targets();
+        for (var i = 0; i < targets.length; ++i) {
+            if (targets[i].debuggerModel.isPaused()) {
+                WebInspector.context.setFlavor(WebInspector.Target, targets[i]);
+                break;
+            }
+        }
     },
 
     _togglePauseOnExceptions: function()

commit 2c097f351e1643ad9292af2a63875269de68c3a1

DevTools: Do not automatically switch back to paused target upon running target selection
This is a regression from r188216

R=aandrey

Review URL: https://codereview.chromium.org/874533003

git-svn-id: svn://svn.chromium.org/blink/trunk@188889 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/sources/SourcesPanel.js
@@ -282,6 +282,7 @@ WebInspector.SourcesPanel.prototype = {
         this._paused = false;
         this._clearInterface();
         this._toggleDebuggerSidebarButton.disabled = false;
+        this._switchToPausedTargetTimeout = setTimeout(this._switchToPausedTarget.bind(this), 500);
     },
 
     /**
@@ -457,7 +458,6 @@ WebInspector.SourcesPanel.prototype = {
 
         if (this._switchToPausedTargetTimeout)
             clearTimeout(this._switchToPausedTargetTimeout);
-        this._switchToPausedTargetTimeout = setTimeout(this._switchToPausedTarget.bind(this), 500);
     },
 
     _switchToPausedTarget: function()